3efbd29773287fb293db2515d01916e7b1d83b13
fengyuan <feng1.yuan@intel.com>
2019-11-28 15:46:34 +0800

    mute PyTorch compilation do not compile pytorch any more use generated files for current stage we will generate files by ourselves in future so it is unnecessary to take efforts to integrate pytorch gen system
    Signed-off-by: fengyuan <feng1.yuan@intel.com>

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -9,13 +9,15 @@ endif()
 
 # PyTorch
 # TODO: generate aten related files for now
-set(USE_SYCL OFF CACHE BOOL "enable SYCL")
-set(USE_CUDA OFF CACHE BOOL "enable CUDA")
-set(USE_FBGEMM OFF CACHE BOOL "enable FBGEMM")
-set(USE_MKLDNN OFF CACHE BOOL "enable MKLDNN")
-set(USE_NNPACK OFF CACHE BOOL "enable NNPACK")
-set(USE_DISTRIBUTED OFF CACHE BOOL "enable DISTRIBUTED")
-set(BUILD_CAFFE2_OPS OFF CACHE BOOL "enable Caffe2 OPs")
-
-set(DPCPP_DEP torch)
-add_subdirectory(third_party/pytorch)
+# mute compilation. see CMakeLists
+#
+# set(USE_SYCL OFF CACHE BOOL "enable SYCL")
+# set(USE_CUDA OFF CACHE BOOL "enable CUDA")
+# set(USE_FBGEMM OFF CACHE BOOL "enable FBGEMM")
+# set(USE_MKLDNN OFF CACHE BOOL "enable MKLDNN")
+# set(USE_NNPACK OFF CACHE BOOL "enable NNPACK")
+# set(USE_DISTRIBUTED OFF CACHE BOOL "enable DISTRIBUTED")
+# set(BUILD_CAFFE2_OPS OFF CACHE BOOL "enable Caffe2 OPs")
+# 
+# set(DPCPP_DEP torch)
+# add_subdirectory(third_party/pytorch)
