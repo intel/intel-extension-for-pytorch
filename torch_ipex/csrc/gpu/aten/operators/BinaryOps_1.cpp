5df0c0d137e44673d179d71cfdea3607515564a8
Ma, Jing1 <jing1.ma@intel.com>
2020-03-23 17:50:05 -0700

    [GPU] Separate BinaryOps into BinaryOps_x files

---
+++ torch_ipex/csrc/gpu/aten/operators/BinaryOps_1.cpp
@@ -0,0 +1,154 @@
+#include <ATen/Context.h>
+#include <ATen/Dispatch.h>
+#include <ATen/native/BinaryOps.h>
+#include <ATen/native/TensorIterator.h>
+
+#include <core/DPCPP.h>
+#include <utils/Pointwise.h>
+
+#include "Loops.h"
+
+using namespace at::dpcpp;
+
+namespace at {
+namespace AtenIpexTypeDPCPP {
+namespace impl {
+
+// Note: dpcpp compiler does not support uname type in template.
+class SyclOpAdd {};
+
+static void add_kernel_dpcpp(TensorIterator& iter, Scalar alpha_scalar) {
+  AT_DISPATCH_ALL_TYPES_AND(at::ScalarType::Half, iter.dtype(), "add", [&]() {
+    auto alpha = alpha_scalar.to<scalar_t>();
+    dpcpp_kernel_for_tensor_iter<SyclOpAdd>(
+        iter,
+        [=](scalar_t a, scalar_t b) -> scalar_t { return a + alpha * b; });
+  });
+}
+
+static void sub_kernel_dpcpp(TensorIterator& iter, Scalar alpha_scalar) {
+  return add_kernel_dpcpp(iter, -alpha_scalar);
+}
+
+// alpha_check
+static inline void alpha_check(const TensorIterator& iter, Scalar alpha) {
+  AT_CHECK(
+      !alpha.isBoolean() || iter.dtype() == ScalarType::Bool,
+      "Boolean alpha only supported for Boolean results.");
+  AT_CHECK(
+      isFloatingType(iter.dtype()) || alpha.isIntegral(true),
+      "For integral input tensors, argument alpha must not be a floating "
+      "point number.");
+}
+
+// scalar to tensor
+static Tensor wrapped_scalar_tensor(Scalar scalar) {
+  auto tensor = scalar_to_tensor(scalar);
+  tensor.unsafeGetTensorImpl()->set_wrapped_number(true);
+  return tensor;
+}
+
+// Basic checking for all sub functions.
+static inline void sub_check(const Tensor& self, const Tensor& other) {
+  AT_CHECK(
+      self.scalar_type() != kBool || other.scalar_type() != kBool,
+      "Subtraction, the `-` operator, with two bool tensors is not supported. "
+      "Use the `^` or `logical_xor()` operator instead.");
+  AT_CHECK(
+      self.scalar_type() != kBool && other.scalar_type() != kBool,
+      "Subtraction, the `-` operator, with a bool tensor is not supported. "
+      "If you are trying to invert a mask, use the `~` or `logical_not()` "
+      "operator instead.");
+}
+
+} // namespace impl
+
+Tensor& add_out(
+    Tensor& result,
+    const Tensor& self,
+    const Tensor& other,
+    Scalar alpha) {
+  auto iter = TensorIterator::binary_op(
+      result,
+      self,
+      other,
+      /*check_mem_overlap=*/true);
+  impl::alpha_check(iter, alpha);
+  impl::add_kernel_dpcpp(iter, alpha);
+  TORCH_INTERNAL_ASSERT(result.scalar_type() == iter.output().dtype());
+  return result;
+}
+
+Tensor add(const Tensor& self, const Tensor& other, Scalar alpha) {
+  Tensor result;
+  auto iter = TensorIterator::binary_op(result, self, other);
+  impl::alpha_check(iter, alpha);
+  impl::add_kernel_dpcpp(iter, alpha);
+  return iter.output();
+}
+
+Tensor& add_(Tensor& self, const Tensor& other, Scalar alpha) {
+  return at::AtenIpexTypeDPCPP::add_out(self, self, other, alpha);
+}
+
+Tensor add(const Tensor& self, Scalar other, Scalar alpha) {
+  return at::AtenIpexTypeDPCPP::add(
+      self, impl::wrapped_scalar_tensor(other), alpha);
+}
+
+Tensor& add_(Tensor& self, Scalar other, Scalar alpha) {
+  return at::AtenIpexTypeDPCPP::add_(
+      self, impl::wrapped_scalar_tensor(other), alpha);
+}
+
+Tensor& sub_out(
+    Tensor& result,
+    const Tensor& self,
+    const Tensor& other,
+    Scalar alpha) {
+  impl::sub_check(self, other);
+  auto iter = TensorIterator::binary_op(
+      result,
+      self,
+      other,
+      /*check_mem_overlap=*/true);
+  impl::alpha_check(iter, alpha);
+  impl::sub_kernel_dpcpp(iter, alpha);
+  TORCH_INTERNAL_ASSERT(result.scalar_type() == iter.output().dtype());
+  return result;
+}
+
+Tensor sub(const Tensor& self, const Tensor& other, Scalar alpha) {
+  impl::sub_check(self, other);
+  Tensor result;
+  auto iter = TensorIterator::binary_op(result, self, other);
+  impl::alpha_check(iter, alpha);
+  impl::sub_kernel_dpcpp(iter, alpha);
+  return iter.output();
+}
+
+Tensor& sub_(Tensor& self, const Tensor& other, Scalar alpha) {
+  return at::AtenIpexTypeDPCPP::sub_out(self, self, other, alpha);
+}
+
+Tensor rsub(const Tensor& self, const Tensor& other, Scalar alpha) {
+  return at::AtenIpexTypeDPCPP::sub(other, self, alpha);
+}
+
+Tensor sub(const Tensor& self, Scalar other, Scalar alpha) {
+  return at::AtenIpexTypeDPCPP::sub(
+      self, impl::wrapped_scalar_tensor(other), alpha);
+}
+
+Tensor& sub_(Tensor& self, Scalar other, Scalar alpha) {
+  return at::AtenIpexTypeDPCPP::sub_(
+      self, impl::wrapped_scalar_tensor(other), alpha);
+}
+
+Tensor rsub(const Tensor& self, Scalar other, Scalar alpha) {
+  return at::AtenIpexTypeDPCPP::rsub(
+      self, impl::wrapped_scalar_tensor(other), alpha);
+}
+
+} // namespace AtenIpexTypeDPCPP
+} // namespace at
