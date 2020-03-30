7181eb889888a7a4d28d2ed51beeef220c766dba
fengyuan <feng1.yuan@intel.com>
2020-03-30 09:59:25 +0800

    [GPU] clean unit test failures       support bool type for mul
    Signed-off-by: fengyuan <feng1.yuan@intel.com>

--- torch_ipex/csrc/gpu/aten/operators/BinaryOps_2.cpp
+++ torch_ipex/csrc/gpu/aten/operators/BinaryOps_2.cpp
@@ -19,9 +19,10 @@ class SyclOpMul {};
 class SyclOpDiv {};
 
 static void mul_kernel_dpcpp(TensorIterator& iter) {
-  AT_DISPATCH_ALL_TYPES_AND(at::ScalarType::Half, iter.dtype(), "mul", [&]() {
-    dpcpp_kernel_for_tensor_iter<SyclOpMul>(
-        iter, [=](scalar_t a, scalar_t b) -> scalar_t { return a * b; });
+  AT_DISPATCH_ALL_TYPES_AND2(
+      at::ScalarType::Half, at::ScalarType::Bool, iter.dtype(), "mul", [&]() {
+        dpcpp_kernel_for_tensor_iter<SyclOpMul>(
+            iter, [=](scalar_t a, scalar_t b) -> scalar_t { return a * b; });
   });
 }
 
