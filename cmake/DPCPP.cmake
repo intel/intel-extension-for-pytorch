14e718517eaca35428353df4d8ac4d76990352ee
chengjun <chengjun.lu@intel.com>
2020-05-07 18:51:00 +0800

    Add dpcpp beta6 support

--- cmake/DPCPP.cmake
+++ cmake/DPCPP.cmake
@@ -47,6 +47,7 @@ find_file(INTEL_SYCL_VERSION
         ${sycl_root_hints}
     PATH_SUFFIXES
         include/CL/sycl
+        include/sycl/CL/sycl
         lib/clang/11.0.0/include/CL/sycl
         lib/clang/10.0.0/include/CL/sycl
         lib/clang/9.0.0/include/CL/sycl
