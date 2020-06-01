181f7a1dceec95880ac87fc2743ba3102a21ffc7
Wang, Eikan <eikan.wang@intel.com>
2020-06-01 15:20:02 +0800

    Hide the BF16 buffer in DNNL tensor and reorder to FP32 if current OP does not support BF16

--- torch_ipex/csrc/cpu/ShadeDataContext.h
+++ torch_ipex/csrc/cpu/ShadeDataContext.h
@@ -13,25 +13,35 @@ namespace cpu {
 
 enum SHADE_DATA_TYPE {CPU_RAW, DIL};
 
+enum MIX_PREC_TYPE {NONE, MIX_BF_FP32};
+
 struct ShadeDataContext {
   c10::optional<dil::tensor> dil_tensor; ///< DNNL memory buffer for lazy reorder
   void              *cpu_raw_data; ///< The raw memory buffer of storage
   c10::DeleterFnPtr  cpu_del_fun;  ///< Delete function to release cpu_raw_data
 
   SHADE_DATA_TYPE    data_type;    ///< Memory buffer type
+  MIX_PREC_TYPE      mix_prec_type; ///< Record if the aten tensor is mix-precision
 
   ShadeDataContext() : dil_tensor(),
                        cpu_raw_data(nullptr),
                        cpu_del_fun(nullptr),
-                       data_type(SHADE_DATA_TYPE::CPU_RAW) {}
+                       data_type(SHADE_DATA_TYPE::CPU_RAW),
+                       mix_prec_type(MIX_PREC_TYPE::NONE) {}
 
   ~ShadeDataContext() {
     if (this->data_type == SHADE_DATA_TYPE::DIL) { // DIL Tensor
       TORCH_INTERNAL_ASSERT_DEBUG_ONLY(this->dil_tensor.has_value());
       if (this->dil_tensor->is_public_format()) {
-        TORCH_INTERNAL_ASSERT_DEBUG_ONLY(this->cpu_raw_data != nullptr);
-        TORCH_INTERNAL_ASSERT_DEBUG_ONLY(this->dil_tensor->get_data_handle() == this->cpu_raw_data);
-        TORCH_INTERNAL_ASSERT_DEBUG_ONLY(this->cpu_del_fun == &(c10::detail::deleteNothing));
+        if (this->mix_prec_type == MIX_PREC_TYPE::MIX_BF_FP32) {
+          TORCH_INTERNAL_ASSERT_DEBUG_ONLY(this->cpu_raw_data == nullptr);
+          TORCH_INTERNAL_ASSERT_DEBUG_ONLY(this->dil_tensor->get_data_handle() != this->cpu_raw_data);
+          TORCH_INTERNAL_ASSERT_DEBUG_ONLY(this->cpu_del_fun == nullptr);
+        } else {
+          TORCH_INTERNAL_ASSERT_DEBUG_ONLY(this->cpu_raw_data != nullptr);
+          TORCH_INTERNAL_ASSERT_DEBUG_ONLY(this->dil_tensor->get_data_handle() == this->cpu_raw_data);
+          TORCH_INTERNAL_ASSERT_DEBUG_ONLY(this->cpu_del_fun == &(c10::detail::deleteNothing));
+        }
       } else {
         // If dil tensor is block format, the cpu raw data means nothing here.
         TORCH_INTERNAL_ASSERT_DEBUG_ONLY(this->cpu_raw_data == nullptr);
@@ -47,7 +57,7 @@ struct ShadeDataContext {
 
   /**
    * The deleter function to release @class ShadeDataContext
-   * 
+   *
    * @param raw_data Raw pointer of @class ShadeDataContext
    */
   static void freeShadeDataContext(void *raw_data) {
@@ -68,9 +78,9 @@ struct ShadeDataContext {
 
   /**
    * Check the buffer of aten tensor is DNNL buffer or raw CPU buffer
-   * 
+   *
    * @param tensor input aten tensor
-   * 
+   *
    * @note If the storage contains both DNNL buffer and CPU buffer, and the DNNL buffer shares
    * all data with CPU buffer, then the tensor is DNNL tensor. Besides that if current storage
    * only contains DNNL buffer, it obiviouly is DNNL tensor
@@ -127,18 +137,18 @@ struct ShadeDataContext {
 
   /**
    * Check if the input tensor only contains CPU buffer.
-   * 
+   *
    * @param tensor input aten tensor
    */
   static inline bool isCpuTensor(const at::Tensor &tensor) {
-    return !isDilTensor(tensor);
+    return !isDilOwnTheTensor(tensor);
   }
 
   /**
    * Unpack DNNL buffer from the input tensor
-   * 
+   *
    * @param tensor input aten tensor
-   * 
+   *
    * @return If the input tensor does not contain DNNL buffer, the function will return
    * an empty DNNL buffer. The caller should check the return buffer is empty or not.
    */
@@ -154,9 +164,9 @@ struct ShadeDataContext {
 
   /**
    * Unpack raw CPU buffer from the input tensor
-   * 
+   *
    * @param tensor input aten tensor
-   * 
+   *
    * @return If the input tensor contains CPU buffer, the buffer will be unpacked from @class ShadeDataContext
    * and return it to the caller. Otherwise, the function will return nullptr
    */
@@ -173,6 +183,34 @@ struct ShadeDataContext {
       return nullptr;
     }
   }
+
+  /**
+   * Check if the buffer of input tensor is owned by DNNL.
+   *
+   * @param tensor input aten tensor
+   */
+  static inline bool isDilOwnTheTensor(const at::Tensor &tensor) {
+    void *storage_context = tensor.storage().data_ptr().get_context();
+    ShadeDataContext *shade_data_context = (ShadeDataContext*)storage_context;
+    auto data_type = shade_data_context->data_type;
+    TORCH_INTERNAL_ASSERT_DEBUG_ONLY((data_type == SHADE_DATA_TYPE::CPU_RAW) || (data_type == SHADE_DATA_TYPE::DIL));
+    return data_type == SHADE_DATA_TYPE::DIL;
+  }
+
+
+  /**
+   * Check if the data type of dnnl buffer is as same as the data type of aten tensor.
+   *
+   * @param tensor input aten tensor
+   */
+  static inline bool isTensorMixPrecision(const at::Tensor &tensor) {
+    auto dil_tensor_type = getDilTensor(tensor).get_data_type();
+    auto aten_tensor_type = tensor.scalar_type();
+    TORCH_INTERNAL_ASSERT_DEBUG_ONLY(aten_tensor_type == at::kFloat);
+    TORCH_INTERNAL_ASSERT_DEBUG_ONLY(dil_tensor_type == dil::data_type::bf16 || dil_tensor_type == dil::data_type::f32);
+    return dil_tensor_type == dil::data_type::bf16 && aten_tensor_type == at::kFloat;
+  }
+
 };
 
 }  // namespace cpu
