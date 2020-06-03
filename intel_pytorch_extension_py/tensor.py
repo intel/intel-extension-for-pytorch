32feca5795307ae4f3f1b1f8768fded3d135bfe1
Wang, Eikan <eikan.wang@intel.com>
2020-06-03 13:48:48 +0800

    Fix the crash issue if the tensor is not IPEX

--- intel_pytorch_extension_py/tensor.py
+++ intel_pytorch_extension_py/tensor.py
@@ -1,11 +1,13 @@
 import torch
 
+org_tensor_deep_copy = torch.Tensor.__deepcopy__
+
 def __ipex_tensor_deepcopy__(self, memo):
     if self.device.type == 'dpcpp':
         with torch.no_grad():
             new_tensor = self.clone()
             return new_tensor
     else:
-        return self.__deepcopy__(memo)
+        return org_tensor_deep_copy(self, memo)
 
 torch.Tensor.__deepcopy__ = __ipex_tensor_deepcopy__
