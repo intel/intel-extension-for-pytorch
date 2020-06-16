ebf8659f2a5aa7d15c6e838c0700b124eedb2168
Wang Weihan <eikan.wang@intel.com>
2020-06-16 11:35:05 +0800

    Add PyThon APIs to control optimizations (#71)
    1. Add python api to enable bf16 auto-mix-precision
    2. Add python api to enable auto oneDNN dispatch

--- intel_pytorch_extension_py/__init__.py
+++ intel_pytorch_extension_py/__init__.py
@@ -5,3 +5,21 @@ from .tensor import *
 from .optim import *
 from .ops import *
 import _torch_ipex as core
+
+def get_auto_optimization():
+    return core.get_auto_dnnl()
+
+def enable_auto_optimization(enable = True):
+    if enable:
+        core.enable_auto_dnnl()
+    else:
+        core.disable_auto_dnnl()
+
+def get_auto_mix_precision(bf16 = True):
+    return core.get_mix_bf16_fp32()
+
+def enable_auto_mix_precision(bf16 = True):
+    if bf16:
+        core.enable_mix_bf16_fp32()
+    else:
+        core.disable_mix_bf16_fp32()
