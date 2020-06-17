59eb8602e5460abae36a4b3ea05c1a6afadc52b5
Pinzhen Xu <pinzhen.xu@intel.com>
2020-06-17 14:31:37 +0800

    force all inputs to be dpcpp tensor (#77)
    fix convolution_overrideable cannot be fallbacked to cpu

--- tests/cpu/test_lazy_reorder.py
+++ tests/cpu/test_lazy_reorder.py
@@ -13,6 +13,8 @@ import sys
 import torch
 import intel_pytorch_extension as ipex
 
+from common_ipex_conf import AutoMixPrecision, AutoDNNL
+
 import torch.nn as nn
 import torch.backends.cudnn as cudnn
 from torch.nn import Parameter
@@ -882,7 +884,7 @@ class TestTensorShape(TestCase):
             y = torch.randn(new_shape)
             out_cpu = x_cpu_view * y
             # test if the shape of x_dpcpp_view is compatible with y
-            out_dpcpp = x_dpcpp_view * y
+            out_dpcpp = x_dpcpp_view * y.to(device)
             self.assertTrue(ipex.is_dil_tensor(out_dpcpp))
             self.assertEqual(ipex.get_dil_tensor_sizes(out_dpcpp), [1, 4, 4, 4])
             self.assertEqual(ipex.get_dil_tensor_strides(out_dpcpp), [64, 16, 4, 1])
