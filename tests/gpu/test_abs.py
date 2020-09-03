edff32590322434464517ce79acbbec074beccfe
yangba2x <baochenx.yang@intel.com>
2020-09-03 17:59:59 +0800

    add more dtype test for abs

--- tests/gpu/test_abs.py
+++ tests/gpu/test_abs.py
@@ -1,5 +1,5 @@
 import torch
-from torch.testing._internal.common_utils import TestCase
+from torch.testing._internal.common_utils import TestCase, repeat_test_for_types
 import torch_ipex
 
 cpu_device = torch.device("cpu")
@@ -7,11 +7,13 @@ dpcpp_device = torch.device("dpcpp")
 
 
 class TestTorchMethod(TestCase):
+    @repeat_test_for_types([torch.float, torch.half, torch.bfloat16])
     def test_abs(self, dtype=torch.float):
         data = [[-0.2911, -1.3204,  -2.6425,  -2.4644,  -
-                 0.6018, -0.0839, -0.1322, -0.4713, -0.3586, -0.8882]]
-        x = torch.tensor(data, device=cpu_device)
-        x_dpcpp = x.to(dpcpp_device)
-        y = torch.abs(x)
+                 0.6018, -0.0839, -0.1322, -0.4713, -0.3586, -0.8882, 0.0000, 0.0000, 1.1111, 2.2222,3.3333]]
+        excepted = [[0.2911, 1.3204,  2.6425,  2.4644,  
+                             0.6018, 0.0839, 0.1322, 0.4713, 0.3586, 0.8882, 0.0000, 0.0000, 1.1111, 2.2222,3.3333]]
+        x_dpcpp = torch.tensor(data, device=dpcpp_device)
+        y = torch.tensor(excepted, device=dpcpp_device)
         y_dpcpp = torch.abs(x_dpcpp)
-        self.assertEqual(y, y_dpcpp.to(cpu_device))
+        self.assertEqual(y.to(cpu_device), y_dpcpp.to(cpu_device))
