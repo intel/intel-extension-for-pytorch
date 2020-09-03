2cbb295ff4c8001794d39f965b777defd4925592
yangba2x <baochenx.yang@intel.com>
2020-09-03 18:31:12 +0800

    add dtype

--- tests/gpu/test_abs.py
+++ tests/gpu/test_abs.py
@@ -10,9 +10,9 @@ class TestTorchMethod(TestCase):
     @repeat_test_for_types([torch.float, torch.half, torch.bfloat16])
     def test_abs(self, dtype=torch.float):
         data = [[-0.2911, -1.3204,  -2.6425,  -2.4644,  -
-                 0.6018, -0.0839, -0.1322, -0.4713, -0.3586, -0.8882, 0.0000, 0.0000, 1.1111, 2.2222,3.3333]]
-        excepted = [[0.2911, 1.3204,  2.6425,  2.4644,  
-                             0.6018, 0.0839, 0.1322, 0.4713, 0.3586, 0.8882, 0.0000, 0.0000, 1.1111, 2.2222,3.3333]]
+                 0.6018, -0.0839, -0.1322, -0.4713, -0.3586, -0.8882, 0.0000, 0.0000, 1.1111, 2.2222, 3.3333]]
+        excepted = [[0.2911, 1.3204,  2.6425,  2.4644,
+                     0.6018, 0.0839, 0.1322, 0.4713, 0.3586, 0.8882, 0.0000, 0.0000, 1.1111, 2.2222, 3.3333]]
         x_dpcpp = torch.tensor(data, device=dpcpp_device)
         y = torch.tensor(excepted, device=dpcpp_device)
         y_dpcpp = torch.abs(x_dpcpp)
