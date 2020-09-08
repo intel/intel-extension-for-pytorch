3f5b8dfb4d995ba9fd980ea169e9e04b36e6e727
Gu Jinghui <jinghui.gu@intel.com>
2020-09-09 07:18:27 +0800

    Force invoke queue wait in sync memory copy bwt host and device
    rever change in commit: a1f23db1c88f66ce7417a7eff4dfaa8dfaa3adcb
    Without queue wait, resource exhaustion occurs due to never release kernel events.
    Need to confirm with compiler team the root cause here.

--- torch_ipex/csrc/gpu/aten/operators/Copy.cpp
+++ torch_ipex/csrc/gpu/aten/operators/Copy.cpp
@@ -245,12 +245,16 @@ void copy_kernel_dpcpp(TensorIterator& iter, bool non_blocking) {
   int64_t nbytes = iter.numel() * iter.element_size(0);
 
   if (non_blocking) {
-    dpcppMemcpyAsync(dst, src, nbytes, kind);
     // here do the dpcpp copy synchronisation.
     // we use a very simple version for the singleton sycl queue.
     // TODO: enhance this for the multi-queue.
+    dpcppMemcpyAsync(dst, src, nbytes, kind);
   } else {
     dpcppMemcpy(dst, src, nbytes, kind);
+    // FIXME: Without queue wait, resource exhaustion occurs due to never release kernel events.
+    // Need to confirm with compiler team the root cause here.
+    auto& queue = getCurrentDPCPPStream().dpcpp_queue();
+    queue.wait();
   }
 }
 
