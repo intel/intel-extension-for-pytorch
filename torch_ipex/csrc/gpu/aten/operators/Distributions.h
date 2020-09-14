43852936e83a9dd7de7256ae5503737271acc4fa
chengjun <chengjun.lu@intel.com>
2020-09-14 19:20:55 +0800

    Use the torch Philox random engine. The cuda rand style for porting distribution from cuda.

---
+++ torch_ipex/csrc/gpu/aten/operators/Distributions.h
@@ -0,0 +1,91 @@
+//
+// Created by johnlu on 2020/8/25.
+//
+
+#ifndef TORCH_IPEX_DISTRIBUTIONS_H
+#define TORCH_IPEX_DISTRIBUTIONS_H
+
+#include "Random.h"
+#include "Loops.h"
+#include <utils/Numerics.h>
+
+namespace at {
+namespace AtenIpexTypeDPCPP {
+
+
+template <typename...>
+class exponential_sycl_ker {};
+template<typename scalar_t, typename accscalar_t, typename dist_t, typename transform_t>
+void distribution_elementwise_grid_stride_kernel(at::TensorIterator& iter,
+                                                 int numel,
+                                                 std::pair<uint64_t, uint64_t> seeds,
+                                                 const dist_t dist_func,
+                                                 const transform_t transform_func) {
+
+  auto &sycl_queue = dpcpp::getCurrentDPCPPStream().dpcpp_queue();
+
+  auto offset_calc = at::dpcpp::make_offset_calculator<1>(iter);
+  auto cgf = DPCPP_Q_CGF(cgh) {
+    auto out_data = get_buffer<dpcpp_discard_w_mode>(cgh, (char*)iter.data_ptr(0));
+    auto kfn = DPCPP_Q_KFN(DPCPP::item<1> item_id)  {
+      size_t sample_id = item_id.get_id(0);
+      auto out_ptr = get_pointer(out_data);
+      RandomState<Philox4_32_10> state(seeds.first, sample_id, seeds.second);
+      auto rand = dist_func(&state);
+      accscalar_t r = ScalarConvert<scalar_t, accscalar_t>::to(rand);
+      scalar_t ret = transform_func(r);
+      auto offset = offset_calc.get(sample_id)[0];
+      scalar_t* out = (scalar_t*)(out_ptr + offset);
+      *out = ret;
+    };
+
+    cgh.parallel_for<exponential_sycl_ker<scalar_t, accscalar_t, dist_t, transform_t>>(
+      DPCPP::range<1>(numel),
+      kfn);
+  };
+
+  DPCPP_Q_ASYNC_SUBMIT(sycl_queue, cgf);
+}
+
+template<typename scalar_t,
+  typename accscalar_t,
+  typename dist_t,
+  typename transform_t>
+void distribution_nullary_kernel(at::TensorIterator& iter,
+                                 at::DPCPPGenerator* gen,
+                                 const dist_t& dist_func,
+                                 const transform_t transform_func) {
+  int64_t numel = iter.numel();
+  if (numel == 0) {
+    return;
+  }
+
+  std::pair<uint64_t, uint64_t> rng_engine_inputs;
+  {
+    // See Note [Acquire lock when using random generators]
+    std::lock_guard<std::mutex> lock(gen->mutex_);
+    rng_engine_inputs = gen->philox_engine_inputs(1);
+  }
+
+  if (!iter.can_use_32bit_indexing()) {
+    for (auto& sub_iter : iter.with_32bit_indexing()) {
+      distribution_nullary_kernel<scalar_t, accscalar_t>(sub_iter,
+                                                         gen,
+                                                         dist_func,
+                                                         transform_func);
+    }
+    return;
+  }
+
+  distribution_elementwise_grid_stride_kernel<scalar_t, accscalar_t>(
+    iter,
+    numel,
+    rng_engine_inputs,
+    dist_func,
+    transform_func);
+}
+
+}
+}
+
+#endif //TORCH_IPEX_DISTRIBUTIONS_H
