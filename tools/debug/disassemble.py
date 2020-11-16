c7ffa1a87cd27fcfb1039f44afde33b373689b3c
johnlu <chengjun.lu@intel.com>
2020-11-16 23:40:43 +0800

    Temp check in the debug disassemble GPU code script.

---
+++ tools/debug/disassemble.py
@@ -0,0 +1,28 @@
+"Helper to disassemble the GPU code"
+import multiprocessing
+import os
+import re
+from subprocess import check_call, check_output
+import sys
+import distutils
+import distutils.sysconfig
+from distutils.version import LooseVersion
+import json
+
+
+def read_compile_commands(file):
+    with open(file) as json_file:
+        compile_database = json.load(json_file)
+
+    print("compiled objects number ", len(compile_database))
+    return compile_database
+
+
+def get_compile_options(command):
+    options = re.match(r'^(\S*)(\s*)(.*)(-o)', command)
+    return options.groups()[2]
+
+
+def disassemble(file):
+
+    
