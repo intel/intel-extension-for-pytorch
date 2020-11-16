eeaf6d02571e7a87badfc84f643a8271345ff268
haozhe.zhu <haozhe.zhu@intel.com>
2020-11-15 23:26:08 -0800

    remove redundant if

--- torch_ipex/csrc/cpu/aten/operators/embedding_bag.cpp
+++ torch_ipex/csrc/cpu/aten/operators/embedding_bag.cpp
@@ -73,23 +73,21 @@ embedding_bag_impl(const at::Tensor & weight, const at::Tensor & indices,
   const at::Tensor & per_sample_weights, bool include_last_offset) {
 
   at::Tensor offsets_ = offsets.contiguous();
-  if (embedding_bag_fast_path_sum(weight, per_sample_weights, mode)) {
     at::Tensor bag_size;
     at::Tensor offset2bag;
-    if (weight.requires_grad()) {
-      // in MODE_SUM, only initialize bag_size if we need gradients
-      bag_size = at::native::full(offsets_.sizes(), 0, indices.options());
-      offset2bag = at::empty({0}, offsets_.options());
-    }
-
-    at::Tensor output;
-    if(is_bfloat16_tensor(weight)) {
-       output = _embedding_bag_index_add_select_fast<at::BFloat16>(indices, weight, offsets_, include_last_offset);
-    } else {
-       output = _embedding_bag_index_add_select_fast<float>(indices, weight, offsets_, include_last_offset);
-    }
-    return std::tuple<at::Tensor, at::Tensor, at::Tensor, at::Tensor>(output, offset2bag, bag_size, bag_size);
+  if (weight.requires_grad()) {
+    // in MODE_SUM, only initialize bag_size if we need gradients
+    bag_size = at::native::full(offsets_.sizes(), 0, indices.options());
+    offset2bag = at::empty({0}, offsets_.options());
   }
+
+  at::Tensor output;
+  if(is_bfloat16_tensor(weight)) {
+      output = _embedding_bag_index_add_select_fast<at::BFloat16>(indices, weight, offsets_, include_last_offset);
+  } else {
+      output = _embedding_bag_index_add_select_fast<float>(indices, weight, offsets_, include_last_offset);
+  }
+  return std::tuple<at::Tensor, at::Tensor, at::Tensor, at::Tensor>(output, offset2bag, bag_size, bag_size);
 }
 
 static inline at::Tensor expand_values_if_needed(const at::Tensor& values) {
