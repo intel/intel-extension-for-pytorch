b2f9e16daf4dcc4d68ebb229c85c2e3fa48826b4
johnlu <chengjun.lu@intel.com>
2020-11-20 21:04:22 +0800

    Port the logical xor to pytorch 1.7 changes

--- torch_ipex/csrc/gpu/aten/operators/LogicalXor.cpp
+++ torch_ipex/csrc/gpu/aten/operators/LogicalXor.cpp
@@ -8,39 +8,48 @@ namespace at {
 namespace AtenIpexTypeXPU {
 namespace impl {
 
-DPCPP_DEF_K1(logical_xor);
+DPCPP_DEF_K1(logical_xor_bool);
+DPCPP_DEF_K1(logical_xor_scalar);
 static void logical_xor_kernel(TensorIterator& iter) {
-  IPEX_DISPATCH_ALL_TYPES_AND2(
-      kBool, kHalf, iter.dtype(1), "logical_xor_dpcpp", [&]() {
-        using self_t = scalar_t;
-        IPEX_DISPATCH_ALL_TYPES_AND2(
-            kBool, kHalf, iter.dtype(2), "logical_xor_dpcpp", [&]() {
-              using other_t = scalar_t;
-              IPEX_DISPATCH_ALL_TYPES_AND2(
-                  kBool, kHalf, iter.dtype(0), "logical_xor_dpcpp", [&]() {
-                    dpcpp_kernel_for_tensor_iter<DPCPP_K(
-                        logical_xor, self_t, other_t)>(
-                        iter, [](self_t a, other_t b) -> scalar_t {
-                          return static_cast<scalar_t>(bool(a) != bool(b));
-                        });
-                  });
-            });
-      });
+  if (iter.dtype() == ScalarType::Bool) {
+    IPEX_DISPATCH_ALL_TYPES_AND2(kBool, kHalf, iter.input_dtype(), "logical_xor_kernel", [&]() {
+        dpcpp_kernel_for_tensor_iter<DPCPP_K(logical_xor_bool, scalar_t)>(iter,
+                   [](scalar_t a, scalar_t b) -> bool {
+                       return bool(a) != bool(b);
+                   });
+    });
+  } else {
+    IPEX_DISPATCH_ALL_TYPES_AND2(kBool, kHalf, iter.dtype(), "logical_xor_kernel", [&]() {
+        dpcpp_kernel_for_tensor_iter<DPCPP_K(logical_xor_scalar, scalar_t)>(iter,
+                   [](scalar_t a, scalar_t b) -> scalar_t {
+                       return static_cast<scalar_t>(bool(a) != bool(b));
+                   });
+    });
+  }
 }
 
 } // namespace impl
+
+static void check_convert(Scalar scalar, ScalarType scalarType) {
+  // Validate that is possible to convert scalar to tensor dtype without overflow
+  IPEX_DISPATCH_ALL_TYPES_AND_COMPLEX_AND3(at::ScalarType::Bool, at::ScalarType::BFloat16, at::ScalarType::Half, scalarType, "check_convert", [&]{
+      scalar.to<scalar_t>();
+  });
+}
 
 Tensor& logical_xor_out(
     Tensor& result,
     const Tensor& self,
     const Tensor& other) {
-  auto iter = TensorIteratorConfig()
-  .check_all_same_dtype(false)
-  .set_check_mem_overlap(true)
-  .add_output(result)
-  .add_input(self)
-  .add_input(other)
-  .build();
+  if (self.scalar_type() != other.scalar_type()) {
+    if (self.dim() != 0 && other.dim() == 0) {
+      check_convert(other.item(), self.scalar_type());
+    } else if (self.dim() == 0 && other.dim() != 0) {
+      check_convert(self.item(), other.scalar_type());
+    }
+  }
+
+  auto iter = TensorIterator::comparison_op(result, self, other);
   impl::logical_xor_kernel(iter);
   return result;
 }
