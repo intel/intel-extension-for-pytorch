95e830b6805e5a0df813926f218012cbe16e2ba4
ling1 <xiao.ling@intel.com>
2021-02-22 11:20:33 +0800

    fix issues in review

--- tests/gpu/examples/test_random.py
+++ tests/gpu/examples/test_random.py
@@ -17,13 +17,13 @@ class TestNNMethod(TestCase):
 
         t = t.fill_(-1)
         t = t.random_(lb, ub)
-        #self.assertEqual(t.min(), lb)
-        #self.assertEqual(t.max(), ub - 1)
+        self.assertEqual(t.min(), lb)
+        self.assertEqual(t.max(), ub - 1)
 
         t = t.fill_(-1)
         t = t.random_(ub)
-        #self.assertEqual(t.min(), 0)
-        #self.assertEqual(t.max(), ub - 1)
+        self.assertEqual(t.min(), 0)
+        self.assertEqual(t.max(), ub - 1)
 
     def test_random_bool(self):
         size = 2000
@@ -59,21 +59,23 @@ class TestNNMethod(TestCase):
                 if to_ > from_:
                     if not (min_val <= from_ <= max_val) or not (min_val <= (to_ - 1) <= max_val):
                         if not (min_val <= from_ <= max_val):
-                            self.assertWarnsRegex(
-                                lambda: t.random_(from_, to_),
-                                "from is out of bounds"
-                            )
+                            return True
+                    #        self.assertWarnsRegex(
+                    #            lambda: t.random_(from_, to_),
+                    #            "from is out of bounds"
+                    #        )
                         if not (min_val <= (to_ - 1) <= max_val):
-                            self.assertWarnsRegex(
-                                lambda: t.random_(from_, to_),
-                                "to - 1 is out of bounds"
-                            )
+                            return True
+                    #        self.assertWarnsRegex(
+                    #            lambda: t.random_(from_, to_),
+                    #            "to - 1 is out of bounds"
+                    #        )
                     else:
                         t.random_(from_, to_)
                         range_ = to_ - from_
                         delta = 1
-                        #self.assertTrue(from_ <= t.to(torch.int).min() < (from_ + delta))
-                        #self.assertTrue((to_ - delta) <= t.to(torch.int).max() < to_)
+                        self.assertTrue(from_ <= t.to(torch.int).min() < (from_ + delta))
+                        self.assertTrue((to_ - delta) <= t.to(torch.int).max() < to_)
                 else:
                     self.assertRaisesRegex(
                         RuntimeError,
@@ -83,8 +85,8 @@ class TestNNMethod(TestCase):
 
     def test_random_full_range(self):
         # TODO: https://github.com/pytorch/pytorch/issues/33793
-        torch.set_default_dtype(torch.double)
-
+        dtype = torch.double
+        
         size = 2000
         alpha = 0.1
 
@@ -106,13 +108,13 @@ class TestNNMethod(TestCase):
 
         t.random_(from_, None)
         delta = max(1, alpha * range_)
-        #self.assertTrue(from_ <= t.to(torch.double).min() < (from_ + delta))
-        #self.assertTrue((to_inc_ - delta) < t.to(torch.double).max() <= to_inc_)
+        self.assertTrue(from_ <= t.to(torch.double).min() < (from_ + delta))
+        self.assertTrue((to_inc_ - delta) < t.to(torch.double).max() <= to_inc_)
 
     def test_random_from_to(self):
         # TODO: https://github.com/pytorch/pytorch/issues/33793
-        torch.set_default_dtype(torch.double)
-
+        dtype = torch.double
+        
         size = 2000
         alpha = 0.1
 
@@ -167,21 +169,22 @@ class TestNNMethod(TestCase):
                         if dtype == torch.bfloat16:
                             # Less strict checks because of rounding errors
                             # TODO investigate rounding errors
-                            #self.assertTrue(from_ <= t.to(torch.double).min() < (from_ + delta))
-                            #self.assertTrue((to_ - delta) < t.to(torch.double).max() <= to_)
+                            self.assertTrue(from_ <= t.to(torch.double).min() < (from_ + delta))
+                            self.assertTrue((to_ - delta) < t.to(torch.double).max() <= to_)
                         else:
-                            #self.assertTrue(from_ <= t.to(torch.double).min() < (from_ + delta))
-                            #self.assertTrue((to_ - delta) <= t.to(torch.double).max() < to_)
+                            self.assertTrue(from_ <= t.to(torch.double).min() < (from_ + delta))
+                            self.assertTrue((to_ - delta) <= t.to(torch.double).max() < to_)
                 else:
                     self.assertRaisesRegex(
                         RuntimeError,
                         "random_ expects 'from' to be less than 'to', but got from=" + str(from_) + " >= to=" + str(to_),
                         lambda: t.random_(from_, to_)
+                    )
 
     def test_random_to(self):
         # TODO: https://github.com/pytorch/pytorch/issues/33793
-        torch.set_default_dtype(torch.double)
-
+        dtype = torch.double
+        
         size = 2000
         alpha = 0.1
 
@@ -225,11 +228,11 @@ class TestNNMethod(TestCase):
                     if dtype == torch.bfloat16:
                         # Less strict checks because of rounding errors
                         # TODO investigate rounding errors
-                        #self.assertTrue(from_ <= t.to(torch.double).min() < (from_ + delta))
-                        #self.assertTrue((to_ - delta) < t.to(torch.double).max() <= to_)
+                        self.assertTrue(from_ <= t.to(torch.double).min() < (from_ + delta))
+                        self.assertTrue((to_ - delta) < t.to(torch.double).max() <= to_)
                     else:
-                        #self.assertTrue(from_ <= t.to(torch.double).min() < (from_ + delta))
-                        #self.assertTrue((to_ - delta) <= t.to(torch.double).max() < to_)
+                        self.assertTrue(from_ <= t.to(torch.double).min() < (from_ + delta))
+                        self.assertTrue((to_ - delta) <= t.to(torch.double).max() < to_)
             else:
                 self.assertRaisesRegex(
                     RuntimeError,
@@ -239,7 +242,7 @@ class TestNNMethod(TestCase):
 
     def test_random_default(self):
         # TODO: https://github.com/pytorch/pytorch/issues/33793
-        torch.set_default_dtype(torch.double)
+        dtype = torch.double
 
         size = 2000
         alpha = 0.1
@@ -257,6 +260,6 @@ class TestNNMethod(TestCase):
 
         t = torch.empty(size, device=sycl_device)
         t.random_()
-        #self.assertTrue(0 <= t.to(torch.double).min() < alpha * to_inc)
-        #self.assertTrue((to_inc - alpha * to_inc) < t.to(torch.double).max() <= to_inc)
+        self.assertTrue(0 <= t.to(torch.double).min() < alpha * to_inc)
+        self.assertTrue((to_inc - alpha * to_inc) < t.to(torch.double).max() <= to_inc)
 
