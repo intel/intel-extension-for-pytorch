419776b5f79d97241376dcf1233c678e10c7b938
Ma, Jing1 <jing1.ma@intel.com>
2021-02-25 14:03:24 +0800

    Modify Eltwise oneDNN integration naming

--- torch_ipex/csrc/gpu/oneDNN/Eltwise.h
+++ torch_ipex/csrc/gpu/oneDNN/Eltwise.h
@@ -38,13 +38,13 @@ static inline void eltwise(
   auto format_data = get_dnnl_default_format(src.dim());
   auto src_md = memory::desc({src_tz}, data_t, format_data);
 
-  memory src_usr_memory;
+  memory src_memory;
   if (!lazy_reorder_enabled()) {
-    src_usr_memory = dpcpp_onednn_memory(src_md, engine, src.data_ptr());
+    src_memory = dpcpp_onednn_memory(src_md, engine, src.data_ptr());
   } else {
     auto src_ctx = at::AtenIpexTypeXPU::DPCPPTensorContext::get_tensor_ctx(src);
     src_md = src_ctx.is_plain() ? src_md : src_ctx.meta();
-    src_usr_memory = dpcpp_onednn_memory(src_md, engine, src.data_ptr());
+    src_memory = dpcpp_onednn_memory(src_md, engine, src.data_ptr());
   }
 
 
@@ -56,26 +56,26 @@ static inline void eltwise(
   eltwise_forward::desc eltwise_eltwiseFwd_desc(prop_kind::forward, alg_kind, src_md, alpha, beta);
   auto eltwise_forward_pd = eltwise_forward::primitive_desc(eltwise_eltwiseFwd_desc, engine);
 
-  memory dst_usr_memory;
+  memory dst_memory;
   if (!lazy_reorder_enabled()) {
     if (!dst.defined())
       dst = at::empty_like(src);
-    dst_usr_memory = dpcpp_onednn_memory(eltwise_forward_pd.dst_desc(), engine, dst.data_ptr());
+    dst_memory = dpcpp_onednn_memory(eltwise_forward_pd.dst_desc(), engine, dst.data_ptr());
   } else {
     if (dst.defined()) {
       auto dst_ctx = at::AtenIpexTypeXPU::DPCPPTensorContext::get_tensor_ctx(dst);
       auto dst_md = dst_ctx.is_plain() ? src_md : dst_ctx.meta();
-      dst_usr_memory = dpcpp_onednn_memory(dst_md, engine, dst.data_ptr());
+      dst_memory = dpcpp_onednn_memory(dst_md, engine, dst.data_ptr());
     } else {
       auto plain_dst_md = memory::desc({src_tz}, data_t, format_data);
       auto expected_dst_md = eltwise_forward_pd.dst_desc();
       if (plain_dst_md != expected_dst_md) {
         dst = at::AtenIpexTypeXPU::empty_opaque_tensor(
             expected_dst_md, src.options(), c10::nullopt);
-        dst_usr_memory = dpcpp_onednn_memory(expected_dst_md, engine, dst.data_ptr());
+        dst_memory = dpcpp_onednn_memory(expected_dst_md, engine, dst.data_ptr());
       } else {
         dst = at::empty_like(src);
-        dst_usr_memory = dpcpp_onednn_memory(plain_dst_md, engine, dst.data_ptr());
+        dst_memory = dpcpp_onednn_memory(plain_dst_md, engine, dst.data_ptr());
       }
     }
   }
@@ -92,8 +92,8 @@ static inline void eltwise(
   DPCPP_ONEDNN_EXEC(
       eltwise_fwd,
       strm,
-      {{DNNL_ARG_SRC, src_usr_memory},
-       {DNNL_ARG_DST, dst_usr_memory}});
+      {{DNNL_ARG_SRC, src_memory},
+       {DNNL_ARG_DST, dst_memory}});
 }
 
 template <algorithm alg_kind>
@@ -117,18 +117,18 @@ static inline void eltwise_backward(
   auto src_dst_md = memory::desc({src_dst_tz}, data_t, format_data);
   auto diff_dst_md = memory::desc({src_dst_tz}, data_t, format_data);
 
-  memory src_dst_usr_memory, diff_dst_usr_memory;
+  memory src_dst_memory, diff_dst_memory;
   if (!lazy_reorder_enabled()) {
-    src_dst_usr_memory  = dpcpp_onednn_memory(src_dst_md, engine, src_dst.data_ptr());
-    diff_dst_usr_memory = dpcpp_onednn_memory(diff_dst_md, engine, diff_dst.data_ptr());
+    src_dst_memory  = dpcpp_onednn_memory(src_dst_md, engine, src_dst.data_ptr());
+    diff_dst_memory = dpcpp_onednn_memory(diff_dst_md, engine, diff_dst.data_ptr());
   } else {
     auto src_dst_ctx = at::AtenIpexTypeXPU::DPCPPTensorContext::get_tensor_ctx(src_dst);
     src_dst_md = src_dst_ctx.is_plain() ? src_dst_md : src_dst_ctx.meta();
-    src_dst_usr_memory = dpcpp_onednn_memory(src_dst_md, engine, src_dst.data_ptr());
+    src_dst_memory = dpcpp_onednn_memory(src_dst_md, engine, src_dst.data_ptr());
 
     auto diff_dst_ctx = at::AtenIpexTypeXPU::DPCPPTensorContext::get_tensor_ctx(diff_dst);
     diff_dst_md = diff_dst_ctx.is_plain() ? diff_dst_md : diff_dst_ctx.meta();
-    diff_dst_usr_memory = dpcpp_onednn_memory(diff_dst_md, engine, diff_dst.data_ptr());
+    diff_dst_memory = dpcpp_onednn_memory(diff_dst_md, engine, diff_dst.data_ptr());
   }
 
   eltwise_forward::desc eltwise_eltwiseFwd_desc(
@@ -138,23 +138,23 @@ static inline void eltwise_backward(
   auto eltwise_backward_pd = eltwise_backward::primitive_desc(
       eltwise_eltwiseBwd_desc, engine, eltwise_forward_pd);
 
-  memory diff_src_usr_memory;
+  memory diff_src_memory;
   if (!lazy_reorder_enabled()) {
     if (!diff_src.defined()) {
       diff_src = at::empty_like(src_dst);
     }
     auto diff_src_md = memory::desc({src_dst_tz, data_t, format_data});
-    diff_src_usr_memory = dpcpp_onednn_memory(diff_src_md, engine, diff_src.data_ptr());
+    diff_src_memory = dpcpp_onednn_memory(diff_src_md, engine, diff_src.data_ptr());
   } else {
     auto plain_diff_src_md = memory::desc({src_dst_tz}, data_t, format_data);
     auto expected_diff_src_md = eltwise_backward_pd.diff_src_desc();
     if (plain_diff_src_md != expected_diff_src_md) {
       diff_src = at::AtenIpexTypeXPU::empty_opaque_tensor(
             expected_diff_src_md, src_dst.options(), c10::nullopt);
-      diff_src_usr_memory = dpcpp_onednn_memory(expected_diff_src_md, engine, diff_src.data_ptr());
+      diff_src_memory = dpcpp_onednn_memory(expected_diff_src_md, engine, diff_src.data_ptr());
     } else {
       diff_src = at::empty_like(src_dst);
-      diff_src_usr_memory = dpcpp_onednn_memory(plain_diff_src_md, engine, diff_src.data_ptr());
+      diff_src_memory = dpcpp_onednn_memory(plain_diff_src_md, engine, diff_src.data_ptr());
     }
   }
 
@@ -162,14 +162,14 @@ static inline void eltwise_backward(
   auto eltwise_bwd = dnnl::eltwise_backward(eltwise_backward_pd);
   if (alg_kind == algorithm::eltwise_logistic_use_dst_for_bwd) {
     DPCPP_ONEDNN_EXEC(eltwise_bwd, strm,
-        {{DNNL_ARG_DST, src_dst_usr_memory},
-        {DNNL_ARG_DIFF_DST, diff_dst_usr_memory},
-        {DNNL_ARG_DIFF_SRC, diff_src_usr_memory}});
+        {{DNNL_ARG_DST, src_dst_memory},
+        {DNNL_ARG_DIFF_DST, diff_dst_memory},
+        {DNNL_ARG_DIFF_SRC, diff_src_memory}});
   } else {
     DPCPP_ONEDNN_EXEC(eltwise_bwd, strm,
-        {{DNNL_ARG_SRC, src_dst_usr_memory},
-        {DNNL_ARG_DIFF_DST, diff_dst_usr_memory},
-        {DNNL_ARG_DIFF_SRC, diff_src_usr_memory}});
+        {{DNNL_ARG_SRC, src_dst_memory},
+        {DNNL_ARG_DIFF_DST, diff_dst_memory},
+        {DNNL_ARG_DIFF_SRC, diff_src_memory}});
   }
 }
 
