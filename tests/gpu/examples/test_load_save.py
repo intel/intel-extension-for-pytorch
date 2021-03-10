f62f6f01ee006505aedd14966d1c1fb7fe3be237
fengyuan <feng1.yuan@intel.com>
2021-03-10 09:13:40 +0800

    enable torch.load/torch.save unit test
    Signed-off-by: fengyuan <feng1.yuan@intel.com>

---
+++ tests/gpu/examples/test_load_save.py
@@ -0,0 +1,19 @@
+import torch
+from torch.testing._internal.common_utils import TestCase
+import torch_ipex
+import pytest
+
+cpu_device = torch.device("cpu")
+xpu_device = torch.device("xpu")
+
+class TestTorchMethod(TestCase):
+    def test_load_save(self, dtype=torch.float):
+        save = torch.randn(2, 2)
+        print("save: ", save)
+        save = save.to('xpu')
+        torch.save(save, 'tensor.pt')
+
+        load = torch.load('tensor.pt', map_location=xpu_device)
+        print("torch.load('tensor.pt', map_location=xpu_device): ", load.to(cpu_device))
+
+        self.assertEqual(save.to(cpu_device), load.to(cpu_device))
