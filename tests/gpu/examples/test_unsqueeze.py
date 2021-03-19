10dc2ef097e8ee711cb27d9c923ed0032e4bede1
Huang, Zhiyuan <zhiyuan.huang@intel.com>
2021-03-19 13:49:19 +0800

    Add torch.unsqueeze() UT

---
+++ tests/gpu/examples/test_unsqueeze.py
@@ -0,0 +1,19 @@
+import torch
+from torch.testing._internal.common_utils import TestCase
+import torch_ipex
+
+cpu_device = torch.device("cpu")
+xpu_device = torch.device("xpu")
+
+class TestTorchMethod(TestCase):
+    def test_unsqueeze(self, dtype=torch.float):
+
+        x = torch.tensor([1, 2, 3, 4], device=cpu_device)
+        y_cpu = x.unsqueeze(1)
+        print("y = ", y_cpu)
+
+        x_xpu = x.to("xpu")
+        y_xpu = x_xpu.unsqueeze(1)
+        print("y_xpu ", y_xpu.cpu())
+
+        self.assertEqual(y_cpu, y_xpu.to(cpu_device))
