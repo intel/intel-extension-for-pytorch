8007bc978464d03223c87f7a77f7ee7b4c61de48
Chen, Jian Ping <jian.ping.chen@intel.com>
2021-04-21 16:54:49 -0700

    align cat, tr and mm buffer in interaction_forward

--- torch_ipex/csrc/cpu/ExtendOPs.cpp
+++ torch_ipex/csrc/cpu/ExtendOPs.cpp
@@ -190,6 +190,7 @@ static inline void flat_triangle(const T *in, T *out, size_t size) {
   RECORD_FUNCTION("ExtendOps::_flat_triangle", std::vector<c10::IValue>({}));
 #endif
   size_t offset = 0;
+  #pragma unroll(2)
   for (int i = 1; i < size; i++) {
     move_ker(&out[offset], &in[i * size], i);
     offset += i;
@@ -265,25 +266,24 @@ inline at::Tensor _interaction_forward(const std::vector<at::Tensor> &input) {
   auto vector_nums = total_feature_size / vector_size;
   TORCH_INTERNAL_ASSERT_DEBUG_ONLY(total_feature_size % vector_size == 0);
   auto interact_feature_size = vector_nums * (vector_nums - 1) / 2;
-  auto tr_vector_size = sizeof(T) == 4 ? vector_size : vector_size / 2;
-  auto out = at::empty({batch_size, interact_feature_size + vector_size},
-                       input[0].options());
-  auto out_data = out.data_ptr<T>();
   auto out_data_line_len = interact_feature_size + vector_size;
+  auto tr_vector_size = sizeof(T) == 4 ? vector_size : vector_size / 2;
+  auto out = at::empty({batch_size, out_data_line_len}, input[0].options());
+  auto out_data = out.data_ptr<T>();
 
   auto mm_kernel = get_mm_kernel<T>(vector_nums, vector_nums, vector_size);
   auto tr_kernel = get_tr_kernel(tr_vector_size, vector_nums, vector_nums);
 
   at::parallel_for(0, batch_size, 0, [&](int64_t start, int64_t end) {
-    T cat_buf[vector_nums * vector_size];
-    T tr_buf[vector_nums * vector_size];
-    T mm_buf[vector_nums * vector_nums];
+    T cat_buf[vector_nums * vector_size] __attribute__((aligned(64)));
+    T tr_buf[vector_nums * vector_size] __attribute__((aligned(64)));
+    T mm_buf[vector_nums * vector_nums] __attribute__((aligned(64)));
     for (int64_t i = start; i < end; i++) {
       cat<T>(cat_buf, input_data, feature_sizes, i);
       tr_kernel(cat_buf, &tr_vector_size, tr_buf, &vector_nums);
-      move_ker(&out_data[i * out_data_line_len], &input_data[0][i * vector_size], vector_size);
       mm_kernel((xsmm_dtype<T> *)tr_buf, (xsmm_dtype<T> *)cat_buf,
                 (xsmm_dtype<T> *)mm_buf);
+      move_ker(&out_data[i * out_data_line_len], &input_data[0][i * vector_size], vector_size);
       T* flat_buf = (T*)(&out_data[i * out_data_line_len] + vector_size);
       flat_triangle<T>(mm_buf, flat_buf, vector_nums);
     }
