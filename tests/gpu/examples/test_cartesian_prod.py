ca91d2200b02b72919d0416405b12aa197bff0e6
Xunsong, Huang <xunsong.huang@intel.com>
2021-06-07 21:49:38 +0800

    To add UT for operator `cartesian_prod` (#75)
    Signed-off-by: Xunsong, Huang <xunsong.huang@intel.com>
    
    Co-authored-by: Jinghui <jinghui.gu@intel.com>

---
+++ tests/gpu/examples/test_cartesian_prod.py
@@ -0,0 +1,19 @@
+import torch
+import torch_ipex
+import pytest
+from torch.testing._internal.common_utils import TestCase
+
+cpu_device = torch.device('cpu')
+xpu_device = torch.device('xpu')
+
+class TestTorchMethod(TestCase):
+    def test_cartesian_prod(self, dtype=torch.float):
+        a_cpu = torch.tensor([1, 2, 3]).to(cpu_device)
+        b_cpu = torch.tensor([4, 5]).to(cpu_device)
+        y_cpu = torch.cartesian_prod(a_cpu, b_cpu)
+
+        a_xpu = a_cpu.to(xpu_device)
+        b_xpu = b_cpu.to(xpu_device)
+        y_xpu = torch.cartesian_prod(a_xpu, b_xpu)
+
+        self.assertEqual(y_cpu, y_xpu)
