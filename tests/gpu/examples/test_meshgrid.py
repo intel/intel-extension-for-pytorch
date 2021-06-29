f76c0466bff300ff048053495e61736112245f62
KevinYuk <kevin.yu@intel.com>
2021-06-29 09:14:52 +0800

    add test script for meshgrid and tensordot (#100)
    Co-authored-by: Jinghui <jinghui.gu@intel.com>

---
+++ tests/gpu/examples/test_meshgrid.py
@@ -0,0 +1,26 @@
+import torch
+import numpy as np
+from torch.testing._internal.common_utils import TestCase
+import torch_ipex
+
+cpu_device = torch.device("cpu")
+dpcpp_device = torch.device("xpu")
+
+class TestTensorMethod(TestCase):
+    def test_meshgrid(self, dtype=torch.float):
+        x = torch.tensor([1, 2, 3])
+        y = torch.tensor([4, 5, 6])
+        grid_x, grid_y = torch.meshgrid(x, y)
+        print("\ngrid_x: ", grid_x)
+        print("\ngrid_y: ", grid_y)
+
+        print("---")
+        x_dpcpp = x.to(dpcpp_device)
+        y_dpcpp = y.to(dpcpp_device)
+        grid_x_dpcpp, grid_y_dpcpp = torch.meshgrid(x_dpcpp, y_dpcpp)
+        print("\ngrid_x_dpcpp: ", grid_x_dpcpp.to(cpu_device))
+        print("\ngrid_y_dpcpp: ", grid_y_dpcpp.to(cpu_device))
+
+
+        self.assertEqual(grid_x, grid_x_dpcpp.to(cpu_device))
+        self.assertEqual(grid_y, grid_y_dpcpp.to(cpu_device))
---
+++ tests/gpu/examples/test_tensordot.py
@@ -0,0 +1,22 @@
+import torch
+import numpy as np
+from torch.testing._internal.common_utils import TestCase
+import torch_ipex
+
+cpu_device = torch.device("cpu")
+dpcpp_device = torch.device("xpu")
+
+class TestTensorMethod(TestCase):
+    def test_tensordot(self, dtype=torch.float):
+        a = torch.arange(60.).reshape(3, 4, 5)
+        b = torch.arange(24.).reshape(4, 3, 2)
+        c = torch.tensordot(a, b, dims=([1, 0], [0, 1]))
+        print(c)
+
+        print("---")
+        a_dpcpp = a.to(dpcpp_device)
+        b_dpcpp = b.to(dpcpp_device)
+        c_dpcpp = torch.tensordot(a_dpcpp, b_dpcpp, dims=([1, 0], [0, 1]))
+        print(c_dpcpp.to(cpu_device))
+
+        self.assertEqual(c, c_dpcpp.to(cpu_device))
