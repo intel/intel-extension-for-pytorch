e5462b263a31db6c6886e35d9ec434958197ca00
Xunsong, Huang <xunsong.huang@intel.com>
2021-07-01 20:49:39 +0800

    Write UT for op trapz (#147)
    Copied test_trapz from test_torch.py within PyTorch to IPEX
    
    Signed-off-by: Xunsong, Huang <xunsong.huang@intel.com>
    
    Co-authored-by: Jinghui <jinghui.gu@intel.com>

---
+++ tests/gpu/examples/test_trapz.py
@@ -0,0 +1,50 @@
+import torch
+import torch_ipex
+from torch.testing._internal.common_utils import TestCase
+import pytest
+
+try:
+    import numpy as np
+except ImportError:
+    TEST_NUMPY = False
+else:
+    TEST_NUMPY = True
+
+@pytest.mark.skipif("not TEST_NUMPY")
+class TestTorchMethod(TestCase):
+    def test_trapz(self, device=torch.device('xpu')):
+        def test_dx(sizes, dim, dx, device):
+            t = torch.randn(sizes, device=device)
+            actual = torch.trapz(t, dx=dx, dim=dim)
+            expected = np.trapz(t.cpu().numpy(), dx=dx, axis=dim)
+            self.assertEqual(expected.shape, actual.shape)
+            self.assertEqual(expected, actual)
+
+        def test_x(sizes, dim, x, device):
+            t = torch.randn(sizes, device=device)
+            actual = torch.trapz(t, x=torch.tensor(x, device=device), dim=dim)
+            expected = np.trapz(t.cpu().numpy(), x=x, axis=dim)
+            self.assertEqual(expected.shape, actual.shape)
+            self.assertEqual(expected, actual.cpu())
+
+        test_dx((2, 3, 4), 1, 1, device)
+        test_dx((10, 2), 0, 0.1, device)
+        test_dx((1, 10), 0, 2.3, device)
+        test_dx((0, 2), 0, 1.0, device)
+        test_dx((0, 2), 1, 1.0, device)
+        test_x((2, 3, 4), 1, [1.0, 2.0, 3.0], device)
+        test_x((10, 2), 0, [2.0, 3.0, 4.0, 7.0, 11.0, 14.0, 22.0, 26.0, 26.1, 30.3], device)
+        test_x((1, 10), 0, [1.0], device)
+        test_x((0, 2), 0, [], device)
+        test_x((0, 2), 1, [1.0, 2.0], device)
+        with self.assertRaisesRegex(
+                IndexError,
+                'Dimension out of range'):
+            test_x((2, 3), 2, [], device)
+            test_dx((2, 3), 2, 1.0, device)
+        with self.assertRaisesRegex(
+                RuntimeError,
+                'There must be one `x` value for each sample point'):
+            test_x((2, 3), 1, [1.0, 2.0], device)
+            test_x((2, 3), 1, [1.0, 2.0, 3.0, 4.0], device)
+
