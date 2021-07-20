08f85bb15ea0679339cc27dad5c200d52d3876c8
Xiao <xiao.ling@intel.com>
2021-07-20 09:45:19 +0800

    enable op cauchy_ (#188)
    * enable op cauchy_
    
    Signed-off-by: Ling,Xiao <xiao.ling@intel.com>
    
    * enable op cauchy_
    
    Signed-off-by: Ling,Xiao <xiao.ling@intel.com>
    
    Co-authored-by: wuhuikx <hui.h.wu@intel.com>
    Co-authored-by: Jinghui <jinghui.gu@intel.com>

---
+++ csrc/aten/operators/DistributionCauchy.cpp
@@ -0,0 +1,80 @@
+#include <ATen/ATen.h>
+#include <ATen/native/TensorIterator.h>
+
+#include <utils/DPCPP.h>
+#include <core/Generator.h>
+#include "comm/ATDispatch.h"
+
+#include <ATen/AtenIpexTypeXPU.h>
+#include "Random.h"
+#include "Distributions.h"
+
+#ifdef USE_ONEMKL
+#include <mkl.h>
+#include <oneapi/mkl.hpp>
+#include <utils/oneMKLUtils.h>
+#endif
+
+namespace at {
+namespace AtenIpexTypeXPU {
+
+void cauchy_kernel(TensorIterator& iter, double median_, double sigma_, c10::optional<Generator> generator) {
+  auto gen = get_generator_or_default<xpu::dpcpp::DPCPPGeneratorImpl>(
+      generator, xpu::dpcpp::detail::getDefaultDPCPPGenerator());
+  IPEX_DISPATCH_FLOATING_TYPES_AND2(at::ScalarType::Half, at::ScalarType::BFloat16, iter.dtype(), "cauchy_dpcpp", [&] {
+    using accscalar_t = DiscreteDistributionType<scalar_t>::type;
+    auto median = static_cast<accscalar_t>(median_);
+    auto sigma = static_cast<accscalar_t>(sigma_);
+    auto cauchy_func = [median, sigma] (accscalar_t rand) {
+      return static_cast<scalar_t>(median + sigma * at::tan(static_cast<accscalar_t>(M_PI) * (rand - static_cast<accscalar_t>(0.5))));
+    };
+    AtenIpexTypeXPU::distribution_nullary_kernel<scalar_t, accscalar_t>(iter,
+      gen,
+      [] (RandomState<Philox4_32_10>* state) { return state->uniform<scalar_t>(); },
+      cauchy_func);
+  });
+}
+
+template<typename scalar_t, typename accscalar_t>
+void cauchy_mkl_kernel(Tensor& self,
+    int64_t numel,
+    accscalar_t median_,
+    accscalar_t sigma_,
+    c10::optional<Generator> generator) {
+  auto gen = get_generator_or_default<xpu::dpcpp::DPCPPGeneratorImpl>(
+      generator, xpu::dpcpp::detail::getDefaultDPCPPGenerator());
+  std::pair<uint64_t, uint64_t> rng_engine_inputs;
+  {
+    // See Note [Acquire lock when using random generators]
+    std::lock_guard<std::mutex> lock(gen->mutex_);
+    rng_engine_inputs = gen->philox_engine_inputs(1);
+  }
+  std::initializer_list<std::uint64_t> seed = { rng_engine_inputs.first, 0, rng_engine_inputs.second };
+  auto& sycl_queue = dpcppGetCurrentQueue();
+
+  auto self_ptr = self.data_ptr<scalar_t>();
+  auto median = static_cast<double>(median_);
+  auto sigma = static_cast<double>(sigma_);
+  oneapi::mkl::rng::philox4x32x10 engine(sycl_queue, seed);
+  oneapi::mkl::rng::cauchy<scalar_t> distr(median, sigma);
+  oneapi::mkl::rng::generate(distr, engine, numel, self_ptr);
+}
+
+Tensor & cauchy_(Tensor & self, double median, double sigma, c10::optional<Generator> generator){
+  #ifdef USE_ONEMKL
+  int64_t numel = self.numel();
+  IPEX_DISPATCH_FLOATING_TYPES(self.scalar_type(), "cauchy_mkl_dpcpp", [&] {
+    using accscalar_t = DiscreteDistributionType<scalar_t>::type; 
+    auto median_ = static_cast<accscalar_t>(median);
+    auto sigma_ = static_cast<accscalar_t>(sigma);
+    cauchy_mkl_kernel<scalar_t>(self, numel, median_, sigma_, generator);
+  });
+  #else
+  auto iter = TensorIterator::nullary_op(self);
+  cauchy_kernel(iter, median, sigma, generator);
+  #endif
+  return self;
+}
+
+}} // namespace at::AtenIpexTypeXPU
+
---
+++ tests/gpu/examples/test_cauchy.py
@@ -0,0 +1,37 @@
+import torch
+import torch_ipex
+from torch.distributions import Cauchy
+import torch.nn as nn
+from torch.testing._internal.common_utils import TestCase
+import pytest
+import math
+
+cpu_device = torch.device("cpu")
+sycl_device = torch.device("xpu")
+
+
+def is_all_nan(tensor):
+    """
+    Checks if all entries of a tensor is nan.
+    """
+    return (tensor != tensor).all()
+
+class TestTorchMethod(TestCase):
+    def test_cauchy(self):
+        loc = torch.zeros(5, 5, requires_grad=True, device=sycl_device)
+        scale = torch.ones(5, 5, requires_grad=True, device=sycl_device)
+
+        loc_1d = torch.zeros(1, requires_grad=True, device=sycl_device)
+        scale_1d = torch.ones(1, requires_grad=True, device=sycl_device)
+
+        eps = loc.new(loc.size()).cauchy_()
+        c = Cauchy(loc, scale).rsample()
+        c.backward(torch.ones_like(c))
+
+        self.assertTrue(is_all_nan(Cauchy(loc_1d, scale_1d).mean))
+        self.assertEqual(Cauchy(loc_1d, scale_1d).variance, math.inf)
+        self.assertEqual(Cauchy(loc, scale).sample().size(), (5, 5))
+        self.assertEqual(Cauchy(loc, scale).sample((7,)).size(), (7, 5, 5))
+        self.assertEqual(Cauchy(loc_1d, scale_1d).sample().size(), (1,))
+        self.assertEqual(Cauchy(loc_1d, scale_1d).sample((1,)).size(), (1, 1))
+
