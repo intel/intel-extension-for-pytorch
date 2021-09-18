d46d8b5b38b86becf1533da4b0da88cf37106134
Jing Xu <jing.xu@intel.com>
2021-09-18 18:09:21 +0900

    Jingxu10/itt (#312)
    * fix ITT not working issue
    
    * add ittapi as third_party dependency. searching for itt will follow order as user assign > third_party/ittapi
    
    * bug fix and fine tune in FindITT.cmake
    
    * fine tuned CMakeLists.txt files to decouple libipex_python.so with libittnotify.a
    
    Co-authored-by: Jinghui <jinghui.gu@intel.com>

--- cmake/Modules/FindITT.cmake
+++ cmake/Modules/FindITT.cmake
@@ -2,39 +2,51 @@
 #
 
 if (NOT ITT_FOUND)
-set (ITT_FOUND OFF)
+SET (ITT_FOUND OFF)
 
-set (ITT_LIBRARIES)
-set (ITT_INCLUDE_DIR)
+SET (ITT_LIBRARIES)
+SET (ITT_INCLUDE_DIR)
 
-set(oneapi_root_hint)
 if(DEFINED INTELONEAPIROOT)
-    set(oneapi_root_hint ${INTELONEAPIROOT})
+    SET(ITT_ROOT "${INTELONEAPIROOT}/vtune/latest")
 elseif(DEFINED ENV{INTELONEAPIROOT})
-    set(oneapi_root_hint $ENV{INTELONEAPIROOT})
+    SET(ITT_ROOT "$ENV{INTELONEAPIROOT}/vtune/latest")
+else()
+    SET(ITT_ROOT "${PROJECT_SOURCE_DIR}/third_party/ittapi")
+    FIND_PATH(ITT_INCLUDE_DIR ittnotify.h PATHS ${ITT_ROOT} PATH_SUFFIXES include)
+    IF (NOT ITT_INCLUDE_DIR)
+        EXECUTE_PROCESS(
+            COMMAND git${CMAKE_EXECUTABLE_SUFFIX} submodule update --init ittapi
+            WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}/third_party)
+    ENDIF(NOT ITT_INCLUDE_DIR)
+    IF(NOT CMAKE_BUILD_TYPE)
+      SET(CMAKE_BUILD_TYPE "Release" CACHE STRING
+          "Choose the type of build, options are: Debug Release"
+          FORCE)
+    ENDIF(NOT CMAKE_BUILD_TYPE)
+    ADD_SUBDIRECTORY(${ITT_ROOT} ${CMAKE_BINARY_DIR}/ittapi)
 endif()
-set (ITT_ROOT "${oneapi_root_hint}/vtune/latest")
 
-find_path(ITT_INCLUDE_DIR
-        NAMES ittnotify.h
-        HINTS ${ITT_ROOT} $ENV{ITT_ROOT}
-        PATH_SUFFIXES include)
+FIND_PATH(ITT_INCLUDE_DIR
+          NAMES ittnotify.h
+          HINTS ${ITT_ROOT} $ENV{ITT_ROOT}
+          PATH_SUFFIXES include)
 
 # Find ITT
-find_library(ITT_LIBRARY
-          NAMES ittnotify
-          HINTS ${ITT_ROOT} $ENV{ITT_ROOT}
-          PATH_SUFFIXES lib/x64 lib lib64)
+FIND_LIBRARY(ITT_LIBRARY
+             NAMES ittnotify
+             HINTS ${ITT_ROOT} $ENV{ITT_ROOT}
+             PATH_SUFFIXES lib/x64 lib lib64)
 
 if (NOT ITT_INCLUDE_DIR)
     MESSAGE(WARNING "ITT headers not found!")
 endif (NOT ITT_INCLUDE_DIR)
 
 if (NOT ITT_LIBRARY)
-    MESSAGE(WARNING "ITT libraries not found!")
+    SET(ITT_LIBRARY ittnotify)
 endif (NOT ITT_LIBRARY)
 
-include(FindPackageHandleStandardArgs)
+INCLUDE(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(
         ITT
         FOUND_VAR ITT_FOUND
