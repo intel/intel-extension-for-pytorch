ffe690545605e0f630b31569f1a967eee6cb8d27
Xunsong, Huang <xunsong.huang@intel.com>
2021-10-14 12:48:44 +0800

    Rewrite Power OPs with TensorIterator (#369)
    Signed-off-by: Xunsong, Huang <xunsong.huang@intel.com>

--- csrc/aten/operators/Pow.cpp
+++ csrc/aten/operators/Pow.cpp
@@ -1,8 +1,10 @@
 #include <core/TensorImplUtils.h>
+#include "Loops.h"
 #include "comm/ATDispatch.h"
 #include "comm/ApplyUtils.h"
 #include "comm/Numerics.h"
 #include "comm/Pointwise.h"
+#include "comm/ScalarOps.h"
 
 using namespace xpu::dpcpp;
 
@@ -10,200 +12,144 @@ namespace at {
 namespace AtenIpexTypeXPU {
 namespace impl {
 
-template <typename scalar_t>
-typename std::enable_if<!IS_FLOAT(scalar_t), void>::type pow(
-    Tensor& self_,
-    const Tensor& src,
-    scalar_t value) {
-  if (TensorImpl_Unwrap(self_) == TensorImpl_Unwrap(src)) {
-    if (Numerics<scalar_t>::eq(value, ScalarConvert<int, scalar_t>::to(1))) {
-      DPCPP_tensor_apply1<scalar_t>(self_, TensorPowOp<scalar_t, 1>(value));
-    } else if (Numerics<scalar_t>::eq(
-                   value, ScalarConvert<int, scalar_t>::to(2))) {
-      DPCPP_tensor_apply1<scalar_t>(self_, TensorPowOp<scalar_t, 2>(value));
-    } else if (Numerics<scalar_t>::eq(
-                   value, ScalarConvert<int, scalar_t>::to(3))) {
-      DPCPP_tensor_apply1<scalar_t>(self_, TensorPowOp<scalar_t, 3>(value));
-    } else {
-      DPCPP_tensor_apply1<scalar_t>(self_, TensorPowOp<scalar_t, -3>(value));
-    }
+void pow_tensor_tensor_kernel(TensorIterator& iter) {
+  // TODO: support complex dtype for power
+  // if (isFloatingType(iter.dtype()) || isComplexType(iter.dtype())) {
+  //   IPEX_DISPATCH_FLOATING_AND_COMPLEX_TYPES(iter.dtype(), "pow", [&]() {
+  //     dpcpp_kernel_for_tensor_iter(
+  //         iter, [](scalar_t base, scalar_t exp) -> scalar_t {
+  //           return Numerics<scalar_t>::pow(base, exp);
+  //         });
+  //   });
+  if (isFloatingType(iter.dtype())) {
+    IPEX_DISPATCH_FLOATING_TYPES(iter.dtype(), "pow", [&]() {
+      dpcpp_kernel_for_tensor_iter(
+          iter, [](scalar_t base, scalar_t exp) -> scalar_t {
+            return Numerics<scalar_t>::pow(base, exp);
+          });
+    });
   } else {
-    self_.resize_as_(src);
-    if (Numerics<scalar_t>::eq(value, ScalarConvert<int, scalar_t>::to(1))) {
-      DPCPP_tensor_apply2<scalar_t, scalar_t>(
-          self_, src, TensorPowOp<scalar_t, 1>(value));
-    } else if (Numerics<scalar_t>::eq(
-                   value, ScalarConvert<int, scalar_t>::to(2))) {
-      DPCPP_tensor_apply2<scalar_t, scalar_t>(
-          self_, src, TensorPowOp<scalar_t, 2>(value));
-    } else if (Numerics<scalar_t>::eq(
-                   value, ScalarConvert<int, scalar_t>::to(3))) {
-      DPCPP_tensor_apply2<scalar_t, scalar_t>(
-          self_, src, TensorPowOp<scalar_t, 3>(value));
-    } else {
-      DPCPP_tensor_apply2<scalar_t, scalar_t>(
-          self_, src, TensorPowOp<scalar_t, -3>(value));
-    }
+    IPEX_DISPATCH_INTEGRAL_TYPES(iter.dtype(), "pow", [&]() {
+      dpcpp_kernel_for_tensor_iter(
+          iter, [](scalar_t base, scalar_t exp) -> scalar_t {
+            return Numerics<scalar_t>::pow(base, exp);
+          });
+    });
   }
 }
 
-template <typename scalar_t>
-typename std::enable_if<IS_FLOAT(scalar_t), void>::type pow(
-    Tensor& self_,
-    const Tensor& src,
-    scalar_t value) {
-  if (TensorImpl_Unwrap(self_) == TensorImpl_Unwrap(src)) {
-    if (Numerics<scalar_t>::eq(value, ScalarConvert<int, scalar_t>::to(1))) {
-      DPCPP_tensor_apply1<scalar_t>(self_, TensorPowOp<scalar_t, 1>(value));
-    } else if (Numerics<scalar_t>::eq(
-                   value, ScalarConvert<int, scalar_t>::to(2))) {
-      DPCPP_tensor_apply1<scalar_t>(self_, TensorPowOp<scalar_t, 2>(value));
-    } else if (Numerics<scalar_t>::eq(
-                   value, ScalarConvert<int, scalar_t>::to(3))) {
-      DPCPP_tensor_apply1<scalar_t>(self_, TensorPowOp<scalar_t, 3>(value));
-    } else if (Numerics<scalar_t>::eq(
-                   value, ScalarConvert<int, scalar_t>::to(-1))) {
-      DPCPP_tensor_apply1<scalar_t>(self_, TensorPowOp<scalar_t, -1>(value));
-    } else if (Numerics<scalar_t>::eq(
-                   value, ScalarConvert<int, scalar_t>::to(-2))) {
-      DPCPP_tensor_apply1<scalar_t>(self_, TensorPowOp<scalar_t, -2>(value));
-    } else {
-      DPCPP_tensor_apply1<scalar_t>(self_, TensorPowOp<scalar_t, -3>(value));
-    }
-  } else {
-    self_.resize_as_(src);
-    if (Numerics<scalar_t>::eq(value, ScalarConvert<int, scalar_t>::to(1))) {
-      DPCPP_tensor_apply2<scalar_t, scalar_t>(
-          self_, src, TensorPowOp<scalar_t, 1>(value));
-    } else if (Numerics<scalar_t>::eq(
-                   value, ScalarConvert<int, scalar_t>::to(2))) {
-      DPCPP_tensor_apply2<scalar_t, scalar_t>(
-          self_, src, TensorPowOp<scalar_t, 2>(value));
-    } else if (Numerics<scalar_t>::eq(
-                   value, ScalarConvert<int, scalar_t>::to(3))) {
-      DPCPP_tensor_apply2<scalar_t, scalar_t>(
-          self_, src, TensorPowOp<scalar_t, 3>(value));
-    } else if (Numerics<scalar_t>::eq(
-                   value, ScalarConvert<int, scalar_t>::to(-1))) {
-      DPCPP_tensor_apply2<scalar_t, scalar_t>(
-          self_, src, TensorPowOp<scalar_t, -1>(value));
-    } else if (Numerics<scalar_t>::eq(
-                   value, ScalarConvert<int, scalar_t>::to(-2))) {
-      DPCPP_tensor_apply2<scalar_t, scalar_t>(
-          self_, src, TensorPowOp<scalar_t, -2>(value));
-    } else {
-      DPCPP_tensor_apply2<scalar_t, scalar_t>(
-          self_, src, TensorPowOp<scalar_t, -3>(value));
-    }
+void pow_tensor_scalar_kernel(TensorIterator& iter, Scalar exp_scalar) {
+  if (isFloatingType(iter.dtype())) {
+    const auto exp = exp_scalar.to<double>();
+
+    IPEX_DISPATCH_FLOATING_TYPES(iter.dtype(), "pow", [&]() {
+      if (exp == 0.5) {
+        dpcpp_kernel_for_tensor_iter(iter, [](scalar_t base) -> scalar_t {
+          return Numerics<scalar_t>::sqrt(base);
+        });
+      } else if (exp == 2) {
+        dpcpp_kernel_for_tensor_iter(
+            iter, [](scalar_t base) -> scalar_t { return base * base; });
+      } else if (exp == 3) {
+        dpcpp_kernel_for_tensor_iter(
+            iter, [](scalar_t base) -> scalar_t { return base * base * base; });
+      } else if (exp == -0.5) {
+        dpcpp_kernel_for_tensor_iter(iter, [](scalar_t base) -> scalar_t {
+          return Numerics<scalar_t>::rsqrt(base);
+        });
+      } else if (exp == -1) {
+        dpcpp_kernel_for_tensor_iter(
+            iter, [](scalar_t base) -> scalar_t { return 1.0 / base; });
+      } else if (exp == -2) {
+        dpcpp_kernel_for_tensor_iter(iter, [](scalar_t base) -> scalar_t {
+          return 1.0 / (base * base);
+        });
+      } else {
+        dpcpp_kernel_for_tensor_iter(iter, [=](scalar_t base) -> scalar_t {
+          return Numerics<scalar_t>::pow(base, exp);
+        });
+      }
+    });
+  } else if (isComplexType(iter.dtype())) {
+    // TODO: support complex dtype
+    TORCH_CHECK(0, "DPCPP power didn't support complex dtype yet.");
   }
 }
 
-template <typename scalar_t>
-void cpow(Tensor& self_, const Tensor& src1, const Tensor& src2) {
-  TORCH_CHECK(src1.numel() == src2.numel(), "sizes do not match");
-  if (TensorImpl_Unwrap(self_) == TensorImpl_Unwrap(src1)) {
-    DPCPP_tensor_apply2<scalar_t, scalar_t>(
-        self_, src2, TensorCPowOp<scalar_t>());
-  } else {
-    self_.resize_as_(src1);
-    DPCPP_tensor_apply3<scalar_t, scalar_t, scalar_t>(
-        self_, src1, src2, TensorCPowOp<scalar_t>());
-  }
-}
-
-template <typename scalar_t>
-void tpow(Tensor& self_, scalar_t value, const Tensor& src) {
-  if (TensorImpl_Unwrap(self_) == TensorImpl_Unwrap(src)) {
-    DPCPP_tensor_apply1<scalar_t>(self_, TensorTPowOp<scalar_t>(value));
-  } else {
-    self_.resize_as_(src);
-    DPCPP_tensor_apply2<scalar_t, scalar_t>(
-        self_, src, TensorTPowOp<scalar_t>(value));
-  }
-}
 } // namespace impl
 
-Tensor& pow_out(Tensor& out, const Tensor& self, Scalar exponent) {
-  IPEX_DISPATCH_ALL_TYPES_AND2(
-      at::ScalarType::BFloat16,
-      at::ScalarType::Half,
-      self.scalar_type(),
-      "pow",
-      [&]() { impl::pow<scalar_t>(out, self, exponent.to<scalar_t>()); });
-  return out;
-}
-
-Tensor pow(const Tensor& self, Scalar exponent) {
-  auto result = at::empty({0}, self.options());
-  IPEX_DISPATCH_ALL_TYPES_AND2(
-      at::ScalarType::BFloat16,
-      at::ScalarType::Half,
-      self.scalar_type(),
-      "pow",
-      [&]() { impl::pow<scalar_t>(result, self, exponent.to<scalar_t>()); });
+Tensor& pow_out(Tensor& result, const Tensor& base, const Tensor& exp) {
+  auto iter = TensorIterator::binary_op(result, base, exp);
+  impl::pow_tensor_tensor_kernel(iter);
   return result;
 }
 
-Tensor& pow_(Tensor& self, Scalar exponent) {
-  IPEX_DISPATCH_ALL_TYPES_AND2(
-      at::ScalarType::BFloat16,
-      at::ScalarType::Half,
-      self.scalar_type(),
-      "pow",
-      [&]() { impl::pow<scalar_t>(self, self, exponent.to<scalar_t>()); });
-  return self;
-}
+Tensor& pow_out(Tensor& result, const Tensor& base, Scalar exp) {
+  TORCH_CHECK(
+      !(isIntegralType(base.scalar_type(), true) && exp.isIntegral(true) &&
+        exp.toLong() < 0),
+      "Intergers to negative integer powers are not allowed.");
 
-Tensor& pow_(Tensor& self, const Tensor& exponent) {
-  IPEX_DISPATCH_ALL_TYPES_AND2(
-      at::ScalarType::BFloat16,
-      at::ScalarType::Half,
-      self.scalar_type(),
-      "cpow",
-      [&]() { impl::cpow<scalar_t>(self, self, exponent); });
-  return self;
-}
+  auto common_dtype = at::result_type(base, exp);
+  TORCH_CHECK(
+      at::can_cast(common_dtype, result.scalar_type()),
+      "result type ",
+      common_dtype,
+      "can't be cast to the desired output type ",
+      result.scalar_type());
 
-Tensor& pow_out(Tensor& out, const Tensor& self, const Tensor& exponent) {
-  IPEX_DISPATCH_ALL_TYPES_AND2(
-      at::ScalarType::BFloat16,
-      at::ScalarType::Half,
-      self.scalar_type(),
-      "cpow",
-      [&]() { impl::cpow<scalar_t>(out, self, exponent); });
-  return out;
-}
-
-Tensor pow(const Tensor& self, const Tensor& exponent) {
-  auto result = at::empty({0}, self.options());
-  IPEX_DISPATCH_ALL_TYPES_AND2(
-      at::ScalarType::BFloat16,
-      at::ScalarType::Half,
-      self.scalar_type(),
-      "cpow",
-      [&]() { impl::cpow<scalar_t>(result, self, exponent); });
+  if (exp.isComplex() && (exp.toComplexDouble() == 0.0)) {
+    result.resize_as_(base).fill_(1);
+  } else if (exp.isComplex() && (exp.toComplexDouble() == 1.0)) {
+    result.resize_as_(base).fill_(base);
+  } else if (!exp.isComplex() && (exp.toDouble() == 0.0)) {
+    result.resize_as_(base).fill_(1);
+  } else if (!exp.isComplex() && (exp.toDouble() == 1.0)) {
+    result.resize_as_(base).copy_(base);
+  } else {
+    auto iter = TensorIterator::unary_op(result, base.to(common_dtype));
+    impl::pow_tensor_scalar_kernel(iter, exp);
+  }
   return result;
 }
 
-Tensor& pow_out(Tensor& out, Scalar self, const Tensor& exponent) {
-  IPEX_DISPATCH_ALL_TYPES_AND2(
-      at::ScalarType::BFloat16,
-      at::ScalarType::Half,
-      out.scalar_type(),
-      "tpow",
-      [&]() { impl::tpow<scalar_t>(out, self.to<scalar_t>(), exponent); });
-  return out;
+Tensor& pow_out(Tensor& result, Scalar base, const Tensor& exp) {
+  if (base.isComplex() && base.toComplexDouble() == 1.0) {
+    result.resize_as_(exp).fill_(1);
+  } else if (!base.isComplex() && base.toDouble() == 1.0) {
+    result.resize_as_(exp).fill_(1);
+  } else {
+    at::AtenIpexTypeXPU::pow_out(result, wrapped_scalar_tensor(base), exp);
+  }
+  return result;
 }
 
-Tensor pow(Scalar self, const Tensor& exponent) {
-  auto result = at::empty({0}, exponent.options());
-  IPEX_DISPATCH_ALL_TYPES_AND2(
-      at::ScalarType::BFloat16,
-      at::ScalarType::Half,
-      exponent.scalar_type(),
-      "tpow",
-      [&]() { impl::tpow<scalar_t>(result, self.to<scalar_t>(), exponent); });
-  return result;
+Tensor& pow_(Tensor& base, const Tensor& other) {
+  return at::AtenIpexTypeXPU::pow_out(base, base, other);
+}
+
+Tensor& pow_(Tensor& base, Scalar alpha) {
+  return at::AtenIpexTypeXPU::pow_out(base, base, alpha);
+}
+
+Tensor pow(const Tensor& base, const Tensor& exp) {
+  auto dtype = at::result_type(base, exp);
+  Tensor result = at::empty({0}, base.options().dtype(dtype));
+  return at::AtenIpexTypeXPU::pow_out(result, base, exp);
+}
+
+Tensor pow(const Tensor& base, Scalar exp) {
+  auto dtype = at::result_type(base, exp);
+  Tensor result =
+      at::empty_like(base, base.options().dtype(dtype), MemoryFormat::Preserve);
+  return at::AtenIpexTypeXPU::pow_out(result, base, exp);
+}
+
+Tensor pow(Scalar base, const Tensor& exp) {
+  auto dtype = at::result_type(base, exp);
+  Tensor result =
+      at::empty_like(exp, exp.options().dtype(dtype), MemoryFormat::Preserve);
+  return at::AtenIpexTypeXPU::pow_out(result, base, exp);
 }
 
 } // namespace AtenIpexTypeXPU
