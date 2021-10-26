df336ff09eb224593e645b8cf19be4f89c72a13b
jiayisunx <jiayi.sun@intel.com>
2021-10-26 17:07:53 +0800

    port AdaptiveMaxPool2d and AvgPool2d (#268)
    * port AvgPool2d(https://github.com/pytorch/pytorch/pull/66927)
    
    * port AdaptiveMaxPool2d (https://github.com/pytorch/pytorch/pull/66929)

---
+++ torch_ipex/csrc/cpu/AdaptiveMaxPooling.h
@@ -0,0 +1,70 @@
+#pragma once
+
+#include <ATen/ATen.h>
+
+namespace torch_ipex {
+namespace cpu {
+
+static inline int64_t start_index(int64_t a, int64_t b, int64_t c) {
+  return (int64_t)std::floor((float)(a * c) / b);
+}
+
+static inline int64_t end_index(int64_t a, int64_t b, int64_t c) {
+  return (int64_t)std::ceil((float)((a + 1) * c) / b);
+}
+
+template <typename scalar_t, typename accscalar_t>
+void cpu_adaptive_max_pool(
+    const at::Tensor& output_,
+    const at::Tensor& indices_,
+    const at::Tensor& input_,
+    at::IntArrayRef output_size);
+
+template <typename scalar_t>
+void cpu_adaptive_max_pool_channels_last(
+    const at::Tensor& output_,
+    const at::Tensor& indices_,
+    const at::Tensor& input_,
+    at::IntArrayRef output_size);
+
+template <>
+void cpu_adaptive_max_pool_channels_last<at::BFloat16>(
+    const at::Tensor& output_,
+    const at::Tensor& indices_,
+    const at::Tensor& input_,
+    at::IntArrayRef output_size);
+
+template <typename scalar_t>
+void cpu_adaptive_max_pool_backward(
+    const at::Tensor& grad_input_,
+    const at::Tensor& grad_output_,
+    const at::Tensor& indices_);
+
+template <typename scalar_t>
+void cpu_adaptive_max_pool_backward_channels_last(
+    const at::Tensor& grad_input_,
+    const at::Tensor& grad_output_,
+    const at::Tensor& indices_);
+
+void adaptive_max_pool2d_kernel_impl(
+    const at::Tensor& output,
+    const at::Tensor& indices,
+    const at::Tensor& input,
+    at::IntArrayRef output_size);
+
+void adaptive_max_pool2d_backward_kernel_impl(
+    const at::Tensor& grad_input,
+    const at::Tensor& grad_output,
+    const at::Tensor& indices);
+
+std::tuple<at::Tensor, at::Tensor> adaptive_max_pool2d_out_cpu(
+    const at::Tensor& input,
+    at::IntArrayRef output_size);
+
+at::Tensor adaptive_max_pool2d_backward_out_cpu(
+    const at::Tensor& grad_output,
+    const at::Tensor& input,
+    const at::Tensor& indices);
+
+} // namespace cpu
+} // namespace torch_ipex
