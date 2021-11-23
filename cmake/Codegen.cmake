2198c3fce1f9b24e6803fd48211a7af020e8e99b
Guo Yejun <yejun.guo@intel.com>
2021-11-23 14:04:25 +0000

    codegen/cmake: generate XPU dispatch code during build

--- cmake/Codegen.cmake
+++ cmake/Codegen.cmake
@@ -1,4 +1,24 @@
+Function(GEN_BACKEND file_cpp file_autograd_cpp file_h file_yaml)
+add_custom_command(OUTPUT
+        ${IPEX_GPU_ATEN_GENERATED}/ATen/${file_cpp}
+        ${IPEX_GPU_ATEN_GENERATED}/ATen/${file_autograd_cpp}
+        ${IPEX_GPU_ATEN_GENERATED}/ATen/${file_h}
+        COMMAND
+        mkdir -p ${IPEX_GPU_ATEN_GENERATED} && mkdir -p ${IPEX_GPU_ATEN_GENERATED}/ATen
+        COMMAND
+        "${PYTHON_EXECUTABLE}" -m tools.codegen.gen_backend_stubs
+        --output_dir ${IPEX_GPU_ATEN_GENERATED}/ATen
+        --source_yaml ${PROJECT_SOURCE_DIR}/scripts/tools/codegen/yaml/${file_yaml}
+        WORKING_DIRECTORY ${IPEX_ROOT_DIR}/scripts
+        DEPENDS
+        ${PROJECT_SOURCE_DIR}/scripts/tools/codegen/gen_backend_stubs.py
+        ${PROJECT_SOURCE_DIR}/scripts/tools/codegen/yaml/${file_yaml})
+endfunction(GEN_BACKEND)
+
+GEN_BACKEND(RegisterXPU.cpp RegisterAutogradXPU.cpp XPUNativeFunctions.h xpu_functions.yaml)
+
 list(APPEND gpu_generated_src ${IPEX_GPU_ATEN_GENERATED}/ATen/AtenIpexTypeXPU.cpp
+        ${IPEX_GPU_ATEN_GENERATED}/ATen/RegisterXPU.cpp
         ${IPEX_GPU_ATEN_GENERATED}/ATen/AtenIpexTypeQuantizedXPU.cpp
         ${IPEX_GPU_ATEN_GENERATED}/ATen/AtenIpexTypeSparseXPU.cpp)
 
