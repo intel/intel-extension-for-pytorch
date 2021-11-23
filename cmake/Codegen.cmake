763fc3423054de91670a9615186389122dbe9b86
Guo Yejun <yejun.guo@intel.com>
2021-11-23 14:37:54 +0000

    dispatch/QuantizedXPU: add all 33 ops from QUANTIZEDDPCPPGPUType.h to quantizedxpu_functions.yaml
    33 ops in total, 15 ops commented, 18 ops enabled.
    
    1 op for different schema
    1 op for dispatch False
    13 ops for different signatures

--- cmake/Codegen.cmake
+++ cmake/Codegen.cmake
@@ -1,3 +1,19 @@
+Function(GEN_BACKEND_ONECPP file_cpp file_h file_yaml)
+add_custom_command(OUTPUT
+        ${IPEX_GPU_ATEN_GENERATED}/ATen/${file_cpp}
+        ${IPEX_GPU_ATEN_GENERATED}/ATen/${file_h}
+        COMMAND
+        mkdir -p ${IPEX_GPU_ATEN_GENERATED} && mkdir -p ${IPEX_GPU_ATEN_GENERATED}/ATen
+        COMMAND
+        "${PYTHON_EXECUTABLE}" -m tools.codegen.gen_backend_stubs
+        --output_dir ${IPEX_GPU_ATEN_GENERATED}/ATen
+        --source_yaml ${PROJECT_SOURCE_DIR}/scripts/tools/codegen/yaml/${file_yaml}
+        WORKING_DIRECTORY ${IPEX_ROOT_DIR}/scripts
+        DEPENDS
+        ${PROJECT_SOURCE_DIR}/scripts/tools/codegen/gen_backend_stubs.py
+        ${PROJECT_SOURCE_DIR}/scripts/tools/codegen/yaml/${file_yaml})
+endfunction(GEN_BACKEND_ONECPP)
+
 Function(GEN_BACKEND file_cpp file_autograd_cpp file_h file_yaml)
 add_custom_command(OUTPUT
         ${IPEX_GPU_ATEN_GENERATED}/ATen/${file_cpp}
@@ -16,9 +32,11 @@ add_custom_command(OUTPUT
 endfunction(GEN_BACKEND)
 
 GEN_BACKEND(RegisterXPU.cpp RegisterAutogradXPU.cpp XPUNativeFunctions.h xpu_functions.yaml)
+GEN_BACKEND_ONECPP(RegisterQuantizedXPU.cpp QuantizedXPUNativeFunctions.h quantizedxpu_functions.yaml)
 
 list(APPEND gpu_generated_src ${IPEX_GPU_ATEN_GENERATED}/ATen/AtenIpexTypeXPU.cpp
         ${IPEX_GPU_ATEN_GENERATED}/ATen/RegisterXPU.cpp
+        ${IPEX_GPU_ATEN_GENERATED}/ATen/RegisterQuantizedXPU.cpp
         ${IPEX_GPU_ATEN_GENERATED}/ATen/AtenIpexTypeQuantizedXPU.cpp
         ${IPEX_GPU_ATEN_GENERATED}/ATen/AtenIpexTypeSparseXPU.cpp)
 
