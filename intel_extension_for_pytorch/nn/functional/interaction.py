39aea87d79700403f51c90717a395a4ad1d38041
Jing Xu <jing.xu@intel.com>
2021-11-23 09:29:01 +0900

    correct github repo name in conf.py and multiplication mark in interaction.py (#349)

--- intel_extension_for_pytorch/nn/functional/interaction.py
+++ intel_extension_for_pytorch/nn/functional/interaction.py
@@ -20,8 +20,8 @@ def interaction(*args):
         *args: Multiple tensors which represent different features
 
     Shape
-        - Input: :math:`N X (B, D)`, where N is the number of different kinds of features, B is the batch size, D is feature size
-        - Output: :math:`(B, D + N*(N-1)/2)`
+        - Input: :math:`N * (B, D)`, where N is the number of different kinds of features, B is the batch size, D is feature size
+        - Output: :math:`(B, D + N * ( N - 1 ) / 2)`
     """
 
     if torch.is_grad_enabled():
