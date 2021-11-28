f24e1a331e4e6e54a5e1d9a111df420a9278f84d
Guo Yejun <yejun.guo@intel.com>
2021-11-28 06:55:08 +0000

    dispatch/SparseXPU: add all 12 ops from SPARSEDPCPPGPUType.h to sparsexpu_functions.yaml
    12 ops in total, 9 ops enabled,
    2 ops different signature, 1 op different schema

--- cmake/Codegen.cmake
+++ cmake/Codegen.cmake
@@ -33,10 +33,12 @@ endfunction(GEN_BACKEND)
 
 GEN_BACKEND(RegisterXPU.cpp RegisterAutogradXPU.cpp XPUNativeFunctions.h xpu_functions.yaml)
 GEN_BACKEND_ONECPP(RegisterQuantizedXPU.cpp QuantizedXPUNativeFunctions.h quantizedxpu_functions.yaml)
+GEN_BACKEND_ONECPP(RegisterSparseXPU.cpp SparseXPUNativeFunctions.h sparsexpu_functions.yaml)
 
 list(APPEND gpu_generated_src ${IPEX_GPU_ATEN_GENERATED}/ATen/AtenIpexTypeXPU.cpp
         ${IPEX_GPU_ATEN_GENERATED}/ATen/RegisterXPU.cpp
         ${IPEX_GPU_ATEN_GENERATED}/ATen/RegisterQuantizedXPU.cpp
+        ${IPEX_GPU_ATEN_GENERATED}/ATen/RegisterSparseXPU.cpp
         ${IPEX_GPU_ATEN_GENERATED}/ATen/AtenIpexTypeQuantizedXPU.cpp
         ${IPEX_GPU_ATEN_GENERATED}/ATen/AtenIpexTypeSparseXPU.cpp)
 
