09977b04d27a6819e27dd18ec392704fe33b116f
Guo Yejun <yejun.guo@intel.com>
2021-11-30 15:39:56 +0000

    oldcodegen: do not run old codegen and add the generated files into repo
    the reason is that we need to modify the header files to fix the
    undefined symbols as a tmp solution.
    
    we'll remove all these things after the rebase is finished.

--- cmake/Codegen.cmake
+++ cmake/Codegen.cmake
@@ -1,26 +1,3 @@
-add_custom_command(OUTPUT
-        ${IPEX_GPU_ATEN_GENERATED}/ATen/AtenIpexTypeXPU.cpp
-        ${IPEX_GPU_ATEN_GENERATED}/ATen/AtenIpexTypeQuantizedXPU.cpp
-        ${IPEX_GPU_ATEN_GENERATED}/ATen/AtenIpexTypeSparseXPU.cpp
-        ${IPEX_GPU_ATEN_GENERATED}/ATen/AtenIpexTypeXPU.h
-        ${IPEX_GPU_ATEN_GENERATED}/ATen/AtenIpexTypeQuantizedXPU.h
-        ${IPEX_GPU_ATEN_GENERATED}/ATen/AtenIpexTypeSparseXPU.h
-        COMMAND
-        mkdir -p ${IPEX_GPU_ATEN_GENERATED} && mkdir -p ${IPEX_GPU_ATEN_GENERATED}/ATen
-        COMMAND
-        "${PYTHON_EXECUTABLE}" -m scripts.gpu.gen_code --declarations-path
-        ${PROJECT_SOURCE_DIR}/scripts/declarations/Declarations.yaml
-        --out ${IPEX_GPU_ATEN_GENERATED}/ATen/
-        --source-path ${PROJECT_SOURCE_DIR}
-        WORKING_DIRECTORY
-        ${IPEX_ROOT_DIR}/
-        DEPENDS
-        ${PROJECT_SOURCE_DIR}/scripts/declarations/Declarations.yaml
-        ${PROJECT_SOURCE_DIR}/scripts/gpu/gen_code.py
-        ${PROJECT_SOURCE_DIR}/scripts/gpu/DPCPPGPUType.h
-        ${PROJECT_SOURCE_DIR}/scripts/gpu/QUANTIZEDDPCPPGPUType.h
-        ${PROJECT_SOURCE_DIR}/scripts/gpu/SPARSEDPCPPGPUType.h)
-
 list(APPEND gpu_generated_src ${IPEX_GPU_ATEN_GENERATED}/ATen/AtenIpexTypeXPU.cpp
         ${IPEX_GPU_ATEN_GENERATED}/ATen/AtenIpexTypeQuantizedXPU.cpp
         ${IPEX_GPU_ATEN_GENERATED}/ATen/AtenIpexTypeSparseXPU.cpp)
