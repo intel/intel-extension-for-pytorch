8edfb9d93cb2fad2c20d1d3fc60a52cab9d0827b
Jinghui <jinghui.gu@intel.com>
2021-12-20 07:16:06 +0800

    Revert "Split op src files to speedup build and link process (#596)" (#600)
    Revert this commit to avoid blocking 1.10 rebase
    
    This reverts commit 6343787e0f90d9259ed65dc5eb9a098a7cc9d2bb.

--- csrc/aten/operators/BinaryOps_4.cpp
+++ csrc/aten/operators/BinaryOps_4.cpp
@@ -66,5 +66,27 @@ Tensor& atan2_(Tensor& self, const Tensor& other) {
   return at::AtenIpexTypeXPU::atan2_out(self, self, other);
 }
 
+IPEX_OUT_ALL_CALLABLE_0_BINARY_OPS(remainder_out, TensorCRemainderOp)
+
+Tensor remainder(const Tensor& self, const Tensor& other) {
+  auto out = at::empty_like(self);
+  return at::AtenIpexTypeXPU::remainder_out(out, self, other);
+}
+
+Tensor& remainder_(Tensor& self, const Tensor& other) {
+  return at::AtenIpexTypeXPU::remainder_out(self, self, other);
+}
+
+IPEX_OUT_ALL_CALLABLE_0_BINARY_OPS(fmod_out, TensorCFmodOp)
+
+Tensor fmod(const Tensor& self, const Tensor& other) {
+  auto out = at::empty_like(self);
+  return at::AtenIpexTypeXPU::fmod_out(out, self, other);
+}
+
+Tensor& fmod_(Tensor& self, const Tensor& other) {
+  return at::AtenIpexTypeXPU::fmod_out(self, self, other);
+}
+
 } // namespace AtenIpexTypeXPU
 } // namespace at
--- csrc/aten/operators/UnaryOps_1.cpp
+++ csrc/aten/operators/UnaryOps_1.cpp
@@ -15,6 +15,17 @@ namespace at {
 namespace AtenIpexTypeXPU {
 namespace impl {
 
+void bitwise_not_kernel_dpcpp(TensorIterator& iter) {
+  if (iter.dtype() == ScalarType::Bool) {
+    dpcpp_kernel_for_tensor_iter(iter, [](bool a) -> bool { return !a; });
+  } else {
+    IPEX_DISPATCH_INTEGRAL_TYPES(iter.dtype(), "bitwise_not_dpcpp", [&]() {
+      dpcpp_kernel_for_tensor_iter(
+          iter, [](scalar_t a) -> scalar_t { return ~a; });
+    });
+  }
+}
+
 void logical_not_kernel(TensorIterator& iter) {
   IPEX_DISPATCH_ALL_TYPES_AND2(
       kBool, kHalf, iter.dtype(1), "logical_not_dpcpp", [&]() {
@@ -28,7 +39,33 @@ void logical_not_kernel(TensorIterator& iter) {
       });
 }
 
+void frac_kernel(TensorIterator& iter) {
+  IPEX_DISPATCH_FLOATING_TYPES_AND(at::kHalf, iter.dtype(), "frac_xpu", [&]() {
+    dpcpp_kernel_for_tensor_iter(iter, [](scalar_t a) -> scalar_t {
+      return a - Numerics<scalar_t>::trunc(a);
+    });
+  });
+}
+
 } // namespace impl
+
+Tensor bitwise_not(const Tensor& self) {
+  Tensor result = at::empty({0}, self.options());
+  return at::AtenIpexTypeXPU::bitwise_not_out(result, self);
+}
+
+Tensor& bitwise_not_(Tensor& self) {
+  return at::AtenIpexTypeXPU::bitwise_not_out(self, self);
+}
+
+Tensor& bitwise_not_out(Tensor& out, const Tensor& self) {
+  auto iter = TensorIterator::unary_op(out, self);
+  impl::bitwise_not_kernel_dpcpp(iter);
+#ifdef BUILD_NAMEDTENSOR
+  at::namedinference::propagate_names(out, self);
+#endif
+  return out;
+}
 
 Tensor logical_not(const Tensor& self) {
   Tensor result = at::empty({0}, self.options().dtype(kBool));
@@ -48,6 +85,21 @@ Tensor& logical_not_out(Tensor& result, const Tensor& self) {
                             .build();
   impl::logical_not_kernel(iter);
   return result;
+}
+
+Tensor& frac_out(Tensor& result, const Tensor& self) {
+  auto iter = TensorIterator::unary_op(result, self);
+  impl::frac_kernel(iter);
+  return result;
+}
+
+Tensor frac(const Tensor& self) {
+  Tensor result = at::empty({0}, self.options());
+  return at::AtenIpexTypeXPU::frac_out(result, self);
+}
+
+Tensor& frac_(Tensor& self) {
+  return at::AtenIpexTypeXPU::frac_out(self, self);
 }
 
 } // namespace AtenIpexTypeXPU
