1a7136b5a90d3e3df82ae46c8f309317b3e8c806
Xunsong, Huang <xunsong.huang@intel.com>
2021-12-24 09:06:32 +0800

    Update IPEX version to gpu0.3.0 (#636)
    * fix svd ut
    
    * mm --> matmul for 3d tensor
    
    * t --> transpose(-2,-1) for 3d tensor
    
    * fix diag
    
    * Update IPEX version to gpu0.3.0
    
    Signed-off-by: Huang, Xunsong <xunsong.huang@intel.com>
    
    Co-authored-by: baocheny <baochen.yang@intel.com>
    Co-authored-by: Jinghui <jinghui.gu@intel.com>

--- tests/gpu/examples/test_svd.py
+++ tests/gpu/examples/test_svd.py
@@ -1,10 +1,7 @@
-import time
-
 import torch
 from torch.testing._internal.common_utils import TestCase
 
 import ipex
-
 import pytest
 
 cpu_device = torch.device("cpu")
@@ -12,7 +9,7 @@ dpcpp_device = torch.device("xpu")
 
 
 class TestTorchMethod(TestCase):
-    @pytest.mark.skipif("not torch.xpu.has_onemkl()")
+    @pytest.mark.skipif(not torch.xpu.has_onemkl(), reason="not torch.xpu.has_onemkl()")
     def test_svd(self, dtype=torch.float):
         # Since U and V of an SVD is not unique, each vector can be multiplied by an arbitrary phase factor e^iϕ
         # while the SVD result is still correct. Different platforms, like Numpy, or inputs on different device types,
@@ -22,22 +19,14 @@ class TestTorchMethod(TestCase):
         a_xpu = a.to('xpu')
 
         u, s, v = torch.svd(a)
-        # print(u)
-        # print(s)
-        # print(v)
-        r_cpu = u * s * v
-        # print(r_cpu)
+        r_cpu = torch.mm(torch.mm(u, torch.diag(s)), v.t())
 
         u_xpu, s_xpu, v_xpu = torch.svd(a_xpu)
-        # print(u_xpu.cpu())
-        # print(s_xpu.cpu())
-        # print(v_xpu.cpu())
-        r_xpu = u_xpu * s_xpu * v_xpu
-        # print(r_xpu.cpu())
+        r_xpu = torch.mm(torch.mm(u_xpu, torch.diag(s_xpu)), v_xpu.t())
 
         self.assertEqual(r_cpu, r_xpu.cpu())
 
-    @pytest.mark.skipif("not torch.xpu.has_onemkl()")
+    @pytest.mark.skipif(not torch.xpu.has_onemkl(), reason="not torch.xpu.has_onemkl()")
     def test_batch_svd(self, dtype=torch.float):
         # Since U and V of an SVD is not unique, each vector can be multiplied by an arbitrary phase factor e^iϕ
         # while the SVD result is still correct. Different platforms, like Numpy, or inputs on different device types,
@@ -46,15 +35,9 @@ class TestTorchMethod(TestCase):
         a_xpu = a.to('xpu')
 
         u, s, v = torch.svd(a)
-        # print(u)
-        # print(s)
-        # print(v)
-        r_cpu = u * s * v
+        r_cpu = torch.matmul(torch.matmul(u, torch.diag_embed(s)), v.transpose(-2, -1))
 
         u_xpu, s_xpu, v_xpu = torch.svd(a_xpu)
-        # print(u_xpu.cpu())
-        # print(s_xpu.cpu())
-        # print(v_xpu.cpu())
-        r_xpu = u_xpu * s_xpu * v_xpu
+        r_xpu = torch.matmul(torch.matmul(u_xpu, torch.diag_embed(s_xpu)), v_xpu.transpose(-2, -1))
 
         self.assertEqual(r_cpu, r_xpu.cpu())
