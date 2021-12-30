57a94b010522d36913571ef44453c3799b61f2e5
wuhuikx <hui.h.wu@intel.com>
2021-12-31 00:04:42 +0800

    Enable vector load/store for fused_dropout fwd/bwd kernel (#651)
    * Remove the non IndexToOffset branch
    
    * Enable vec_size
    
    * Add the initial version of vec_masked_scale_kernel
    
    * Fused_dropout bwd with vector load/store pass correctness checking
    
    * Refine fused_dropout bwd code
    
    * Improve the code path coverage in test_dropout
    
    * Update the test case to cover the non-contiguous path
    
    * Enable vector load/store for fused_dropout_kernel fwd path
    
    * Fused_dropout fwd kernel with vector load/store pass test case
    
    * Remove some log info
    
    * Refine vec_size strategy
    
    * Refine code
    
    * Refine code
    
    * Add comments and refine code
    
    * Refine code
    
    Co-authored-by: Kim <kimyangbaochen@vip.qq.com>
    Co-authored-by: Jinghui <jinghui.gu@intel.com>

--- csrc/aten/operators/FusedDropout.cpp
+++ csrc/aten/operators/FusedDropout.cpp
@@ -13,6 +13,8 @@
 #include <utils/oneMKLUtils.h>
 #endif
 
+#include <aten/operators/MemoryAccess.h>
+
 using namespace at;
 using namespace xpu::dpcpp::detail;
 using namespace xpu::dpcpp;
@@ -84,6 +86,143 @@ Tensor bernoulliDistr_impl(
   return rand;
 }
 
+template <
+    int vec_size,
+    typename scalar_t,
+    typename accscalar_t,
+    typename rscalar_t>
+void vec_fused_dropout_kernel_impl(
+    const Tensor& self,
+    const Tensor& rand,
+    Tensor& ret,
+    Tensor& mask,
+    accscalar_t p) {
+  auto& sycl_queue = dpcppGetCurrentQueue();
+  int64_t numel = self.numel();
+  int64_t global_range = (numel + vec_size - 1) / vec_size;
+
+  auto self_ptr = self.data_ptr<scalar_t>();
+  auto ret_ptr = ret.data_ptr<scalar_t>();
+  using vec_t =
+      typename at::native::Memory::aligned_vector<scalar_t, vec_size>::type;
+  using elem_t = typename at::native::Memory::
+      aligned_vector<scalar_t, vec_size>::element_type;
+  vec_t* self_vec = reinterpret_cast<vec_t*>(self_ptr);
+  vec_t* ret_vec = reinterpret_cast<vec_t*>(ret_ptr);
+
+  rscalar_t* rand_ptr = rand.data_ptr<rscalar_t>();
+  using rscalar_vec_t =
+      typename at::native::Memory::aligned_vector<rscalar_t, vec_size>::type;
+  rscalar_vec_t* rand_vec = reinterpret_cast<rscalar_vec_t*>(rand_ptr);
+
+  auto mask_ptr = mask.data_ptr<uint8_t>();
+  using mask_vec_t =
+      typename at::native::Memory::aligned_vector<uint8_t, vec_size>::type;
+  using mask_elem_t = typename at::native::Memory::
+      aligned_vector<uint8_t, vec_size>::element_type;
+  mask_vec_t* mask_vec = reinterpret_cast<mask_vec_t*>(mask_ptr);
+
+  accscalar_t pinv = accscalar_t(1) / p;
+  auto cgf = DPCPP_Q_CGF(cgh) {
+    auto kfn = DPCPP_Q_KFN(DPCPP::item<1> item_id) {
+      auto index = item_id.get_linear_id();
+      int remaining = numel - index * vec_size;
+
+      if (remaining < vec_size) {
+        for (int id = 0; id < remaining; ++id) {
+          auto offset = index * vec_size + id;
+          auto self_val = self_ptr[offset];
+          auto rand_val = rand_ptr[offset];
+          auto rv = accscalar_t(rand_val) < p;
+          ret_ptr[offset] = static_cast<scalar_t>(self_val * rv * pinv);
+          mask_ptr[offset] = static_cast<uint8_t>(rv);
+        }
+      } else {
+        vec_t self_value = self_vec[index];
+        rscalar_vec_t rand_value = rand_vec[index];
+        vec_t ret_value;
+        mask_vec_t mask_value;
+#pragma unroll
+        for (int id = 0; id < vec_size; ++id) {
+          auto self_val = at::native::Memory::detail::bitwise_cast<scalar_t>(
+              self_value[id]);
+          auto rand_val = at::native::Memory::detail::bitwise_cast<rscalar_t>(
+              rand_value[id]);
+          auto rv = accscalar_t(rand_val) < p;
+          auto ret_val = static_cast<scalar_t>(self_val * rv * pinv);
+          auto mask_val = static_cast<uint8_t>(rv);
+          ret_value[id] =
+              at::native::Memory::detail::bitwise_cast<elem_t>(ret_val);
+          mask_value[id] =
+              at::native::Memory::detail::bitwise_cast<mask_elem_t>(mask_val);
+        }
+        ret_vec[index] = ret_value;
+        mask_vec[index] = mask_value;
+      }
+    };
+    cgh.parallel_for(DPCPP::range<1>(global_range), kfn);
+  };
+
+  DPCPP_Q_SUBMIT(sycl_queue, cgf);
+}
+
+template <typename scalar_t, typename accscalar_t, typename rscalar_t>
+void vec_fused_dropout_kernel(
+    const Tensor& self,
+    const Tensor& rand,
+    Tensor& ret,
+    Tensor& mask,
+    accscalar_t p) {
+  // Two tensors (self and ret) are in scalar_t
+  // Only one tensor (rand) is in rscalar_t
+  // Only one tensor (mask) is in uint8_t
+  // so scalar_t is used to query the vec_size
+  auto self_ptr = self.data_ptr<scalar_t>();
+  auto ret_ptr = ret.data_ptr<scalar_t>();
+  auto vec_size_self = at::native::Memory::can_vectorize_up_to<scalar_t>(
+      getDeviceIdOfCurrentQueue(), reinterpret_cast<char*>(self_ptr));
+  auto vec_size_ret = at::native::Memory::can_vectorize_up_to<scalar_t>(
+      getDeviceIdOfCurrentQueue(), reinterpret_cast<char*>(ret_ptr));
+  // set the min value of self and rand as the final vec_size
+  auto vec_size = std::min(vec_size_self, vec_size_ret);
+
+#define VEC_FUSED_DROPOUT_KERNEL_IMPL(vec_size)                                \
+  {                                                                            \
+    vec_fused_dropout_kernel_impl<vec_size, scalar_t, accscalar_t, rscalar_t>( \
+        self, rand, ret, mask, p);                                             \
+  }
+
+  // TODO: we need to consider how to reduce the kernel size
+  switch (vec_size) {
+    case 16: {
+      VEC_FUSED_DROPOUT_KERNEL_IMPL(16);
+      break;
+    }
+    case 8: {
+      VEC_FUSED_DROPOUT_KERNEL_IMPL(8);
+      break;
+    }
+    case 4: {
+      VEC_FUSED_DROPOUT_KERNEL_IMPL(4);
+      break;
+    }
+    case 2: {
+      VEC_FUSED_DROPOUT_KERNEL_IMPL(2);
+      break;
+    }
+    case 1: {
+      VEC_FUSED_DROPOUT_KERNEL_IMPL(1);
+      break;
+    }
+    default:
+      TORCH_INTERNAL_ASSERT(
+          false,
+          "Unexpected vectorization size for FusedDropout. vec size ",
+          vec_size);
+  }
+#undef VEC_FUSED_DROPOUT_KERNEL_IMPL
+}
+
 template <typename scalar_t, typename accscalar_t, typename rscalar_t>
 void fused_dropout_kernel(
     const Tensor& self,
@@ -103,31 +242,17 @@ void fused_dropout_kernel(
     auto ret_ptr = ret.data_ptr<scalar_t>();
     auto mask_ptr = mask.data_ptr<uint8_t>();
 
-    // There's big performance gap between w and w/o IndexToOffset,
-    // thus we use two pathes to handle different cases
-    if (self.is_contiguous()) {
-      auto kfn = DPCPP_Q_KFN(DPCPP::item<1> item_id) {
-        auto index = item_id.get_linear_id();
-        auto rv = accscalar_t(rand_ptr[index]) < p;
-        if (index < numel) {
-          ret_ptr[index] = self_ptr[index] * rv * pinv;
-          mask_ptr[index] = (uint8_t)rv;
-        }
-      };
-      cgh.parallel_for(DPCPP::range<1>(numel), kfn);
-    } else {
-      auto kfn = DPCPP_Q_KFN(DPCPP::item<1> item_id) {
-        auto index = item_id.get_linear_id();
-        auto self_offset =
-            IndexToOffset<scalar_t, uint64_t>::get(index, self_info);
-        auto rv = accscalar_t(rand_ptr[index]) < p;
-        if (index < numel) {
-          ret_ptr[index] = self_ptr[self_offset] * rv * pinv;
-          mask_ptr[index] = (uint8_t)rv;
-        }
-      };
-      cgh.parallel_for(DPCPP::range<1>(numel), kfn);
-    }
+    auto kfn = DPCPP_Q_KFN(DPCPP::item<1> item_id) {
+      auto index = item_id.get_linear_id();
+      auto self_offset =
+          IndexToOffset<scalar_t, uint64_t>::get(index, self_info);
+      auto rv = accscalar_t(rand_ptr[index]) < p;
+      if (index < numel) {
+        ret_ptr[index] = self_ptr[self_offset] * rv * pinv;
+        mask_ptr[index] = (uint8_t)rv;
+      }
+    };
+    cgh.parallel_for(DPCPP::range<1>(numel), kfn);
   };
 
   DPCPP_Q_SUBMIT(sycl_queue, cgf);
@@ -151,30 +276,131 @@ void masked_scale_kernel(
     auto ret_ptr = ret.data_ptr<scalar_t>();
     auto mask_ptr = mask.data_ptr<uint8_t>();
 
-    // There's big performance gap between w and w/o IndexToOffset,
-    // thus we use two pathes to handle different cases
-    if (self.is_contiguous() && mask.is_contiguous()) {
-      auto kfn = DPCPP_Q_KFN(DPCPP::item<1> item_id) {
-        auto index = item_id.get_linear_id();
-        ret_ptr[index] =
-            self_ptr[index] * static_cast<uint8_t>(mask_ptr[index]) * scale;
-      };
-      cgh.parallel_for(DPCPP::range<1>(numel), kfn);
-    } else {
-      auto kfn = DPCPP_Q_KFN(DPCPP::item<1> item_id) {
-        auto index = item_id.get_linear_id();
-        auto self_offset =
-            IndexToOffset<scalar_t, uint64_t>::get(index, self_info);
-        auto mask_offset =
-            IndexToOffset<uint8_t, uint64_t>::get(index, mask_info);
-        ret_ptr[index] = self_ptr[self_offset] *
-            static_cast<uint8_t>(mask_ptr[mask_offset]) * scale;
-      };
-      cgh.parallel_for(DPCPP::range<1>(numel), kfn);
-    }
+    auto kfn = DPCPP_Q_KFN(DPCPP::item<1> item_id) {
+      auto index = item_id.get_linear_id();
+      auto self_offset =
+          IndexToOffset<scalar_t, uint64_t>::get(index, self_info);
+      auto mask_offset =
+          IndexToOffset<uint8_t, uint64_t>::get(index, mask_info);
+      ret_ptr[index] = self_ptr[self_offset] *
+          static_cast<uint8_t>(mask_ptr[mask_offset]) * scale;
+    };
+    cgh.parallel_for(DPCPP::range<1>(numel), kfn);
   };
-
   DPCPP_Q_SUBMIT(sycl_queue, cgf);
+}
+
+template <int vec_size, typename scalar_t, typename accscalar_t>
+void vec_masked_scale_kernel_impl(
+    at::Tensor& ret,
+    const Tensor& self,
+    const Tensor mask,
+    accscalar_t scale) {
+  auto& sycl_queue = dpcppGetCurrentQueue();
+  int64_t numel = self.numel();
+  int64_t global_range = (numel + vec_size - 1) / vec_size;
+  auto cgf = DPCPP_Q_CGF(cgh) {
+    auto self_ptr = self.data_ptr<scalar_t>();
+    auto ret_ptr = ret.data_ptr<scalar_t>();
+    using vec_t =
+        typename at::native::Memory::aligned_vector<scalar_t, vec_size>::type;
+    using elem_t = typename at::native::Memory::
+        aligned_vector<scalar_t, vec_size>::element_type;
+    vec_t* self_vec = reinterpret_cast<vec_t*>(self_ptr);
+    vec_t* ret_vec = reinterpret_cast<vec_t*>(ret_ptr);
+
+    auto mask_ptr = mask.data_ptr<uint8_t>();
+    using mask_vec_t =
+        typename at::native::Memory::aligned_vector<uint8_t, vec_size>::type;
+    mask_vec_t* mask_vec = reinterpret_cast<mask_vec_t*>(mask_ptr);
+
+    auto kfn = DPCPP_Q_KFN(DPCPP::item<1> item_id) {
+      auto index = item_id.get_linear_id();
+      int remaining = numel - vec_size * index;
+
+      if (remaining < vec_size) {
+        for (int id = 0; id < remaining; ++id) {
+          auto offset = index * vec_size + id;
+          auto self_val = self_ptr[offset];
+          auto mask_val = mask_ptr[offset];
+          scalar_t ret_val = static_cast<scalar_t>(self_val * mask_val * scale);
+          ret_ptr[offset] = ret_val;
+        }
+      } else {
+        auto self_value = self_vec[index];
+        auto mask_value = mask_vec[index];
+        vec_t ret_value;
+#pragma unroll
+        for (int id = 0; id < vec_size; ++id) {
+          auto self_val = at::native::Memory::detail::bitwise_cast<scalar_t>(
+              self_value[id]);
+          auto mask_val =
+              at::native::Memory::detail::bitwise_cast<uint8_t>(mask_value[id]);
+          scalar_t ret_val = static_cast<scalar_t>(self_val * mask_val * scale);
+          ret_value[id] =
+              at::native::Memory::detail::bitwise_cast<elem_t>(ret_val);
+        }
+        ret_vec[index] = ret_value;
+      }
+    };
+    cgh.parallel_for(DPCPP::range<1>(global_range), kfn);
+  };
+  DPCPP_Q_SUBMIT(sycl_queue, cgf);
+}
+
+template <typename scalar_t, typename accscalar_t>
+void vec_masked_scale_kernel(
+    at::Tensor& ret,
+    const Tensor& self,
+    const Tensor mask,
+    accscalar_t scale) {
+  // Two tensors (self and ret) are in scalar_t
+  // Only one tensor (mask) is in uint8_t
+  // So the scalar_t is used to query the vec_size
+  auto self_ptr = self.data_ptr<scalar_t>();
+  auto ret_ptr = ret.data_ptr<scalar_t>();
+  auto vec_size_self = at::native::Memory::can_vectorize_up_to<scalar_t>(
+      getDeviceIdOfCurrentQueue(), reinterpret_cast<char*>(self_ptr));
+  auto vec_size_ret = at::native::Memory::can_vectorize_up_to<scalar_t>(
+      getDeviceIdOfCurrentQueue(), reinterpret_cast<char*>(ret_ptr));
+  // set the min value of self and ret as the final vec_size
+  auto vec_size = std::min(vec_size_self, vec_size_ret);
+
+#define VEC_MASKED_SCALE_KERNEL_IMPL(vec_size)                     \
+  {                                                                \
+    vec_masked_scale_kernel_impl<vec_size, scalar_t, accscalar_t>( \
+        ret, self, mask, scale);                                   \
+  }
+
+  // TODO: we need to consider how to reduce the kernel size
+  switch (vec_size) {
+    case 16: {
+      VEC_MASKED_SCALE_KERNEL_IMPL(16);
+      break;
+    }
+    case 8: {
+      VEC_MASKED_SCALE_KERNEL_IMPL(8);
+      break;
+    }
+    case 4: {
+      VEC_MASKED_SCALE_KERNEL_IMPL(4);
+      break;
+    }
+    case 2: {
+      VEC_MASKED_SCALE_KERNEL_IMPL(2);
+      break;
+    }
+    case 1: {
+      VEC_MASKED_SCALE_KERNEL_IMPL(1);
+      break;
+    }
+    default:
+      TORCH_INTERNAL_ASSERT(
+          false,
+          "Unexpected vectorization size for MaskedScale. vec size ",
+          vec_size);
+  }
+#undef VEC_MASKED_SCALE_KERNEL_IMPL
 }
 
 } // namespace impl
@@ -198,14 +424,24 @@ std::tuple<Tensor, Tensor> _fused_dropout(
         accscalar_t pa = (accscalar_t)(p);
 #ifdef USE_ONEMKL
         Tensor rand = impl::bernoulliDistr_impl(self, p, gen_);
-        impl::fused_dropout_kernel<scalar_t, accscalar_t, int32_t>(
-            self, rand, ret, mask, pa);
+        if (self.is_contiguous()) {
+          impl::vec_fused_dropout_kernel<scalar_t, accscalar_t, int32_t>(
+              self, rand, ret, mask, pa);
+        } else {
+          impl::fused_dropout_kernel<scalar_t, accscalar_t, int32_t>(
+              self, rand, ret, mask, pa);
+        }
 #else
         at::Tensor rand = at::empty_like(self, self.suggest_memory_format());
         rand.bernoulli_(1 - p);
         rand.div_(1 - p);
-        impl::fused_dropout_kernel<scalar_t, accscalar_t, scalar_t>(
-            self, rand, ret, mask, pa);
+        if (self.is_contiguous()) {
+          impl::vec_fused_dropout_kernel<scalar_t, accscalar_t, scalar_t>(
+              self, rand, ret, mask, pa);
+        } else {
+          impl::fused_dropout_kernel<scalar_t, accscalar_t, scalar_t>(
+              self, rand, ret, mask, pa);
+        }
 #endif
       });
 
@@ -225,7 +461,12 @@ Tensor _masked_scale(const Tensor& self, const Tensor& mask, double scale) {
       [&] {
         using accscalar_t = DiscreteDistributionType<scalar_t>::type;
         accscalar_t pa = (accscalar_t)(scale);
-        impl::masked_scale_kernel<scalar_t, accscalar_t>(ret, self, mask, pa);
+        if (self.is_contiguous() && mask.is_contiguous()) {
+          impl::vec_masked_scale_kernel<scalar_t, accscalar_t>(
+              ret, self, mask, pa);
+        } else {
+          impl::masked_scale_kernel<scalar_t, accscalar_t>(ret, self, mask, pa);
+        }
       });
   return ret;
 }
--- tests/gpu/examples/test_dropout.py
+++ tests/gpu/examples/test_dropout.py
@@ -12,7 +12,41 @@ device = sycl_device
 class TestNNMethod(TestCase):
     def test_dropout(self):
         p = 0.2
+        # cover the code path without tailing
+        input = torch.Tensor(1024)
+        input = input.to(device).fill_(1 - p)  # input valuses are 0.8
+
+        module = nn.Dropout(p)
+        input_var = input.clone().requires_grad_()
+        output = module(input_var)  # output values are 0.0 and 1.0 (input_value * 1/(1-p))
+        # for Bernoulli distribution:
+        # x=0, p
+        # x=1, (1-p)
+        # Thus the mean value of output tensor is (1-p)
+        self.assertLess(abs(output.cpu().data.mean() - (1 - p)), 0.05)
+        output.backward(input)
+        self.assertLess(abs(input_var.grad.cpu().data.mean() - (1 - p)), 0.05)
+
+
+        # cover the code path with tailing
         input = torch.Tensor(1000)
+        input = input.to(device).fill_(1 - p)  # input valuses are 0.8
+
+        module = nn.Dropout(p)
+        input_var = input.clone().requires_grad_()
+        output = module(input_var)  # output values are 0.0 and 1.0 (input_value * 1/(1-p))
+        # for Bernoulli distribution:
+        # x=0, p
+        # x=1, (1-p)
+        # Thus the mean value of output tensor is (1-p)
+        self.assertLess(abs(output.cpu().data.mean() - (1 - p)), 0.05)
+        output.backward(input)
+        self.assertLess(abs(input_var.grad.cpu().data.mean() - (1 - p)), 0.05)
+
+
+        # cover the code path with non-contiguous input
+        input_orig = torch.Tensor(1000, 5)
+        input = input_orig.transpose(1, 0)
         input = input.to(device).fill_(1 - p)  # input valuses are 0.8
 
         module = nn.Dropout(p)
