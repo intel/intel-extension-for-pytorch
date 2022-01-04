21f20603723fc55a6bae53491ebe3f1d28610fb9
jianan-gu <jianan.gu@intel.com>
2022-01-04 11:36:24 +0800

    Enable inplace mkldnn softmax (#418)
    * enable inplace mkldnn softmax
    
    * add seperate softmax_ and replace subgraph when matching and no alias/multi-use
    
    * add softmax ut
    
    * minor fix
    
    * (1) enable inplace context (2) modify ut
    
    * minor fix
    
    * add filter at graph rewiter for  noncontiguous input
    
    * add assert in softmax impl for non-contiguous input
    
    * code refine for assert in softmax impl
    
    * refine softmax ut
    
    * fix format

--- intel_extension_for_pytorch/csrc/aten/cpu/Softmax.cpp
+++ intel_extension_for_pytorch/csrc/aten/cpu/Softmax.cpp
@@ -6,13 +6,28 @@ namespace cpu {
 
 // softmax kernel for inference mode with oneDNN implementation
 at::Tensor softmax_impl(const at::Tensor& input, const int64_t dim) {
+  // do not support non-contiguous input, which should go into aten::softmax
+  TORCH_CHECK(
+      input.is_contiguous(),
+      "ipex::softmax: Expected contiguous tensor input!");
   const int64_t wrapped_dim = at::maybe_wrap_dim(dim, input.dim());
-  auto input_ = input.is_contiguous() ? input : input.contiguous();
-  ideep::tensor mkldnn_input = itensor_view_from_dense(input_);
-  auto output = at::empty_like(input_);
+  ideep::tensor mkldnn_input = itensor_view_from_dense(input);
+  auto output = at::empty_like(input);
   ideep::tensor mkldnn_output = itensor_view_from_dense(output);
   ideep::softmax_forward::compute(mkldnn_input, mkldnn_output, wrapped_dim);
   return output;
+}
+
+// inplace softmax kernel for inference mode with oneDNN implementation
+void softmax_impl_(at::Tensor& input, const int64_t dim) {
+  // do not support non-contiguous input, which should go into aten::softmax
+  TORCH_CHECK(
+      input.is_contiguous(),
+      "ipex::softmax_: Expected contiguous tensor input!");
+  const int64_t wrapped_dim = at::maybe_wrap_dim(dim, input.dim());
+  ideep::tensor mkldnn_input = itensor_view_from_dense(input);
+  ideep::tensor mkldnn_output = itensor_view_from_dense(input);
+  ideep::softmax_forward::compute(mkldnn_input, mkldnn_output, wrapped_dim);
 }
 
 } // namespace cpu
--- intel_extension_for_pytorch/csrc/aten/cpu/Softmax.h
+++ intel_extension_for_pytorch/csrc/aten/cpu/Softmax.h
@@ -8,5 +8,6 @@ namespace torch_ipex {
 namespace cpu {
 
 at::Tensor softmax_impl(const at::Tensor& input, const int64_t dim);
+void softmax_impl_(at::Tensor& input, const int64_t dim);
 } // namespace cpu
 } // namespace torch_ipex
