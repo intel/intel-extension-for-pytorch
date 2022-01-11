6c31ab35b455b372e363b5ee428032a7b0477f8c
Da Shi <56560583+DaShi-Git@users.noreply.github.com>
2022-01-11 13:51:36 +0100

    add reflection_pad2d (#661)
    * add reflection_pad2d
    
    Signed-off-by: Da Shi <da.shi@intel.com>
    
    * fix a typo
    
    Co-authored-by: baocheny <baochen.yang@intel.com>
    Co-authored-by: xytintel <yutao.xu@intel.com>

--- csrc/aten/operators/ReflectionPad.cpp
+++ csrc/aten/operators/ReflectionPad.cpp
@@ -36,6 +36,44 @@ inline std::pair<int64_t, int64_t> get_index_mapping1d(
       input_offset + input_x, output_offset + output_x);
 }
 
+inline std::pair<int64_t, int64_t> get_index_mapping2d(
+    int64_t input_dim_x,
+    int64_t input_dim_y,
+    int64_t output_dim_x,
+    int64_t output_dim_y,
+    int64_t pad_l,
+    int64_t pad_t,
+    int64_t output_xy,
+    const DPCPP::nd_item<3> item) {
+  // 3D grid of 1D blocks
+  auto input_offset =
+      (item.get_group(1) + item.get_group(2) * item.get_group_range(1)) *
+      input_dim_x * input_dim_y;
+  auto output_offset =
+      (item.get_group(1) + item.get_group(2) * item.get_group_range(1)) *
+      output_dim_x * output_dim_y;
+
+  auto output_x = output_xy % output_dim_x;
+  auto output_y = output_xy / output_dim_x;
+
+  auto i_start_x = Max(int64_t(0), -pad_l);
+  auto i_start_y = Max(int64_t(0), -pad_t);
+  auto o_start_x = Max(int64_t(0), pad_l);
+  auto o_start_y = Max(int64_t(0), pad_t);
+
+  int64_t input_x = Numerics<int64_t>::abs(output_x - pad_l) -
+      Numerics<int64_t>::abs(output_x - (input_dim_x + pad_l - 1)) - output_x +
+      2 * pad_l + input_dim_x - 1 - o_start_x + i_start_x;
+
+  int64_t input_y = Numerics<int64_t>::abs(output_y - pad_t) -
+      Numerics<int64_t>::abs(output_y - (input_dim_y + pad_t - 1)) - output_y +
+      2 * pad_t + input_dim_y - 1 - o_start_y + i_start_y;
+
+  return std::make_pair<int64_t, int64_t>(
+      input_offset + input_y * input_dim_x + input_x,
+      output_offset + output_y * output_dim_x + output_x);
+}
+
 template <typename scalar_t>
 void reflection_pad1d_out_kernel(
     scalar_t* input,
@@ -62,6 +100,57 @@ void reflection_pad1d_out_kernel(
         // input index and output index mapping
         auto index_pair =
             get_index_mapping1d(input_w, output_w, output_x, pad_l, item);
+        output_ptr[index_pair.second] = input_ptr[index_pair.first];
+      }
+    };
+    cgh.parallel_for(
+        DPCPP::nd_range<3>(
+            DPCPP::range<3>(work_group_size * work_group_num, nplane, nbatch),
+            DPCPP::range<3>(work_group_size, 1, 1)),
+        kfn);
+  };
+  DPCPP_Q_SUBMIT(queue, cgf);
+}
+
+template <typename scalar_t>
+void reflection_pad2d_out_kernel(
+    scalar_t* input,
+    scalar_t* output,
+    int64_t input_dim_x,
+    int64_t input_dim_y,
+    int64_t pad_t,
+    int64_t pad_b,
+    int64_t pad_l,
+    int64_t pad_r,
+    int64_t nbatch,
+    int64_t nplane) {
+  int64_t output_dim_x = input_dim_x + pad_l + pad_r;
+  int64_t output_dim_y = input_dim_y + pad_t + pad_b;
+
+  auto& queue = dpcppGetCurrentQueue();
+  int work_group_size =
+      output_dim_x * output_dim_y > 256 ? 256 : output_dim_x * output_dim_y;
+  int work_group_num = CeilDiv(output_dim_x * output_dim_y, (int64_t)256);
+
+  auto cgf = DPCPP_Q_CGF(cgh) {
+    auto input_data = input;
+    auto output_data = output;
+    auto kfn = DPCPP_Q_KFN(DPCPP::nd_item<3> item) {
+      auto input_ptr = input_data;
+      auto output_ptr = output_data;
+      auto output_xy = item.get_global_id(0);
+
+      if (output_xy < output_dim_x * output_dim_y) {
+        // input index and output index mapping
+        auto index_pair = get_index_mapping2d(
+            input_dim_x,
+            input_dim_y,
+            output_dim_x,
+            output_dim_y,
+            pad_l,
+            pad_t,
+            output_xy,
+            item);
         output_ptr[index_pair.second] = input_ptr[index_pair.first];
       }
     };
@@ -147,6 +236,104 @@ void reflection_pad1d_out_template(
       });
 }
 
+void reflection_pad2d_out_template(
+    Tensor& output,
+    const Tensor& input_,
+    IntArrayRef padding) {
+  TORCH_CHECK(
+      xpu::dpcpp::detail::canUse32BitIndexMath(input_),
+      "input tensor must fit into 32-bit index math");
+
+  int plane_dim = 0;
+  int dim_h = 1;
+  int dim_w = 2;
+  int nbatch = 1;
+
+  bool valid_dims = input_.size(1) != 0 && input_.size(2) != 0;
+  TORCH_CHECK(
+      (input_.ndimension() == 3 && valid_dims) ||
+          (input_.ndimension() == 4 && valid_dims && input_.size(3) != 0),
+      "3D or 4D (batch mode) tensor expected for input, but got: ",
+      input_);
+
+  if (input_.ndimension() == 4) {
+    nbatch = input_.size(0);
+    plane_dim++;
+    dim_h++;
+    dim_w++;
+  }
+
+  int64_t pad_l = padding[0];
+  int64_t pad_r = padding[1];
+  int64_t pad_t = padding[2];
+  int64_t pad_b = padding[3];
+
+  int64_t nplane = input_.size(plane_dim);
+  int64_t input_h = input_.size(dim_h);
+  int64_t input_w = input_.size(dim_w);
+
+  TORCH_CHECK(
+      pad_l < input_w && pad_r < input_w,
+      "Padding size should be less than the corresponding input dimension, but "
+      "got: padding (",
+      pad_l,
+      ", ",
+      pad_r,
+      ") at dimension ",
+      dim_w,
+      " of input ",
+      input_.sizes());
+
+  TORCH_CHECK(
+      pad_t < input_h && pad_b < input_h,
+      "Padding size should be less than the corresponding input dimension, but "
+      "got: padding (",
+      pad_t,
+      ", ",
+      pad_b,
+      ") at dimension ",
+      dim_h,
+      " of input ",
+      input_.sizes());
+  int64_t output_h = input_h + pad_t + pad_b;
+  int64_t output_w = input_w + pad_l + pad_r;
+
+  TORCH_CHECK(
+      output_w >= 1 || output_h >= 1,
+      "input (H: ",
+      input_h,
+      ", W: ",
+      input_w,
+      ")is too small.  Calculated "
+      "output H: ",
+      output_h,
+      " W: ",
+      output_w);
+
+  if (input_.ndimension() == 3) {
+    output.resize_({nplane, output_h, output_w});
+  } else {
+    output.resize_({nbatch, nplane, output_h, output_w});
+  }
+
+  Tensor input = input_.contiguous();
+
+  IPEX_DISPATCH_FLOATING_TYPES_AND_HALF(
+      input.scalar_type(), "reflection_pad2d_out_template", [&] {
+        reflection_pad2d_out_kernel<scalar_t>(
+            input.data_ptr<scalar_t>(),
+            output.data_ptr<scalar_t>(),
+            input_w,
+            input_h,
+            pad_t,
+            pad_b,
+            pad_l,
+            pad_r,
+            nbatch,
+            nplane);
+      });
+}
+
 template <typename scalar_t>
 void reflection_pad1d_backward_out_kernel(
     scalar_t* grad_input,
@@ -173,6 +360,58 @@ void reflection_pad1d_backward_out_kernel(
         // grad input index and grad output index mapping
         auto index_pair =
             get_index_mapping1d(input_w, output_w, output_x, pad_l, item);
+        atomicAdd(
+            (dpcpp_global_ptr_pt<scalar_t>)&grad_input_ptr[index_pair.first],
+            grad_output_ptr[index_pair.second]);
+      }
+    };
+    cgh.parallel_for(
+        DPCPP::nd_range<3>(
+            DPCPP::range<3>(work_group_size * work_group_num, nplane, nbatch),
+            DPCPP::range<3>(work_group_size, 1, 1)),
+        kfn);
+  };
+  DPCPP_Q_SUBMIT(queue, cgf);
+}
+
+template <typename scalar_t>
+void reflection_pad2d_backward_out_kernel(
+    scalar_t* grad_input,
+    scalar_t* grad_output,
+    int64_t input_dim_x,
+    int64_t input_dim_y,
+    int64_t pad_t,
+    int64_t pad_b,
+    int64_t pad_l,
+    int64_t pad_r,
+    int64_t nbatch,
+    int64_t nplane) {
+  auto& queue = dpcppGetCurrentQueue();
+  int64_t output_dim_x = input_dim_x + pad_l + pad_r;
+  int64_t output_dim_y = input_dim_y + pad_t + pad_b;
+  int work_group_size =
+      output_dim_x * output_dim_y > 256 ? 256 : output_dim_x * output_dim_y;
+  int work_group_num = CeilDiv(output_dim_x * output_dim_y, (int64_t)256);
+
+  auto cgf = DPCPP_Q_CGF(cgh) {
+    auto grad_input_data = grad_input;
+    auto grad_output_data = grad_output;
+    auto kfn = DPCPP_Q_KFN(DPCPP::nd_item<3> item) {
+      auto grad_input_ptr = grad_input_data;
+      auto grad_output_ptr = grad_output_data;
+      auto output_xy = item.get_global_id(0);
+
+      if (output_xy < output_dim_x * output_dim_y) {
+        // grad input index and grad output index mapping
+        auto index_pair = get_index_mapping2d(
+            input_dim_x,
+            input_dim_y,
+            output_dim_x,
+            output_dim_y,
+            pad_l,
+            pad_t,
+            output_xy,
+            item);
         atomicAdd(
             (dpcpp_global_ptr_pt<scalar_t>)&grad_input_ptr[index_pair.first],
             grad_output_ptr[index_pair.second]);
@@ -239,6 +478,81 @@ void reflection_pad1d_backward_out_template(
       });
 }
 
+void reflection_pad2d_backward_out_template(
+    Tensor& grad_input,
+    const Tensor& grad_output_,
+    const Tensor& input,
+    IntArrayRef padding) {
+  TORCH_CHECK(
+      xpu::dpcpp::detail::canUse32BitIndexMath(input),
+      "input tensor must fit into 32-bit index math");
+  TORCH_CHECK(
+      xpu::dpcpp::detail::canUse32BitIndexMath(grad_output_),
+      "output gradient tensor must fit into 32-bit index math");
+
+  int64_t plane_dim = 0;
+  int64_t dim_h = 1;
+  int64_t dim_w = 2;
+  int64_t nbatch = 1;
+
+  if (input.ndimension() == 4) {
+    nbatch = input.size(0);
+    plane_dim++;
+    dim_h++;
+    dim_w++;
+  }
+
+  int64_t pad_l = padding[0];
+  int64_t pad_r = padding[1];
+  int64_t pad_t = padding[2];
+  int64_t pad_b = padding[3];
+
+  int64_t nplane = input.size(plane_dim);
+  int64_t input_h = input.size(dim_h);
+  int64_t input_w = input.size(dim_w);
+
+  int64_t output_h = input_h + pad_t + pad_b;
+  int64_t output_w = input_w + pad_l + pad_r;
+
+  TORCH_CHECK(
+      output_w == grad_output_.size(dim_w),
+      "grad_output width "
+      "unexpected. Expected: ",
+      output_w,
+      ", Got: ",
+      grad_output_.size(dim_w));
+  TORCH_CHECK(
+      output_h == grad_output_.size(dim_h),
+      "grad_output height "
+      "unexpected. Expected: ",
+      output_h,
+      ", Got: ",
+      grad_output_.size(dim_h));
+
+  Tensor grad_output = grad_output_.contiguous();
+
+  TORCH_CHECK(
+      output_w == grad_output.size(dim_w),
+      "gradOutput width unexpected. Expected: ",
+      output_w,
+      ", Got: ",
+      grad_output.size(dim_w));
+  IPEX_DISPATCH_ATOMIC_FLOATING_TYPES(
+      grad_input.scalar_type(), "reflection_pad2d_backward_out_template", [&] {
+        reflection_pad2d_backward_out_kernel<scalar_t>(
+            grad_input.data_ptr<scalar_t>(),
+            grad_output.data_ptr<scalar_t>(),
+            input_w,
+            input_h,
+            pad_t,
+            pad_b,
+            pad_l,
+            pad_r,
+            nbatch,
+            nplane);
+      });
+}
+
 } // namespace impl
 
 Tensor& reflection_pad1d_out(
@@ -254,6 +568,19 @@ Tensor reflection_pad1d(const Tensor& input, IntArrayRef padding) {
   return at::AtenIpexTypeXPU::reflection_pad1d_out(output, input, padding);
 }
 
+Tensor& reflection_pad2d_out(
+    Tensor& output,
+    const Tensor& input,
+    IntArrayRef padding) {
+  impl::reflection_pad2d_out_template(output, input, padding);
+  return output;
+}
+
+Tensor reflection_pad2d(const Tensor& input, IntArrayRef padding) {
+  auto output = at::empty({0}, input.options());
+  return at::AtenIpexTypeXPU::reflection_pad2d_out(output, input, padding);
+}
+
 Tensor& reflection_pad1d_backward_out(
     Tensor& grad_input,
     const Tensor& grad_output,
@@ -266,12 +593,34 @@ Tensor& reflection_pad1d_backward_out(
   return grad_input;
 }
 
+Tensor& reflection_pad2d_backward_out(
+    Tensor& grad_input,
+    const Tensor& grad_output,
+    const Tensor& input,
+    IntArrayRef padding) {
+  grad_input.resize_as_(input);
+  grad_input.zero_();
+  impl::reflection_pad2d_backward_out_template(
+      grad_input, grad_output, input, padding);
+  return grad_input;
+}
+
 Tensor reflection_pad1d_backward(
     const Tensor& grad_output,
     const Tensor& input,
     IntArrayRef padding) {
   auto grad_input = at::zeros_like(input, MemoryFormat::Contiguous);
   impl::reflection_pad1d_backward_out_template(
+      grad_input, grad_output, input, padding);
+  return grad_input;
+}
+
+Tensor reflection_pad2d_backward(
+    const Tensor& grad_output,
+    const Tensor& input,
+    IntArrayRef padding) {
+  auto grad_input = at::zeros_like(input, MemoryFormat::Contiguous);
+  impl::reflection_pad2d_backward_out_template(
       grad_input, grad_output, input, padding);
   return grad_input;
 }
---
+++ tests/gpu/examples/test_reflection_pad.py
@@ -0,0 +1,53 @@
+import torch
+import torch.nn as nn
+from torch.testing._internal.common_utils import TestCase
+
+import ipex
+
+
+class TestNNMethod(TestCase):
+    def test_reflection_pad1d(self, dtype=torch.float):
+        x_cpu = torch.arange(8, dtype=dtype).reshape(1, 2, 4)
+        grad_cpu = torch.arange(16, dtype=dtype).reshape(1, 2, 8)
+        x_dpcpp = x_cpu.to("xpu")
+        grad_dpcpp = grad_cpu.to("xpu")
+        m = nn.ReflectionPad1d(2)
+
+        x_cpu.requires_grad_(True)
+        y_cpu = m(x_cpu)
+        print("y_cpu", y_cpu)
+        output_cpu = y_cpu.backward(grad_cpu)
+        print("x_cpu.grad", x_cpu.grad)
+
+        m.to("xpu")
+        x_dpcpp.requires_grad_(True)
+        y_dpcpp = m(x_dpcpp)
+        print("y_dpcpp", y_dpcpp.to("cpu"))
+        output_dpcpp = y_dpcpp.backward(grad_dpcpp)
+        print("x_dpcpp.grad", x_dpcpp.grad.to("cpu"))
+
+        self.assertEqual(y_cpu, y_dpcpp.to("cpu"))
+        self.assertEqual(x_cpu.grad, x_dpcpp.grad.to("cpu"))
+
+    def test_reflection_pad2d(self, dtype=torch.float):
+        x_cpu = torch.arange(2 * 3 * 6 * 8, dtype=dtype).reshape(2, 3, 6, 8)
+        x_dpcpp = x_cpu.to("xpu")
+        grad_cpu = torch.arange(2 * 3 * 9 * 11, dtype=dtype).reshape(2, 3, 9, 11)
+        grad_dpcpp = grad_cpu.to("xpu")
+
+        m = nn.ReflectionPad2d((1, 2, 3, 0))
+        x_cpu.requires_grad_(True)
+        y_cpu = m(x_cpu)
+        print("y_cpu", y_cpu)
+        output_cpu = y_cpu.backward(grad_cpu)
+        print("x_cpu.grad", x_cpu.grad)
+
+        m = nn.ReflectionPad2d((1, 2, 3, 0)).to("xpu")
+        x_dpcpp.requires_grad_(True)
+        y_dpcpp = m(x_dpcpp)
+        print("y_dpcpp", y_dpcpp.to("cpu"))
+        output_dpcpp = y_dpcpp.backward(grad_dpcpp)
+        print("x_dpcpp.grad", x_dpcpp.grad.to("cpu"))
+
+        self.assertEqual(y_cpu, y_dpcpp.to("cpu"))
+        self.assertEqual(x_cpu.grad, x_dpcpp.grad.to("cpu"))
