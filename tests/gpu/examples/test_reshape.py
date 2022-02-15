415bb77c6ac43edf3d43d36695b9042eb609242b
cchheennhhaaoo <hao3.chen@intel.com>
2022-02-15 21:13:59 +0800

    Add regression ut of shape mismatch issue in reshape&cat (#741)

---
+++ tests/gpu/examples/test_reshape.py
@@ -0,0 +1,29 @@
+import torch
+import torch.nn as nn
+from torch.testing._internal.common_utils import TestCase
+
+import ipex
+import pytest
+
+
+class TestTorchMethod(TestCase):
+
+    @pytest.mark.skipif("not torch.xpu.using_onednn_layout()", reason="only for block format")
+    def test_reshape_cat(self, dtype=torch.float):
+        """
+        Issue: When run SSD-ResNet50 with block format, it raised a shape mismatch error in cat op.
+        Root cause: There was no to_plain_if_needed() in reshape_alias. Without it, the size of tensor was changed by reshape but dims was kept unchanged. It led to the shape mismatch error in the next op cat.
+        Fixed PR: https://github.com/intel-innersource/frameworks.ai.pytorch.ipex-gpu/pull/709
+        """
+        input1 = torch.randn([32, 3, 300, 300]).to("xpu")
+        conv1 = nn.Conv2d(3, 3, kernel_size=3, stride=1, padding=1, bias=False).to("xpu")
+        conv_out1 = conv1(input1)
+        reshape_out1 = conv_out1.reshape(1, 3, 300, 300, 32)
+
+        input2 = torch.randn([8, 3, 300, 300]).to("xpu")
+        conv2 = nn.Conv2d(3, 3, kernel_size=3, stride=1, padding=1, bias=False).to("xpu")
+        conv_out2 = conv2(input2)
+        reshape_out2 = conv_out2.reshape(1, 3, 300, 300, 8)
+
+        cat_out = torch.cat((reshape_out1, reshape_out2), -1)
+        print(cat_out.shape)
