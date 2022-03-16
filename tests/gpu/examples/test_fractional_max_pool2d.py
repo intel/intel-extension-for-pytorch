6a718cec2c5442642759525f27037652aae9efaa
Guo Yejun <yejun.guo@intel.com>
2022-03-16 10:50:01 +0800

    Revert "channels first and channels last optimization for FractionalMaxPool forward and backward (#805)" (#823)
    This reverts commit 1f97d704ace8e4e542109fef464d9b54eb269016.
    
    Co-authored-by: KevinYuk <kevin.yu@intel.com>

--- tests/gpu/examples/test_fractional_max_pool2d.py
+++ tests/gpu/examples/test_fractional_max_pool2d.py
@@ -3,8 +3,6 @@ import torch.nn as nn
 from torch.testing._internal.common_utils import TestCase
 
 import intel_extension_for_pytorch
-import pytest
-
 
 cpu_device = torch.device("cpu")
 dpcpp_device = torch.device("xpu")
@@ -36,38 +34,5 @@ class TestNNMethod(TestCase):
         grad_dpcpp = grad_cpu.to(dpcpp_device)
         y_dpcpp[0].backward(grad_dpcpp)
         print("y_dpcpp backward", x_dpcpp.grad.cpu())
-        self.assertEqual(y_dpcpp[0].is_contiguous(), True)
-        self.assertEqual(x_dpcpp.grad.is_contiguous(), True)
-        self.assertEqual(y_cpu[0], y_dpcpp[0].to(cpu_device))
-        self.assertEqual(x_cpu.grad, x_dpcpp.grad.to(cpu_device))
-
-    @pytest.mark.skipif(torch.xpu.using_onednn_layout(), reason="channels last does not support onednn block format")
-    def test_fractional_max_pool2d_channels_last(self, dtype=torch.float):
-        x_cpu = torch.randn([2, 2, 4, 4], device=cpu_device, dtype=dtype)
-        x_dpcpp = x_cpu.to(memory_format=torch.channels_last).to(dpcpp_device)
-        grad_cpu = torch.randn([2, 2, 2, 2], device=cpu_device)
-        grad_dpcpp = grad_cpu.to(dpcpp_device)
-        max_pool = nn.FractionalMaxPool2d(
-            2, output_size=(2, 2), return_indices=True)
-
-        # cpu
-        x_cpu.requires_grad_(True)
-        y_cpu = max_pool(x_cpu)
-        print("\ny_cpu:\n", y_cpu[0])
-        y_cpu[0].backward(grad_cpu)
-        print("y_cpu backward", x_cpu.grad)
-
-        max_pool = nn.FractionalMaxPool2d(
-            2, output_size=(2, 2), return_indices=True)
-        max_pool.to(dpcpp_device)
-        x_dpcpp.requires_grad_(True)
-        y_dpcpp = max_pool(x_dpcpp)
-
-        print("\ny_dpcpp:\n", y_dpcpp[0].cpu())
-        grad_dpcpp = grad_cpu.to(dpcpp_device)
-        y_dpcpp[0].backward(grad_dpcpp)
-        print("y_dpcpp backward", x_dpcpp.grad.cpu())
-        self.assertEqual(y_dpcpp[0].is_contiguous(memory_format=torch.channels_last), True)
-        self.assertEqual(x_dpcpp.grad.is_contiguous(memory_format=torch.channels_last), True)
         self.assertEqual(y_cpu[0], y_dpcpp[0].to(cpu_device))
         self.assertEqual(x_cpu.grad, x_dpcpp.grad.to(cpu_device))
