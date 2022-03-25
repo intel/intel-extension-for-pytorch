22c5b8afd4a2108fe2d7605220172450578d1196
xytintel <yutao.xu@intel.com>
2022-03-25 20:10:51 +0800

    Add testcase for add_int32 (#793)
    * Add testcase for add_int32
    
    
    Co-authored-by: Kim <kimyangbaochen@vip.qq.com>
    Co-authored-by: Jinghui <jinghui.gu@intel.com>

---
+++ tests/gpu/regression/test_add_int.py
@@ -0,0 +1,18 @@
+import torch
+import torch.nn as nn
+from torch.testing._internal.common_utils import TestCase
+
+import intel_extension_for_pytorch
+import pytest
+import numpy as np
+
+
+class TestAddInt(TestCase):
+    def test_add_int(self):
+        a = torch.empty([2, 512], dtype=torch.int32, device="xpu")
+        b = torch.empty([1, 512], dtype=torch.int32, device="xpu")
+        a_cpu = a.cpu()
+        b_cpu = b.cpu()
+        c = a + b
+        c_cpu = a_cpu + b_cpu
+        self.assertEqual(c.to("cpu"), c_cpu)
