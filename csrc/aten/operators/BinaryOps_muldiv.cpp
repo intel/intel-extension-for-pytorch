f702096b9104bf46b86d4bed98df6e698d0d5bed
cchheennhhaaoo <hao3.chen@intel.com>
2022-03-25 13:27:27 +0800

    Support new op div.out_mode(div trunc & div floor) in PyTorch1.10 (#838)
    * Support new op div.out_mode(div trunc & div floor) in PyTorch1.10
    
    * Revise shape of ut
    
    * Using trunc_impl instead of std::trunc in div_trunc
    
    * Add complex support for div kernel
    
    Co-authored-by: Jinghui <jinghui.gu@intel.com>

--- csrc/aten/operators/BinaryOps_muldiv.cpp
+++ csrc/aten/operators/BinaryOps_muldiv.cpp
@@ -8,6 +8,7 @@
 #include "comm/ScalarOps.h"
 
 #include "Loops.h"
+#include "comm/zmath.h"
 
 using namespace xpu::dpcpp;
 
@@ -29,6 +30,18 @@ static void mul_kernel_dpcpp(TensorIterator& iter) {
 }
 
 static void div_kernel_dpcpp(TensorIterator& iter) {
+  IPEX_DISPATCH_FLOATING_AND_COMPLEX_TYPES_AND2(
+      at::ScalarType::BFloat16,
+      at::ScalarType::Half,
+      iter.dtype(),
+      "div",
+      [&]() {
+        dpcpp_kernel_with_scalars(
+            iter, [](scalar_t a, scalar_t b) -> scalar_t { return a / b; });
+      });
+}
+
+static void div_trunc_kernel_dpcpp(TensorIterator& iter) {
   if (isIntegralType(iter.dtype(), false)) {
     IPEX_DISPATCH_INTEGRAL_TYPES(iter.dtype(), "div", [&] {
       dpcpp_kernel_with_scalars(
@@ -39,10 +52,62 @@ static void div_kernel_dpcpp(TensorIterator& iter) {
         at::ScalarType::BFloat16,
         at::ScalarType::Half,
         iter.dtype(),
-        "div",
+        "div_trunc",
         [&]() {
           dpcpp_kernel_with_scalars(
-              iter, [](scalar_t a, scalar_t b) -> scalar_t { return a / b; });
+              iter, [](scalar_t a, scalar_t b) -> scalar_t {
+                return trunc_impl(a / b);
+              });
+        });
+  }
+}
+
+static void div_floor_kernel_dpcpp(TensorIterator& iter) {
+  if (iter.dtype() == kByte) {
+    return div_trunc_kernel_dpcpp(iter);
+  } else if (isIntegralType(iter.dtype(), false)) {
+    IPEX_DISPATCH_INTEGRAL_TYPES(iter.dtype(), "div", [&] {
+      dpcpp_kernel_with_scalars(iter, [](scalar_t a, scalar_t b) -> scalar_t {
+        if ((a < 0) != (b < 0)) {
+          const auto quot = a / b;
+          const auto rem = a % b;
+          return rem ? quot - 1 : quot;
+        }
+
+        return a / b;
+      });
+    });
+  } else {
+    IPEX_DISPATCH_FLOATING_TYPES_AND2(
+        at::ScalarType::BFloat16,
+        at::ScalarType::Half,
+        iter.dtype(),
+        "div_trunc_cpu",
+        [&]() {
+          dpcpp_kernel_with_scalars(
+              iter, [](scalar_t a, scalar_t b) -> scalar_t {
+                if (C10_UNLIKELY(b == 0)) {
+                  // Divide by zero: return standard IEEE result
+                  return a / b;
+                }
+
+                auto mod = std::fmod(a, b);
+                auto div = (a - mod) / b;
+                if ((mod != 0) && (b < 0) != (mod < 0)) {
+                  div -= scalar_t(1);
+                }
+
+                scalar_t floordiv;
+                if (div != 0) {
+                  floordiv = std::floor(div);
+                  if (div - floordiv > scalar_t(0.5)) {
+                    floordiv += scalar_t(1.0);
+                  }
+                } else {
+                  floordiv = DPCPP::copysign(0.0, scalar_cast<double>(a / b));
+                }
+                return floordiv;
+              });
         });
   }
 }
@@ -80,6 +145,24 @@ Tensor& div_out(Tensor& result, const Tensor& self, const Tensor& other) {
   return result;
 }
 
+Tensor& div_out(
+    Tensor& result,
+    const Tensor& self,
+    const Tensor& other,
+    c10::optional<c10::string_view> rounding_mode) {
+  if (!rounding_mode.has_value()) {
+    auto iter = TensorIterator::binary_float_op(result, self, other);
+    impl::div_kernel_dpcpp(iter);
+  } else if (*rounding_mode == "trunc") {
+    auto iter = TensorIterator::binary_op(result, self, other);
+    impl::div_trunc_kernel_dpcpp(iter);
+  } else if (*rounding_mode == "floor") {
+    auto iter = TensorIterator::binary_op(result, self, other);
+    impl::div_floor_kernel_dpcpp(iter);
+  }
+  return result;
+}
+
 Tensor div(const Tensor& self, const Tensor& other) {
   Tensor result;
   auto iter = TensorIterator::binary_float_op(result, self, other);
@@ -96,9 +179,9 @@ Tensor& floor_divide_out(
     const Tensor& self,
     const Tensor& other) {
   auto iter = TensorIterator::binary_op(result, self, other);
-  impl::div_kernel_dpcpp(iter);
-  if (result.is_floating_point()) {
-    result.trunc_();
+  impl::div_trunc_kernel_dpcpp(iter);
+  if (!result.defined()) {
+    result = iter.output();
   }
   return result;
 }
@@ -106,13 +189,8 @@ Tensor& floor_divide_out(
 Tensor floor_divide(const Tensor& self, const Tensor& other) {
   Tensor result;
   auto iter = TensorIterator::binary_op(result, self, other);
-  impl::div_kernel_dpcpp(iter);
-
-  auto out = iter.output();
-  if (out.is_floating_point()) {
-    out.trunc_();
-  }
-  return out;
+  impl::div_trunc_kernel_dpcpp(iter);
+  return iter.output();
 }
 
 Tensor& floor_divide_(Tensor& self, const Tensor& other) {
--- tests/gpu/examples/test_binary_div.py
+++ tests/gpu/examples/test_binary_div.py
@@ -287,3 +287,23 @@ class TestTorchMethod(TestCase):
                 self.assertEqual(a_xpu.is_contiguous(), True)
                 self.assertEqual(a_xpu.is_contiguous(memory_format=torch.channels_last), False)
             print("passed")
+
+    def test_div_rounding_mode(self, dtype=torch.float):
+        a_cpu = torch.randn([100, 100])
+        a_xpu = a_cpu.to("xpu")
+
+        div_trunc_cpu = torch.div(a_cpu, 5, rounding_mode="trunc")
+        div_trunc_xpu = torch.div(a_xpu, 5, rounding_mode="trunc")
+
+        print(div_trunc_cpu)
+        print(div_trunc_xpu.to("cpu"))
+
+        self.assertEqual(div_trunc_cpu, div_trunc_xpu.to("cpu"))
+
+        div_floor_cpu = torch.div(a_cpu, 5, rounding_mode="floor")
+        div_floor_xpu = torch.div(a_xpu, 5, rounding_mode="floor")
+
+        print(div_floor_cpu)
+        print(div_floor_xpu.to("cpu"))
+
+        self.assertEqual(div_floor_cpu, div_floor_xpu.to("cpu"))
