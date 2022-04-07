dbf3645de5ab9f135f28db55114d927157b02277
cchheennhhaaoo <hao3.chen@intel.com>
2022-04-07 18:00:58 +0800

    Using at::native::clone in QuantizedXPU::clone (#904)
    * Using at::native::clone in QuantizedXPU::clone
    
    We should not call the quantized_clone for QuantizedXPU.
    The quantized_clone only supports QuantizedCPU and QuantizedCUDA.
    We should call aten clone directly, while the quantization
    case will be handled by our copy impl.
    
    * Update Clone.cpp
    
    Add comments
    
    Co-authored-by: Jinghui <jinghui.gu@intel.com>

--- csrc/aten/operators/Clone.cpp
+++ csrc/aten/operators/Clone.cpp
@@ -12,7 +12,11 @@ Tensor clone(const Tensor& self, c10::optional<MemoryFormat> memory_format) {
 namespace AtenIpexTypeQuantizedXPU {
 
 Tensor clone(const Tensor& self, c10::optional<MemoryFormat> memory_format) {
-  return at::native::quantized_clone(self, memory_format);
+  // We should not call the quantized_clone for QuantizedXPU.
+  // The quantized_clone only supports QuantizedCPU and QuantizedCUDA.
+  // We should call aten clone directly, while the quantization
+  // case will be handled by our copy impl.
+  return at::native::clone(self, memory_format);
 }
 
 } // namespace AtenIpexTypeQuantizedXPU
