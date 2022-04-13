3ceed8da863933d1c47de9872f7041c954170a3e
xytintel <yutao.xu@intel.com>
2022-04-13 15:59:40 +0800

    Remove microbench dirs (#926)
    Microbench now is moved to https://github.com/intel-sandbox/IPEX_Microbench.git

--- scripts/tools/microbench/test/simple_add.py
+++
@@ -1,9 +0,0 @@
-import torch
-import intel_extension_for_pytorch
-import microbench
-
-microbench.enable_verbose()
-a = torch.rand(2).cpu()
-b = torch.rand(2).cpu()
-c = a + b
-microbench.disable_verbose()
