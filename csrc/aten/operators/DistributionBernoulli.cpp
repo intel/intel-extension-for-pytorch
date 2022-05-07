f0d567764fb0ffcc646d3618dbf74b066c16fb11
Zhiwei <zhiwei.yan@intel.com>
2022-05-07 13:45:57 +0800

    Handle dtype dispatching in Bernoulli. (#1075)
    * Handle integer(int,long,bool) scalar p in Bernoulli
    * Handle uniform_ in bernoulli, add bernoulli_out
    * Refine code of bernoulli

--- csrc/aten/operators/DistributionBernoulli.cpp
+++ csrc/aten/operators/DistributionBernoulli.cpp
@@ -22,9 +22,16 @@ Tensor& bernoulli_(
     Tensor& self,
     const Tensor& p_,
     c10::optional<Generator> gen_) {
-  at::AtenIpexTypeXPU::uniform_(self, 0.0, 1.0, gen_);
+  Tensor self_float;
+  auto self_type = self.scalar_type();
+  if (!(self_type == at::ScalarType::Float ||
+        self_type == at::ScalarType::Double))
+    self_float = at::empty(self.sizes(), self.options().dtype(at::kFloat));
+  else
+    self_float = self;
+  at::AtenIpexTypeXPU::uniform_(self_float, 0.0, 1.0, gen_);
   auto p = p_.to(kXPU);
-  auto iter = TensorIterator::binary_op(self, self, p);
+  auto iter = TensorIterator::binary_op(self, self_float, p);
   IPEX_DISPATCH_FLOATING_TYPES_AND2(
       at::ScalarType::Half,
       at::ScalarType::BFloat16,
@@ -45,8 +52,9 @@ void bernoulli_scalar_dpcpp(
     c10::optional<Generator> gen_) {
   auto gen = get_generator_or_default<xpu::dpcpp::DPCPPGeneratorImpl>(
       gen_, xpu::dpcpp::detail::getDefaultDPCPPGenerator());
-  IPEX_DISPATCH_FLOATING_TYPES_AND2(
+  IPEX_DISPATCH_ALL_TYPES_AND3(
       at::ScalarType::Half,
+      at::ScalarType::Bool,
       at::ScalarType::BFloat16,
       iter.dtype(),
       "bernoulli_scalar_dpcpp",
@@ -57,13 +65,23 @@ void bernoulli_scalar_dpcpp(
         auto bernoulli_func = [p](accscalar_t rand) {
           return static_cast<scalar_t>(rand < static_cast<accscalar_t>(p));
         };
-        AtenIpexTypeXPU::distribution_nullary_kernel<scalar_t, accscalar_t>(
-            iter,
-            gen,
-            [](RandomState<Philox4_32_10>* state) {
-              return state->uniform<scalar_t>();
-            },
-            bernoulli_func);
+        if (std::is_same<scalar_t, double>::value) {
+          AtenIpexTypeXPU::distribution_nullary_kernel<scalar_t, accscalar_t>(
+              iter,
+              gen,
+              [](RandomState<Philox4_32_10>* state) {
+                return state->uniform<double>();
+              },
+              bernoulli_func);
+        } else {
+          AtenIpexTypeXPU::distribution_nullary_kernel<scalar_t, accscalar_t>(
+              iter,
+              gen,
+              [](RandomState<Philox4_32_10>* state) {
+                return state->uniform<float>();
+              },
+              bernoulli_func);
+        }
       });
 }
 
@@ -71,6 +89,34 @@ Tensor& bernoulli_(Tensor& self, double p, c10::optional<Generator> gen_) {
   auto iter = TensorIterator::nullary_op(self);
   bernoulli_scalar_dpcpp(iter, p, gen_);
   return self;
+}
+
+Tensor& bernoulli_out(
+    const Tensor& self,
+    c10::optional<Generator> generator,
+    Tensor& out) {
+  auto out_type = out.scalar_type();
+  Tensor out_float;
+  if (!(out_type == at::ScalarType::Float ||
+        out_type == at::ScalarType::Double))
+    out_float = self.to(at::ScalarType::Float);
+  else
+    out_float = out;
+  at::AtenIpexTypeXPU::uniform_(out_float, 0.0, 1.0, generator);
+  auto iter = TensorIterator::binary_op(out_float, out_float, self);
+  IPEX_DISPATCH_FLOATING_TYPES_AND2(
+      at::ScalarType::Half,
+      at::ScalarType::BFloat16,
+      iter.dtype(),
+      "bernoulli_tensor_dpcpp_",
+      [&] {
+        dpcpp_kernel_for_tensor_iter(
+            iter, [](scalar_t out, scalar_t p) -> scalar_t {
+              return static_cast<scalar_t>(out < p);
+            });
+      });
+  out = out_float;
+  return out;
 }
 
 } // namespace AtenIpexTypeXPU
