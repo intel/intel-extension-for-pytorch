ab9e3fdecffa7e16d4d6269d2a76ae0830c86acf
Zhiwei <zhiwei.yan@intel.com>
2022-05-14 01:19:24 +0800

    Refine dispatch in erf-releted cases (#1120)
    Co-authored-by: Jinghui <jinghui.gu@intel.com>

--- csrc/aten/operators/UnaryOps_erf.cpp
+++ csrc/aten/operators/UnaryOps_erf.cpp
@@ -15,8 +15,12 @@ namespace AtenIpexTypeXPU {
 
 Tensor& erf_out(const Tensor& self, Tensor& out) {
   auto iter = TensorIterator::unary_float_op(out, self);
-  IPEX_DISPATCH_FLOATING_TYPES_AND(
-      at::ScalarType::BFloat16, iter.dtype(), "erf", [&]() {
+  IPEX_DISPATCH_FLOATING_TYPES_AND2(
+      at::ScalarType::BFloat16,
+      at::ScalarType::Half,
+      iter.common_dtype(),
+      "erf",
+      [&]() {
         dpcpp_kernel_for_tensor_iter(iter, [](scalar_t a) -> scalar_t {
           return Numerics<scalar_t>::erf(a);
         });
@@ -26,8 +30,12 @@ Tensor& erf_out(const Tensor& self, Tensor& out) {
 
 Tensor& erfc_out(const Tensor& self, Tensor& out) {
   auto iter = TensorIterator::unary_float_op(out, self);
-  IPEX_DISPATCH_FLOATING_TYPES_AND(
-      at::ScalarType::BFloat16, iter.dtype(), "erfc", [&]() {
+  IPEX_DISPATCH_FLOATING_TYPES_AND2(
+      at::ScalarType::BFloat16,
+      at::ScalarType::Half,
+      iter.common_dtype(),
+      "erfc",
+      [&]() {
         dpcpp_kernel_for_tensor_iter(iter, [](scalar_t a) -> scalar_t {
           return Numerics<scalar_t>::erfc(a);
         });
@@ -36,9 +44,13 @@ Tensor& erfc_out(const Tensor& self, Tensor& out) {
 }
 
 Tensor& erfinv_out(Tensor& out, const Tensor& self) {
-  auto iter = TensorIterator::unary_op(out, self);
-  IPEX_DISPATCH_FLOATING_TYPES_AND(
-      at::ScalarType::BFloat16, iter.dtype(), "erfinv", [&]() {
+  auto iter = TensorIterator::unary_float_op(out, self);
+  IPEX_DISPATCH_FLOATING_TYPES_AND2(
+      at::ScalarType::BFloat16,
+      at::ScalarType::Half,
+      iter.common_dtype(),
+      "erfinv",
+      [&]() {
         dpcpp_kernel_for_tensor_iter(iter, [](scalar_t a) -> scalar_t {
           scalar_t b;
           TensorErfinvOp<scalar_t>()(b, a);
@@ -46,15 +58,6 @@ Tensor& erfinv_out(Tensor& out, const Tensor& self) {
         });
       });
   return out;
-}
-
-Tensor& erfinv_(Tensor& self) {
-  return at::AtenIpexTypeXPU::erfinv_out(self, self);
-}
-
-Tensor erfinv(const Tensor& self) {
-  auto result = at::empty_like(self);
-  return at::AtenIpexTypeXPU::erfinv_out(result, self);
 }
 
 } // namespace AtenIpexTypeXPU
