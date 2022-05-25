448608bcd2556439126b7abf4e6abf8d78f41655
Kim <baochen.yang@intel.com>
2022-05-25 10:14:42 +0800

    refine save load (#1167)

---
+++ tests/gpu/examples/test_save_load.py
@@ -0,0 +1,14 @@
+import tempfile
+import torch
+from torch.testing._internal.common_utils import TestCase
+import intel_extension_for_pytorch
+
+
+class TestTorchMethod(TestCase):
+    def test_save_load(self):
+        a = torch.ones([10], dtype=torch.float64)
+        a = a.to("xpu")
+        ckpt = tempfile.NamedTemporaryFile()
+        torch.save(a, ckpt.name)
+        b = torch.load(ckpt.name)
+        assert torch.equal(a, b), "tensor saved & loaded not equal"
