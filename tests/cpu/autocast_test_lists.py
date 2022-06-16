d9a2680d2f391eb56e507eb166cb43ae7a8a8f8a
Cao E <e.cao@intel.com>
2022-06-16 16:51:21 +0800

    Add _native_multi_head_attention to low precision cast policy of AutocastCPU (#860)
    * add _native_multi_head_attention to low precision cast policy of AutocastCPU
    
    * fix fcode format

--- tests/cpu/autocast_test_lists.py
+++ tests/cpu/autocast_test_lists.py
@@ -80,6 +80,16 @@ class AutocastCPUTestLists(object):
             ("addbmm", mat0_fp32 + (torch.randn((n, n, n), device=dev, dtype=torch.float32),
                                     torch.randn((n, n, n), device=dev, dtype=torch.float32))),
         ]
+        self.torch_bf16_multi_output = [
+            ("_native_multi_head_attention", (torch.randn((1, 1, 768), device=dev, dtype=torch.float32),
+                                              torch.randn((1, 1, 768), device=dev, dtype=torch.float32),
+                                              torch.randn((1, 1, 768), device=dev, dtype=torch.float32),
+                                              768, 12, torch.randn((2304, 768), device=dev, dtype=torch.float32),
+                                              torch.randn((2304), device=dev, dtype=torch.float32),
+                                              torch.randn((768, 768), device=dev, dtype=torch.float32),
+                                              torch.randn((768), device=dev, dtype=torch.float32),
+                                              None, False, True)),
+        ]
         self.torch_fp32 = [
             ("conv_transpose1d", conv_args_bf16[0]),
             ("conv_transpose2d", conv_args_bf16[1]),
