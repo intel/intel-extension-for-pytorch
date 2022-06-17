7000e7c1def954af8705a3b8b4109ed4c087d331
johnlu <chengjun.lu@intel.com>
2022-06-18 00:02:52 +0800

    Quantize header rename

--- csrc/aten/quantized/QUtil.h
+++ csrc/aten/quantized/QUtil.h
@@ -2,7 +2,7 @@
 #include <ATen/Config.h>
 #include <ATen/NativeFunctions.h>
 #include <ATen/cpp_custom_type_hack.h>
-#include <ATen/native/quantized/packed_params.h>
+#include <ATen/native/quantized/PackedParams.h>
 #include <ATen/native/quantized/cpu/conv_serialization.h>
 #include <torch/custom_class.h>
 #include <torch/custom_class_detail.h>
