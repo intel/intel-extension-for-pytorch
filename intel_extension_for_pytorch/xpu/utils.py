6fa8ebf7f192bd1a735691fbfa3f6c1873445d9b
Xu Han <xu.han@intel.com>
2022-07-01 14:22:06 +0800

    add dpcpp onednn lib in DPCPPExtension (#1233)
    * add dpcpp onednn lib
    * append cxx link parameters
    * add onemkl link options.
    * check complier both CC and CXX.
    * embedded all onemkl link options.
    * link ipex lib
    * fix DPCPPExtesion link binary issue.
    * merge get_onemkl_sycl_libraries to get_onemkl_libraries
    * convert all get directory functions to list.
    
    Co-authored-by: Jinghui <jinghui.gu@intel.com>

--- intel_extension_for_pytorch/xpu/utils.py
+++ intel_extension_for_pytorch/xpu/utils.py
@@ -53,28 +53,36 @@ class _one_api_help:
             raise 'Didn\'t detect dpcpp root. Please source <oneapi_dir>/compiler/<version>/env/vars.sh '        
 
     def get_default_include_dir(self):
-        return os.path.join(self.__default_root, 'include')
+        return [os.path.join(self.__default_root, 'include')]
 
     def get_default_lib_dir(self):
-        return os.path.join(self.__default_root, 'lib')
+        return [os.path.join(self.__default_root, 'lib')]
 
     def get_dpcpp_include_dir(self):
-        return os.path.join(self.__dpcpp_root, 'linux', 'include')
+        return [
+            os.path.join(self.__dpcpp_root, 'linux', 'include'),
+            os.path.join(self.__dpcpp_root, 'linux', 'include', 'sycl')
+        ]
 
     def get_onemkl_include_dir(self):
-        return os.path.join(self.__onemkl_root, 'include')
+        return [os.path.join(self.__onemkl_root, 'include')]
 
     def get_onednn_include_dir(self):
-        return os.path.join(self.__onednn_root, 'include')
+        return [os.path.join(self.__onednn_root, 'include')]
+
+    def get_onednn_lib_dir(self):
+        return [os.path.join(self.__onednn_root, 'lib')]
 
     def is_xpu_compiler_ready(self):
         # TODO: Need to decouple with toolchain env scripts
         # and according to OS to find real path of DPCPP compiler
+
         cc_cfg = os.getenv('CC')
-        if cc_cfg is None:
-            raise 'Please use DPCPP for XPU build.'
+        cxx_cfg = os.getenv('CXX')
+        if cc_cfg is None or cxx_cfg is None:
+            raise 'Please use corrent compiler for XPU build, via CC=icx CXX=dpcpp.'
         # Intel XPU use DPCPP as compiler.
-        if cc_cfg == 'dpcpp':
+        if cc_cfg == 'icx' and cxx_cfg == 'dpcpp':
             return True
         return False
 
@@ -89,15 +97,20 @@ class _one_api_help:
         return True
 
     def get_library_dirs(self):
-        return [self.get_default_lib_dir()]
+        library_dirs = []
+        library_dirs += [f'{x}' for x in self.get_default_lib_dir()]
+        library_dirs += [f'{x}' for x in self.get_onednn_lib_dir()]
+        return library_dirs
 
     def get_include_dirs(self):
-        return [self.get_dpcpp_include_dir(),
-                self.get_onemkl_include_dir(),
-                self.get_onednn_include_dir(),
-                self.get_default_include_dir()]
+        include_dirs = []
+        include_dirs += [f'{x}' for x in self.get_dpcpp_include_dir()]
+        include_dirs += [f'{x}' for x in self.get_onemkl_include_dir()]
+        include_dirs += [f'{x}' for x in self.get_onednn_include_dir()]
+        include_dirs += [f'{x}' for x in self.get_default_include_dir()]
+        return include_dirs
 
-    def get_libraries(self):
+    def get_onemkl_libraries(self):
         MKLROOT = self.__onemkl_root
         return [
             f'{MKLROOT}/lib/intel64/libmkl_sycl.a',
@@ -127,8 +140,8 @@ def DPCPPExtension(name, sources, *args, **kwargs):
         raise 'Please use dpcpp for XPU build.'
 
     library_dirs = kwargs.get('library_dirs', [])
-    library_dirs += oneAPI.get_libraries()
     library_dirs += get_pytorch_lib_dir()
+    library_dirs += oneAPI.get_library_dirs()
     kwargs['library_dirs'] = library_dirs
 
     libraries = kwargs.get('libraries', [])
@@ -136,6 +149,9 @@ def DPCPPExtension(name, sources, *args, **kwargs):
     libraries.append('torch')
     libraries.append('torch_cpu')
     libraries.append('torch_python')
+
+    # Append oneDNN link parameters.
+    libraries.append('dnnl')
     kwargs['libraries'] = libraries
 
     include_dirs = kwargs.get('include_dirs', [])
@@ -144,12 +160,25 @@ def DPCPPExtension(name, sources, *args, **kwargs):
     kwargs['include_dirs'] = include_dirs
 
     kwargs['language'] = 'c++'
-    dlink_libraries = kwargs.get('dlink_libraries', [])
-    dlink_libraries += oneAPI.get_libraries()
-    dlink = kwargs.get('dlink', False) or dlink_libraries
-    if dlink:
-        extra_compile_args = kwargs.get('extra_compile_args', {})
-        # todo: add dpcpp parameter support.
-        kwargs['extra_compile_args'] = extra_compile_args
+
+    extra_compile_args = kwargs.get('extra_compile_args', {})
+    extra_link_args = kwargs.get('extra_link_args', [])    
+
+    # Append oneMKL link parameters, detailed please reference:
+    # https://www.intel.com/content/www/us/en/developer/tools/oneapi/onemkl-link-line-advisor.html
+    extra_link_args += [f'-L{x}' for x in library_dirs]
+    extra_link_args += ['-fsycl-device-code-split=per_kernel']
+    extra_link_args += ['-Wl,--start-group']
+    extra_link_args += [f'{x}' for x in oneAPI.get_onemkl_libraries()]
+    extra_link_args += ['-Wl,--end-group']
+    extra_link_args += ['-lsycl', '-lOpenCL', '-lpthread', '-lm', '-ldl']
+
+    # Append IPEX link parameters.
+    extra_link_args += [f'-L{x}' for x in oneAPI.get_default_lib_dir()]
+    extra_link_args += ['-lintel-ext-pt-gpu']
+
+    # todo: add dpcpp parameter support.
+    kwargs['extra_link_args'] = extra_link_args
+    kwargs['extra_compile_args'] = extra_compile_args
 
     return setuptools.Extension(name, sources, *args, **kwargs)
