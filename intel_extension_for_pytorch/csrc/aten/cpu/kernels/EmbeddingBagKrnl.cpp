1137085358de4eb9f80cf855c441089e2c3e1467
zhuhaozhe <haozhe.zhu@intel.com>
2022-07-07 15:30:01 +0800

    enhance emb perf and ut coverage (#951)
    * enhance emb perf and ut coverage
    
    * add ut for include last offsets
    
    * cover more ut and rename last_indices->last_index
    
    * rename last_offsets->last_offset

--- intel_extension_for_pytorch/csrc/aten/cpu/kernels/EmbeddingBagKrnl.cpp
+++ intel_extension_for_pytorch/csrc/aten/cpu/kernels/EmbeddingBagKrnl.cpp
@@ -49,47 +49,23 @@ static inline at::Tensor _embedding_bag_index_add_select_fast(
     const at::Tensor offsets,
     bool include_last_offset) {
   int64_t ddim = src.size(1);
-  auto* src_data = src.data_ptr<T>();
-  int64_t output_size = offsets.numel() - 1;
-  int64_t* offsets_data = offsets.data_ptr<int64_t>();
-  std::vector<int64_t> offsets_include_last;
-
-  if (!include_last_offset) {
-    output_size = offsets.numel();
-    offsets_include_last.resize(output_size + 1);
-    int64_t* offsets_include_last_data = offsets_include_last.data();
-    int64_t iter_time = (output_size >> 5);
-    int64_t align32_size = (iter_time << 5);
-    int64_t left_size = output_size - align32_size;
-    // std::memcpy(offsets_include_last.data(), offsets_data, sizeof(int64_t) *
-    // output_size);
-    at::parallel_for(0, iter_time, 16, [&](int64_t start, int64_t end) {
-      for (int64_t i = start; i < end; i += 1) {
-        auto start_offset = i << 5;
-        move_ker(
-            &offsets_include_last_data[start_offset],
-            &offsets_data[start_offset],
-            32);
-      }
-    });
-    if (left_size > 0) {
-      move_ker(
-          &offsets_include_last_data[align32_size],
-          &offsets_data[align32_size],
-          left_size);
-    }
-    offsets_include_last[output_size] = indices.numel();
-    offsets_data = offsets_include_last.data();
+  T* src_data = src.data_ptr<T>();
+  int64_t output_size = offsets.numel();
+  if (include_last_offset) {
+    output_size -= 1;
   }
+  int64_t* offsets_data = offsets.data_ptr<int64_t>();
+  auto indices_accessor = indices.accessor<int64_t, 1>();
+  int64_t last_index = indices.numel();
+  int64_t last_offset = output_size - 1;
 
   at::Tensor output = at::empty({output_size, src.size(1)}, src.options());
   auto* output_data = output.data_ptr<T>();
-  auto indices_accessor = indices.accessor<int64_t, 1>();
   at::parallel_for(0, output_size, 16, [&](int64_t start, int64_t end) {
     for (int64_t i = start; i < end; i++) {
       auto* out_data_ptr = &output_data[i * ddim];
       auto inputs_start = offsets_data[i];
-      auto inputs_end = offsets_data[i + 1];
+      auto inputs_end = i == last_offset ? last_index : offsets_data[i + 1];
       if (inputs_end - inputs_start == 1) {
         T* select_data_ptr = &src_data[indices_accessor[inputs_start] * ddim];
         move_ker(out_data_ptr, select_data_ptr, ddim);
@@ -339,34 +315,15 @@ at::Tensor embedding_bag_int8_kernel_impl(
   double scale = at::native::q_scale_quant(qweight);
   int8_t* qweight_data =
       reinterpret_cast<int8_t*>(qweight.data_ptr<at::qint8>());
-  int64_t output_size = offsets.numel() - 1;
-  int64_t* offsets_data = offsets.data_ptr<int64_t>();
-  std::vector<int64_t> offsets_include_last;
-  if (!include_last_offset) {
-    output_size = offsets.numel();
-    offsets_include_last.resize(output_size + 1);
-    int64_t* offsets_include_last_data = offsets_include_last.data();
-    int64_t iter_time = (output_size >> 5);
-    int64_t align32_size = (iter_time << 5);
-    int64_t left_size = output_size - align32_size;
-    at::parallel_for(0, iter_time, 16, [&](int64_t start, int64_t end) {
-      for (int64_t i = start; i < end; i += 1) {
-        auto start_offset = i << 5;
-        move_ker(
-            &offsets_include_last_data[start_offset],
-            &offsets_data[start_offset],
-            32);
-      }
-    });
-    if (left_size > 0) {
-      move_ker(
-          &offsets_include_last_data[align32_size],
-          &offsets_data[align32_size],
-          left_size);
-    }
-    offsets_include_last[output_size] = indices.numel();
-    offsets_data = offsets_include_last.data();
+  int64_t output_size = offsets.numel();
+  if (include_last_offset) {
+    output_size -= 1;
   }
+  int64_t* offsets_data = offsets.data_ptr<int64_t>();
+  auto indices_accessor = indices.accessor<int64_t, 1>();
+  int64_t last_index = indices.numel();
+  int64_t last_offset = output_size - 1;
+
   // init output tensor
   at::QuantizerPtr output_quantizer =
       at::make_per_tensor_affine_quantizer(scale, /*zp=*/0, at::kQInt8);
@@ -375,12 +332,12 @@ at::Tensor embedding_bag_int8_kernel_impl(
       qweight.options(),
       output_quantizer);
   int8_t* output_data = reinterpret_cast<int8_t*>(output.data_ptr<at::qint8>());
-  auto indices_accessor = indices.accessor<int64_t, 1>();
+
   at::parallel_for(0, output_size, 16, [&](int64_t start, int64_t end) {
     for (int64_t i = start; i < end; i++) {
       int8_t* out_data_ptr = &output_data[i * ddim];
       auto inputs_start = offsets_data[i];
-      auto inputs_end = offsets_data[i + 1];
+      auto inputs_end = i == last_offset ? last_index : offsets_data[i + 1];
       if (inputs_start >= inputs_end) {
         zero_ker(out_data_ptr, ddim);
       } else {
--- tests/cpu/test_emb.py
+++ tests/cpu/test_emb.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import unittest
+import itertools
 import copy
 from torch.testing._internal.common_utils import TestCase
 import intel_extension_for_pytorch as ipex
@@ -10,22 +11,31 @@ aten_emb_fn = ipex.nn.functional._embeddingbag.torch_embedding_bag
 
 class TestEMB(TestCase):
 
-    def _test_emb(self, mode):
-        aten_emb = nn.EmbeddingBag(10, 5, mode="sum", sparse=True)
+    def _test_emb(self, mode, per_sample_weights=None, padding_idx=None, include_last_offset=False):
+        aten_emb = nn.EmbeddingBag(
+            10, 33, mode=mode, sparse=True,
+            padding_idx=padding_idx,
+            include_last_offset=include_last_offset
+        )
         aten_emb = aten_emb.bfloat16().float()
         ipex_emb = copy.deepcopy(aten_emb)
         bf16_emb = copy.deepcopy(aten_emb).bfloat16()
         # a batch of 2 samples of 4 indices each
         input = torch.LongTensor([1,2,4,5,4,3,2,9])
-        offsets = torch.LongTensor([0, 4])
+        if per_sample_weights is not None:
+            per_sample_weights = torch.rand_like(input.float())
+        if include_last_offset:
+            offsets = torch.LongTensor([0, 4, 8])
+        else:
+            offsets = torch.LongTensor([0, 4])
         # aten path
         torch.embedding_bag = aten_emb_fn
-        aten_out = aten_emb(input, offsets)
+        aten_out = aten_emb(input, offsets, per_sample_weights)
         aten_out.sum().backward()
 
         # ipex fast path (both fp32/bf16)
         torch.embedding_bag = ipex_emb_fn
-        ipex_out = ipex_emb(input, offsets)
+        ipex_out = ipex_emb(input, offsets, per_sample_weights)
         ipex_out.sum().backward()
 
         self.assertEqual(aten_out, ipex_out)
@@ -36,7 +46,7 @@ class TestEMB(TestCase):
         self.assertEqual(aten_emb.weight.grad.data._indices(), ipex_emb.weight.grad.data._indices())
         self.assertEqual(aten_emb.weight.grad.data._values(), ipex_emb.weight.grad.data._values())
 
-        if mode == 'sum':
+        if mode == 'sum' and padding_idx is None and per_sample_weights is None:
             bf16_out = bf16_emb(input, offsets)
             bf16_out.sum().backward()
             self.assertEqual(aten_out.bfloat16(), bf16_out)
@@ -50,9 +60,18 @@ class TestEMB(TestCase):
 
     def test_emb_fallback_path(self):
         self._test_emb(mode='mean')
+        for options in itertools.product([2, None], [True, None], [True, False]):
+            padding_idx, per_sample_weights, include_last_offset = options
+            self._test_emb(
+                mode='sum',
+                per_sample_weights=per_sample_weights,
+                padding_idx=padding_idx,
+                include_last_offset=include_last_offset
+            )
 
     def test_emb_fast_path(self):
         self._test_emb(mode='sum')
+        self._test_emb(mode='sum', include_last_offset=True)
 
     def test_emb_jit_scriptable(self):
         emb = nn.EmbeddingBag(10, 3, mode='sum', sparse=True)
