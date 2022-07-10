e32ac386fb631cb362b04d3211da5f0ebfb7a464
xiaolil1 <xiaoli.liu@intel.com>
2022-07-10 18:30:42 +0800

    Enhance operator list of AMP (#1226)
    * Enable full operator list for AMP
    * optimize op list
    * remove softmax from black list

--- csrc/aten/amp/autocast_mode.cpp
+++ csrc/aten/amp/autocast_mode.cpp
@@ -17,8 +17,8 @@ namespace autocast {
 enum class CastPolicy : uint8_t {
   lower_precision_fp =
       0, // Cast all inputs to lower_precision_fp before running the op.
-         // Currently, lower_precision_fp is fp16 for AutocastCUDA, and is
-         // defined by user(default bf16) for AutocastCPU.
+         // Currently, lower_precision_fp is bf16 for AutocastXPU, and is
+         // defined by user(default bf16) for AutocastXPU.
   fp32, // Cast all inputs to at::kFloat before running the op.
   fp32_set_opt_dtype, // Treats functions (like softmax) that
                       //   1. we'd like to run in fp32 and
@@ -210,6 +210,23 @@ struct WrapFunction final {
           SIGNATURE,                                       \
           &FUNC>::type::call);
 
+// Less-common but still useful case: redispatching to a function with a new
+// signature (e.g. appending a dtype)
+#define KERNEL_XPU_DIFFERENT_REDISPATCH_SIGNATURE(  \
+    REDISPATCH_FUNC,                                \
+    REGISTER_NAME,                                  \
+    REGISTER_SIGNATURE,                             \
+    REDISPATCH_SIGNATURE,                           \
+    POLICY)                                         \
+  m.impl(                                           \
+      TORCH_SELECTIVE_NAME("aten::" REGISTER_NAME), \
+      &WrapFunction<                                \
+          CastPolicy::POLICY,                       \
+          DeviceType::XPU,                          \
+          REGISTER_SIGNATURE,                       \
+          REDISPATCH_SIGNATURE,                     \
+          &REDISPATCH_FUNC>::type::call);
+
 /*****************************************
 Explicit registration for out-of-place ops
 *****************************************/
@@ -219,6 +236,53 @@ TORCH_LIBRARY_IMPL(_, AutocastXPU, m) {
 
 TORCH_LIBRARY_IMPL(aten, AutocastXPU, m) {
   // lower_precision_fp cast policy
+  KERNEL_XPU(
+      ADD_NS(_convolution),
+      "_convolution.deprecated",
+      Tensor(
+          const Tensor&,
+          const Tensor&,
+          const c10::optional<Tensor>&,
+          IntArrayRef,
+          IntArrayRef,
+          IntArrayRef,
+          bool,
+          IntArrayRef,
+          int64_t,
+          bool,
+          bool,
+          bool),
+      lower_precision_fp)
+  KERNEL_XPU(
+      ADD_NS(_convolution),
+      "_convolution",
+      Tensor(
+          const Tensor&,
+          const Tensor&,
+          const c10::optional<Tensor>&,
+          IntArrayRef,
+          IntArrayRef,
+          IntArrayRef,
+          bool,
+          IntArrayRef,
+          int64_t,
+          bool,
+          bool,
+          bool,
+          bool),
+      lower_precision_fp)
+  KERNEL_XPU(
+      ADD_NS(conv1d),
+      "conv1d",
+      Tensor(
+          const Tensor&,
+          const Tensor&,
+          const c10::optional<Tensor>&,
+          IntArrayRef,
+          IntArrayRef,
+          IntArrayRef,
+          int64_t),
+      lower_precision_fp)
   KERNEL_XPU(
       ADD_NS(conv2d),
       "conv2d",
@@ -231,21 +295,262 @@ TORCH_LIBRARY_IMPL(aten, AutocastXPU, m) {
           IntArrayRef,
           int64_t),
       lower_precision_fp)
+  KERNEL_XPU(
+      ADD_NS(conv3d),
+      "conv3d",
+      Tensor(
+          const Tensor&,
+          const Tensor&,
+          const c10::optional<Tensor>&,
+          IntArrayRef,
+          IntArrayRef,
+          IntArrayRef,
+          int64_t),
+      lower_precision_fp)
+  KERNEL_XPU(
+      ADD_NS(convolution),
+      "convolution",
+      Tensor(
+          const Tensor&,
+          const Tensor&,
+          const c10::optional<Tensor>&,
+          IntArrayRef,
+          IntArrayRef,
+          IntArrayRef,
+          bool,
+          IntArrayRef,
+          int64_t),
+      lower_precision_fp)
+  KERNEL_XPU(
+      ADD_NS(addmm),
+      "addmm",
+      Tensor(
+          const Tensor&,
+          const Tensor&,
+          const Tensor&,
+          const Scalar&,
+          const Scalar&),
+      lower_precision_fp)
+  KERNEL_XPU(
+      ADD_NS(matmul),
+      "matmul",
+      Tensor(const Tensor&, const Tensor&),
+      lower_precision_fp)
+  KERNEL_XPU(
+      ADD_NS(mm),
+      "mm",
+      Tensor(const Tensor&, const Tensor&),
+      lower_precision_fp)
+  KERNEL_XPU(
+      ADD_NS(linear),
+      "linear",
+      Tensor(const Tensor&, const Tensor&, const c10::optional<Tensor>&),
+      lower_precision_fp)
+  KERNEL_XPU(
+      ADD_NS(addbmm),
+      "addbmm",
+      Tensor(
+          const Tensor&,
+          const Tensor&,
+          const Tensor&,
+          const Scalar&,
+          const Scalar&),
+      lower_precision_fp)
+  KERNEL_XPU(
+      ADD_NS(baddbmm),
+      "baddbmm",
+      Tensor(
+          const Tensor&,
+          const Tensor&,
+          const Tensor&,
+          const Scalar&,
+          const Scalar&),
+      lower_precision_fp)
+  KERNEL_XPU(
+      ADD_NS(bmm),
+      "bmm",
+      Tensor(const Tensor&, const Tensor&),
+      lower_precision_fp)
 
   // fp32
   KERNEL_XPU(
-      ADD_NS(avg_pool2d),
-      "avg_pool2d",
+      ADD_NS(binary_cross_entropy),
+      "binary_cross_entropy",
+      Tensor(
+          const Tensor&, const Tensor&, const c10::optional<Tensor>&, int64_t),
+      fp32)
+  KERNEL_XPU(
+      ADD_NS(binary_cross_entropy_with_logits),
+      "binary_cross_entropy_with_logits",
       Tensor(
           const Tensor&,
-          IntArrayRef,
-          IntArrayRef,
-          IntArrayRef,
-          bool,
-          bool,
-          c10::optional<int64_t>),
+          const Tensor&,
+          const c10::optional<Tensor>&,
+          const c10::optional<Tensor>&,
+          int64_t),
       fp32)
-}
+  KERNEL_XPU(
+      ADD_NS(cross_entropy_loss),
+      "cross_entropy_loss",
+      Tensor(
+          const Tensor&,
+          const Tensor&,
+          const c10::optional<Tensor>&,
+          int64_t,
+          int64_t,
+          double),
+      fp32)
+  KERNEL_XPU(
+      ADD_NS(log_softmax),
+      "log_softmax.int",
+      Tensor(const Tensor&, int64_t, c10::optional<ScalarType>),
+      fp32)
+  KERNEL_XPU(
+      ADD_NS(log_softmax),
+      "log_softmax.Dimname",
+      Tensor(const Tensor&, Dimname, c10::optional<ScalarType>),
+      fp32)
+  KERNEL_XPU(
+      ADD_NS(nll_loss),
+      "nll_loss",
+      Tensor(
+          const Tensor&,
+          const Tensor&,
+          const c10::optional<Tensor>&,
+          int64_t,
+          int64_t),
+      fp32)
+  KERNEL_XPU(
+      ADD_NS(nll_loss_nd),
+      "nll_loss_nd",
+      Tensor(
+          const Tensor&,
+          const Tensor&,
+          const c10::optional<Tensor>&,
+          int64_t,
+          int64_t),
+      fp32)
+  KERNEL_XPU(
+      ADD_NS(fft_ifft),
+      "fft_ifft",
+      Tensor(
+          const Tensor&,
+          c10::optional<int64_t>,
+          int64_t,
+          c10::optional<c10::string_view>),
+      fp32)
+  KERNEL_XPU(
+      ADD_NS(fft_fft2),
+      "fft_fft2",
+      Tensor(
+          const Tensor&,
+          c10::optional<at::IntArrayRef>,
+          at::IntArrayRef,
+          c10::optional<c10::string_view>),
+      fp32)
+  KERNEL_XPU(
+      ADD_NS(fft_ifft2),
+      "fft_ifft2",
+      Tensor(
+          const Tensor&,
+          c10::optional<at::IntArrayRef>,
+          at::IntArrayRef,
+          c10::optional<c10::string_view>),
+      fp32)
+  KERNEL_XPU(
+      ADD_NS(fft_fftn),
+      "fft_fftn",
+      Tensor(
+          const Tensor&,
+          c10::optional<at::IntArrayRef>,
+          c10::optional<at::IntArrayRef>,
+          c10::optional<c10::string_view>),
+      fp32)
+  KERNEL_XPU(
+      ADD_NS(fft_ifftn),
+      "fft_ifftn",
+      Tensor(
+          const Tensor&,
+          c10::optional<at::IntArrayRef>,
+          c10::optional<at::IntArrayRef>,
+          c10::optional<c10::string_view>),
+      fp32)
+  KERNEL_XPU(
+      ADD_NS(fft_rfft),
+      "fft_rfft",
+      Tensor(
+          const Tensor&,
+          c10::optional<int64_t>,
+          int64_t,
+          c10::optional<c10::string_view>),
+      fp32)
+  KERNEL_XPU(
+      ADD_NS(fft_irfft),
+      "fft_irfft",
+      Tensor(
+          const Tensor&,
+          c10::optional<int64_t>,
+          int64_t,
+          c10::optional<c10::string_view>),
+      fp32)
+  KERNEL_XPU(
+      ADD_NS(fft_rfft2),
+      "fft_rfft2",
+      Tensor(
+          const Tensor&,
+          c10::optional<at::IntArrayRef>,
+          at::IntArrayRef,
+          c10::optional<c10::string_view>),
+      fp32)
+  KERNEL_XPU(
+      ADD_NS(fft_irfft2),
+      "fft_irfft2",
+      Tensor(
+          const Tensor&,
+          c10::optional<at::IntArrayRef>,
+          at::IntArrayRef,
+          c10::optional<c10::string_view>),
+      fp32)
+  KERNEL_XPU(
+      ADD_NS(fft_rfftn),
+      "fft_rfftn",
+      Tensor(
+          const Tensor&,
+          c10::optional<at::IntArrayRef>,
+          c10::optional<at::IntArrayRef>,
+          c10::optional<c10::string_view>),
+      fp32)
+  KERNEL_XPU(
+      ADD_NS(fft_irfftn),
+      "fft_irfftn",
+      Tensor(
+          const Tensor&,
+          c10::optional<at::IntArrayRef>,
+          c10::optional<at::IntArrayRef>,
+          c10::optional<c10::string_view>),
+      fp32)
+  KERNEL_XPU(
+      ADD_NS(fft_hfft),
+      "fft_hfft",
+      Tensor(
+          const Tensor&,
+          c10::optional<int64_t>,
+          int64_t,
+          c10::optional<c10::string_view>),
+      fp32)
+  KERNEL_XPU(
+      ADD_NS(fft_ihfft),
+      "fft_ihfft",
+      Tensor(
+          const Tensor&,
+          c10::optional<int64_t>,
+          int64_t,
+          c10::optional<c10::string_view>),
+      fp32)
 
+  // promote
+  KERNEL_XPU(ADD_NS(cat), "cat", Tensor(TensorList, int64_t), promote)
+  KERNEL_XPU(ADD_NS(stack), "stack", Tensor(TensorList, int64_t), promote)
+}
 } // namespace autocast
 } // namespace at
