c34752d32d6bff1475f07c0274732efdf0aa7705
Chunyuan WU <chunyuan.wu@intel.com>
2022-07-15 00:51:25 +0000

    bridge: add UT for debug name and profile name (#977)

--- tests/cpu/profile_ipex_op.py
+++ tests/cpu/profile_ipex_op.py
@@ -1,3 +1,4 @@
+import argparse
 import torch
 import torch.nn as nn
 import intel_extension_for_pytorch as ipex
@@ -13,35 +14,50 @@ class inplace_softmax(torch.nn.Module):
         x2 = nn.Softmax(dim=-1)(x1)
         return x2
 
-test = torch.tensor([[1.0,1.0],[1.0,1.0]])
-model = inplace_softmax().eval()
-with torch.no_grad():
-    model = torch.jit.trace(model, test)
-for _ in range(10):
-        res = model(test)
-
-# UT is testing the usecases mentioned in PyTorch doc
-# https://pytorch.org/docs/stable/profiler.html
-
-# usecase 1
-with torch.profiler.profile(
-        activities=[torch.profiler.ProfilerActivity.CPU]
-    ) as prof1:
+def run_profile(model, x):
+    with torch.no_grad():
+        model = torch.jit.trace(model, x)
     for _ in range(10):
-        res = model(test)
-print(prof1.key_averages().table(sort_by="self_cpu_time_total", row_limit=-1))
+            res = model(x)
 
-# usecase 2
-with torch.profiler.profile(
-        activities=[torch.profiler.ProfilerActivity.CPU],
-        schedule=torch.profiler.schedule(
-        wait=2,
-        warmup=3,
-        active=5),
-        on_trace_ready=trace_handler
-    ) as prof2:
-    for _ in range(10):
-        res = model(test)
-        prof2.step()
+    # UT is testing the usecases mentioned in PyTorch doc
+    # https://pytorch.org/docs/stable/profiler.html
 
+    # usecase 1
+    with torch.profiler.profile(
+            activities=[torch.profiler.ProfilerActivity.CPU]
+        ) as prof1:
+        for _ in range(10):
+            res = model(x)
+    print(prof1.key_averages().table(sort_by="self_cpu_time_total", row_limit=-1))
 
+    # usecase 2
+    with torch.profiler.profile(
+            activities=[torch.profiler.ProfilerActivity.CPU],
+            schedule=torch.profiler.schedule(
+            wait=2,
+            warmup=3,
+            active=5),
+            on_trace_ready=trace_handler
+        ) as prof2:
+        for _ in range(10):
+            res = model(x)
+            prof2.step()
+
+def run_model(llga):
+    llga_enabled = ipex._C.is_llga_fp32_bf16_enabled()
+    if llga:
+        ipex._C.set_llga_fp32_bf16_enabled(True)    
+    
+    x = torch.tensor([[1.0,1.0],[1.0,1.0]])
+    model = inplace_softmax().eval()    
+    
+    run_profile(model, x)
+    
+    ipex._C.set_llga_fp32_bf16_enabled(llga_enabled)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--llga", action='store_true', help='use llga for fp32 path', default=False)
+    args = parser.parse_args()
+    run_model(args.llga)
