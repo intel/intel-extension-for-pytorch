5aac3887330402b7b2f147dd31d075ed7afa792a
Jing Xu <jing.xu@intel.com>
2022-08-15 10:28:47 +0900

    update bugs in example scripts (#1046)
    Co-authored-by: Wang Weihan <eikan.wang@intel.com>

--- examples/bert.py
+++ examples/bert.py
@@ -6,7 +6,7 @@ def inference(model, data):
     # warm up
     for _ in range(100):
       model(data)
-    
+
     # measure
     import time
     start = time.time()
@@ -25,36 +25,36 @@ def main(args):
   data = torch.randint(vocab_size, size=[batch_size, seq_length])
 
   import intel_extension_for_pytorch as ipex
-  
+
   if args.dtype == 'float32':
     model = ipex.optimize(model, dtype=torch.float32)
   elif args.dtype == 'bfloat16':
     model = ipex.optimize(model, dtype=torch.bfloat16)
   else: # int8
     from intel_extension_for_pytorch.quantization import prepare, convert
-    
+
     if args.quantization == 'static':
-        qconfig = ipex.quantization.default_static_qconfig
-        model = prepare(model, qconfig, example_inputs=data, inplace=False)
-        
-        # calibration 
-        n_iter = 100
-        for i in range(n_iter):
-            model(data)
-            
-        model = convert(model)
+      qconfig = ipex.quantization.default_static_qconfig
+      model = prepare(model, qconfig, example_inputs=data, inplace=False)
+
+      # calibration
+      n_iter = 100
+      for i in range(n_iter):
+          model(data)
+
+      model = convert(model)
     else:
-        qconfig = ipex.quantization.default_dynamic_qconfig
-        model = prepare(model, qconfig, example_inputs=data)
-        model = convert(model)
-  
-  if args.torchscript:
-    with torch.no_grad():
+      qconfig = ipex.quantization.default_dynamic_qconfig
+      model = prepare(model, qconfig, example_inputs=data)
+      model = convert(model)
+
+  with torch.cpu.amp.autocast(enabled=args.dtype=='bfloat16'):
+    if args.torchscript:
+      with torch.no_grad():
         model = torch.jit.trace(model, data, check_trace=False, strict=False)
         model = torch.jit.freeze(model)
-  
-  with torch.cpu.amp.autocast(enabled=args.dtype=='bfloat16'):
-      inference(model, data)
+
+    inference(model, data)
 
 if __name__ == '__main__':
   import argparse
--- examples/resnet50.py
+++ examples/resnet50.py
@@ -3,10 +3,10 @@ import torchvision.models as models
 
 def inference(model, data):
   with torch.no_grad():
-    # warm up 
+    # warm up
     for _ in range(100):
       model(data)
-    
+
     # measure
     import time
     start = time.time()
@@ -22,35 +22,35 @@ def main(args):
   data = torch.rand(1, 3, 224, 224)
 
   import intel_extension_for_pytorch as ipex
-  
+
   model = model.to(memory_format=torch.channels_last)
   data = data.to(memory_format=torch.channels_last)
-  
+
   if args.dtype == 'float32':
     model = ipex.optimize(model, dtype=torch.float32)
   elif args.dtype == 'bfloat16':
     model = ipex.optimize(model, dtype=torch.bfloat16)
   else: # int8
     from intel_extension_for_pytorch.quantization import prepare, convert
-    
+
     qconfig = ipex.quantization.default_static_qconfig
     model = prepare(model, qconfig, example_inputs=data, inplace=False)
-    
-    # calibration 
+
+    # calibration
     n_iter = 100
     for i in range(n_iter):
         model(data)
-        
+
     model = convert(model)
-    
-  if args.torchscript:
-    with torch.no_grad():
+
+  with torch.cpu.amp.autocast(enabled=args.dtype=='bfloat16'):
+    if args.torchscript:
+      with torch.no_grad():
         model = torch.jit.trace(model, data)
         model = torch.jit.freeze(model)
-  
-  with torch.cpu.amp.autocast(enabled=args.dtype=='bfloat16'):
-      inference(model, data)
-      
+
+    inference(model, data)
+
 if __name__ == '__main__':
   import argparse
   parser = argparse.ArgumentParser()
