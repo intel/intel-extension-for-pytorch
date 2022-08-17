8eac9fe29b8a4d3e8e66d57b9c9a2377855d8b62
Yutao Xu <yutao.xu@intel.com>
2022-08-17 23:51:55 +0800

    distributions: optimize philox engine workflow (#1389)
    * Update Distributions.h
    
    
    Co-authored-by: Feng Yuan <feng1.yuan@intel.com>
    Co-authored-by: Jinghui <jinghui.gu@intel.com>

--- csrc/aten/operators/Distributions.h
+++ csrc/aten/operators/Distributions.h
@@ -19,27 +19,73 @@ void distribution_elementwise_grid_stride_kernel(
     std::pair<uint64_t, uint64_t> seeds,
     const dist_t dist_func,
     const transform_t transform_func) {
+  constexpr int unroll_factor = sizeof(accscalar_t) <= 4 ? 4 : 2;
   auto& sycl_queue = dpcppGetCurrentQueue();
-
-  auto offset_calc = make_offset_calculator<1>(iter);
-  auto cgf = DPCPP_Q_CGF(cgh) {
-    auto out_data = (char*)iter.data_ptr(0);
-    auto kfn = DPCPP_Q_KFN(DPCPP::item<1> item_id) {
-      size_t sample_id = item_id.get_id(0);
-      auto out_ptr = out_data;
-      RandomState<Philox4_32_10> state(seeds.first, sample_id, seeds.second);
-      auto rand = dist_func(&state);
-      accscalar_t r = ScalarConvert<scalar_t, accscalar_t>::to(rand);
-      scalar_t ret = transform_func(r);
-      auto offset = offset_calc.get(sample_id)[0];
-      scalar_t* out = (scalar_t*)(out_ptr + offset);
-      *out = ret;
+  int group_items = dpcppMaxWorkGroupSize(dpcppGetDeviceIdOfCurrentQueue());
+  int group_work_size = group_items * unroll_factor;
+  int num_groups = (numel + group_work_size - 1) / group_work_size;
+  if (iter.is_trivial_1d()) {
+    auto strides = iter.get_inner_strides();
+    int stride0 = strides[0];
+    auto cgf = DPCPP_Q_CGF(cgh) {
+      auto out_data = (char*)iter.data_ptr(0);
+      auto kfn = DPCPP_Q_KFN(DPCPP::nd_item<1> item) {
+        int gid = item.get_group(0);
+        int tid = item.get_local_id(0);
+        RandomState<Philox4_32_10> state(
+            seeds.first, gid * group_items + tid, seeds.second);
+        int sample_id = gid * group_work_size + tid;
+#pragma unroll
+        for (int i = 0; i < unroll_factor; i++) {
+          if (sample_id >= numel)
+            return;
+          auto rand = dist_func(&state);
+          accscalar_t r = ScalarConvert<scalar_t, accscalar_t>::to(rand);
+          scalar_t ret = transform_func(r);
+          auto offset = sample_id * stride0;
+          scalar_t* out = (scalar_t*)(out_data + offset);
+          *out = ret;
+          sample_id += group_items;
+        }
+      };
+      cgh.parallel_for(
+          DPCPP::nd_range<1>(
+              DPCPP::range<1>(num_groups * group_items),
+              DPCPP::range<1>(group_items)),
+          kfn);
     };
-
-    cgh.parallel_for(DPCPP::range<1>(numel), kfn);
-  };
-
-  DPCPP_Q_SUBMIT(sycl_queue, cgf);
+    DPCPP_Q_SUBMIT(sycl_queue, cgf);
+  } else {
+    auto offset_calc = make_offset_calculator<1>(iter);
+    auto cgf = DPCPP_Q_CGF(cgh) {
+      auto out_data = (char*)iter.data_ptr(0);
+      auto kfn = DPCPP_Q_KFN(DPCPP::nd_item<1> item) {
+        int gid = item.get_group(0);
+        int tid = item.get_local_id(0);
+        RandomState<Philox4_32_10> state(
+            seeds.first, gid * group_items + tid, seeds.second);
+        int sample_id = gid * group_work_size + tid;
+#pragma unroll
+        for (int i = 0; i < unroll_factor; i++) {
+          if (sample_id >= numel)
+            return;
+          auto rand = dist_func(&state);
+          accscalar_t r = ScalarConvert<scalar_t, accscalar_t>::to(rand);
+          scalar_t ret = transform_func(r);
+          auto offset = offset_calc.get(sample_id)[0];
+          scalar_t* out = (scalar_t*)(out_data + offset);
+          *out = ret;
+          sample_id += group_items;
+        }
+      };
+      cgh.parallel_for(
+          DPCPP::nd_range<1>(
+              DPCPP::range<1>(num_groups * group_items),
+              DPCPP::range<1>(group_items)),
+          kfn);
+    };
+    DPCPP_Q_SUBMIT(sycl_queue, cgf);
+  }
 }
 
 template <
