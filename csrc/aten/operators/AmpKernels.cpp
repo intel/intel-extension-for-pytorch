313db2e6ff43ccd07251433bd760607204eb86f9
Yu, Guangye <106960996+guangyey@users.noreply.github.com>
2022-08-31 23:59:43 +0800

    add a new amp op (#1432)
    * add a new amp op
    
    * support bfloat16 dtype
    
    Co-authored-by: Jinghui <jinghui.gu@intel.com>

--- csrc/aten/operators/AmpKernels.cpp
+++ csrc/aten/operators/AmpKernels.cpp
@@ -1,14 +1,173 @@
 #include <ATen/ATen.h>
 #include <core/Memory.h>
 #include <runtime/Utils.h>
-#include "comm/RegistrationDeclarations.h"
+#include "comm/ATDispatch.h"
+#include "comm/Numerics.h"
 
+#include "ForeachFunctors.h"
 #include "Loops.h"
 
 using namespace xpu::dpcpp;
 
 namespace at {
 namespace AtenIpexTypeXPU {
+
+// Single-tensor fallback for _amp_foreach_non_finite_check_and_unscale_dpcpp_.
+// Handles individual tensors that are acceptable to unscale but not MTA-safe.
+void _amp_non_finite_check_and_unscale_dpcpp_(
+    Tensor& scaled_grad,
+    Tensor& found_inf,
+    const Tensor& inv_scale) {
+  // The only way we reach this function is through
+  // _amp_foreach_non_finite_check_and_unscale_dpcpp_, so no input checks.
+
+  // Acts on scaled_grad in place.
+  auto iter = TensorIterator::unary_op(scaled_grad, scaled_grad);
+
+  IPEX_DISPATCH_FLOATING_TYPES_AND2(
+      at::ScalarType::Half,
+      at::ScalarType::BFloat16,
+      iter.dtype(),
+      "_amp_non_finite_check_and_unscale_dpcpp",
+      [&iter, &found_inf, &inv_scale] {
+        auto* found_inf_ptr = found_inf.data_ptr<float>();
+        auto* inv_scale_ptr = inv_scale.data_ptr<float>();
+
+        using opmath_t = at::opmath_type<scalar_t>;
+
+        dpcpp_kernel_for_tensor_iter(
+            iter, [found_inf_ptr, inv_scale_ptr](scalar_t val_in) -> scalar_t {
+              auto val = static_cast<opmath_t>(val_in);
+              if (Numerics<opmath_t>::isinf(val) ||
+                  Numerics<opmath_t>::isnan(val)) {
+                *found_inf_ptr = 1.f;
+              }
+              // Every thread accesses inv_scale, but it will hit in cache.
+              const auto inv_scale_val = *inv_scale_ptr;
+              return static_cast<scalar_t>(
+                  inv_scale_val == 1.f ? val : val * inv_scale_val);
+            });
+      });
+}
+
+// Multiplies each tensor in scaled_grads by inv_scale in-place.
+// If any element of any tensor in scaled_grads is inf or NaN, sets found_inf
+// to 1.0. Uses multi tensor apply (MTA) to process all MTA-safe tensors.
+//
+// Args:
+// scaled_grads:  A TensorList of scaled gradient tensors.  May contain infs or
+// NaNs. found_inf:  A single-element float tensor to which 1.0 will be written
+// if any gradient contain infs/nans.
+//             Pre-zeroing found_inf, if appropriate, is the responsibility of
+//             the caller.
+// inv_scale:  The inverse of the scale factor by which scaled_grads are
+// currently multiplied.
+void _amp_foreach_non_finite_check_and_unscale_(
+    TensorList scaled_grads,
+    Tensor& found_inf,
+    const Tensor& inv_scale) {
+  if (scaled_grads.size() == 0) {
+    return;
+  }
+
+  TORCH_CHECK(inv_scale.is_xpu(), "inv_scale must be a XPU tensor.");
+  TORCH_CHECK(found_inf.is_xpu(), "found_inf must be a XPU tensor.");
+  TORCH_CHECK(inv_scale.numel() == 1, "inv_scale must be a 1-element tensor.");
+  TORCH_CHECK(found_inf.numel() == 1, "found_inf must be a 1-element tensor.");
+  TORCH_CHECK(
+      inv_scale.scalar_type() == at::ScalarType::Float,
+      "inv_scale must be a float tensor.");
+  TORCH_CHECK(
+      found_inf.scalar_type() == at::ScalarType::Float,
+      "found_inf must be a float tensor.");
+
+  // Ensures client code (GradScaler) filtered scaled_grads by dtype.
+  at::native::check_foreach_api_restrictions(scaled_grads);
+
+  std::vector<std::vector<at::Tensor>> tensor_lists;
+
+  // is_non_overlapping_and_dense() is not available in Python.
+  // GradScaler can't filter for it. We need to filter here.
+  if (at::native::can_use_fast_route(scaled_grads)) {
+    // Hopefully common case.
+    // can_use_fast_route is true, which confirms:
+    //  - all scaled_grads are strided
+    //  - all scaled_grads are non overlapping and dense
+    //  - all scaled_grads are on the same device
+    //  - all scaled_grads are of the same dtype
+    TORCH_CHECK(scaled_grads[0].is_xpu(), "scaled_grads must be XPU tensors.");
+    // Sets up MTA launch to use scaled_grads as-is.
+    tensor_lists.emplace_back(scaled_grads.vec());
+  } else {
+    // Hopefully uncommon case.
+    // can_use_fast_route is an all-or-nothing check.  In this path it was
+    // false, so any of the above confirmations could have gone wrong. We filter
+    // MTA-safe tensors into an MTA-able list. If a tensor is acceptable but not
+    // MTA-safe, we fall back to the TensorIterator kernel. If a tensor is
+    // unacceptable, we throw an error to blame GradScaler.
+    tensor_lists.resize(1);
+    tensor_lists[0].reserve(scaled_grads.size());
+    auto expected_device = scaled_grads[0].device();
+    const auto expected_dtype = scaled_grads[0].scalar_type();
+    for (const Tensor& t : scaled_grads) {
+      // Ensures GradScaler filtered scaled_grads by device.
+      TORCH_CHECK(t.is_xpu(), "one of scaled_grads was not a XPU tensor.");
+      TORCH_CHECK(
+          t.device() == expected_device,
+          "scaled_grads must be on the same device.");
+      TORCH_CHECK(
+          t.layout() == at::kStrided,
+          "one of scaled_grads was not a strided tensor.");
+      if (!t.is_non_overlapping_and_dense() ||
+          t.scalar_type() != expected_dtype) {
+        // t is acceptable but not MTA-safe.  Falls back to single-tensor
+        // TensorIterator kernel.
+        _amp_non_finite_check_and_unscale_dpcpp_(
+            const_cast<Tensor&>(t), found_inf, inv_scale);
+      } else {
+        tensor_lists[0].push_back(t);
+      }
+    }
+    if (tensor_lists[0].size() == 0) {
+      return;
+    }
+  }
+
+  IPEX_DISPATCH_FLOATING_TYPES_AND2(
+      at::ScalarType::Half,
+      at::ScalarType::BFloat16,
+      tensor_lists[0][0].scalar_type(),
+      "_amp_foreach_non_finite_check_and_unscale_dpcpp",
+      [&tensor_lists, &found_inf, &inv_scale] {
+        auto* found_inf_ptr = found_inf.data_ptr<float>();
+        auto* inv_scale_ptr = inv_scale.data_ptr<float>();
+
+        using opmath_t = at::opmath_type<scalar_t>;
+
+        // multi_tensor_apply guards onto tensor_lists[0][0], no need to guard
+        // explicitly.
+        multi_tensor_apply<1>(
+            tensor_lists,
+            UnaryOpFunctor<
+                scalar_t,
+                /* depth */ 1,
+                /* r_args_depth */ 1,
+                /* res_arg_index */ 0>(),
+            [found_inf_ptr, inv_scale_ptr](opmath_t val) -> opmath_t {
+              // There is a slight asymmetry here with the TensorIterator kernel
+              // above. MTA Functors ensure val comes in as opmath_t rather than
+              // scalar_t.
+              if (Numerics<opmath_t>::isinf(val) ||
+                  Numerics<opmath_t>::isnan(val)) {
+                *found_inf_ptr = 1.f;
+              }
+              // Every thread accesses inv_scale, but it will hit in cache.
+              const auto inv_scale_val = *inv_scale_ptr;
+              return static_cast<opmath_t>(
+                  inv_scale_val == 1.f ? val : val * inv_scale_val);
+            });
+      });
+}
 
 // _amp_update_scale_kernel is launched with a single work-item to compute the
 // new scale. The scale factor is maintained and updated on the XPU
--- tests/gpu/examples/test_grad_scaling_update_scale.py
+++ tests/gpu/examples/test_grad_scaling_update_scale.py
@@ -1,6 +1,7 @@
 import torch
 import intel_extension_for_pytorch # noqa
-from torch.testing._internal.common_utils import TestCase
+from torch.testing._internal.common_utils import (TestCase,
+                                                  repeat_test_for_types)
 
 
 class TestTorchMethod(TestCase):
@@ -25,3 +26,52 @@ class TestTorchMethod(TestCase):
         torch._amp_update_scale_(scale, growth_tracker, found_inf, growth, backoff, growth_interval)
         self.assertEqual(growth_tracker, 0)
         self.assertEqual(scale, 2.0)
+
+    @repeat_test_for_types([torch.float, torch.half, torch.bfloat16])
+    def test_grad_scaling_unscale(self, dtype=torch.float):
+        inv_scale = torch.full((1,), 0.25, dtype=torch.float, device="xpu")
+        found_inf = torch.full((1,), 0.0, dtype=torch.float, device="xpu")
+
+        size = 10
+        g = torch.full((size, size), 4.0, dtype=dtype, device="xpu")
+        ginf = g.clone()
+        ginf[2, 2] = float('inf')
+        gnan = g.clone()
+        gnan[2, 2] = float('nan')
+
+        # Tries selected combinations of
+        #  - contiguous grads
+        #  - g.clone().t() which is not contiguous but still non overlapping and dense
+        #  - variants of g.clone()[:, :5] which are not non overlapping and dense
+        # Non overlapping and dense grads route into a multi tensor apply kernel,
+        # others use a fallback per-tensor kernel, so we should try both.
+        cases = (
+            ([g.clone(), g.clone()], False),
+            ([g.clone(), g.clone().t()], False),
+            ([g.clone(), g.clone()[:, :5]], False),
+            ([g.clone()[:, :5], g.clone()[:, :5]], False),
+            ([g.clone(), ginf.clone()], True),
+            ([g.clone(), gnan.clone()], True),
+            ([g.clone(), ginf.clone()[:, :5]], True),
+            ([g.clone(), gnan.clone()[:, :5]], True),
+            ([ginf.clone(), g.clone()[:, :5]], True),
+            ([ginf.clone()[:, :5], g.clone()[:, :5]], True),
+        )
+
+        for grads, has_inf in cases:
+            found_inf.zero_()
+            torch._amp_foreach_non_finite_check_and_unscale_(grads, found_inf, inv_scale)
+            if has_inf:
+                self.assertEqual(found_inf, 1.0)
+            else:
+                self.assertEqual(found_inf, 0.0)
+                for grad in grads:
+                    self.assertEqual(grad, torch.ones_like(grad), rtol=1e-5, atol=1e-7)
+
+        # When passing lists with mismatched dtypes to a raw
+        # _amp_foreach_non_finite_check_and_unscale_ call,
+        # it's expected to fall back to single-tensor TensorIterator kernel.
+        grads = [g.clone(), g.to(dtype=torch.float16)]
+        torch._amp_foreach_non_finite_check_and_unscale_(grads, found_inf, inv_scale)
+        for grad in grads:
+            self.assertEqual(grad, torch.ones_like(grad), rtol=1e-5, atol=1e-7)
