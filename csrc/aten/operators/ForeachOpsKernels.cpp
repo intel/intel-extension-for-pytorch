d9c8ef9b6e8556f3f8f5f2d21b714ff9eb30bea3
Jinghui <jinghui.gu@intel.com>
2022-09-07 12:09:59 +0800

    Refine code, correct kernel name (#1459)
    Signed-off-by: Gu, Jinghui <jinghui.gu@intel.com>
    
    Signed-off-by: Gu, Jinghui <jinghui.gu@intel.com>

--- csrc/aten/operators/ForeachOpsKernels.cpp
+++ csrc/aten/operators/ForeachOpsKernels.cpp
@@ -321,7 +321,7 @@ void _foreach_zero_(TensorList tensors) {
   tensor_lists.emplace_back(tensors.vec());
 
   IPEX_DISPATCH_FLOATING_AND_COMPLEX_TYPES_AND1(
-      ScalarType::Half, tensors[0].scalar_type(), "foreach_zero_cuda_", [&]() {
+      ScalarType::Half, tensors[0].scalar_type(), "foreach_zero_xpu_", [&]() {
         multi_tensor_apply<1>(
             tensor_lists,
             ZeroFunctor<
