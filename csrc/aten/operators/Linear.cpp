8e557314685b5c02fb7baf305905142bb76a3c9f
Weishi.Deng <weishi.deng@intel.com>
2022-09-23 13:39:14 +0800

    add op: tensordot (#1497)
    Co-authored-by: Jinghui <jinghui.gu@intel.com>
    Co-authored-by: Kim <baochen.yang@intel.com>

--- csrc/aten/operators/Linear.cpp
+++ csrc/aten/operators/Linear.cpp
@@ -1,4 +1,5 @@
 #include <ATen/ATen.h>
+#include <ATen/WrapDimUtilsMulti.h>
 #include <ATen/record_function.h>
 #include <core/TensorImplUtils.h>
 #include <intrinsic/intrinsic.h>
@@ -120,6 +121,112 @@ Tensor linear_add(
 
   auto output = at::linear(input, weight, bias);
   auto result = at::add(output, accumu, alpha.to<float>());
+  return result;
+}
+
+Tensor tensordot(
+    const Tensor& input1,
+    const Tensor& input2,
+    IntArrayRef dims1,
+    IntArrayRef dims2) {
+  TORCH_CHECK(
+      dims1.size() == dims2.size(),
+      "both dimension lists should have same length");
+  int64_t csize = 1; // total size of the contracted dimensions
+  Tensor t1 = input1;
+  Tensor t2 = input2;
+  for (const auto i : c10::irange(dims1.size())) {
+    int s1 = input1.size(dims1[i]);
+    int s2 = input2.size(dims2[i]);
+    if (s2 == 1) { // broadcasted dimensions can be summed right away
+      t1 = t1.sum(dims1[i], true);
+    } else if (s1 == 1) {
+      t2 = t2.sum(dims2[i], true);
+    } else {
+      TORCH_CHECK(
+          s1 == s2,
+          "contracted dimensions need to match, but first has size ",
+          s1,
+          " in dim ",
+          dims1[i],
+          " and second has size ",
+          s2,
+          " in dim ",
+          dims2[i]);
+      csize *= s1;
+    }
+  }
+  auto cdims1 = at::dim_list_to_bitset(dims1, input1.dim());
+  auto cdims2 = at::dim_list_to_bitset(dims2, input2.dim());
+  std::vector<int64_t> p1, p2,
+      rsizes; // p1, p2: input permutations, rsizes: sizes of the result
+  p1.reserve(input1.dim());
+  p2.reserve(input2.dim());
+  rsizes.reserve(input1.dim() + input2.dim() - (int64_t)dims1.size());
+  int64_t size1 = 1; // number of non-contracted elements in input1
+  int64_t size2 = 1; // number of non-contracted elements in input2
+
+  // fill the permutations and compute sizes
+  for (const auto i : c10::irange(input1.dim())) {
+    if (!cdims1[i]) {
+      p1.emplace_back(i);
+      size1 *= t1.size(i);
+      rsizes.emplace_back(t1.size(i));
+    }
+  }
+  for (const auto x : dims1) {
+    p1.emplace_back(x);
+  }
+  for (const auto x : dims2) {
+    p2.emplace_back(x);
+  }
+  for (const auto i : c10::irange(input2.dim())) {
+    if (!cdims2[i]) {
+      p2.emplace_back(i);
+      size2 *= t2.size(i);
+      rsizes.emplace_back(t2.size(i));
+    }
+  }
+  // permut and reshape for matrix multiplication
+  t1 = t1.permute(p1).reshape({size1, csize});
+  t2 = t2.permute(p2).reshape({csize, size2});
+  // multiply and reshape to target size
+  return at::mm(t1, t2).reshape(rsizes);
+}
+
+Tensor& tensordot_out(
+    const Tensor& input1,
+    const Tensor& input2,
+    IntArrayRef dims1,
+    IntArrayRef dims2,
+    Tensor& result) {
+  Tensor result_tmp = at::tensordot(input1, input2, dims1, dims2);
+  auto result_dtype = result_tmp.scalar_type();
+  auto output_tensor_dtype = result.scalar_type();
+  auto output_device = result.device();
+  auto input1_device = input1.device();
+  auto input2_device = input2.device();
+  // check if the input & output tensors are on the same device.
+  TORCH_CHECK(
+      (output_device == input1_device) && (input1_device == input2_device),
+      "tensordot: Expected the output and input tensors to be on the "
+      "same device, but got the output tensor on ",
+      output_device,
+      ", input tensor a on ",
+      input1_device,
+      ", and input tensor b on ",
+      input2_device);
+  // check if the computed result has the same dtype as the out tensor
+  // (because tensordot does not support type promotion)
+  TORCH_CHECK(
+      result_dtype == output_tensor_dtype,
+      "tensordot",
+      ": Expected the output tensor to have dtype ",
+      result_dtype,
+      ", but got an output tensor with dtype ",
+      output_tensor_dtype);
+  at::native::resize_output(result, result_tmp.sizes());
+  result.copy_(result_tmp);
   return result;
 }
 
--- tests/gpu/examples/test_linalg_eig.py
+++ tests/gpu/examples/test_linalg_eig.py
@@ -2,12 +2,14 @@ import torch
 import intel_extension_for_pytorch # noqa
 from torch.testing._internal.common_utils import TestCase
 import pytest
+import numpy as np
 
 cpu_device = torch.device("cpu")
 dpcpp_device = torch.device("xpu")
 
-@pytest.mark.skipif(not torch.has_mkl, reason="torch build w/o mkl support")
 class TestTorchMethod(TestCase):
+
+    @pytest.mark.skipif(not torch.has_mkl, reason="torch build w/o mkl support")
     def test_linalg_eig(self, dtype=torch.complex128):
         input = torch.randn(2, 2, dtype=torch.complex128)
         input_xpu = input.to(dpcpp_device)
@@ -16,3 +18,33 @@ class TestTorchMethod(TestCase):
         L_xpu, V_xpu = torch.linalg.eig(input_xpu)
         self.assertEqual(L, L_xpu.to(cpu_device))
         self.assertEqual(V, V_xpu.to(cpu_device))
+
+    def test_tensordot(self, device="xpu"):
+        a = torch.arange(60., device=device).reshape(3, 4, 5)
+        b = torch.arange(24., device=device).reshape(4, 3, 2)
+        c = torch.tensordot(a, b, dims=([1, 0], [0, 1])).cpu()
+        cn = torch.from_numpy(np.tensordot(a.cpu().numpy(), b.cpu().numpy(),
+                                           axes=([1, 0], [0, 1])))
+        self.assertEqual(c, cn)
+
+        cout = torch.zeros((5, 2), device=device)
+        torch.tensordot(a, b, dims=([1, 0], [0, 1]), out=cout).cpu()
+        self.assertEqual(c, cout)
+
+        a = torch.randn(2, 3, 4, 5, device=device)
+        b = torch.randn(4, 5, 6, 7, device=device)
+        c = torch.tensordot(a, b, dims=2).cpu()
+        cn = torch.from_numpy(np.tensordot(a.cpu().numpy(), b.cpu().numpy(),
+                                           axes=2))
+
+        with self.assertRaisesRegex(RuntimeError, "expects dims >= 0"):
+            torch.tensordot(a, b, dims=-1)
+
+        self.assertEqual(c, cn)
+        c = torch.tensordot(a, b).cpu()
+        cn = torch.from_numpy(np.tensordot(a.cpu().numpy(), b.cpu().numpy()))
+        self.assertEqual(c, cn)
+
+        a = torch.tensordot(torch.tensor(0.), torch.tensor(0.), 0)
+        an = torch.from_numpy(np.tensordot(np.zeros((), dtype=np.float32), np.zeros((), dtype=np.float32), 0))
+        self.assertEqual(a, an)
