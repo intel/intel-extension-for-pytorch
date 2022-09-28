720ac6fd137e61e3b4ae7c6d94b226c88088f05f
min-jean-cho <min.jean.cho@intel.com>
2022-09-28 08:56:11 -0700

    Minjean/hypertune (#768)
    * hypertune initial commit
    
    * Delete .nfs00000000fd2dbd3000000288
    
    * Delete .nfs00000000fd067e1f00000282
    
    * minor update
    
    * cleaned up code
    
    * added ipex model hp support
    
    * updated readme
    
    * updated readme
    
    * added examples
    
    * delete example.yaml
    
    * delete record.csv
    
    * updated readme
    
    * updated readme and examples
    
    * update
    
    * Delete intel_extension_for_pytorch/cpu/hypertune/example directory
    
    * Delete intel_extension_for_pytorch/cpu/hypertune/tests directory
    
    * undo launcher changes
    
    * undo launcher changers
    
    * update hypertune readme
    
    * update hypertune readme
    
    * added example
    
    * update hypertune readme
    
    * make hypertune a module
    
    * update readme
    
    * simpler option to ncore_per_instance
    
    * Delete intel_extension_for_pytorch/cpu/hypertune/utils directory
    
    * change variable name
    
    * minor change
    
    * update readme
    
    * make launcher a module and hypertune a submodule of launcher
    
    * make hypertune submodule of launcher
    
    * make hypertune submodule of launcher
    
    * remove space
    
    * update readme
    
    * update example yaml
    
    * clean up code
    
    * clean up
    
    * clean up
    
    * clean up
    
    * update readme
    
    * move hypertune to intel_extension_for_pytorch.cpu.hypertune
    
    * remove lines
    
    * update README (1) add strategy img (2) update to intel_extension_for_pytorch.cpu.hypertune
    
    Co-authored-by: min-jean-cho <minjeanc@mlp-prod-skx-7825.ra.intel.com>

---
+++ intel_extension_for_pytorch/cpu/hypertune/strategy/__init__.py
@@ -0,0 +1,11 @@
+import glob
+from os.path import dirname, basename, isfile, join
+from .strategy import STRATEGIES
+
+modules = glob.glob(join(dirname(__file__), "*.py"))
+
+for f in modules:
+    if isfile(f) and not f.startswith('__') and not f.endswith('__init__.py'):
+        __import__(basename(f)[:-3], globals(), locals(), level=1)
+
+__all__ = ["STRATEGIES"]
---
+++ intel_extension_for_pytorch/cpu/hypertune/strategy/random.py
@@ -0,0 +1,22 @@
+import itertools 
+import numpy as np 
+from .strategy import strategy_registry, TuneStrategy
+
+@strategy_registry
+class RandomTuneStrategy(TuneStrategy):
+    def __init__(self, conf):
+        super().__init__(conf)
+        self.combinations = list(itertools.product(*(self.hyperparam2searchspace[hp] for hp in self.hyperparams)))
+        self.total_idx = set([i for i in range(len(self.combinations))])
+        self.record_idx = set()
+        
+    def next_tune_cfg(self):
+        while len(self.total_idx) > 0:
+            idx = np.random.choice(list(self.total_idx))
+            self.record_idx.add(idx)
+            self.total_idx = self.total_idx - self.record_idx 
+            
+            cfg = self.combinations[idx]
+            tune_cfg = dict(zip(self.hyperparams, cfg))
+            yield tune_cfg 
+        return 
