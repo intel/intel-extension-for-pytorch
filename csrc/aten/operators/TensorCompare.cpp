139466aa080e5f923524d83964ca893480003852
Weishi.Deng <weishi.deng@intel.com>
2022-09-30 13:50:46 +0800

    isin.Tensor_Scalar_out, isin.Scalar_Tensor_out, isin.Tensor_Tensor_out (#1401)
    * add ops: isin
    
    Co-authored-by: Jinghui <jinghui.gu@intel.com>
    Co-authored-by: Ye Ting <ting.ye@intel.com>
    Co-authored-by: Kim <baochen.yang@intel.com>

--- csrc/aten/operators/TensorCompare.cpp
+++ csrc/aten/operators/TensorCompare.cpp
@@ -2,11 +2,18 @@
 #include <c10/macros/Macros.h>
 #include "comm/AccumulateType.h"
 
+#include <ATen/ExpandUtils.h>
+#include <ATen/Functions.h>
+#include <ATen/native/TensorIterator.h>
+#include <ATen/quantized/QTensorImpl.h>
 #include <core/Memory.h>
+#include <core/TensorImplUtils.h>
 #include <runtime/Utils.h>
+#include "PSTLFunctions.h"
 #include "comm/ApplyUtils.h"
 #include "comm/Numerics.h"
 #include "comm/RegistrationDeclarations.h"
+#include "comm/ScalarOps.h"
 
 #include "comm/ATDispatch.h"
 
@@ -33,6 +40,21 @@ void where_kernel(TensorIterator& iter, ScalarType condition_type) {
                   -> scalar_t { return cond_val ? self_val : other_val; });
         }
       });
+}
+
+// Composite op implementation for simplicity. This materializes the cross
+// product of elements and test elements, so it is not very memory efficient
+void isin_default_kernel(
+    const Tensor& elements,
+    const Tensor& test_elements,
+    bool invert,
+    Tensor& out) {
+  out.resize_as_(elements);
+  std::vector<int64_t> bc_shape(elements.dim(), 1);
+  bc_shape.push_back(-1);
+  out.copy_(
+      invert ? elements.unsqueeze(-1).ne(test_elements.view(bc_shape)).all(-1)
+             : elements.unsqueeze(-1).eq(test_elements.view(bc_shape)).any(-1));
 }
 
 } // namespace impl
@@ -152,6 +174,155 @@ Tensor& isposinf_out(const Tensor& self, Tensor& out) {
           });
         });
   }
+  return out;
+}
+
+static inline void check_for_unsupported_isin_dtype(const ScalarType type) {
+  // Bail out for dtypes unsupported by the sorting algorithm to keep the
+  // interface consistent.
+  TORCH_CHECK(
+      type != ScalarType::Bool && type != ScalarType::BFloat16 &&
+          type != ScalarType::ComplexFloat && type != ScalarType::ComplexDouble,
+      "Unsupported input type encountered for isin(): ",
+      type);
+}
+
+// Sorting-based algorithm for isin(); used when the number of test elements is
+// large.
+static void isin_sorting(
+    const Tensor& elements,
+    const Tensor& test_elements,
+    bool assume_unique,
+    bool invert,
+    Tensor& out) {
+  // 1. Concatenate unique elements with unique test elements in 1D form. If
+  //    assume_unique is true, skip calls to unique().
+  Tensor elements_flat, test_elements_flat, unique_order;
+  if (assume_unique) {
+    elements_flat = elements.ravel();
+    test_elements_flat = test_elements.ravel();
+  } else {
+    std::tie(elements_flat, unique_order) = at::_unique(elements, false, true);
+    std::tie(test_elements_flat, std::ignore) =
+        at::_unique(test_elements, false);
+  }
+
+  // 2. Stable sort all elements, maintaining order indices to reverse the
+  //    operation. Stable sort is necessary to keep elements before test
+  //    elements within the sorted list.
+  Tensor all_elements = at::_cat({elements_flat, test_elements_flat});
+  // use pstl sort here, equals to "all_elements.sort(true, 0, false)"
+  auto index_options = all_elements.options().dtype(kLong);
+  Tensor sorted_elements = all_elements.clone().reshape(-1);
+  int64_t num_inp = sorted_elements.numel();
+  Tensor sorted_order = at::empty({num_inp}, index_options);
+  auto sorted_indices_begin = sorted_order.data_ptr<int64_t>();
+  xpu::pstl::iota(
+      sorted_indices_begin, sorted_indices_begin + num_inp, (int64_t)0);
+  auto scalar_t = all_elements.scalar_type();
+
+  if (num_inp > 0) {
+    IPEX_DISPATCH_ALL_TYPES_AND2(
+        at::ScalarType::Half,
+        at::ScalarType::BFloat16,
+        all_elements.scalar_type(),
+        "isin_sort",
+        [&]() {
+          xpu::pstl::sort<scalar_t, int64_t>(
+              all_elements.data_ptr<scalar_t>(),
+              sorted_elements.data_ptr<scalar_t>(),
+              sorted_order.data_ptr<int64_t>(),
+              num_inp,
+              false);
+        });
+  }
+
+  // 3. Create a mask for locations of adjacent duplicate values within the
+  //    sorted list. Duplicate values are in both elements and test elements.
+  Tensor duplicate_mask =
+      at::empty_like(sorted_elements, TensorOptions(ScalarType::Bool));
+  Tensor sorted_except_first = sorted_elements.slice(0, 1, at::indexing::None);
+  Tensor sorted_except_last = sorted_elements.slice(0, 0, -1);
+  duplicate_mask.slice(0, 0, -1).copy_(
+      invert ? sorted_except_first.ne(sorted_except_last)
+             : sorted_except_first.eq(sorted_except_last));
+  duplicate_mask.index_put_({-1}, invert);
+
+  // 4. Reorder the mask to match the pre-sorted element order.
+  Tensor mask = at::empty_like(duplicate_mask);
+  mask.index_copy_(0, sorted_order, duplicate_mask);
+
+  // 5. Index the mask to match the pre-unique element order. If
+  //    assume_unique is true, just take the first N items of the mask,
+  //    where N is the original number of elements.
+  if (assume_unique) {
+    out.copy_(mask.slice(0, 0, elements.numel()).view_as(out));
+  } else {
+    out.copy_(at::index(mask, {c10::optional<Tensor>(unique_order)}));
+  }
+}
+
+Tensor& isin_out(
+    const Tensor& elements,
+    const Tensor& test_elements,
+    bool assume_unique,
+    bool invert,
+    Tensor& out) {
+  check_for_unsupported_isin_dtype(elements.scalar_type());
+  check_for_unsupported_isin_dtype(test_elements.scalar_type());
+  if (elements.numel() == 0) {
+    return out;
+  }
+
+  // Heuristic taken from numpy's implementation.
+  // See
+  // https://github.com/numpy/numpy/blob/fb215c76967739268de71aa4bda55dd1b062bc2e/numpy/lib/arraysetops.py#L575
+  if (test_elements.numel() <
+      static_cast<int64_t>(
+          10.0f *
+          Numerics<double>::pow(
+              static_cast<double>(elements.numel()), 0.145))) {
+    out.fill_(invert);
+    impl::isin_default_kernel(elements, test_elements, invert, out);
+  } else {
+    isin_sorting(elements, test_elements, assume_unique, invert, out);
+  }
+  return out;
+}
+
+Tensor& isin_out(
+    const Tensor& elements,
+    const Scalar& test_elements,
+    bool assume_unique,
+    bool invert,
+    Tensor& out) {
+  check_for_unsupported_isin_dtype(elements.scalar_type());
+  check_for_unsupported_isin_dtype(test_elements.type());
+
+  // redispatch to eq / ne
+  if (invert) {
+    at::AtenIpexTypeXPU::ne_out(elements, test_elements, out);
+  } else {
+    at::AtenIpexTypeXPU::eq_out(elements, test_elements, out);
+  }
+  return out;
+}
+
+Tensor& isin_out(
+    const Scalar& elements,
+    const Tensor& test_elements,
+    bool assume_unique,
+    bool invert,
+    Tensor& out) {
+  check_for_unsupported_isin_dtype(elements.type());
+  check_for_unsupported_isin_dtype(test_elements.scalar_type());
+  // redispatch
+  at::AtenIpexTypeXPU::isin_out(
+      wrapped_scalar_tensor(elements, test_elements.device()),
+      test_elements,
+      assume_unique,
+      invert,
+      out);
   return out;
 }
 
---
+++ tests/gpu/examples/test_isin.py
@@ -0,0 +1,113 @@
+import torch
+import intel_extension_for_pytorch # noqa
+from torch.testing._internal.common_utils import TestCase
+from torch.testing._internal.common_dtype import (
+    all_types, all_types_and
+)
+from itertools import product
+import numpy as np
+
+class TestTorchMethod(TestCase):
+    def test_isin(self, device="xpu", dtype=torch.float):
+        def assert_isin_equal(a, b):
+            # Compare to the numpy reference implementation.
+            x = torch.isin(a, b)
+            a = a.cpu().numpy() if torch.is_tensor(a) else np.array(a)
+            b = b.cpu().numpy() if torch.is_tensor(b) else np.array(b)
+            y = np.isin(a, b)
+            self.assertEqual(x, y)
+
+        # multi-dim tensor, multi-dim tensor
+        a = torch.arange(24, device=device, dtype=dtype).reshape([2, 3, 4])
+        b = torch.tensor([[10, 20, 30], [0, 1, 3], [11, 22, 33]], device=device, dtype=dtype)
+        assert_isin_equal(a, b)
+
+        # zero-dim tensor
+        zero_d = torch.tensor(3, device=device, dtype=dtype)
+        assert_isin_equal(zero_d, b)
+        assert_isin_equal(a, zero_d)
+        assert_isin_equal(zero_d, zero_d)
+
+        # empty tensor
+        empty = torch.tensor([], device=device, dtype=dtype)
+        assert_isin_equal(empty, b)
+        assert_isin_equal(a, empty)
+        assert_isin_equal(empty, empty)
+
+        # scalar
+        assert_isin_equal(a, 6)
+        assert_isin_equal(5, b)
+
+        def define_expected(lst, invert=False):
+            expected = torch.tensor(lst, device=device)
+            if invert:
+                expected = expected.logical_not()
+            return expected
+
+        # Adapted from numpy's in1d tests
+        for mult in [1, 10]:
+            for invert in [False, True]:
+                a = torch.tensor([5, 7, 1, 2], device=device, dtype=dtype)
+                b = torch.tensor([2, 4, 3, 1, 5] * mult, device=device, dtype=dtype)
+                ec = define_expected([True, False, True, True], invert=invert)
+                c = torch.isin(a, b, assume_unique=True, invert=invert)
+                self.assertEqual(c, ec)
+
+                a[0] = 8
+                ec = define_expected([False, False, True, True], invert=invert)
+                c = torch.isin(a, b, assume_unique=True, invert=invert)
+                self.assertEqual(c, ec)
+
+                a[0], a[3] = 4, 8
+                ec = define_expected([True, False, True, False], invert=invert)
+                c = torch.isin(a, b, assume_unique=True, invert=invert)
+                self.assertEqual(c, ec)
+
+                a = torch.tensor([5, 4, 5, 3, 4, 4, 3, 4, 3, 5, 2, 1, 5, 5], device=device, dtype=dtype)
+                b = torch.tensor([2, 3, 4] * mult, device=device, dtype=dtype)
+                ec = define_expected([False, True, False, True, True, True, True, True, True,
+                                      False, True, False, False, False], invert=invert)
+                c = torch.isin(a, b, invert=invert)
+                self.assertEqual(c, ec)
+
+                b = torch.tensor([2, 3, 4] * mult + [5, 5, 4] * mult, device=device, dtype=dtype)
+                ec = define_expected([True, True, True, True, True, True, True, True, True, True,
+                                      True, False, True, True], invert=invert)
+                c = torch.isin(a, b, invert=invert)
+                self.assertEqual(c, ec)
+
+                a = torch.tensor([5, 7, 1, 2], device=device, dtype=dtype)
+                b = torch.tensor([2, 4, 3, 1, 5] * mult, device=device, dtype=dtype)
+                ec = define_expected([True, False, True, True], invert=invert)
+                c = torch.isin(a, b, invert=invert)
+                self.assertEqual(c, ec)
+
+                # multi-dimensional input case using sort-based algo
+                for assume_unique in [False, True]:
+                    a = torch.arange(6, device=device, dtype=dtype).reshape([2, 3])
+                    b = torch.arange(3, 30, device=device, dtype=dtype)
+                    ec = define_expected([[False, False, False], [True, True, True]], invert=invert)
+                    c = torch.isin(a, b, invert=invert, assume_unique=assume_unique)
+                    self.assertEqual(c, ec)
+
+    def test_isin_different_dtypes(self, device="xpu"):
+        supported_types = all_types() if device == 'cpu' else all_types_and(torch.half)
+        for mult in [1, 10]:
+            for assume_unique in [False, True]:
+                for dtype1, dtype2 in product(supported_types, supported_types):
+                    a = torch.tensor([1, 2, 3], device=device, dtype=dtype1)
+                    b = torch.tensor([3, 4, 5] * mult, device=device, dtype=dtype2)
+                    ec = torch.tensor([False, False, True], device=device)
+                    c = torch.isin(a, b, assume_unique=assume_unique)
+                    self.assertEqual(c, ec)
+
+    def test_isin_different_devices(self, device="xpu", dtype=torch.float):
+        a = torch.arange(6, device=device, dtype=dtype).reshape([2, 3])
+        b = torch.arange(3, 30, device='cpu', dtype=dtype)
+        with self.assertRaises(RuntimeError):
+            torch.isin(a, b)
+
+        c = torch.arange(6, device='cpu', dtype=dtype).reshape([2, 3])
+        d = torch.arange(3, 30, device=device, dtype=dtype)
+        with self.assertRaises(RuntimeError):
+            torch.isin(c, d)
