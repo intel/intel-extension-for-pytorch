1aa97ca28738aae30d374af9d326145272d0b651
Jyothis V James - Intel <jyothisx.v.james@intel.com>
2022-10-12 17:55:57 +0530

    Fix typo in _tensor_method.py

--- intel_extension_for_pytorch/nn/functional/_tensor_method.py
+++ intel_extension_for_pytorch/nn/functional/_tensor_method.py
@@ -3,7 +3,7 @@ import warnings
 
 def _numpy(x):
     if x.dtype==torch.bfloat16:
-        warnings.warn("calling in ipex numpy which is not share mermory with torch tensor for bfloat16 input.")
+        warnings.warn("calling in ipex numpy which is not share memory with torch tensor for bfloat16 input.")
         return torch._C._TensorBase.numpy(x.float())
     else:
         return torch._C._TensorBase.numpy(x)
