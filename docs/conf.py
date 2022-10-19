b86fb88855ea1e93e9d1d85ade57b57096220c19
Ye Ting <ting.ye@intel.com>
2022-10-19 20:53:11 +0800

    [doc] Add make files for Sphinx documentation (covering python and C++) (#1635)
    * add docs directory for tutorials
    * Clean up contents for CPU only
    * add automatic cpp api generation feature
    
    
    Co-authored-by: Xu, Jing <jing.xu@intel.com>
    Co-authored-by: mypintel <yuanpeng.mao@intel.com>

---
+++ docs/conf.py
@@ -0,0 +1,108 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+import os
+import sys
+import intel_extension_for_pytorch as ipex
+sys.path.insert(0, os.path.abspath('.'))
+
+
+# -- Project information -----------------------------------------------------
+
+project = 'intel_extension_for_pytorch'
+copyright = 'Intel(R)'
+author = ''
+
+# The full version, including alpha/beta/rc tags
+version = ipex.__version__
+release = ipex.__version__
+
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = ['sphinx.ext.githubpages',
+              'sphinx.ext.autodoc',
+              'sphinx.ext.napoleon',
+              'recommonmark',
+              'sphinx_markdown_tables',
+              'sphinx_md',
+              'sphinxemoji.sphinxemoji',
+              'breathe',
+]
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+
+# The suffix(es) of source filenames.
+# You can specify multiple suffix as a list of string:
+#
+source_suffix = ['.rst', '.md']
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+# Configuration of breathe
+breathe_projects = { "ipexcpp": "./xml/" }
+breathe_default_project = "ipexcpp"
+
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+#html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
+html_theme_options = {
+    'logo_only': False,
+    'display_version': True,
+    'prev_next_buttons_location': 'bottom',
+    'style_external_links': False,
+    'vcs_pageview_mode': '',
+    #'style_nav_header_background': 'white',
+    # Toc options
+    'collapse_navigation': True,
+    'sticky_navigation': True,
+    'navigation_depth': -1,
+    'includehidden': True,
+    'titles_only': False
+}
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named 'default.css' will overwrite the builtin 'default.css'.
+html_static_path = ['_static']
+
+def setup(app):
+   app.add_css_file("custom.css")
+
+from os import getenv
+
+sphinx_md_useGitHubURL = True
+baseBranch = "main"
+commitSHA = getenv('GITHUB_SHA')
+githubBaseURL = 'https://github.com/' + (getenv('GITHUB_REPOSITORY') or 'intel/intel-extension-for-pytorch') + '/'
+githubFileURL = githubBaseURL + "blob/"
+githubDirURL = githubBaseURL + "tree/"
+if commitSHA:
+    githubFileURL = githubFileURL + commitSHA + "/"
+    githubDirURL = githubDirURL + commitSHA + "/"
+else:
+    githubFileURL = githubFileURL + baseBranch + "/"
+    githubDirURL = githubDirURL + baseBranch + "/"
+sphinx_md_githubFileURL = githubFileURL
+sphinx_md_githubDirURL = githubDirURL
