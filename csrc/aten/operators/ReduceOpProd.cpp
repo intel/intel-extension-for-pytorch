3443494ab17d07d0a32f2e61ee795ac6fad68bb7
Stonepia <tong.su@intel.com>
2022-11-02 09:48:01 +0800

    add bool to prod (#1751)

--- csrc/aten/operators/ReduceOpProd.cpp
+++ csrc/aten/operators/ReduceOpProd.cpp
@@ -40,7 +40,8 @@ void prod_kernel_impl(TensorIterator& iter) {
 }
 
 static void prod_kernel(TensorIterator& iter) {
-  IPEX_DISPATCH_ALL_TYPES_AND_COMPLEX_AND2(
+  IPEX_DISPATCH_ALL_TYPES_AND_COMPLEX_AND3(
+      at::ScalarType::Bool,
       at::ScalarType::Half,
       at::ScalarType::BFloat16,
       iter.dtype(),
