ef798a213c1c9e74adb9bc7442186477bf8c354c
Zhiwei <zhiwei.yan@intel.com>
2022-11-19 22:30:16 +0800

    Add scalar dispatching entry for binary op mul (#1900)
    Co-authored-by: Jinghui <jinghui.gu@intel.com>

--- csrc/gpu/aten/operators/BinaryOps_mul.cpp
+++ csrc/gpu/aten/operators/BinaryOps_mul.cpp
@@ -47,5 +47,13 @@ Tensor& mul_(Tensor& self, const Tensor& other) {
   return at::AtenIpexTypeXPU::mul_out(self, other, self);
 }
 
+Tensor mul(const Tensor& self, const Scalar& other) {
+  return at::AtenIpexTypeXPU::mul(self, wrapped_scalar_tensor(other));
+}
+
+Tensor& mul_(Tensor& self, const Scalar& other) {
+  return at::AtenIpexTypeXPU::mul_out(self, wrapped_scalar_tensor(other), self);
+}
+
 } // namespace AtenIpexTypeXPU
 } // namespace at
