140e3a3a4e656b83d8680fd16adcf763f81cc2e1
Jinghui <jinghui.gu@intel.com>
2022-11-21 20:52:52 +0800

    Enable cases for onednn layout (#1914)
    Signed-off-by: Gu, Jinghui <jinghui.gu@intel.com>

--- tests/gpu/examples/test_inplace_binary_and_relu.py
+++ tests/gpu/examples/test_inplace_binary_and_relu.py
@@ -18,28 +18,28 @@ cpu_device = torch.device("cpu")
 
 class TestTorchMethod(TestCase):
 
-    @pytest.mark.skipif("not torch.xpu.using_onednn_layout()", reason="only for block format")
     def test_inplace_binary_and_relu(self, dtype=torch.float):
-        input_cpu = torch.randn([32, 64, 300, 300])
+        with torch.xpu.onednn_layout():
+            input_cpu = torch.randn([32, 64, 300, 300])
 
-        conv_cpu = nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1, bias=False)
-        relu_cpu = nn.ReLU(inplace=True)
+            conv_cpu = nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1, bias=False)
+            relu_cpu = nn.ReLU(inplace=True)
 
-        out_cpu = conv_cpu(input_cpu)
-        out_cpu += input_cpu
-        out_cpu = relu_cpu(out_cpu)
-        out_cpu_shape = out_cpu.shape
-        print("\n")
-        print(out_cpu_shape)
+            out_cpu = conv_cpu(input_cpu)
+            out_cpu += input_cpu
+            out_cpu = relu_cpu(out_cpu)
+            out_cpu_shape = out_cpu.shape
+            print("\n")
+            print(out_cpu_shape)
 
-        input_dpcpp = input_cpu.to(dpcpp_device)
+            input_dpcpp = input_cpu.to(dpcpp_device)
 
-        conv_dpcpp = conv_cpu.to(dpcpp_device)
-        relu_dpcpp = relu_cpu.to(dpcpp_device)
+            conv_dpcpp = conv_cpu.to(dpcpp_device)
+            relu_dpcpp = relu_cpu.to(dpcpp_device)
 
-        out_dpcpp = conv_dpcpp(input_dpcpp)
-        out_dpcpp += input_dpcpp
-        out_dpcpp = relu_dpcpp(out_dpcpp)
-        out_dpcpp_shape = out_dpcpp.shape
-        print(out_dpcpp_shape)
-        self.assertEqual(out_cpu_shape, out_dpcpp_shape)
+            out_dpcpp = conv_dpcpp(input_dpcpp)
+            out_dpcpp += input_dpcpp
+            out_dpcpp = relu_dpcpp(out_dpcpp)
+            out_dpcpp_shape = out_dpcpp.shape
+            print(out_dpcpp_shape)
+            self.assertEqual(out_cpu_shape, out_dpcpp_shape)
--- tests/gpu/examples/test_reshape.py
+++ tests/gpu/examples/test_reshape.py
@@ -7,7 +7,6 @@ import pytest
 
 class TestTorchMethod(TestCase):
 
-    @pytest.mark.skipif("not torch.xpu.using_onednn_layout()", reason="only for block format")
     def test_reshape_cat(self, dtype=torch.float):
         """
         Issue: When run SSD-ResNet50 with block format, it raised a shape mismatch error in cat op.
@@ -16,16 +15,17 @@ class TestTorchMethod(TestCase):
         It led to the shape mismatch error in the next op cat.
         Fixed PR: https://github.com/intel-innersource/frameworks.ai.pytorch.ipex-gpu/pull/709
         """
-        input1 = torch.randn([32, 3, 300, 300]).to("xpu")
-        conv1 = nn.Conv2d(3, 3, kernel_size=3, stride=1, padding=1, bias=False).to("xpu")
-        conv_out1 = conv1(input1)
-        reshape_out1 = conv_out1.reshape(1, 3, 300, 300, 32)
+        with torch.xpu.onednn_layout():
+            input1 = torch.randn([32, 3, 300, 300]).to("xpu")
+            conv1 = nn.Conv2d(3, 3, kernel_size=3, stride=1, padding=1, bias=False).to("xpu")
+            conv_out1 = conv1(input1)
+            reshape_out1 = conv_out1.reshape(1, 3, 300, 300, 32)
 
-        input2 = torch.randn([8, 3, 300, 300]).to("xpu")
-        conv2 = nn.Conv2d(3, 3, kernel_size=3, stride=1, padding=1, bias=False).to("xpu")
-        conv_out2 = conv2(input2)
-        reshape_out2 = conv_out2.reshape(1, 3, 300, 300, 8)
+            input2 = torch.randn([8, 3, 300, 300]).to("xpu")
+            conv2 = nn.Conv2d(3, 3, kernel_size=3, stride=1, padding=1, bias=False).to("xpu")
+            conv_out2 = conv2(input2)
+            reshape_out2 = conv_out2.reshape(1, 3, 300, 300, 8)
 
-        cat_out = torch.cat((reshape_out1, reshape_out2), -1)
+            cat_out = torch.cat((reshape_out1, reshape_out2), -1)
 
-        self.assertEqual(cat_out.shape, torch.Size([1, 3, 300, 300, 40]))
+            self.assertEqual(cat_out.shape, torch.Size([1, 3, 300, 300, 40]))
