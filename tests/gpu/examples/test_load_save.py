b2a45f93e67a3cf0e2103a4918baeda3d753bd03
Yu, Guangye <106960996+guangyey@users.noreply.github.com>
2022-11-26 00:10:27 +0800

    add load and save UT (#1945)

--- tests/gpu/examples/test_load_save.py
+++ tests/gpu/examples/test_load_save.py
@@ -1,4 +1,5 @@
 import torch
+import torch.nn as nn
 from torch.testing._internal.common_utils import TestCase
 
 import intel_extension_for_pytorch # noqa
@@ -8,15 +9,32 @@ cpu_device = torch.device("cpu")
 xpu_device = torch.device("xpu")
 
 
+class TheModelClass(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(TheModelClass, self).__init__()
+        self.linear = nn.Linear(in_channels, out_channels, bias=True)
+
+    def forward(self, x):
+        x = torch.log(self.linear(x))
+        return x
+
 class TestTorchMethod(TestCase):
     def test_load_save(self, dtype=torch.float):
-        save = torch.randn(2, 2)
-        print("save: ", save)
-        save = save.to('xpu')
+        save = torch.randn((2, 2), device='xpu', dtype=dtype)
         ckpt = tempfile.NamedTemporaryFile()
         torch.save(save, ckpt.name)
-
         load = torch.load(ckpt.name, map_location=xpu_device)
-        print("torch.load(ckpt.name, map_location=xpu_device): ", load.to(cpu_device))
-
         self.assertEqual(save.to(cpu_device), load.to(cpu_device))
+
+        torch.save(save, ckpt.name, _use_new_zipfile_serialization=False)
+        load = torch.load(ckpt.name, map_location=xpu_device)
+        self.assertEqual(save.to(cpu_device), load.to(cpu_device))
+
+        save = TheModelClass(2, 3).to('xpu')
+        torch.save(save, ckpt.name)
+        load = torch.load(ckpt.name, map_location=xpu_device)
+        self.assertEqual(save.to(cpu_device).__str__(), load.to(cpu_device).__str__())
+
+        torch.save(save, ckpt.name, _use_new_zipfile_serialization=False)
+        load = torch.load(ckpt.name, map_location=xpu_device)
+        self.assertEqual(save.to(cpu_device).__str__(), load.to(cpu_device).__str__())
