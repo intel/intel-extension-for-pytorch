27423ef398c6145a74f049f8a671ced1cbc1477e
Zhiwei <zhiwei.yan@intel.com>
2022-11-29 12:42:54 +0800

    Im2col 1.13 dispatching (#1961)

--- csrc/gpu/aten/operators/Im2Col.cpp
+++ csrc/gpu/aten/operators/Im2Col.cpp
@@ -97,8 +97,8 @@ static void im2col_out_template(
   output.resize_({batch_size, n_output_plane, output_length});
   output.zero_();
 
-  IPEX_DISPATCH_FLOATING_TYPES_AND_HALF(
-      input.scalar_type(), "im2col_out_dpcpp", [&] {
+  IPEX_DISPATCH_FLOATING_AND_COMPLEX_TYPES_AND2(
+      kHalf, kBFloat16, input.scalar_type(), "im2col_out_dpcpp", [&] {
         Tensor input_n;
         Tensor output_n;
 
