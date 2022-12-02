159b24501868ba663ce518abae89862266849df7
Zhiwei <zhiwei.yan@intel.com>
2022-12-02 23:50:12 +0800

    Add graph-level device check in jit (#1976)

--- csrc/jit/initialization.cpp
+++ csrc/jit/initialization.cpp
@@ -1,7 +1,10 @@
 #include "initialization.h"
+#include <ATen/core/jit_type.h>
 #include <stdio.h>
+#include <torch/csrc/jit/ir/ir.h>
 #include <torch/csrc/jit/passes/autocast.h>
 #include <torch/csrc/jit/passes/pass_manager.h>
+#include <torch/csrc/jit/runtime/graph_iterator.h>
 #include <torch/torch.h>
 #include <regex>
 #include <string>
@@ -11,11 +14,51 @@
 
 namespace torch_ipex {
 
+bool isXPUValue(torch::jit::Value* v) {
+  // it is not a tensor type
+  if (!v->type()->isSubtypeOf(c10::TensorType::get())) {
+    return false;
+  }
+
+  auto device = v->type()->expectRef<c10::TensorType>().device();
+
+  if (!device) {
+    return false; // this tensor has not device info
+  }
+  return (device->is_xpu() ? true : false);
+}
+
+bool isXPUNode(torch::jit::Node* node) {
+  bool is_xpu = false;
+  for (const auto& output : node->outputs()) {
+    is_xpu = is_xpu || isXPUValue(output);
+    if (is_xpu)
+      return true;
+  }
+  for (const auto& input : node->inputs()) {
+    is_xpu = is_xpu || isXPUValue(input);
+    if (is_xpu)
+      return true;
+  }
+  return false;
+}
+
+bool hasXPUNodes(std::shared_ptr<torch::jit::Graph>& graph) {
+  torch::jit::DepthFirstGraphNodeIterator it(graph);
+  for (auto* node = it.next(); node != nullptr; node = it.next()) {
+    if (isXPUNode(node))
+      return true;
+  }
+  return false;
+}
+
 void init_jit_fusion_pass() {
   // jit fusion pass
   torch::jit::registerPrePass([](std::shared_ptr<torch::jit::Graph>& g) {
-    if (AutoOptConfig::singleton().get_jit_fuse()) {
-      torch_ipex::jit::FusionPass(g);
+    if (torch::jit::getProfilingMode() && (!hasXPUNodes(g))) {
+      if (AutoOptConfig::singleton().get_jit_fuse()) {
+        torch_ipex::jit::FusionPass(g);
+      }
     }
   });
 }
