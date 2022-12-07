c19daf8531200726bfaa14541766d2f13592a11a
Yu, Guangye <106960996+guangyey@users.noreply.github.com>
2022-12-07 21:29:20 +0800

    fix header file (#2000)
    * fix header file
    
    
    Co-authored-by: Jinghui <jinghui.gu@intel.com>

--- csrc/gpu/include/xpu/Profiler.h
+++ csrc/gpu/include/xpu/Profiler.h
@@ -18,7 +18,13 @@
 
 #pragma once
 
+#if __has_include(<sycl/sycl.hpp>)
+#include <sycl/sycl.hpp>
+#elif __has_include(<CL/sycl.hpp>)
 #include <CL/sycl.hpp>
+#else
+#error "Unsupported compiler"
+#endif
 #include <c10/core/Device.h>
 #include "Macros.h"
 
--- tests/gpu/standalone/fft_standalone.cpp
+++ tests/gpu/standalone/fft_standalone.cpp
@@ -13,7 +13,13 @@
  *******************************************************************************/
 
 #define _USE_MATH_DEFINES
+#if __has_include(<sycl/sycl.hpp>)
+#include <sycl/sycl.hpp>
+#elif __has_include(<CL/sycl.hpp>)
 #include <CL/sycl.hpp>
+#else
+#error "Unsupported compiler"
+#endif
 #include <oneapi/mkl/dfti.hpp>
 #include <cmath>
 #include <iostream>
