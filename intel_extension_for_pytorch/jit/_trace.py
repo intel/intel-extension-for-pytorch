5012903ed7fbeafb3599a0f2e423faa7e555dd18
zejun <zejun.chen@intel.com>
2022-12-15 09:27:31 +0800

    Add the torch.xpu.has_2d_block_array judgement after the example input (#2044)
    device checking for cpu only
    
    Signed-off-by: Chen, Zejun <zejun.chen@intel.com>
    Co-authored-by: Jinghui <jinghui.gu@intel.com>

--- intel_extension_for_pytorch/jit/_trace.py
+++ intel_extension_for_pytorch/jit/_trace.py
@@ -7,39 +7,40 @@ from functools import wraps
 
 
 def need_to_disable_check_trace_for_XPU(*args, **kwargs):
-    if not torch.xpu.has_2d_block_array():
-        device_type_list = []
+    device_type_list = []
 
-        def check_input_tensor(arg):
-            for elm in arg:
-                if isinstance(elm, torch.Tensor):
-                    device_type_list.append(elm.device.type)
-                else:
-                    check_input_tensor(elm)
-
-        for arg in args:
-            if isinstance(arg, torch.Tensor):
-                device_type_list.append(arg.device.type)
-            elif isinstance(arg, tuple) or isinstance(arg, list):
-                check_input_tensor(arg)
-            elif isinstance(arg, dict):
-                check_input_tensor(list(arg.values()))
+    def check_input_tensor(arg):
+        for elm in arg:
+            if isinstance(elm, torch.Tensor):
+                device_type_list.append(elm.device.type)
             else:
-                pass
+                check_input_tensor(elm)
 
-        if 'example_inputs' in kwargs:
-            example_inputs = kwargs['example_inputs']
-            if isinstance(example_inputs, torch.Tensor):
-                device_type_list.append(example_inputs.device.type)
-            elif isinstance(example_inputs, tuple) or isinstance(example_inputs, list):
-                check_input_tensor(example_inputs)
-            elif isinstance(example_inputs, dict):
-                check_input_tensor(list(example_inputs.values()))
-            else:
-                pass
+    for arg in args:
+        if isinstance(arg, torch.Tensor):
+            device_type_list.append(arg.device.type)
+        elif isinstance(arg, tuple) or isinstance(arg, list):
+            check_input_tensor(arg)
+        elif isinstance(arg, dict):
+            check_input_tensor(list(arg.values()))
+        else:
+            pass
 
-        if all([elm == 'xpu' for elm in device_type_list]) and ('check_trace' not in kwargs):
-            return True
+    if 'example_inputs' in kwargs:
+        example_inputs = kwargs['example_inputs']
+        if isinstance(example_inputs, torch.Tensor):
+            device_type_list.append(example_inputs.device.type)
+        elif isinstance(example_inputs, tuple) or isinstance(example_inputs, list):
+            check_input_tensor(example_inputs)
+        elif isinstance(example_inputs, dict):
+            check_input_tensor(list(example_inputs.values()))
+        else:
+            pass
+
+    is_xpu = all([elm == 'xpu' for elm in device_type_list])
+    if is_xpu and ('check_trace' not in kwargs) and (not torch.xpu.has_2d_block_array()):
+        return True
+
     return False
 
 
