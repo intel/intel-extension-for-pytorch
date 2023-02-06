5139ec5309c6e3e0d480a1806db7a3776c03b773
Yutao Xu <yutao.xu@intel.com>
2023-02-06 14:50:53 +0800

    roi_align: base operator support (#2234)
    roi_align: base operator support
    
    ---------
    
    Co-authored-by: Jinghui <jinghui.gu@intel.com>

---
+++ csrc/gpu/aten/operators/ROIAlign.cpp
@@ -0,0 +1,508 @@
+#include <ATen/ATen.h>
+#include <ATen/Functions.h>
+#include <ATen/native/Activation.h>
+#include <ATen/record_function.h>
+#include <torch/library.h>
+
+#include <core/Memory.h>
+#include <runtime/Utils.h>
+#include <utils/DPCPP.h>
+
+#include "RandomEngine.h"
+#include "comm/ATDispatch.h"
+#include "comm/AccumulateType.h"
+#include "comm/ApplyUtils.h"
+#include "comm/Atomics.h"
+#include "comm/Numerics.h"
+#include "comm/TensorOptions.h"
+
+#include <aten/operators/MemoryAccess.h>
+#include "utils/CustomOperatorRegistration.h"
+
+using namespace xpu::dpcpp;
+using namespace xpu::dpcpp::detail;
+
+namespace at {
+namespace AtenIpexTypeXPU {
+
+template <typename T>
+inline T bilinear_interpolate(
+    const T* input,
+    int height,
+    int width,
+    T y,
+    T x,
+    int index /* index for debug only*/) {
+  // deal with cases that inverse elements are out of feature map boundary
+  if (y < -1.0 || y > height || x < -1.0 || x > width) {
+    // empty
+    return 0;
+  }
+
+  if (y <= 0)
+    y = 0;
+  if (x <= 0)
+    x = 0;
+
+  int y_low = (int)y;
+  int x_low = (int)x;
+  int y_high;
+  int x_high;
+
+  if (y_low >= height - 1) {
+    y_high = y_low = height - 1;
+    y = (T)y_low;
+  } else {
+    y_high = y_low + 1;
+  }
+
+  if (x_low >= width - 1) {
+    x_high = x_low = width - 1;
+    x = (T)x_low;
+  } else {
+    x_high = x_low + 1;
+  }
+
+  T ly = y - y_low;
+  T lx = x - x_low;
+  T hy = 1. - ly, hx = 1. - lx;
+
+  // do bilinear interpolation
+  T v1 = input[y_low * width + x_low];
+  T v2 = input[y_low * width + x_high];
+  T v3 = input[y_high * width + x_low];
+  T v4 = input[y_high * width + x_high];
+  T w1 = hy * hx, w2 = hy * lx, w3 = ly * hx, w4 = ly * lx;
+
+  T val = (w1 * v1 + w2 * v2 + w3 * v3 + w4 * v4);
+
+  return val;
+}
+
+template <typename T, typename item_t>
+inline void roi_align_forward_kernel_impl(
+    item_t& item,
+    int nthreads,
+    const T* input,
+    const T spatial_scale,
+    int channels,
+    int height,
+    int width,
+    int pooled_height,
+    int pooled_width,
+    int sampling_ratio,
+    bool aligned,
+    const T* rois,
+    T* output) {
+  int lid = item.get_group(0) * item.get_local_range(0) + item.get_local_id(0);
+  for (int index = lid; index < nthreads;
+       index += item.get_group_range(0) * item.get_local_range(0)) {
+    // (n, c, ph, pw) is an element in the pooled output
+    int pw = index % pooled_width;
+    int ph = (index / pooled_width) % pooled_height;
+    int c = (index / pooled_width / pooled_height) % channels;
+    int n = index / pooled_width / pooled_height / channels;
+
+    const T* offset_rois = rois + n * 5;
+    int roi_batch_ind = offset_rois[0];
+
+    // Do not using rounding; this implementation detail is critical
+    T offset = aligned ? (T)0.5 : (T)0.0;
+    T roi_start_w = offset_rois[1] * spatial_scale - offset;
+    T roi_start_h = offset_rois[2] * spatial_scale - offset;
+    T roi_end_w = offset_rois[3] * spatial_scale - offset;
+    T roi_end_h = offset_rois[4] * spatial_scale - offset;
+
+    T roi_width = roi_end_w - roi_start_w;
+    T roi_height = roi_end_h - roi_start_h;
+    if (!aligned) {
+      // Force malformed ROIs to be 1x1
+      roi_width = std::max(roi_width, (T)1.);
+      roi_height = std::max(roi_height, (T)1.);
+    }
+
+    T bin_size_h = static_cast<T>(roi_height) / static_cast<T>(pooled_height);
+    T bin_size_w = static_cast<T>(roi_width) / static_cast<T>(pooled_width);
+
+    const T* offset_input =
+        input + (roi_batch_ind * channels + c) * height * width;
+
+    // We use roi_bin_grid to sample the grid and mimic integral
+    int roi_bin_grid_h = (sampling_ratio > 0)
+        ? sampling_ratio
+        : std::ceil(roi_height / pooled_height); // e.g., = 2
+    int roi_bin_grid_w = (sampling_ratio > 0)
+        ? sampling_ratio
+        : std::ceil(roi_width / pooled_width);
+
+    // We do average (integral) pooling inside a bin
+    // When the grid is empty, output zeros.
+    const T count = std::max(roi_bin_grid_h * roi_bin_grid_w, 1); // e.g. = 4
+
+    T output_val = 0.;
+    for (int iy = 0; iy < roi_bin_grid_h; iy++) // e.g., iy = 0, 1
+    {
+      const T y = roi_start_h + ph * bin_size_h +
+          static_cast<T>(iy + .5f) * bin_size_h /
+              static_cast<T>(roi_bin_grid_h); // e.g., 0.5, 1.5
+      for (int ix = 0; ix < roi_bin_grid_w; ix++) {
+        const T x = roi_start_w + pw * bin_size_w +
+            static_cast<T>(ix + .5f) * bin_size_w /
+                static_cast<T>(roi_bin_grid_w);
+
+        T val = bilinear_interpolate(offset_input, height, width, y, x, index);
+        output_val += val;
+      }
+    }
+    output_val /= count;
+
+    output[index] = output_val;
+  }
+}
+
+template <typename T>
+inline void bilinear_interpolate_gradient(
+    int height,
+    int width,
+    T y,
+    T x,
+    T& w1,
+    T& w2,
+    T& w3,
+    T& w4,
+    int& x_low,
+    int& x_high,
+    int& y_low,
+    int& y_high,
+    int index /* index for debug only*/) {
+  // deal with cases that inverse elements are out of feature map boundary
+  if (y < -1.0 || y > height || x < -1.0 || x > width) {
+    // empty
+    w1 = w2 = w3 = w4 = 0.;
+    x_low = x_high = y_low = y_high = -1;
+    return;
+  }
+
+  if (y <= 0)
+    y = 0;
+  if (x <= 0)
+    x = 0;
+
+  y_low = (int)y;
+  x_low = (int)x;
+
+  if (y_low >= height - 1) {
+    y_high = y_low = height - 1;
+    y = (T)y_low;
+  } else {
+    y_high = y_low + 1;
+  }
+
+  if (x_low >= width - 1) {
+    x_high = x_low = width - 1;
+    x = (T)x_low;
+  } else {
+    x_high = x_low + 1;
+  }
+
+  T ly = y - y_low;
+  T lx = x - x_low;
+  T hy = 1. - ly, hx = 1. - lx;
+
+  w1 = hy * hx, w2 = hy * lx, w3 = ly * hx, w4 = ly * lx;
+}
+
+template <typename T, typename item_t>
+inline void roi_align_backward_kernel_impl(
+    item_t& item,
+    int nthreads,
+    const T* grad_output,
+    const T spatial_scale,
+    int channels,
+    int height,
+    int width,
+    int pooled_height,
+    int pooled_width,
+    int sampling_ratio,
+    bool aligned,
+    T* grad_input,
+    const T* rois,
+    int n_stride,
+    int c_stride,
+    int h_stride,
+    int w_stride) {
+  int lid = item.get_group(0) * item.get_local_range(0) + item.get_local_id(0);
+  for (int index = lid; index < nthreads;
+       index += item.get_group_range(0) * item.get_local_range(0)) {
+    // (n, c, ph, pw) is an element in the pooled output
+    int pw = index % pooled_width;
+    int ph = (index / pooled_width) % pooled_height;
+    int c = (index / pooled_width / pooled_height) % channels;
+    int n = index / pooled_width / pooled_height / channels;
+
+    const T* offset_rois = rois + n * 5;
+    int roi_batch_ind = offset_rois[0];
+
+    // Do not using rounding; this implementation detail is critical
+    T offset = aligned ? (T)0.5 : (T)0.0;
+    T roi_start_w = offset_rois[1] * spatial_scale - offset;
+    T roi_start_h = offset_rois[2] * spatial_scale - offset;
+    T roi_end_w = offset_rois[3] * spatial_scale - offset;
+    T roi_end_h = offset_rois[4] * spatial_scale - offset;
+
+    T roi_width = roi_end_w - roi_start_w;
+    T roi_height = roi_end_h - roi_start_h;
+    if (!aligned) {
+      // Force malformed ROIs to be 1x1
+      roi_width = std::max(roi_width, (T)1.);
+      roi_height = std::max(roi_height, (T)1.);
+    }
+
+    T bin_size_h = static_cast<T>(roi_height) / static_cast<T>(pooled_height);
+    T bin_size_w = static_cast<T>(roi_width) / static_cast<T>(pooled_width);
+
+    T* offset_grad_input =
+        grad_input + ((roi_batch_ind * channels + c) * height * width);
+
+    // We need to index the gradient using the tensor strides to access the
+    // correct values.
+    int output_offset = n * n_stride + c * c_stride;
+    const T* offset_grad_output = grad_output + output_offset;
+    const T grad_output_this_bin =
+        offset_grad_output[ph * h_stride + pw * w_stride];
+
+    // We use roi_bin_grid to sample the grid and mimic integral
+    int roi_bin_grid_h = (sampling_ratio > 0)
+        ? sampling_ratio
+        : std::ceil(roi_height / pooled_height); // e.g., = 2
+    int roi_bin_grid_w = (sampling_ratio > 0)
+        ? sampling_ratio
+        : std::ceil(roi_width / pooled_width);
+
+    // We do average (integral) pooling inside a bin
+    const T count = roi_bin_grid_h * roi_bin_grid_w; // e.g. = 4
+
+    for (int iy = 0; iy < roi_bin_grid_h; iy++) // e.g., iy = 0, 1
+    {
+      const T y = roi_start_h + ph * bin_size_h +
+          static_cast<T>(iy + .5f) * bin_size_h /
+              static_cast<T>(roi_bin_grid_h); // e.g., 0.5, 1.5
+      for (int ix = 0; ix < roi_bin_grid_w; ix++) {
+        const T x = roi_start_w + pw * bin_size_w +
+            static_cast<T>(ix + .5f) * bin_size_w /
+                static_cast<T>(roi_bin_grid_w);
+
+        T w1, w2, w3, w4;
+        int x_low, x_high, y_low, y_high;
+
+        bilinear_interpolate_gradient(
+            height,
+            width,
+            y,
+            x,
+            w1,
+            w2,
+            w3,
+            w4,
+            x_low,
+            x_high,
+            y_low,
+            y_high,
+            index);
+
+        T g1 = grad_output_this_bin * w1 / count;
+        T g2 = grad_output_this_bin * w2 / count;
+        T g3 = grad_output_this_bin * w3 / count;
+        T g4 = grad_output_this_bin * w4 / count;
+
+        if (x_low >= 0 && x_high >= 0 && y_low >= 0 && y_high >= 0) {
+          atomicAdd(
+              (dpcpp_global_ptr_pt<
+                  T>)(offset_grad_input + y_low * width + x_low),
+              static_cast<T>(g1));
+          atomicAdd(
+              (dpcpp_global_ptr_pt<
+                  T>)(offset_grad_input + y_low * width + x_high),
+              static_cast<T>(g2));
+          atomicAdd(
+              (dpcpp_global_ptr_pt<
+                  T>)(offset_grad_input + y_high * width + x_low),
+              static_cast<T>(g3));
+          atomicAdd(
+              (dpcpp_global_ptr_pt<
+                  T>)(offset_grad_input + y_high * width + x_high),
+              static_cast<T>(g4));
+        } // if
+      } // ix
+    } // iy
+  }
+}
+
+at::Tensor roi_align_forward_kernel(
+    const at::Tensor& input,
+    const at::Tensor& rois,
+    double spatial_scale,
+    int64_t pooled_height,
+    int64_t pooled_width,
+    int64_t sampling_ratio,
+    bool aligned) {
+  TORCH_CHECK(input.is_xpu(), "input must be a XPU tensor");
+  TORCH_CHECK(rois.is_xpu(), "rois must be a XPU tensor");
+  TORCH_CHECK(rois.size(1) == 5, "rois must have shape as Tensor[K, 5]");
+
+  at::TensorArg input_t{input, "input", 1}, rois_t{rois, "rois", 2};
+
+  at::CheckedFrom c = "roi_align_forward_kernel";
+  at::checkAllSameGPU(c, {input_t, rois_t});
+  at::checkAllSameType(c, {input_t, rois_t});
+
+  auto num_rois = rois.size(0);
+  auto channels = input.size(1);
+  auto height = input.size(2);
+  auto width = input.size(3);
+
+  at::Tensor output = at::zeros(
+      {num_rois, channels, pooled_height, pooled_width}, input.options());
+
+  auto output_size = num_rois * pooled_height * pooled_width * channels;
+  int items_per_group = 512;
+  int num_groups =
+      std::min((output_size + 512 - 1) / 512, static_cast<int64_t>(4096));
+
+  if (output.numel() == 0)
+    return output;
+
+  auto input_ = input.contiguous(), rois_ = rois.contiguous();
+  auto& dpcpp_queue = dpcppGetCurrentQueue();
+  IPEX_DISPATCH_FLOATING_TYPES_AND_HALF(
+      input.scalar_type(), "roi_align_forward_kernel", [&] {
+        auto cgf = DPCPP_Q_CGF(cgh) {
+          auto input_ptr = (scalar_t*)input_.data_ptr();
+          auto rois_ptr = (scalar_t*)rois_.data_ptr();
+          auto output_ptr = (scalar_t*)output.data_ptr();
+          auto kfn = DPCPP_Q_KFN(sycl::nd_item<1> item) {
+            roi_align_forward_kernel_impl<scalar_t>(
+                item,
+                output_size,
+                input_ptr,
+                spatial_scale,
+                channels,
+                height,
+                width,
+                pooled_height,
+                pooled_width,
+                sampling_ratio,
+                aligned,
+                rois_ptr,
+                output_ptr);
+          };
+          cgh.parallel_for(
+              sycl::nd_range<1>(
+                  sycl::range<1>(num_groups * items_per_group),
+                  sycl::range<1>(items_per_group)),
+              kfn);
+        };
+        DPCPP_Q_SUBMIT(dpcpp_queue, cgf);
+      });
+  return output;
+}
+
+at::Tensor roi_align_backward_kernel(
+    const at::Tensor& grad,
+    const at::Tensor& rois,
+    double spatial_scale,
+    int64_t pooled_height,
+    int64_t pooled_width,
+    int64_t batch_size,
+    int64_t channels,
+    int64_t height,
+    int64_t width,
+    int64_t sampling_ratio,
+    bool aligned) {
+  TORCH_CHECK(grad.is_xpu(), "grad must be a XPU tensor");
+  TORCH_CHECK(rois.is_xpu(), "rois must be a XPU tensor");
+
+  at::TensorArg grad_t{grad, "grad", 1}, rois_t{rois, "rois", 2};
+
+  at::CheckedFrom c = "roi_align_backward_kernel";
+  at::checkAllSameGPU(c, {grad_t, rois_t});
+  at::checkAllSameType(c, {grad_t, rois_t});
+
+  at::Tensor grad_input =
+      at::zeros({batch_size, channels, height, width}, grad.options());
+
+  int items_per_group = 512;
+  int num_groups =
+      std::min((grad.numel() + 512 - 1) / 512, static_cast<int64_t>(4096));
+
+  // handle possibly empty gradients
+  if (grad.numel() == 0) {
+    return grad_input;
+  }
+
+  int n_stride = grad.stride(0);
+  int c_stride = grad.stride(1);
+  int h_stride = grad.stride(2);
+  int w_stride = grad.stride(3);
+
+  auto rois_ = rois.contiguous();
+  auto& dpcpp_queue = dpcppGetCurrentQueue();
+  IPEX_DISPATCH_FLOATING_TYPES_AND_HALF(
+      grad.scalar_type(), "roi_align_backward_kernel", [&] {
+        auto cgf = DPCPP_Q_CGF(cgh) {
+          auto grad_ptr = (scalar_t*)grad.data_ptr();
+          auto grad_input_ptr = (scalar_t*)grad_input.data_ptr();
+          auto rois_ptr = (scalar_t*)rois_.data_ptr();
+          auto grad_numel = grad.numel();
+          auto kfn = DPCPP_Q_KFN(sycl::nd_item<1> item) {
+            roi_align_backward_kernel_impl<scalar_t>(
+                item,
+                grad_numel,
+                grad_ptr,
+                spatial_scale,
+                channels,
+                height,
+                width,
+                pooled_height,
+                pooled_width,
+                sampling_ratio,
+                aligned,
+                grad_input_ptr,
+                rois_ptr,
+                n_stride,
+                c_stride,
+                h_stride,
+                w_stride);
+          };
+          cgh.parallel_for(
+              sycl::nd_range<1>(
+                  sycl::range<1>(num_groups * items_per_group),
+                  sycl::range<1>(items_per_group)),
+              kfn);
+        };
+        DPCPP_Q_SUBMIT(dpcpp_queue, cgf);
+      });
+  return grad_input;
+}
+
+} // namespace AtenIpexTypeXPU
+} // namespace at
+
+namespace {
+IPEX_LIBRARY_FRAGMENT() {
+  IPEX_OP_REGISTER(
+      "roi_align.xpu", at::AtenIpexTypeXPU::roi_align_forward_kernel);
+  IPEX_OP_REGISTER(
+      "_roi_align_backward.xpu",
+      at::AtenIpexTypeXPU::roi_align_backward_kernel);
+}
+
+IPEX_TORCH_LIBRARY_IMPL(torchvision, XPU, m) {
+  m.impl(
+      TORCH_SELECTIVE_NAME("torchvision::roi_align"),
+      TORCH_FN((&at::AtenIpexTypeXPU::roi_align_forward_kernel)));
+  m.impl(
+      TORCH_SELECTIVE_NAME("torchvision::_roi_align_backward"),
+      TORCH_FN((&at::AtenIpexTypeXPU::roi_align_backward_kernel)));
+}
+} // namespace
---
+++ tests/gpu/examples/test_roi_align.py
@@ -0,0 +1,103 @@
+# -*- coding: utf-8 -*-
+
+import torch
+from torch.testing._internal.common_utils import TestCase
+import math
+import numpy as np
+import intel_extension_for_pytorch # noqa
+import pytest
+
+
+def bilinear_interpolate(data, y, x, snap_border=False):
+    height, width = data.shape
+    if snap_border:
+        if -1 < y <= 0:
+            y = 0
+        elif height - 1 <= y < height:
+            y = height - 1
+        if -1 < x <= 0:
+            x = 0
+        elif width - 1 <= x < width:
+            x = width - 1
+    y_low = int(math.floor(y))
+    x_low = int(math.floor(x))
+    y_high = y_low + 1
+    x_high = x_low + 1
+    wy_h = y - y_low
+    wx_h = x - x_low
+    wy_l = 1 - wy_h
+    wx_l = 1 - wx_h
+    val = 0
+    for wx, xp in zip((wx_l, wx_h), (x_low, x_high)):
+        for wy, yp in zip((wy_l, wy_h), (y_low, y_high)):
+            if 0 <= yp < height and 0 <= xp < width:
+                val += wx * wy * data[yp, xp]
+    return val
+
+
+def expected_fn(in_data, rois, pool_h, pool_w, spatial_scale=1, sampling_ratio=-1,
+                aligned=False,
+                device=None,
+                dtype=torch.float64,
+                ):
+    if device is None:
+        device = torch.device("cpu")
+    n_channels = in_data.size(1)
+    out_data = torch.zeros(rois.size(0), n_channels,
+                           pool_h, pool_w, dtype=dtype, device=device)
+    offset = 0.5 if aligned else 0.0
+    for r, roi in enumerate(rois):
+        batch_idx = int(roi[0])
+        j_begin, i_begin, j_end, i_end = (
+            x.item() * spatial_scale - offset for x in roi[1:])
+        roi_h = i_end - i_begin
+        roi_w = j_end - j_begin
+        bin_h = roi_h / pool_h
+        bin_w = roi_w / pool_w
+        for i in range(0, pool_h):
+            start_h = i_begin + i * bin_h
+            grid_h = sampling_ratio if sampling_ratio > 0 else int(
+                np.ceil(bin_h))
+            for j in range(0, pool_w):
+                start_w = j_begin + j * bin_w
+                grid_w = sampling_ratio if sampling_ratio > 0 else int(
+                    np.ceil(bin_w))
+                for channel in range(0, n_channels):
+                    val = 0
+                    for iy in range(0, grid_h):
+                        y = start_h + (iy + 0.5) * bin_h / grid_h
+                        for ix in range(0, grid_w):
+                            x = start_w + (ix + 0.5) * bin_w / grid_w
+                            val += bilinear_interpolate(
+                                in_data[batch_idx, channel, :, :], y, x, snap_border=True)
+                    val /= grid_h * grid_w
+                    out_data[r, channel, i, j] = val
+    return out_data
+
+
+class TestNNMethod(TestCase):
+    def roi_align_forward_(self, dtype_):
+        device = torch.device('xpu')
+        x_dtype = dtype_
+        rois_dtype = dtype_
+        pool_size = 5
+        n_channels = 2 * (pool_size**2)
+        x = torch.rand(2, n_channels, 10, 10, dtype=x_dtype, device=device)
+        rois = torch.tensor(
+            [[0, 0, 0, 9, 9], [0, 0, 5, 4, 9], [0, 5, 5, 9, 9], [1, 0, 0, 9, 9]],  # format is (xyxy)
+            dtype=rois_dtype,
+            device=device,
+        )
+        pool_h, pool_w = pool_size, pool_size
+        y = torch.xpu.roi_align(x, rois, [pool_h, pool_w], spatial_scale=1, sampling_ratio=-1)
+        assert y.dtype == x.dtype
+        gt_y = expected_fn(
+            x, rois, pool_h, pool_w, spatial_scale=1, sampling_ratio=-1, device=device, dtype=x_dtype
+        )
+        tol = 1e-2 if (x_dtype is torch.half or rois_dtype is torch.half) else 1e-5
+        torch.testing.assert_close(gt_y.to(y), y, rtol=tol, atol=tol)
+
+    def test_roi_align_forward(self):
+        for dtype in [torch.float, torch.half]:
+            print('testing dtype:', dtype)
+            self.roi_align_forward_(dtype)
