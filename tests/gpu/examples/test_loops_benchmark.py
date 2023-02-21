1fb0c9c2e47868f9d4569f6bd0794d22217a88b3
Yutao Xu <yutao.xu@intel.com>
2023-02-22 01:06:07 +0800

    loops: enable performance regression test for pre-CI (#1496)
    * Update test_loops_backbone.py
    * Update and rename test_loops_backbone.py to test_loops_benchmark.py
    
    ---------
    
    Co-authored-by: Jinghui <jinghui.gu@intel.com>

--- tests/gpu/examples/test_loops_benchmark.py
+++ tests/gpu/examples/test_loops_benchmark.py
@@ -1,4 +1,6 @@
 import torch
+import threading
+import numpy as np
 from torch.testing._internal.common_utils import TestCase
 try:
     import intel_extension_for_pytorch
@@ -8,6 +10,7 @@ try:
     import microbench as mb
 except Exception:
     mb = None
+mb = None
 PRINT_TIME_INFO = False
 VERBOSE_ONLY = False
 
@@ -102,7 +105,7 @@ def run_binary_fn(info, fn, dtype, dynamiccast=False, runs=10):
     dur_ = durs[len(durs) // 2]
     maxdiff_ = maxdiffs[len(maxdiffs) - 1]
     assert(maxdiff_ < 1e-4)
-    print('maxdiff: ' + str(maxdiff_) + ', dur(us): ' + str(dur_))
+    print('maxdiff: ' + str(maxdiff_) + ', dur(us): ' + str(dur_) + '  ' + str(info) + ' ' + str(dtype))
     return dur_
 
 
@@ -231,24 +234,172 @@ maxdiff: 0.0, dur(us): 20.0
 """
 
 
-def perf_gap(index, ref, runtime):
-    print(ref / runtime)
+baseline_pvcb4 = """
+maxdiff: 0.0, dur(us): 7.2
+maxdiff: 0.0, dur(us): 7.2
+maxdiff: 0.0, dur(us): 7.2
+maxdiff: 0.0, dur(us): 16.16
+maxdiff: 0.0, dur(us): 16.16
+maxdiff: 0.0, dur(us): 14.88
+maxdiff: 0.0, dur(us): 14.08
+maxdiff: 0.0, dur(us): 20.0
+maxdiff: 0.0, dur(us): 19.2
+maxdiff: 0.0, dur(us): 12.96
+maxdiff: 0.0, dur(us): 12.96
+maxdiff: 0.0, dur(us): 12.64
+maxdiff: 0.0, dur(us): 17.28
+maxdiff: 0.0, dur(us): 16.48
+maxdiff: 0.0, dur(us): 17.12
+maxdiff: 0.0, dur(us): 1388.0
+maxdiff: 0.0, dur(us): 1378.0
+maxdiff: 0.0, dur(us): 1393.0
+maxdiff: 0.0, dur(us): 55.04
+maxdiff: 0.0, dur(us): 54.88
+maxdiff: 0.0, dur(us): 55.68
+maxdiff: 0.0, dur(us): 436.0
+maxdiff: 0.0, dur(us): 418.56
+maxdiff: 0.0, dur(us): 421.28
+maxdiff: 0.0, dur(us): 21.12
+maxdiff: 0.0, dur(us): 21.28
+maxdiff: 0.0, dur(us): 10.88
+maxdiff: 0.0, dur(us): 662.72
+maxdiff: 0.0, dur(us): 643.36
+maxdiff: 0.0, dur(us): 635.68
+maxdiff: 0.0, dur(us): 19.68
+maxdiff: 0.0, dur(us): 19.68
+maxdiff: 0.0, dur(us): 19.52
+maxdiff: 0.0, dur(us): 12.8
+maxdiff: 0.0, dur(us): 12.8
+maxdiff: 0.0, dur(us): 11.52
+maxdiff: 0.0, dur(us): 13.28
+maxdiff: 0.0, dur(us): 19.2
+maxdiff: 0.0, dur(us): 19.36
+maxdiff: 0.0, dur(us): 19.52
+maxdiff: 0.0, dur(us): 14.88
+maxdiff: 0.0, dur(us): 12.96
+maxdiff: 0.0, dur(us): 12.96
+maxdiff: 0.0, dur(us): 12.8
+maxdiff: 0.0, dur(us): 14.88
+maxdiff: 0.0, dur(us): 17.28
+maxdiff: 0.0, dur(us): 17.28
+maxdiff: 0.0, dur(us): 17.28
+maxdiff: 0.0, dur(us): 15.52
+maxdiff: 0.0, dur(us): 16.32
+maxdiff: 0.0, dur(us): 16.16
+maxdiff: 0.0, dur(us): 16.48
+maxdiff: 0.0, dur(us): 15.68
+maxdiff: 0.0, dur(us): 664.96
+maxdiff: 0.0, dur(us): 663.68
+maxdiff: 0.0, dur(us): 661.6
+maxdiff: 0.0, dur(us): 1352.0
+maxdiff: 0.0, dur(us): 31.52
+maxdiff: 0.0, dur(us): 31.52
+maxdiff: 0.0, dur(us): 31.68
+maxdiff: 0.0, dur(us): 161.92
+maxdiff: 0.0, dur(us): 226.08
+maxdiff: 0.0, dur(us): 224.8
+maxdiff: 0.0, dur(us): 223.84
+maxdiff: 0.0, dur(us): 913.6
+maxdiff: 0.0, dur(us): 16.48
+maxdiff: 0.0, dur(us): 16.64
+maxdiff: 0.0, dur(us): 16.48
+maxdiff: 0.0, dur(us): 16.16
+maxdiff: 0.0, dur(us): 589.28
+maxdiff: 0.0, dur(us): 591.04
+maxdiff: 0.0, dur(us): 583.84
+maxdiff: 0.0, dur(us): 916.96
+maxdiff: 0.0, dur(us): 18.56
+maxdiff: 0.0, dur(us): 18.56
+maxdiff: 0.0, dur(us): 18.56
+maxdiff: 0.0, dur(us): 24.64
+maxdiff: 0.0, dur(us): 13.12
+maxdiff: 0.0, dur(us): 12.96
+maxdiff: 0.0, dur(us): 13.92
+maxdiff: 0.0, dur(us): 13.6
+maxdiff: 0.0, dur(us): 24.64
+maxdiff: 0.0, dur(us): 24.32
+maxdiff: 0.0, dur(us): 21.44
+maxdiff: 0.0, dur(us): 15.2
+maxdiff: 0.0, dur(us): 12.96
+maxdiff: 0.0, dur(us): 12.96
+maxdiff: 0.0, dur(us): 12.96
+maxdiff: 0.0, dur(us): 15.2
+maxdiff: 0.0, dur(us): 20.96
+maxdiff: 0.0, dur(us): 20.96
+maxdiff: 0.0, dur(us): 17.44
+maxdiff: 0.0, dur(us): 15.68
+maxdiff: 0.0, dur(us): 16.0
+maxdiff: 0.0, dur(us): 16.16
+maxdiff: 0.0, dur(us): 16.32
+maxdiff: 0.0, dur(us): 16.0
+maxdiff: 0.0, dur(us): 665.44
+maxdiff: 0.0, dur(us): 672.16
+maxdiff: 0.0, dur(us): 660.16
+maxdiff: 0.0, dur(us): 1350.0
+maxdiff: 0.0, dur(us): 32.16
+maxdiff: 0.0, dur(us): 32.32
+maxdiff: 0.0, dur(us): 32.32
+maxdiff: 0.0, dur(us): 162.08
+maxdiff: 0.0, dur(us): 225.92
+maxdiff: 0.0, dur(us): 228.48
+maxdiff: 0.0, dur(us): 229.76
+maxdiff: 0.0, dur(us): 915.36
+maxdiff: 0.0, dur(us): 16.64
+maxdiff: 0.0, dur(us): 16.64
+maxdiff: 0.0, dur(us): 16.32
+maxdiff: 0.0, dur(us): 16.32
+maxdiff: 0.0, dur(us): 602.4
+maxdiff: 0.0, dur(us): 600.32
+maxdiff: 0.0, dur(us): 587.84
+maxdiff: 0.0, dur(us): 916.48
+maxdiff: 0.0, dur(us): 18.72
+maxdiff: 0.0, dur(us): 18.72
+maxdiff: 0.0, dur(us): 18.56
+maxdiff: 0.0, dur(us): 24.8
+"""
+
+
+def perf_gap(index, ref, runtime, enabled):
+    perfreg = ref / runtime
+    print(perfreg)
+    if enabled:
+        if perfreg < 0.5: # not stable here
+            raise
     return index + 1
 
 
+def get_binary_bandwidth():
+    print('get_binary_bandwidth...')
+    if mb:
+        mb.disable_verbose()
+    shape0 = [16, 16, 512, 512]
+    timeus = run_binary_fn([shape0, shape0], '+', 'float')
+    total_bytes = np.prod(shape0) * 3 * 4
+    bw = total_bytes / (timeus/1000000.0) / (1024**3)
+    print('bw: ', bw)
+    if mb:
+        mb.enable_verbose()
+    return bw
+
+
 def run_tests():
+    bw = get_binary_bandwidth()
+    enable_perf_test = False
+    if bw > 99999:
+        print('enable perf test')
+        enable_perf_test = True
     test_types = ['float', 'half', 'bfloat16']
-    if len(baseline_a100) > 100:
-        baselines = baseline_a100.strip().split('\n')
+    if len(baseline_pvcb4) > 100:
+        baselines = baseline_pvcb4.strip().split('\n')
         baselines = [float(line.split()[-1]) for line in baselines]
         index = 0
         for dtype in test_types:
             for item in test_shapes:
-                index = perf_gap(index, baselines[index], run_binary_fn(item, '+', dtype))
-                index = perf_gap(index, baselines[index], run_binary_fn(item, '-', dtype))
-                index = perf_gap(index, baselines[index], run_binary_fn(item, '*', dtype))
+                index = perf_gap(index, baselines[index], run_binary_fn(item, '+', dtype), enable_perf_test)
+                index = perf_gap(index, baselines[index], run_binary_fn(item, '-', dtype), enable_perf_test)
+                index = perf_gap(index, baselines[index], run_binary_fn(item, '*', dtype), enable_perf_test)
                 if dtype != 'float':  # dynamic cast
-                    index = perf_gap(index, baselines[index], run_binary_fn(item, '*', dtype, True))
+                    index = perf_gap(index, baselines[index], run_binary_fn(item, '*', dtype, True), enable_perf_test)
     else:
         for dtype in test_types:
             for item in test_shapes:
@@ -265,4 +416,6 @@ class TestTensorMethod(TestCase):
 
 
 if __name__ == '__main__':
-    run_tests()
+    mutex = threading.Lock()
+    with mutex:
+        run_tests()
