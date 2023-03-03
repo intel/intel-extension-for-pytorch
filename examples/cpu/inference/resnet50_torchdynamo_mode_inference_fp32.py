bedb7c8868c6259d6acc985dcb0dbcf7d2bbefbb
ZailiWang <zaili.wang@intel.com>
2023-03-03 15:24:58 +0800

    Add TorchDynamo fp32 inference examples (resnet50 & BERT)

---
+++ examples/cpu/inference/resnet50_torchdynamo_mode_inference_fp32.py
@@ -0,0 +1,15 @@
+
+import torch
+import torchvision.models as models
+
+model = models.resnet50(weights=models.ResNet50_Weights.DEFAULT)
+model.eval()
+data = torch.rand(1, 3, 224, 224)
+
+#################### code changes ####################
+import intel_extension_for_pytorch as ipex
+model = torch.compile(model, backend="ipex")
+######################################################
+
+with torch.no_grad():
+    model(data)
