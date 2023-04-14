3b1eaec6249108f7b059da38b0a2811189a839ee
Chunyuan WU <chunyuan.wu@intel.com>
2023-04-14 06:55:51 +0000

    cpu-device: support AutoTP for dynamic quant (#1539)
    * cpu-device: support AutoTP for dynamic quant
    
    * remove redundant code
    
    * remove some comment
    
    * reuse quantize_dynamic for quant conversion

--- intel_extension_for_pytorch/quantization/_quantize.py
+++ intel_extension_for_pytorch/quantization/_quantize.py
@@ -1,13 +1,20 @@
 import copy
+import functools
+import os
 from typing import Tuple, Any
 import warnings
 
 import torch
 from torch.ao.quantization import PlaceholderObserver, QConfig, QConfigMapping
 import torch.fx.experimental.optimization as optimization
+from torch.ao.nn.quantized.modules.utils import _quantize_weight
+import torch.ao.nn.quantized.dynamic as nnqd
+from torch.ao.quantization.quantize import propagate_qconfig_
+from torch.ao.quantization.quantization_mappings import get_default_dynamic_quant_module_mappings
 
 import intel_extension_for_pytorch._C as core
 from intel_extension_for_pytorch.utils.linear_bn_folding import linear_bn_fuse
+from intel_extension_for_pytorch.nn.utils._weight_prepack import may_import_deepspeed_modules
 from ._quantize_utils import auto_prepare, auto_convert, copy_prepared_model
 from .. import nn
 
@@ -15,7 +22,8 @@ def prepare(
     model,
     configure,
     example_inputs,
-    inplace=False):
+    inplace=False,
+    bn_folding=True):
     r"""
     Prepare an FP32 torch.nn.Module model to do calibration or to convert to quantized model.
 
@@ -25,6 +33,8 @@ def prepare(
         example_inputs (tuple or torch.Tensor): A tuple of example inputs that
             will be passed to the function while running to init quantization state.
         inplace: (bool): It will change the given model in-place if True. The default value is ``False``.
+        bn_folding: (bool): whether to perform ``conv_bn`` and ``linear_bn`` folding.
+        The default value is ``True``.
 
     Returns:
         torch.nn.Module
@@ -43,18 +53,20 @@ def prepare(
     from ..frontend import auto_channels_last, _convert_convNd_weight_memory_format
     if auto_channels_last:
         _convert_convNd_weight_memory_format(model)
-    try:
-        prepare_model = optimization.fuse(model, inplace=inplace)
-        prepare_model = linear_bn_fuse(prepare_model, inplace=inplace)
-    except:  # noqa E722
-        if inplace:
-            prepare_model = model
-        else:
-            try:
-                prepare_model = copy.deepcopy(model)
-            except:
-                assert False, "The model's copy is failed, please try set inplace to True to do the prepare"
-        warnings.warn("Conv BatchNorm folding failed during the prepare process.")
+    if inplace:
+        prepare_model = model
+    else:
+        try:
+            prepare_model = copy.deepcopy(model)
+        except:
+            assert False, "The model's copy is failed, please try set inplace to True to do the prepare"        
+    if bn_folding:
+        try:
+            prepare_model = optimization.fuse(prepare_model, inplace=inplace)
+            prepare_model = linear_bn_fuse(prepare_model, inplace=inplace)
+        except:
+            warnings.warn("BatchNorm folding failed during the prepare process.")
+
     # replace dropout with identity to enable more fusion pattern.
     nn.utils._model_convert.replace_dropout_with_identity(prepare_model)
     # Special case for common case of passing a single Tensor
@@ -63,6 +75,135 @@ def prepare(
     elif not isinstance(example_inputs, tuple):
         example_inputs = tuple(example_inputs)
     return auto_prepare(prepare_model, configure, example_inputs)
+
+@functools.lru_cache(None)
+def IPEX_DYNAMIC_QUANTIZATION_MODULE_CPU():
+    # TODO: have to override Linear here for GPT-J performance.
+    # AutoTP model does not support deepcopy, thus need to do inplace convert.
+    # The str(module) is called in GPT-J, which invokes the __repr__ function:
+    #
+    #   torch/ao/nn/quantized/modules/linear.py:__repr__()
+    # -> return _hide_packed_params_repr(self, LinearPackedParams)
+    #   torch/ao/nn/quantized/modules/utils.py:_hide_packed_params_repr()
+    # -> extra_repr = self.extra_repr()
+    #   torch/ao/nn/quantized/dynamic/modules/linear.py:extra_repr()
+    # -> extra_repr_str += ', qscheme={}'.format(self.weight().qscheme())
+    #   torch/ao/nn/quantized/modules/linear.py:weight()
+    # -> return self._weight_bias()[0]
+    #   torch/ao/nn/quantized/modules/linear.py:_weight_bias()
+    # -> return self._packed_params._weight_bias()
+    # > torch/ao/nn/quantized/modules/linear.py:_weight_bias()
+    # -> return torch.ops.quantized.linear_unpack(self._packed_params)
+    #
+    # The quantized::linear_unpack function is quite slow.
+    # We will override the __repr__ function of nnqd.Linear in DynamicQuantizedLinearLayer.    
+    torch_modules = {
+        torch.nn.Linear: DynamicQuantizedLinearLayer,
+    }
+
+    deepspeed_modules = may_import_deepspeed_modules()
+    if deepspeed_modules is not None:
+        LinearAllreduce, LinearLayer = deepspeed_modules
+        deepspeed_modules = {
+            LinearLayer: DynamicQuantizedLinearLayer,
+            LinearAllreduce: DynamicQuantizedLinearAllreduce,
+        }
+        torch_modules.update(deepspeed_modules)
+    return torch_modules
+
+class _IPEXDynamicQuantizedLinear(nnqd.Linear):
+    @classmethod
+    def from_float(cls, mod):
+        assert type(mod) in cls._float_module(), \
+            'DynamicQuantizedLinearLayer.from_float only works for one of' + \
+            str([float_mod.__name__ for float_mod in cls._float_module()])
+        assert hasattr(mod, 'qconfig'), 'Input float module must have qconfig defined'        
+
+        if mod.qconfig is not None and mod.qconfig.weight is not None:
+            weight_observer = mod.qconfig.weight()
+        else:
+            from torch.ao.quantization.qconfig import default_dynamic_qconfig
+            weight_observer = default_dynamic_qconfig.weight()
+
+        dtype = weight_observer.dtype
+        assert dtype in [torch.qint8], "The only supported dtypes for " \
+            "DynamicQuantizedLinearLayer is qint8 got: {}".format(dtype)
+        weight_observer(mod.weight)
+        qweight = _quantize_weight(mod.weight.float(), weight_observer)
+        
+        qlinear = cls._init_cls(mod, dtype, qweight)
+        return qlinear         
+
+class DynamicQuantizedLinearLayer(_IPEXDynamicQuantizedLinear):
+    @classmethod
+    def _init_cls(cls, mod, dtype, qweight):
+        qlinear = cls(mod.weight.size()[1], mod.weight.size()[0], dtype=dtype)
+        qlinear.set_weight_bias(qweight, mod.bias)
+        return qlinear
+
+    @classmethod
+    @functools.lru_cache(None)
+    def _float_module(self):
+        _FLOAT_MODULE = [torch.nn.Linear]
+        deepspeed_modules = may_import_deepspeed_modules()
+        if deepspeed_modules is not None:
+            _, LinearLayer = deepspeed_modules
+            _FLOAT_MODULE.extend([LinearLayer])
+        return _FLOAT_MODULE
+
+    def __repr__(self):
+        return 'DynamicQuantizedLinearLayer()'
+
+class DynamicQuantizedLinearAllreduce(_IPEXDynamicQuantizedLinear):
+    @classmethod
+    def _init_cls(cls, mod, dtype, qweight):
+        qlinear = cls(mod.weight.size()[1], mod.weight.size()[0], mod.mp_group, mod.bias, dtype=dtype)
+        # For bias handling, please refer to the comment in __init__ of _IPEXLinearAllreduce
+        qlinear.set_weight_bias(qweight, None)
+        return qlinear
+
+    @classmethod
+    @functools.lru_cache(None)
+    def _float_module(self):
+        deepspeed_modules = may_import_deepspeed_modules()
+        assert deepspeed_modules is not None, "DynamicQuantizedLinearAllreduce requires deepspeed to be installed"
+        LinearAllreduce, _ = deepspeed_modules
+        _FLOAT_MODULE = [LinearAllreduce]
+        return _FLOAT_MODULE
+
+    def __init__(self, in_features, out_features, mp_group, bias_value, bias_=True, dtype=torch.qint8):
+        # Save the original bias here
+        # For bias handling, please refer to the comment in __init__ of _IPEXLinearAllreduce
+        super().__init__(in_features, out_features, bias_, dtype=dtype)
+        self.mp_group = mp_group
+        self.original_bias = bias_value
+
+    def forward(self, x):
+        if self._packed_params.dtype == torch.qint8:
+            if self.version is None or self.version < 4:
+                Y = torch.ops.quantized.linear_dynamic(
+                    x, self._packed_params._packed_params)
+            else:
+                Y = torch.ops.quantized.linear_dynamic(
+                    x, self._packed_params._packed_params, reduce_range=True)
+        elif self._packed_params.dtype == torch.float16:
+            Y = torch.ops.quantized.linear_dynamic_fp16(
+                x, self._packed_params._packed_params)
+        else:
+            raise RuntimeError('Unsupported dtype on dynamic quantized linear!')
+        output = Y.to(x.dtype)
+
+        if self.mp_group is not None:
+            ar = torch.ops.aten.all_reduce(output, 'sum', "", list(torch.arange(int(os.environ['WORLD_SIZE']))), int(os.environ['WORLD_SIZE']))
+            output = torch.ops.aten.wait_tensor(ar)
+
+        if self.original_bias is not None:
+            output += self.original_bias
+        return output        
+
+    def __repr__(self):
+        return 'DynamicQuantizedLinearAllreduce()'
+
 
 def convert(
     model,
@@ -92,6 +233,7 @@ def convert(
     # If the module's activation's qconfig is PlaceholderObserver,
     # we can say that the module want to run dynamic quantization path.
     if isinstance(convert_model.q_config.activation(), PlaceholderObserver):
+        module_mappings = get_default_dynamic_quant_module_mappings()
         qconfig_spec = {
             torch.nn.Linear : convert_model.q_config,
             torch.nn.LSTM : convert_model.q_config,
@@ -100,7 +242,19 @@ def convert(
             torch.nn.RNNCell : convert_model.q_config,
             torch.nn.GRUCell : convert_model.q_config,
         }
-        return torch.quantization.quantize_dynamic(convert_model, qconfig_spec=qconfig_spec, inplace=True)
+
+        deepspeed_modules = may_import_deepspeed_modules()
+        if deepspeed_modules is not None:        
+
+            LinearAllreduce, LinearLayer = deepspeed_modules
+            module_mappings.update(IPEX_DYNAMIC_QUANTIZATION_MODULE_CPU())
+            deepspeed_qconfig_spec = {
+                LinearLayer : convert_model.q_config,
+                LinearAllreduce : convert_model.q_config,
+            }
+            qconfig_spec.update(deepspeed_qconfig_spec)
+
+        return torch.quantization.quantize_dynamic(convert_model, qconfig_spec=qconfig_spec, mapping = module_mappings, inplace=True)
 
     # Convert linear, conv, and Embedding's weight dtype when use autocast,
     # which will reduce the dtype conversion.
--- tests/cpu/test_deepspeed.py
+++ tests/cpu/test_deepspeed.py
@@ -7,6 +7,8 @@ import torch.nn as nn
 from torch.testing._internal.common_utils import TestCase
 import intel_extension_for_pytorch as ipex
 from intel_extension_for_pytorch.nn.utils._weight_prepack import may_import_deepspeed_modules, _IPEXLinear, _IPEXLinearAllreduce
+from intel_extension_for_pytorch.quantization import prepare, convert
+from intel_extension_for_pytorch.quantization._quantize import DynamicQuantizedLinearLayer, DynamicQuantizedLinearAllreduce
 
 from test_weight_prepack import module_found
 
@@ -59,20 +61,24 @@ class DeepSpeedTestM(nn.Module):
         return z
 
 class DeepspeedTester(TestCase):
+    def _get_ds_model(self, m_linear):
+        import deepspeed
+        ds_world_size = int(os.getenv('WORLD_SIZE', '1'))
+        assert ds_world_size > 1, "expect ds_world_size > 1, you could try launching the script with: deepspeed --num_gpus 2 --bind_cores_to_rank tests/cpu/test_deepspeed.py"
+        engine = deepspeed.init_inference(model=m_linear, mp_size=ds_world_size, dtype=torch.float32,
+                                        replace_method="auto")
+        ds_model = engine.module
+        return ds_model  
+
     def test_ipex_optimize(self):
         deepspeed_modules = may_import_deepspeed_modules()
         if deepspeed_modules is not None:
             LinearAllreduce, LinearLayer = deepspeed_modules
-            import deepspeed
             x = torch.randn(2, 4)
             m_linear = DeepSpeedTestM().eval()
             y = m_linear(x)
 
-            ds_world_size = int(os.getenv('WORLD_SIZE', '1'))
-            assert ds_world_size > 1, "expect ds_world_size > 1, you could try launching the script with: deepspeed --num_gpus 2 --bind_cores_to_rank tests/cpu/test_deepspeed.py"
-            engine = deepspeed.init_inference(model=m_linear, mp_size=ds_world_size, dtype=torch.float32,
-                                            replace_method="auto")
-            ds_model = engine.module
+            ds_model = self._get_ds_model(m_linear)
             self.assertTrue(module_found(ds_model, LinearLayer))
             self.assertTrue(module_found(ds_model, LinearAllreduce))
 
@@ -82,6 +88,26 @@ class DeepspeedTester(TestCase):
 
             optimized = optimized(x)
             self.assertEqual(y, optimized)
+
+    def test_dynamic_quantization(self):
+        deepspeed_modules = may_import_deepspeed_modules()
+        if deepspeed_modules is not None:
+            LinearAllreduce, LinearLayer = deepspeed_modules
+            x = torch.randn(2, 4)
+            m_linear = DeepSpeedTestM().eval()
+            y = m_linear(x)
+
+            ds_model = self._get_ds_model(m_linear)
+            self.assertTrue(module_found(ds_model, LinearLayer))
+            self.assertTrue(module_found(ds_model, LinearAllreduce))
+
+            dynamic_qconfig = ipex.quantization.default_dynamic_qconfig
+            prepared_model = prepare(ds_model, dynamic_qconfig, example_inputs=(x), inplace=True, bn_folding=False)
+            converted = convert(prepared_model, inplace=True)
+            self.assertTrue(module_found(converted, DynamicQuantizedLinearLayer))
+            self.assertTrue(module_found(converted, DynamicQuantizedLinearAllreduce))
+            quantized = converted(x)
+            self.assertEqual(y, quantized, atol=0.005, rtol=1.3e-6)
 
 if __name__ == '__main__':
     deepspeed_modules = may_import_deepspeed_modules()
