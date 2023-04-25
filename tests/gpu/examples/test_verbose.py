4be6ebaccb38f16e3eeb6097cd2ec589f0516a62
Chao1Han <107970637+Chao1Han@users.noreply.github.com>
2023-04-25 20:50:27 +0800

    modify test_verbose (#2582)
    * modify test_verbose

--- tests/gpu/examples/test_verbose.py
+++ tests/gpu/examples/test_verbose.py
@@ -3,47 +3,34 @@ import intel_extension_for_pytorch # noqa
 from torch.testing._internal.common_utils import TestCase
 
 import os
+import sys
 import tempfile
-
-
-class Module(torch.nn.Module):
-    def __init__(self):
-        super(Module, self).__init__()
-        self.conv = torch.nn.Conv2d(1, 10, 5, 1)
-
-    def forward(self, x):
-        y = self.conv(x)
-        return y
-
-def run_model(level):
-    fname = tempfile.mkdtemp() + "_case_verbose"
-    stdout = os.dup(1)
-    verbose = os.open(fname, os.O_WRONLY | os.O_CREAT)
-    os.dup2(verbose, 1)
-
-    m = Module()
-    d = torch.rand(1, 1, 112, 112)
-    m = m.to('xpu')
-    d = d.to('xpu')
-    with torch.xpu.onednn_verbose(level):
-        m(d)
-
-    os.dup2(stdout, 1)
-    os.close(verbose)
-    f = open(fname)
-    num = 0
-    for line in f.readlines():
-        if line.strip().startswith("onednn_verbose"):
-            num = num + 1
-    f.close()
-    os.remove(fname)
-    return num > 0
 
 class TestVerbose(TestCase):
     def test_verbose_on(self):
-        verb_on = run_model(2)
+        fname = tempfile.mkdtemp() + "_case_verbose" 
+        cmd = f'''DNNL_VERBOSE=2 python -c "import torch;import intel_extension_for_pytorch;conv = torch.nn.Conv2d(1, 10, 5, 1).to('xpu');d = torch.rand(1, 1, 112, 112).to('xpu');conv(d)" > {fname}'''
+        os.system(cmd)
+        f = open(fname)
+        num = 0
+        for line in f.readlines():
+            if line.strip().startswith("onednn_verbose"):
+                num = num + 1
+        f.close()
+        os.remove(fname)
+        verb_on = num > 0
         assert verb_on, 'oneDNN verbose messages not found.'
 
     def test_verbose_off(self):
-        verb_on = run_model(0)
+        fname = tempfile.mkdtemp() + "_case_verbose" 
+        cmd = f'''DNNL_VERBOSE=0 python -c "import torch;import intel_extension_for_pytorch;conv = torch.nn.Conv2d(1, 10, 5, 1).to('xpu');d = torch.rand(1, 1, 112, 112).to('xpu');conv(d)" > {fname}'''
+        os.system(cmd)
+        f = open(fname)
+        num = 0
+        for line in f.readlines():
+            if line.strip().startswith("onednn_verbose"):
+                num = num + 1
+        f.close()
+        os.remove(fname)
+        verb_on = num > 0
         assert not verb_on, 'unexpected oneDNN verbose messages found.'
