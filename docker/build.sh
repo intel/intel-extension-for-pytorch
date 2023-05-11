76a9f0a40ec3a9286e095f3402b0860f62965efd
Srikanth Ramakrishna <srikanth.ramakrishna@intel.com>
2023-05-11 05:00:15 -0700

    Update IPEX base image to 1.13.120+xpu (#2596)
    * update driver version
    * update latest ipex and oneapi
    * add hotfix
    * change device arg location
    
    ---------
    
    Co-authored-by: Jitendra Patil <jitendra.patil@intel.com>
    Co-authored-by: Jinghui <jinghui.gu@intel.com>

--- docker/build.sh
+++ docker/build.sh
@@ -15,12 +15,14 @@ BUILD_ARGS=""
 
 if [[ $IMAGE_TYPE == "xpu-flex" ]];then
     IMAGE_NAME=intel-extension-for-pytorch:xpu-flex
+    BUILD_ARGS="--build-arg DEVICE=flex"
 fi
 if [[ ${IMAGE_TYPE} == "xpu-max" ]];then
     IMAGE_NAME=intel-extension-for-pytorch:xpu-max
-    BUILD_ARGS="--build-arg CCL_VER=2021.8.0-25371
-                --build-arg ONECCL_BIND_PT_VERSION=1.13.100
-                --build-arg ONECCL_BIND_PT_WHL_URL=https://developer.intel.com/ipex-whl-stable-xpu"
+    BUILD_ARGS="--build-arg CCL_VER=2021.9.0-43543 \
+                --build-arg ONECCL_BIND_PT_VERSION=1.13.200 \
+                --build-arg ONECCL_BIND_PT_WHL_URL=https://developer.intel.com/ipex-whl-stable-xpu \
+                --build-arg DEVICE=max"
 fi
 
 if [[ ${IMAGE_NAME} != "" ]]; then
@@ -29,15 +31,15 @@ if [[ ${IMAGE_NAME} != "" ]]; then
                  --build-arg no_proxy=$no_proxy \
                  --build-arg UBUNTU_VERSION=22.04 \
                  --build-arg PYTHON=python3.10 \
-                 --build-arg ICD_VER=22.43.24595.35+i538~22.04 \
-                 --build-arg LEVEL_ZERO_GPU_VER=1.3.24595.35+i538~22.04 \
-                 --build-arg LEVEL_ZERO_VER=1.8.8+i524~u22.04 \
-                 --build-arg LEVEL_ZERO_DEV_VER=1.8.8+i524~u22.04 \
-                 --build-arg DPCPP_VER=2023.0.0-25370 \
-                 --build-arg MKL_VER=2023.0.0-25398 \
-                 --build-arg TORCH_VERSION=1.13.0a0+gitb1dde16 \
-                 --build-arg IPEX_VERSION=1.13.10+xpu \
-                 --build-arg TORCHVISION_VERSION=0.14.1a0+0504df5 \
+                 --build-arg ICD_VER=23.05.25593.18-601~22.04 \
+                 --build-arg LEVEL_ZERO_GPU_VER=1.3.25593.18-601~22.04 \
+                 --build-arg LEVEL_ZERO_VER=1.9.4+i589~22.04 \
+                 --build-arg LEVEL_ZERO_DEV_VER=1.9.4+i589~22.04 \
+                 --build-arg DPCPP_VER=2023.1.0-46305 \
+                 --build-arg MKL_VER=2023.1.0-46342 \
+                 --build-arg TORCH_VERSION=1.13.0a0+git6c9b55e \
+                 --build-arg IPEX_VERSION=1.13.120+xpu \
+                 --build-arg TORCHVISION_VERSION=0.14.1a0+5e8e2f1 \
                  --build-arg TORCH_WHL_URL=https://developer.intel.com/ipex-whl-stable-xpu \
                  --build-arg IPEX_WHL_URL=https://developer.intel.com/ipex-whl-stable-xpu \
                  --build-arg TORCHVISION_WHL_URL=https://developer.intel.com/ipex-whl-stable-xpu \
