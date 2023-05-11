5472a54d30d83fb65b9b18a836f834759a233400
zejun <zejun.chen@intel.com>
2023-05-11 21:08:26 +0800

    Redo torch.mode (#2495)
    * [Mode] 1. redo mode
    1. fix the index error in model_fused_impl
    2. enlarge the reduce function for mode with scrtach
    3. make sure bitonic sort is stable
    4. implement a conditional scan for mode
    5. using global fence, instead of local fence in non-fused mode
    6. change the functor to get stable sorted result
    7. change the scan policy for mode
    8. change the barrier fence scope for mode
    9. Add sort in fused mode kernel
    10. Change the wg number heuristic
    11. add barrier for huge number issue
    
    ---------
    
    Signed-off-by: Chen, Zejun <zejun.chen@intel.com>
    Co-authored-by: Jinghui <jinghui.gu@intel.com>

---
+++ tests/gpu/examples/test_torch_mode.py
@@ -0,0 +1,51 @@
+import torch
+from torch.testing._internal.common_utils import TestCase
+import intel_extension_for_pytorch # noqa
+
+cpu_device = torch.device("cpu")
+xpu_device = torch.device("xpu")
+value_range = 30
+
+class TestTorchMethod(TestCase):
+    def test_mode(self):
+        def mode_test_helper(input_list):
+            for input_cpu in input_list:
+                # test from innerest dim to outerest dim
+                for dim in reversed(range(input_cpu.dim())):
+                    print('input shape = ', input_cpu.shape)
+                    print('input dtype = ', input_cpu.dtype)
+                    print('dim = ', dim)
+                    output_cpu, output_indices_cpu = torch.mode(input_cpu, dim)
+                    input_xpu = input_cpu.detach().to(xpu_device)
+                    output_xpu, output_indices_xpu = torch.mode(input_xpu, dim)
+                    print('checking mode value:')
+                    self.assertEqual(output_cpu, output_xpu.cpu())
+                    # TODO: The cpu result indices are error in some cases
+                    # Here is the issue link to PyTorch
+                    # https://github.com/pytorch/pytorch/issues/90261
+                    # So skip testing the indices
+                    # print('checking mode indice:')
+                    # self.assertEqual(output_indices_cpu, output_indices_xpu.cpu())
+                    del input_xpu
+
+        # consider host mem intensity, test dtype one by one to save mem
+        for tensor_dtype in [torch.int64, torch.bfloat16, torch.bool]:
+            input_list = []
+            if tensor_dtype == torch.bool:
+                # for bool, we small the size
+                value_range = 2
+            else:
+                value_range = 30
+            # one dim
+            input_list.append(torch.randint(value_range, (1024,)).to(dtype=tensor_dtype))
+            # one dim, large input
+            input_list.append(torch.randint(value_range, (4099,)).to(dtype=tensor_dtype))
+            # two dim, large input
+            input_list.append(torch.randint(value_range, (2048, 2048)).to(dtype=tensor_dtype))
+            # two dim, large input, odd number
+            input_list.append(torch.randint(value_range, (1025, 1999)).to(dtype=tensor_dtype))
+            # three dim, odd number
+            input_list.append(torch.randint(value_range, (9, 19, 1999)).to(dtype=tensor_dtype))
+            # four dim
+            input_list.append(torch.randint(value_range, (15, 127, 8, 128)).to(dtype=tensor_dtype))
+            mode_test_helper(input_list)
