c69c34d9bb342b0ff04952657471687af05e3313
Yu, Guangye <guangye.yu@intel.com>
2023-05-15 05:44:11 +0000

    fix to_sparse

--- csrc/gpu/aten/operators/SparseTensor.cpp
+++ csrc/gpu/aten/operators/SparseTensor.cpp
@@ -95,8 +95,66 @@ SparseTensor to_sparse(const Tensor& self, int64_t sparse_dim) {
   return sparse._coalesced_(true);
 }
 
-SparseTensor to_sparse(const Tensor& self) {
-  return to_sparse(self, self.dim());
+SparseTensor to_sparse(
+    const Tensor& self,
+    c10::optional<c10::Layout> layout,
+    OptionalIntArrayRef blocksize,
+    c10::optional<int64_t> dense_dim) {
+  if (layout.has_value()) {
+    if (blocksize.has_value() &&
+        !(*layout == kSparseBsr || *layout == kSparseBsc)) {
+      AT_ERROR(
+          "to_sparse for ",
+          self.layout(),
+          " to ",
+          *layout,
+          " conversion does not use specified blocksize");
+    }
+    if (self.layout() == *layout) {
+      return self;
+    }
+    switch (*layout) {
+      case kStrided:
+        return self;
+      case kSparse:
+        return to_sparse(self, self.dim() - dense_dim.value_or(0));
+      case kSparseCsr:
+        return self.to_sparse_csr(dense_dim);
+      case kSparseCsc:
+        return self.to_sparse_csc(dense_dim);
+      case kSparseBsr:
+        if (blocksize.has_value()) {
+          return self.to_sparse_bsr(*blocksize, dense_dim);
+        }
+        AT_ERROR(
+            "to_sparse for ",
+            self.layout(),
+            " to ",
+            *layout,
+            " conversion requires blocksize");
+        break;
+      case kSparseBsc:
+        if (blocksize.has_value()) {
+          return self.to_sparse_bsc(*blocksize, dense_dim);
+        }
+        break;
+        AT_ERROR(
+            "to_sparse for ",
+            self.layout(),
+            " to ",
+            *layout,
+            " conversion requires blocksize");
+      default:
+        break;
+    }
+    AT_ERROR(
+        "to_sparse not implemented for ",
+        self.layout(),
+        " to ",
+        *layout,
+        " conversion");
+  }
+  return to_sparse(self, self.dim() - dense_dim.value_or(0));
 }
 
 } // namespace AtenIpexTypeXPU
