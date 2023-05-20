62ae9ecd6e77605c7c5222bda4c4631cd6187f79
zejun <zejun.chen@intel.com>
2023-05-20 12:24:30 +0800

    [frontend] remove legacy function in intel_extension_for_pytorch/__init__.py (#2665)
    * remove legacy function in intel_extension_for_pytorch/__init__.py
    
    ---------
    
    Signed-off-by: Chen, Zejun <zejun.chen@intel.com>
    Co-authored-by: Jinghui <jinghui.gu@intel.com>

--- intel_extension_for_pytorch/__init__.py
+++ intel_extension_for_pytorch/__init__.py
@@ -123,47 +123,6 @@ def version():
     print("submodule ideep sha:      {}".format(__cpu_ideep_gitrev__))
 
 
-def _find_dpcpp_home():
-    pass
-
-
-def _here_paths():
-    here = os.path.abspath(__file__)
-    ipex_path = os.path.dirname(here)
-    return ipex_path
-
-
-def include_paths():
-    """
-    Get the include paths required to build a C++ extension.
-
-    Returns:
-        A list of include path strings.
-    """
-    ipex_path = _here_paths()
-    lib_include = os.path.join(ipex_path, "include")
-    paths = [
-        lib_include,
-        # os.path.join(lib_include, 'more')
-    ]
-    return paths
-
-
-def library_paths():
-    """
-    Get the library paths required to build a C++ extension.
-
-    Returns:
-        A list of library path strings.
-    """
-    ipex_path = _here_paths()
-    lib_path = os.path.join(ipex_path, "lib")
-    paths = [
-        lib_path,
-    ]
-    return paths
-
-
 # Path to folder containing CMake definitions for torch ipex package
 cmake_prefix_path = os.path.join(os.path.dirname(__file__), "share", "cmake")
 
