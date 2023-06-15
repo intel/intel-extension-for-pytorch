6851857977223f8b6ebb443d0b33852ce78668b4
Pleaplusone <38376071+ganyi1996ppo@users.noreply.github.com>
2023-06-15 17:01:14 +0800

    add optimize transformers into __init__.py (#2782)

--- intel_extension_for_pytorch/__init__.py
+++ intel_extension_for_pytorch/__init__.py
@@ -98,7 +98,7 @@ from . import nn
 from . import jit
 from . import optim
 
-from .frontend import optimize, enable_onednn_fusion
+from .frontend import optimize, enable_onednn_fusion, optimize_transformers
 from .frontend import enable_auto_channels_last, disable_auto_channels_last
 from .frontend import set_fp32_math_mode, get_fp32_math_mode, FP32MathMode
 from .cpu._auto_kernel_selection import _enable_dnnl, _disable_dnnl, _using_dnnl
