320f4da8224857c87eee76259b2626a3324e7abc
Agata Radys <agata.radys@intel.com>
2023-07-17 15:08:44 +0200

    fix obsolete names (#2930)
    Co-authored-by: bjarzemb <bartlomiej.jarzembinski@intel.com>

--- tests/gpu/examples/test_verbose.py
+++ tests/gpu/examples/test_verbose.py
@@ -7,7 +7,7 @@ import tempfile
 class TestVerbose(TestCase):
     def test_verbose_on(self):
         fname = tempfile.mkdtemp() + "_case_verbose"
-        cmd = f"""DNNL_VERBOSE=2 python -c "import torch;import intel_extension_for_pytorch;\
+        cmd = f"""ONEDNN_VERBOSE=2 python -c "import torch;import intel_extension_for_pytorch;\
         conv = torch.nn.Conv2d(1, 10, 5, 1).to('xpu');d = torch.rand(1, 1, 112, 112).to('xpu');conv(d)" > {fname}"""
         os.system(cmd)
         f = open(fname)
@@ -22,7 +22,7 @@ class TestVerbose(TestCase):
 
     def test_verbose_off(self):
         fname = tempfile.mkdtemp() + "_case_verbose"
-        cmd = f"""DNNL_VERBOSE=0 python -c "import torch;import intel_extension_for_pytorch;\
+        cmd = f"""ONEDNN_VERBOSE=0 python -c "import torch;import intel_extension_for_pytorch;\
         conv = torch.nn.Conv2d(1, 10, 5, 1).to('xpu');d = torch.rand(1, 1, 112, 112).to('xpu');conv(d)" > {fname}"""
         os.system(cmd)
         f = open(fname)
