eed330e6816fd9f05919b6efd905eb289b8d8995
Yu, Guangye <guangye.yu@intel.com>
2023-07-18 07:47:39 +0000

    add comments about override

--- intel_extension_for_pytorch/_inductor/xpu/__init__.py
+++ intel_extension_for_pytorch/_inductor/xpu/__init__.py
@@ -3,4 +3,6 @@ from .utils import has_triton
 
 
 if has_triton():
+    # Here we have to override decode_device since it is hardcode with CUDA in
+    # PyTorch inductor.
     override_decode_device()
