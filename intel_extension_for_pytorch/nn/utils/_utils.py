83b38f69719205b2a8f85ca1db1c7bc2a74c28f3
Gan Yi <yi.gan@intel.com>
2023-08-02 07:33:07 +0000

    add condition on memory reserved

--- intel_extension_for_pytorch/nn/utils/_utils.py
+++ intel_extension_for_pytorch/nn/utils/_utils.py
@@ -462,8 +462,10 @@ def ipex_beam_search(
     # origin_input_ids size is [batch_size * beam_size, seq_len]
     out = torch.ops.torch_ipex.update_output_sequence(origin_input_ids, out, batch_size)
     IPEXTransformerAtten.release_all_static_cached_resources()
-    torch.xpu.synchronize()
-    torch.xpu.empty_cache()
+    reserved_mem = round(torch.xpu.memory_reserved() / 1024**3, 3)
+    if reserved_mem  > 50:
+        torch.xpu.synchronize()
+        torch.xpu.empty_cache()
     if hasattr(self, "token_latency") and self.token_latency:
         return out, latency_list
     return out
