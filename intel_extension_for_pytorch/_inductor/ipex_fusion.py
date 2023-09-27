2acad57d0cd61c9e74bbfb2a365d724676ccb039
jiayisunx <jiayi.sun@intel.com>
2023-09-27 15:17:16 +0800

    disable bmm_add fusion in ipex backend (#2108) (#2113)
    * disable bmm_add fusion in ipex backend
    
    * fix format issue
    
    Co-authored-by: Chunyuan WU <chunyuan.wu@intel.com>

--- intel_extension_for_pytorch/_inductor/ipex_fusion.py
+++ intel_extension_for_pytorch/_inductor/ipex_fusion.py
@@ -1,18 +1,17 @@
 import torch
-from torch._inductor.fx_passes.post_grad import register_lowering_pattern
-from torch._inductor.pattern_matcher import Arg, CallFunction, PatternMatcherPass, Match
-from torch._inductor.lowering import lowerings as L
+from torch._inductor.pattern_matcher import PatternMatcherPass
 
 patterns = PatternMatcherPass()
 
 
-@register_lowering_pattern(
-    CallFunction(
-        torch.ops.aten.add, CallFunction(torch.ops.aten.bmm, Arg(), Arg()), Arg()
-    ),
-)
-def bmm_add(match: Match, mat1, mat2, mat3):
-    return L[torch.ops.torch_ipex.bmm_add](mat3, mat1, mat2, 1.0)
+# disable bmm_add fusion since oneDNN matmul still has performance issue.
+# @register_lowering_pattern(
+#     CallFunction(
+#         torch.ops.aten.add, CallFunction(torch.ops.aten.bmm, Arg(), Arg()), Arg()
+#     ),
+# )
+# def bmm_add(match: Match, mat1, mat2, mat3):
+#     return L[torch.ops.torch_ipex.bmm_add](mat3, mat1, mat2, 1.0)
 
 
 def _ipex_fusion_passes(gm: torch.fx.GraphModule):
