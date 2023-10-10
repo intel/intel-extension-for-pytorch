9f3f06d62938ccd130a19b379b5801b27ca6644c
min-jean-cho <min.jean.cho@intel.com>
2023-10-09 18:15:29 -0700

    fix ut on windows (#3307)
    Co-authored-by: Jinghui <jinghui.gu@intel.com>

--- tests/gpu/examples/test_verbose.py
+++ tests/gpu/examples/test_verbose.py
@@ -7,9 +7,11 @@ import tempfile
 class TestVerbose(TestCase):
     def test_verbose_on(self):
         fname = tempfile.mkdtemp() + "_case_verbose"
-        cmd = f"""ONEDNN_VERBOSE=2 python -c "import torch;import intel_extension_for_pytorch;\
+        os.environ['ONEDNN_VERBOSE'] = '2'
+        cmd = f"""python -c "import torch;import intel_extension_for_pytorch;\
         conv = torch.nn.Conv2d(1, 10, 5, 1).to('xpu');d = torch.rand(1, 1, 112, 112).to('xpu');conv(d)" > {fname}"""
         os.system(cmd)
+        del os.environ['ONEDNN_VERBOSE']
         f = open(fname)
         num = 0
         for line in f.readlines():
@@ -22,9 +24,11 @@ class TestVerbose(TestCase):
 
     def test_verbose_off(self):
         fname = tempfile.mkdtemp() + "_case_verbose"
-        cmd = f"""ONEDNN_VERBOSE=0 python -c "import torch;import intel_extension_for_pytorch;\
+        os.environ['ONEDNN_VERBOSE'] = '0'
+        cmd = f"""python -c "import torch;import intel_extension_for_pytorch;\
         conv = torch.nn.Conv2d(1, 10, 5, 1).to('xpu');d = torch.rand(1, 1, 112, 112).to('xpu');conv(d)" > {fname}"""
         os.system(cmd)
+        del os.environ['ONEDNN_VERBOSE']
         f = open(fname)
         num = 0
         for line in f.readlines():
