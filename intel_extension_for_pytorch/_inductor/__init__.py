b8d12f7ba52e4c316f715d23a2d6dc4ac93e62b3
Yu, Guangye <106960996+guangyey@users.noreply.github.com>
2023-10-15 16:22:21 +0800

    [Inductor] mute assert_size_stride (#3350)
    * mute assert_size_stride
    
    ---------
    
    Co-authored-by: Jinghui <jinghui.gu@intel.com>

--- intel_extension_for_pytorch/_inductor/__init__.py
+++ intel_extension_for_pytorch/_inductor/__init__.py
@@ -1,9 +1,12 @@
 import torch
+
+from .overrides import override_size_asserts
+
 from torch._inductor.codegen.common import register_backend_for_device
 
 from .codegen.triton import XPUTritonScheduling
 from .codegen.wrapper import XPUTritonWrapperCodeGen
 
-# Register triton XPU backend in PyTorch _inductor.
 if torch.xpu.is_available():
+    override_size_asserts()
     register_backend_for_device("xpu", XPUTritonScheduling, XPUTritonWrapperCodeGen)
---
+++ intel_extension_for_pytorch/_inductor/overrides.py
@@ -0,0 +1,24 @@
+import os
+import sys
+
+import torch
+
+def override_size_asserts():
+    """
+    We have to mute assert_size_stride here because conv1D in IPEX has the
+    support feature of the channel last. But conv1D in PyTorch has no support
+    for channel last, resulting in a stride size assertion. So we have to turn
+    off `TORCHINDUCTOR_SIZE_ASSERTS`.
+    """
+    if "torch._inductor.config" not in sys.modules:
+        print("1")
+        os.environ["TORCHINDUCTOR_SIZE_ASSERTS"] = "0"
+        return
+
+    from torch._inductor import config
+    config.size_asserts = False
+
+    if "torch._inductor.ir" not in sys.modules:
+        torch._inductor.config = config
+    else:
+        torch._inductor.ir.config = config
