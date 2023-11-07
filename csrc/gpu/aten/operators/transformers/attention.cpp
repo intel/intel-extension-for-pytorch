b44bd6ceb5ff4dc43762d86d46dcb57b339226c9
guanbaoy <108706657+guanbaoy@users.noreply.github.com>
2023-11-07 19:48:17 +0800

    SDP: convert fp16 inputs to fp32 in naive math path (#3440)
    * Convert fp16 inputs to fp32 in sdp_math
    
    * Fix clang format error
    
    ---------
    
    Co-authored-by: Ye Ting <ting.ye@intel.com>

--- csrc/gpu/aten/operators/transformers/attention.cpp
+++ csrc/gpu/aten/operators/transformers/attention.cpp
@@ -1,4 +1,5 @@
 #include <ATen/ATen.h>
+#include <ATen/TensorSubclassLikeUtils.h>
 #include <ATen/record_function.h>
 #include <runtime/Utils.h>
 #include <utils/DPCPP.h>
@@ -77,6 +78,119 @@ inline Tensor _scaled_dot_product_efficient_attention_impl(
   // auto result = naive_scaled_dot_product(query, key, value, is_causal);
   // return std::forward_as_tuple(std::get<0>(result), std::get<1>(result));
 #endif
+}
+
+std::tuple<Tensor, Tensor> _scaled_dot_product_attention_math_impl(
+    const Tensor& query_,
+    const Tensor& key,
+    const Tensor& value,
+    const c10::optional<Tensor>& attn_mask_,
+    double dropout_p,
+    bool is_causal,
+    const c10::optional<Tensor>& dropout_mask,
+    c10::optional<double> scale) {
+  if (query_.is_nested() || key.is_nested() || value.is_nested()) {
+    TORCH_CHECK(
+        query_.is_contiguous() && key.is_contiguous() && value.is_contiguous(),
+        "scaled_dot_product_attention: If inputs are nested tensors they must be contiguous");
+  }
+  auto attn_mask = attn_mask_;
+  // Naive, composite implementation defined here.
+
+  // Scale q, k before matmul for stability see https://tinyurl.com/sudb9s96 for
+  // math
+  bool is_negative_scaling = scale.has_value() && scale.value() < 0.0;
+  const auto scaling_factor =
+      sdp::calculate_scale(
+          query_, is_negative_scaling ? std::abs(scale.value()) : scale)
+          .sqrt();
+
+  const auto query = query_ *
+      (is_negative_scaling ? c10::SymFloat(0.0) - scaling_factor
+                           : scaling_factor);
+  if (is_causal) {
+    TORCH_CHECK(
+        !attn_mask.has_value(),
+        "_scaled_dot_product_attention: Explicit attn_mask should not be set when is_causal=True");
+    TORCH_CHECK(
+        !query.is_nested() && !key.is_nested(),
+        "_scaled_dot_product_attention: Nested tensors for query / key are not supported when is_causal=True");
+
+    // Replace attn_mask with causal mask; lower triangular elements take part
+    // in attention.
+    const auto L = query.sym_size(-2), S = key.sym_size(-2);
+    attn_mask =
+        at::ones_symint({L, S}, query.options().dtype(at::kBool)).tril();
+    attn_mask = sdp::convert_boolean_attn_mask(attn_mask, query.dtype());
+  }
+  auto attn = at::matmul(query, key.transpose(-2, -1) * scaling_factor);
+  if (attn_mask.has_value()) {
+    if (at::areAnyTensorSubclassLike({attn, *attn_mask})) {
+      attn = attn.add(*attn_mask);
+    } else {
+      attn.add_(*attn_mask);
+    }
+  }
+  attn = at::softmax(attn, -1);
+  if (dropout_p > 0.0) {
+    if (dropout_mask.has_value()) {
+      // In order to validate the correctness of the fused kernels, we need to
+      // use the same dropout mask in order to compare the results.
+      TORCH_WARN_ONCE("Dropout mask should only be used for testing purposes.");
+      attn = attn.masked_fill(dropout_mask->logical_not(), 0.0);
+      auto dropout_scaling = 1.0 / (1 - dropout_p);
+      return std::make_tuple(at::matmul(attn, value * dropout_scaling), attn);
+    } else {
+      attn = at::dropout(attn, dropout_p, true);
+    }
+  }
+
+  return std::make_tuple(at::matmul(attn, value), attn);
+}
+
+std::tuple<Tensor, Tensor> _scaled_dot_product_attention_math(
+    const Tensor& query_,
+    const Tensor& key,
+    const Tensor& value,
+    const c10::optional<Tensor>& attn_mask_,
+    double dropout_p,
+    bool is_causal,
+    const c10::optional<Tensor>& dropout_mask,
+    c10::optional<double> scale) {
+  // on ATSM, the efficient_attention path is not available
+  // With naive math path, oneDNN matmul has overflow issue with fp16 inputs
+  // as a WA, convert fp16 inputs to fp32
+  return IPEX_DISPATCH_FLOATING_TYPES_AND2(
+      at::kHalf,
+      at::kBFloat16,
+      query_.scalar_type(),
+      "scaled_dot_product_attention_math",
+      [&] {
+        bool is_half = std::is_same<scalar_t, at::Half>::value;
+        if (is_half) {
+          Tensor query_fp32 = query_.to(at::kFloat);
+          Tensor key_fp32 = key.to(at::kFloat);
+          Tensor value_fp32 = value.to(at::kFloat);
+          return _scaled_dot_product_attention_math_impl(
+              query_fp32,
+              key_fp32,
+              value_fp32,
+              attn_mask_,
+              dropout_p,
+              is_causal,
+              dropout_mask,
+              scale);
+        }
+        return _scaled_dot_product_attention_math_impl(
+            query_,
+            key,
+            value,
+            attn_mask_,
+            dropout_p,
+            is_causal,
+            dropout_mask,
+            scale);
+      });
 }
 
 std::tuple<Tensor, Tensor, Tensor, Tensor>
--- csrc/gpu/aten/operators/transformers/sdp_utils.h
+++ csrc/gpu/aten/operators/transformers/sdp_utils.h
@@ -1,4 +1,7 @@
 #pragma once
+#include <ATen/ATen.h>
+#include <ATen/core/Tensor.h>
+#include <c10/core/SymFloat.h>
 
 namespace sdp {
 
@@ -10,5 +13,34 @@ enum class SDPBackend {
   flash_attention = 1,
   efficient_attention = 2
 };
+
+inline c10::SymFloat calculate_scale(
+    const at::Tensor& query,
+    c10::optional<double> scale) {
+  const auto softmax_scale = scale.has_value()
+      ? scale.value()
+      : (c10::SymFloat(1.0) / (c10::SymFloat(query.sym_size(-1)).sqrt()));
+  return c10::SymFloat(softmax_scale);
+}
+
+c10::optional<at::Tensor> convert_boolean_attn_mask(
+    const c10::optional<at::Tensor>& attn_mask,
+    caffe2::TypeMeta dtype) {
+  // Pass through
+  if (!attn_mask.has_value()) {
+    return c10::nullopt;
+  }
+  // Convert boolean mask to additive mask; need to invert mask to indicate what
+  // to mask *out*.
+  if (attn_mask->dtype() == at::kBool) {
+    auto new_attn_mask = at::zeros_like(attn_mask.value(), dtype);
+    // TODO Use the max type of the input and output
+    new_attn_mask.masked_fill_(
+        attn_mask->logical_not(), -std::numeric_limits<double>::infinity());
+    return new_attn_mask;
+  }
+  // Otherwise, attn_mask represents an additive attention tensor
+  return attn_mask;
+}
 
 } // namespace sdp
