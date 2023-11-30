7baaaa9fdffe2d23ebf3500356f77cef7e462eb7
zhuhong61 <95205772+zhuhong61@users.noreply.github.com>
2023-11-30 14:29:02 +0800

    fix prepare_kv_cache when beam_size changes in multi runs (#3540)
    Co-authored-by: Chen, Zhenhuan <zhenhuan.chen@intel.com>
    Co-authored-by: Ye Ting <ting.ye@intel.com>

--- intel_extension_for_pytorch/transformers/models/xpu/optimize_transformers/modules/transformer_modules/Attention.py
+++ intel_extension_for_pytorch/transformers/models/xpu/optimize_transformers/modules/transformer_modules/Attention.py
@@ -69,11 +69,9 @@ class IPEXTransformerAttnOptimizedFp16(IPEXTransformerAttnNaive):
 
     def prepare_kv_cache(self, hidden_states, kv_head):
         bs_beam, seq_len, embed_dim = self.get_runtime_shape(hidden_states)
-        batch_size = bs_beam // self.beam_size
         if (
             self.runtime_cache.key_cache is None
-            or self.runtime_cache.value_cache is None
-            or batch_size != self.batch_size
+            or self.runtime_cache.key_cache.shape[1] != bs_beam
         ):
             self.prompt_len = seq_len
             cache_len = (
@@ -88,7 +86,6 @@ class IPEXTransformerAttnOptimizedFp16(IPEXTransformerAttnNaive):
             self.runtime_cache.value_cache = torch.empty(
                 cache_shape, device=hidden_states.device, dtype=hidden_states.dtype
             )
-            self.batch_size = batch_size
 
         elif self.ready_for_runtime_cache_update():
             old_cache_len = self.runtime_cache.key_cache.size(0)
