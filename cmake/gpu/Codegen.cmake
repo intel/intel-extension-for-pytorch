a5281633e3bb785c8abad5a8c0d48fe5be25d8e1
zejun <zejun.chen@intel.com>
2023-12-21 15:44:22 +0800

    [NestedTensor1.0] Add 1st part ops(66) support for NestedTensorXPU (#3504)
    * [NestedTensor] Add 13 ops support for NestedTensorXPU
    * Add generate for nested ops
    * Fix the schema name error in nestedtensorxpu_functions.yaml
    * Add to_copy for nested tesnor xpu
    * Add div.Tensor for nested xpu tensor
    * Add gelu support for nested xpu tensor
    * Use torch gen tool for generating the nested op impl
    * make up nested cos into the native functions yaml file
    * Add unimplemented ops in tests/gpu/pytorch/config/dynamic_skipped_cases_list.yaml
    
    
    ---------
    
    Signed-off-by: Chen, Zejun <zejun.chen@intel.com>

--- cmake/gpu/Codegen.cmake
+++ cmake/gpu/Codegen.cmake
@@ -34,11 +34,35 @@ endfunction(GEN_BACKEND)
 GEN_BACKEND(xpu_functions.yaml XPUNativeFunctions.h RegisterXPU.cpp RegisterAutogradXPU.cpp)
 GEN_BACKEND(quantizedxpu_functions.yaml QuantizedXPUNativeFunctions.h RegisterQuantizedXPU.cpp)
 GEN_BACKEND(sparsexpu_functions.yaml SparseXPUNativeFunctions.h RegisterSparseXPU.cpp)
+GEN_BACKEND(nestedtensorxpu_functions.yaml NestedTensorXPUNativeFunctions.h RegisterNestedTensorXPU.cpp)
+
+Function(GEN_NATIVE_IMPL file_yaml file_impl file_head)
+        set(source_file ${PROJECT_SOURCE_DIR}/scripts/tools/torchgen/yaml/${file_yaml})
+        set(output_file ${BUILD_IPEX_GPU_ATEN_GENERATED}/${file_impl})
+        set(header_file ${file_head})
+        set(GENERATE_IMPL "--generate_impl")
+        add_custom_command(OUTPUT
+                ${output_file}
+                COMMAND
+                "${PYTHON_EXECUTABLE}" -m torchgen.gen_backend_stubs
+                --source_yaml ${source_file}
+                --header_file ${header_file}
+                --output_impl_file ${output_file}
+                ${GENERATE_IMPL}
+                ${SIMPLE_TRACE}
+                WORKING_DIRECTORY ${IPEX_ROOT_DIR}/scripts/tools
+                DEPENDS
+                ${source_file})
+endfunction(GEN_NATIVE_IMPL)
+
+GEN_NATIVE_IMPL(nestedtensorxpu_functions.yaml NestedTensorNativeImplOps.cpp NestedTensorXPUNativeFunctions.h)
 
 list(APPEND gpu_generated_src ${BUILD_IPEX_GPU_ATEN_GENERATED}/RegisterXPU.cpp
         ${BUILD_IPEX_GPU_ATEN_GENERATED}/RegisterAutogradXPU.cpp
         ${BUILD_IPEX_GPU_ATEN_GENERATED}/RegisterQuantizedXPU.cpp
-        ${BUILD_IPEX_GPU_ATEN_GENERATED}/RegisterSparseXPU.cpp)
+        ${BUILD_IPEX_GPU_ATEN_GENERATED}/RegisterSparseXPU.cpp
+        ${BUILD_IPEX_GPU_ATEN_GENERATED}/RegisterNestedTensorXPU.cpp
+        ${BUILD_IPEX_GPU_ATEN_GENERATED}/NestedTensorNativeImplOps.cpp)
 
 add_custom_target(IPEX_GPU_GEN_TARGET DEPENDS ${gpu_generated_src})
 set(IPEX_GPU_GEN_FILES ${gpu_generated_src})
