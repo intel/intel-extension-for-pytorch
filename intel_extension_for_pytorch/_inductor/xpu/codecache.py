b128f0b40563e266a9dac004079e9817419923c4
Stonepia <tong.su@intel.com>
2024-01-28 21:57:58 -0800

    align AsyncCompile with upstream

--- intel_extension_for_pytorch/_inductor/xpu/codecache.py
+++ intel_extension_for_pytorch/_inductor/xpu/codecache.py
@@ -6,9 +6,15 @@ import sys
 from concurrent.futures import ProcessPoolExecutor
 from threading import Thread
 from time import sleep
+from types import ModuleType
+from typing import Any, Callable, Dict, List, Optional, Set, Tuple, TYPE_CHECKING, Union
 
 import torch
 
+from torch._dynamo.device_interface import (
+    get_interface_for_device,
+    get_registered_device_interfaces,
+)
 from torch._inductor import config
 from torch._inductor.codecache import (
     _compile_start,
@@ -59,12 +65,15 @@ class XPUAsyncCompile(AsyncCompile):
         multiprocessing.util.Finalize(None, pool.shutdown, exitpriority=sys.maxsize)
         return pool
 
-    def triton(self, kernel_name, source_code):
+    def triton(
+        self, kernel_name: str, source_code: str, device_str: str = "xpu"
+    ) -> Union[TritonFuture, ModuleType]:
         _compile_start()
 
         if config.compile_threads > 1:
-            device = torch.device("xpu", torch.xpu.current_device())
-            cc = None
+            device_interface = get_interface_for_device(device_str)
+            device = torch.device(device_str, device_interface.current_device())
+            cc = device_interface.get_compute_capability(device)
             future = self.process_pool().submit(
                 _worker_compile, kernel_name, source_code, cc, device
             )
