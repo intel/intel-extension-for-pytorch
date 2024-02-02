a7f950eb0bd0e93957b19f82ac2f74a8cd231bb1
Ziming Tan <ziming.tan@intel.com>
2024-02-02 10:07:34 +0800

    support Half type for hardtanh_backward (#3742)
    * support Half type for hardtanh_backward
    * add unit test for half type

--- tests/gpu/examples/test_hardtanh.py
+++ tests/gpu/examples/test_hardtanh.py
@@ -6,13 +6,13 @@ import intel_extension_for_pytorch  # noqa
 
 
 cpu_device = torch.device("cpu")
-dpcpp_device = torch.device("xpu")
+xpu_device = torch.device("xpu")
 
 
 class TestNNMethod(TestCase):
     def test_hardtanh(self, dtype=torch.float):
         grad_output_cpu = torch.randn([1, 1, 8, 8])
-        grad_output_dpcpp = grad_output_cpu.xpu()
+        grad_output_xpu = grad_output_cpu.xpu()
 
         # cpu
         linear = nn.Linear(8, 8)
@@ -29,21 +29,68 @@ class TestNNMethod(TestCase):
         print("linear grad", linear_weight_grad_cpu)
         linear.zero_grad()
 
-        # dpcpp
-        linear_dpcpp = linear.to("xpu")
-        tanh_dpcpp = tanh.to("xpu")
-        print("dpcpp linear weight", linear_dpcpp.weight.cpu())
-        x_dpcpp = x_cpu.to("xpu")
-        print("x_dpcpp", x_dpcpp.cpu())
-        z_dpcpp = linear_dpcpp(x_dpcpp)
-        print("z_dpcpp", z_dpcpp.cpu())
-        y_dpcpp = tanh(z_dpcpp)
-        print("y_dpcpp", y_dpcpp.cpu())
-        y_dpcpp.backward(grad_output_dpcpp)
-        linear_weight_grad_dpcpp = linear.weight.grad.clone()
-        print("dpcpp linear grad", linear_weight_grad_dpcpp.cpu())
-        linear_dpcpp.zero_grad()
+        # xpu
+        linear_xpu = linear.to("xpu")
+        tanh_xpu = tanh.to("xpu")
+        print("xpu linear weight", linear_xpu.weight.cpu())
+        x_xpu = x_cpu.to("xpu")
+        print("x_xpu", x_xpu.cpu())
+        z_xpu = linear_xpu(x_xpu)
+        print("z_xpu", z_xpu.cpu())
+        y_xpu = tanh(z_xpu)
+        print("y_xpu", y_xpu.cpu())
+        y_xpu.backward(grad_output_xpu)
+        linear_weight_grad_xpu = linear.weight.grad.clone()
+        print("xpu linear grad", linear_weight_grad_xpu.cpu())
+        linear_xpu.zero_grad()
 
-        self.assertEqual(z_cpu, z_dpcpp)
-        self.assertEqual(y_cpu, y_dpcpp)
-        self.assertEqual(linear_weight_grad_cpu, linear_weight_grad_dpcpp)
+        self.assertEqual(z_cpu, z_xpu)
+        self.assertEqual(y_cpu, y_xpu)
+        self.assertEqual(linear_weight_grad_cpu, linear_weight_grad_xpu)
+
+    def test_hardtanh_half(self, dtype=torch.float):
+        grad_output_cpu = torch.randn([1, 1, 8, 8])
+        grad_output_xpu = grad_output_cpu.to("xpu", dtype=torch.float16)
+
+        # cpu
+        linear = nn.Linear(8, 8)
+        tanh = nn.Hardtanh()
+        print("linear weight", linear.weight)
+        x_cpu = torch.ones([1, 1, 8, 8], device=cpu_device, dtype=dtype)
+        print("x_cpu", x_cpu)
+        z_cpu = linear(x_cpu)
+        print("z_cpu", z_cpu)
+        y_cpu = tanh(z_cpu)
+        print("y_cpu", y_cpu)
+        y_cpu.backward(grad_output_cpu)
+        linear_weight_grad_cpu = linear.weight.grad.clone()
+        print("linear grad", linear_weight_grad_cpu)
+        linear.zero_grad()
+
+        # xpu
+        linear_xpu = linear.to("xpu", dtype=torch.float16)
+        tanh_xpu = tanh.to("xpu", dtype=torch.float16)
+        print("xpu linear weight", linear_xpu.weight.to("cpu", dtype=torch.float32))
+        x_xpu = x_cpu.to("xpu", dtype=torch.float16)
+        print("x_xpu", x_xpu.to("cpu", dtype=torch.float32))
+        z_xpu = linear_xpu(x_xpu)
+        print("z_xpu", z_xpu.to("cpu", dtype=torch.float32))
+        y_xpu = tanh(z_xpu)
+        print("y_xpu", y_xpu.to("cpu", dtype=torch.float32))
+        y_xpu.backward(grad_output_xpu)
+        linear_weight_grad_xpu = linear.weight.grad.clone()
+        print("xpu linear grad", linear_weight_grad_xpu.to("cpu", dtype=torch.float32))
+        linear_xpu.zero_grad()
+
+        self.assertEqual(
+            z_cpu, z_xpu.to("cpu", dtype=torch.float32), atol=1e-3, rtol=1e-3
+        )
+        self.assertEqual(
+            y_cpu, y_xpu.to("cpu", dtype=torch.float32), atol=1e-3, rtol=1e-3
+        )
+        self.assertEqual(
+            linear_weight_grad_cpu,
+            linear_weight_grad_xpu.to("cpu", dtype=torch.float32),
+            atol=1e-3,
+            rtol=1e-3,
+        )
