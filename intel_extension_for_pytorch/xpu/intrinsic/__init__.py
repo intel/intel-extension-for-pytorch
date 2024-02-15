b6b8b3f5528291d562829b81c897f53c7f219afb
Pleaplusone <38376071+ganyi1996ppo@users.noreply.github.com>
2024-02-15 11:37:35 +0800

    add custom ops for tgi upstream (#3757)
    * add custom ops for tgi upstream
    
    * fix flake8 issue

--- intel_extension_for_pytorch/xpu/intrinsic/__init__.py
+++ intel_extension_for_pytorch/xpu/intrinsic/__init__.py
@@ -17,6 +17,10 @@ __all__ = [
     "EMA",
     "clip_grad_norm_",
     "clip_grad_norm",
+    "varlen_fwd",
+    "reshape_and_cache",
+    "paged_attention_v1",
+    "paged_attention_v2",
 ]
 
 
@@ -145,4 +149,192 @@ def IpexSDP_Index(
         beta,
         dropout_p,
         is_causal,
+    )
+
+
+def varlen_fwd(
+    query,  # [total_q, num_head, head_size]
+    key,  # [total_k, num_head_k, head_size]
+    value,  # [total_k, num_head_k, head_size]
+    out,  # [total_q, num_head, head_size]
+    seqlen_q,  # [batch_size + 1]
+    seqlen_k,  # [batch_size + 1]
+    max_seqlen_q,
+    max_seqlen_k,
+    pdropout,
+    softmax_scale,
+    zero_tensors,
+    is_causal,
+    return_softmax,
+    gen_,
+):
+    assert return_softmax is False, "ipex do not support return_softmax option"
+    assert gen_ is None, "ipex do not support custom random generator"
+    assert zero_tensors is False, "ipex varlen_fwd do not support zero tensors"
+    total_q, num_head, head_size = query.size()
+    total_k, num_head_k, _ = key.size()
+    batch_size = seqlen_q.size(0) - 1
+    seqlen_q_ = seqlen_q.clone()
+    seqlen_q_[:batch_size] = seqlen_q[1:]
+    seqlen_q = (seqlen_q_ - seqlen_q)[:batch_size]
+    seqlen_k_ = seqlen_k.clone()
+    seqlen_k_[:batch_size] = seqlen_k[1:]
+    seqlen_k = (seqlen_k_ - seqlen_k)[:batch_size]
+
+    pad_q = torch.zeros(
+        [batch_size, max_seqlen_q, num_head, head_size],
+        dtype=query.dtype,
+        device=query.device,
+    )
+    pad_k = torch.zeros(
+        [batch_size, max_seqlen_k, num_head_k, head_size],
+        dtype=key.dtype,
+        device=key.device,
+    )
+    pad_v = torch.zeros(
+        [batch_size, max_seqlen_k, num_head_k, head_size],
+        dtype=value.dtype,
+        device=value.device,
+    )
+    q_mask = torch.arange(0, max_seqlen_q, device=query.device)[None, :].repeat(
+        batch_size, 1
+    )
+    q_mask = q_mask < seqlen_q[:, None].repeat(1, q_mask.size(-1))
+    k_mask = torch.arange(0, max_seqlen_k, device=key.device)[None, :].repeat(
+        batch_size, 1
+    )
+    k_mask = k_mask < seqlen_k[:, None].repeat(1, k_mask.size(-1))
+    align_mask_seqlen = (max_seqlen_k + 63) // 64 * 64
+    attn_mask = torch.empty(
+        [batch_size, 1, 1, align_mask_seqlen], dtype=torch.float16, device=query.device
+    ).fill_(float("-inf"))
+    attn_mask[:, :, :, :max_seqlen_k].masked_fill_(k_mask[:, None, None, :], 0)
+
+    pad_q[q_mask] = query
+    pad_k[k_mask] = key
+    pad_v[k_mask] = value
+
+    pad_q = pad_q.permute(0, 2, 1, 3)
+    pad_k = pad_k.permute(0, 2, 1, 3)
+    pad_v = pad_v.permute(0, 2, 1, 3)
+
+    out_ = torch.ops.torch_ipex.xetla_fsdp_forward_atten_mask_alibi_strided(
+        pad_q,
+        pad_k,
+        pad_v,
+        None,
+        attn_mask,
+        None,
+        softmax_scale,
+        1.0,
+        pdropout,
+        is_causal,
+        False,
+    )
+    out_ = out_.permute(0, 2, 1, 3)
+    if out is None:
+        return out_[q_mask]
+    else:
+        out.copy_(out_[q_mask])
+
+
+def reshape_and_cache(
+    key: torch.Tensor,
+    value: torch.Tensor,
+    key_cache: torch.Tensor,
+    value_cache: torch.Tensor,
+    slots: torch.Tensor,
+):
+    return torch.ops.torch_ipex.reshape_and_cache(
+        key, value, key_cache, value_cache, slots
+    )
+
+
+def IpexPaged_attention(
+    out,
+    query,
+    key_cache,
+    value_cache,
+    head_mapping,
+    block_tables,
+    context_lens,
+    head_scale,
+    block_size,
+    max_context_len,
+    alibi_scopes,
+):
+    return torch.ops.torch_ipex.xetla_paged_attention_v1(
+        out,
+        query,
+        key_cache,
+        value_cache,
+        head_mapping,
+        block_tables,
+        context_lens,
+        head_scale,
+        block_size,
+        max_context_len,
+        alibi_scopes,
+    )
+
+
+def paged_attention_v2(
+    out,
+    exp_sums,
+    max_logits,
+    tmp_out,
+    query,
+    key_cache,
+    value_cache,
+    head_mapping,
+    block_tables,
+    context_lens,
+    head_scale,
+    block_size,
+    max_context_len,
+    alibi_scopes,
+):
+    return torch.ops.torch_ipex.xetla_paged_attention_v2(
+        max_logits,
+        exp_sums,
+        tmp_out,
+        out,
+        query,
+        key_cache,
+        value_cache,
+        head_mapping,
+        block_tables,
+        context_lens,
+        head_scale,
+        block_size,
+        max_context_len,
+        alibi_scopes,
+    )
+
+
+def paged_attention_v1(
+    out,
+    query,
+    key_cache,
+    value_cache,
+    head_mapping,
+    head_scale,
+    block_tables,
+    context_lens,
+    block_size,
+    max_context_len,
+    alibi_scopes,
+):
+    return torch.ops.torch_ipex.xetla_paged_attention_v1(
+        out,
+        query,
+        key_cache,
+        value_cache,
+        head_mapping,
+        block_tables,
+        context_lens,
+        head_scale,
+        block_size,
+        max_context_len,
+        alibi_scopes,
     )
