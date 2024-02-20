588fcf06cf87ec5be6dfb2750d7e68a083a89ef9
blzheng <beilei.zheng@intel.com>
2024-02-20 10:36:03 +0800

    add qwen.py (#2616)

---
+++ examples/cpu/inference/python/llm/utils/model_class/qwen.py
@@ -0,0 +1,12 @@
+from .llm import LLMConfig, EXAMPLE_INPUTS_MODE
+
+class QwenConfig(LLMConfig):
+    def __init__(self, model_id):
+        self.name = "qwen"
+        self.model_id = model_id
+        self.to_channels_last = False
+        self.example_inputs_mode = EXAMPLE_INPUTS_MODE.MASK_KV
+
+        # for smooth quant
+        self.use_global_past_key_value = True
+        self.use_ipex_autotune = True
