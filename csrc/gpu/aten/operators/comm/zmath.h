26be453c0b31fea28f35a282a5e1878bd3b74440
Lei Zhenyuan <zhenyuan.lei@intel.com>
2024-03-29 12:12:22 +0800

    add pytorch license for ipex gpu file. (#3901)
    * add part pytorch license
    
    * add pytorch license for other parts
    
    * fix for clang format
    
    ---------
    
    Co-authored-by: Jinghui <jinghui.gu@intel.com>

--- csrc/gpu/aten/operators/comm/zmath.h
+++ csrc/gpu/aten/operators/comm/zmath.h
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 #pragma once
 
 // Complex number math operations that act as no-ops for other dtypes.
