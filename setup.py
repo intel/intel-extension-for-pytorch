78fe3c2250cc14341811fefecfe9bfe5801eabc2
Yizhou Wang <yizhou.wang@intel.com>
2024-04-11 15:58:43 +0800

    use fixed version for ds module (#4082)

--- setup.py
+++ setup.py
@@ -935,7 +935,11 @@ def get_src_py_and_dst():
 class IPEXEggInfoBuild(egg_info, object):
     def write_file(self, what, filename, data):
         if _get_build_target() == "bdist_wheel" and what == "requirements" and IS_LINUX:
-            data += "intel_extension_for_pytorch_deepspeed\n"
+            data += (
+                "intel_extension_for_pytorch_deepspeed=="
+                + str(get_version_num())
+                + "\n"
+            )
         super(IPEXEggInfoBuild, self).write_file(what, filename, data)
 
     def finalize_options(self):
