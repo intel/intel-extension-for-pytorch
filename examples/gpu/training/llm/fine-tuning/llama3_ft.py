b61410edf404669ad2de4e8c476639e1cd5fd095
zhuhong61 <95205772+zhuhong61@users.noreply.github.com>
2024-04-18 16:39:37 +0800

    Llama3: Enable FSDP fine-tuning (#4113)

--- examples/gpu/training/llm/fine-tuning/llama3_ft.py
+++ examples/gpu/training/llm/fine-tuning/llama3_ft.py
@@ -2,65 +2,55 @@
 import torch
 import intel_extension_for_pytorch
 import time
+import os
+
+from dataclasses import dataclass, field
+from typing import Dict, Optional, Sequence
 
 from datasets import load_dataset
+import transformers
 from transformers import AutoModelForSeq2SeqLM, AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
 from transformers import TrainingArguments, Trainer
-import argparse
 
+from peft import LoraConfig, get_peft_model
+from packaging import version
 
-parser = argparse.ArgumentParser("fine-tuning script", add_help=False)
-parser.add_argument(
-    "-m",
-    "--model_name",
-    type=str,
-    default="llama3",
-    help="the path of model",
-)
-args = parser.parse_args()
+@dataclass
+class ModelArguments:
+    model_name_or_path: Optional[str] = field(default="facebook/opt-125m")
+    use_flashattn: bool = field(
+        default=False,
+        metadata={
+            "help": "Whether to use flash attention, now we support [`torch.nn.functional.scaled_dot_product_attention`]"
+        })   
+    use_peft: bool = field(
+        default=False,
+        metadata={
+            "help": "Whether to use PEFT, default is LoRA`]"
+        })   
+    save_model: bool = field(
+        default=False,
+        metadata={
+            "help": "Whether to save model`]"
+        })    
 
-start = time.time()
-
-def print_trainable_parameters(model):
-    """
-    Prints the number of trainable parameters in the model.
-    """
-    trainable_params = 0
-    all_param = 0
-    for _, param in model.named_parameters():
-        all_param += param.numel()
-        if param.requires_grad:
-            trainable_params += param.numel()
-    print(
-        f"trainable params: {trainable_params} || all params: {all_param} || trainable%: {100 * trainable_params / all_param}"
+@dataclass
+class TrainingArguments(transformers.TrainingArguments):
+    custom_mp: bool = field(
+        default=False,
+        metadata={
+            "help": "Use custom mixed precision setting for PEFT LoRA, keep base model weight as bfloat16 and LoRA weight as float32. This setting is for fine-tuning on single card."
+        })
+    max_seq_length: int = field(
+        default=128,
+        metadata={"help": "Maximum sequence length. Sequences will be right padded (and possibly truncated)."},
     )
+    inf_test: bool = field(
+        default=False,
+        metadata={
+            "help": "Test an example for inference."
+        })    
     
-# load model and tokenizer
-tokenizer = AutoTokenizer.from_pretrained(args.model_name)
-if tokenizer.pad_token_id is None:
-    tokenizer.pad_token_id = tokenizer.eos_token_id
-
-model = AutoModelForCausalLM.from_pretrained(args.model_name, torch_dtype = torch.bfloat16)
-
-device = 'xpu'
-model = model.to(device)
-
-# PEFT LoRA setting
-from peft import LoraConfig, get_peft_model, TaskType
-target_modules = ["q_proj", "v_proj"]
-
-lora_config = LoraConfig(
-    r=16, lora_alpha=32, target_modules=target_modules, lora_dropout=0.05, bias="none", task_type="CAUSAL_LM"
-)
-model = get_peft_model(model, lora_config)
-
-# Mixed precision for LoRA: keep base weight as bf16, and lora to fp32
-for name, child in model.named_modules():
-    if ("lora_A" in name) or ("lora_B" in name):   
-        child = child.to(torch.float32)
-
-print_trainable_parameters(model)
-
 # loading dataset
 dataset = load_dataset("financial_phrasebank", "sentences_allagree")
 dataset = dataset["train"].train_test_split(test_size=0.1)
@@ -77,10 +67,9 @@ dataset = dataset.map(
 # data preprocessing
 text_column = "sentence"
 label_column = "text_label"
-max_length = 128
 
 
-def preprocess_function(examples):
+def preprocess_function(examples, tokenizer, max_length):
     batch_size = len(examples[text_column])
     inputs = [f"{text_column} : {x} Label : " for x in examples[text_column]]
     targets = [str(x) for x in examples[label_column]]
@@ -110,48 +99,110 @@ def preprocess_function(examples):
     model_inputs["labels"] = labels["input_ids"]
     return model_inputs
 
+def print_trainable_parameters(model):
+    """
+    Prints the number of trainable parameters in the model.
+    """
+    trainable_params = 0
+    all_param = 0
+    for _, param in model.named_parameters():
+        all_param += param.numel()
+        if param.requires_grad:
+            trainable_params += param.numel()
+    print(
+        f"trainable params: {trainable_params} || all params: {all_param} || trainable%: {100 * trainable_params / all_param}"
+    )
+    
+    
+def train():
+    parser = transformers.HfArgumentParser((ModelArguments, TrainingArguments))
+    model_args, training_args = parser.parse_args_into_dataclasses()
+    start = time.time()
 
-processed_datasets = dataset.map(
-    preprocess_function,
-    batched=True,
-    num_proc=1,
-    remove_columns=dataset["train"].column_names,
-    load_from_cache_file=False,
-    desc="Running tokenizer on dataset",
-)
+    # distributed setup
+    os.environ['CCL_PROCESS_LAUNCHER'] = 'none'
 
-train_dataset = processed_datasets["train"]
-eval_dataset = processed_datasets["validation"]
+    local_rank = int(os.getenv("LOCAL_RANK", "0"))
+    world_size = int(os.getenv("WORLD_SIZE", "1"))
+    os.environ['CCL_LOCAL_SIZE'] = str(world_size)
+    os.environ['CCL_LOCAL_RANK'] = str(local_rank)
 
-training_args = TrainingArguments(
-    output_dir="output",
-    evaluation_strategy="epoch",
-    learning_rate=1e-3,
-    gradient_accumulation_steps=1,
-    auto_find_batch_size=True,
-    num_train_epochs=1,
-    save_steps=100,
-    logging_steps=1,
-    save_total_limit=8,
-)
+    # load model and tokenizer
+    tokenizer = AutoTokenizer.from_pretrained(model_args.model_name_or_path)
+    if tokenizer.pad_token_id is None:
+        tokenizer.pad_token_id = tokenizer.eos_token_id
 
-trainer = Trainer(
-    model=model,
-    args=training_args,
-    train_dataset=train_dataset,
-    eval_dataset=eval_dataset,
-)
-model.config.use_cache = True  # silence the warnings. Please re-enable for inference!
+    if version.parse(transformers.__version__) >= version.parse("4.37.0"):
+        model = AutoModelForCausalLM.from_pretrained(
+            model_args.model_name_or_path,
+            torch_dtype = torch.bfloat16 if training_args.custom_mp else torch.float32, 
+            attn_implementation="sdpa" if model_args.use_flashattn else "eager")
+    else:
+        model = AutoModelForCausalLM.from_pretrained(
+            model_args.model_name_or_path,
+            torch_dtype = torch.bfloat16 if training_args.custom_mp else torch.float32)        
 
-trainer.train()
+    # PEFT LoRA setting
+    if model_args.use_peft:
+        lora_config = LoraConfig(
+            r=16, lora_alpha=32, target_modules=["q_proj", "v_proj"], lora_dropout=0.05, bias="none", task_type="CAUSAL_LM"
+        )
+        model = get_peft_model(model, lora_config)
+    
+    if training_args.custom_mp:
+        # custom_mp is for single card
+        device = 'xpu'
+        model = model.to(device)
+        
+        # Mixed precision for LoRA: keep base weight as bf16, and lora to fp32
+        for name, child in model.named_modules():
+            if ("lora_A" in name) or ("lora_B" in name):   
+                child = child.to(torch.float32)
 
-model.eval()
-input_text = "In January-September 2009 , the Group 's net interest income increased to EUR 112.4 mn from EUR 74.3 mn in January-September 2008 ."
-inputs = tokenizer(input_text, return_tensors="pt").to(device)
+    print_trainable_parameters(model)
 
-outputs = model.generate(input_ids=inputs["input_ids"], max_new_tokens=10)
+    processed_datasets = dataset.map(
+        preprocess_function,
+        batched=True,
+        num_proc=1,
+        remove_columns=dataset["train"].column_names,
+        load_from_cache_file=False,
+        desc="Running tokenizer on dataset",
+        fn_kwargs={"tokenizer": tokenizer, "max_length": training_args.max_seq_length}
+    )
 
-print("input sentence: ", input_text)
-print(" output prediction: ", tokenizer.batch_decode(outputs.detach().cpu().numpy(), skip_special_tokens=True))
+    train_dataset = processed_datasets["train"]
+    eval_dataset = processed_datasets["validation"]
 
-print("total time = %.2f sec." % (time.time() - start))
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        train_dataset=train_dataset,
+        eval_dataset=eval_dataset,
+    )
+    
+    # set fsdp wrap policy, ensure the LoRA part could be warpped separately
+    if model_args.use_peft and trainer.is_fsdp_enabled:
+        from peft.utils.other import fsdp_auto_wrap_policy
+        fsdp_plugin = trainer.accelerator.state.fsdp_plugin
+        fsdp_plugin.auto_wrap_policy = fsdp_auto_wrap_policy(trainer.model)
+
+    model.config.use_cache = True  # silence the warnings. Please re-enable for inference!
+
+    trainer.train()
+    
+    if training_args.inf_test:
+        model.eval()
+        input_text = "In January-September 2009 , the Group 's net interest income increased to EUR 112.4 mn from EUR 74.3 mn in January-September 2008 ."
+        inputs = tokenizer(input_text, return_tensors="pt").to("xpu")
+
+        outputs = model.generate(input_ids=inputs["input_ids"], max_new_tokens=10)
+
+        print("input sentence: ", input_text)
+        print(" output prediction: ", tokenizer.batch_decode(outputs.detach().cpu().numpy(), skip_special_tokens=True))
+
+    print("total time = %.2f sec." % (time.time() - start))
+    
+    
+if __name__ == "__main__":
+    train()
--- examples/gpu/training/llm/fine-tuning/run_llama3_ft.sh
+++ examples/gpu/training/llm/fine-tuning/run_llama3_ft.sh
@@ -2,7 +2,84 @@
 # export http_proxy="http://child-jf.intel.com:912"
 # export https_proxy="http://child-jf.intel.com:912"
 
-# specify the model path
-model='path_to_llama3/llama3'
+# export ZE_AFFINITY_MASK=4
 
-python llama3_ft.py -m ${model} 2>&1 | tee llama3_ft.log
+# export PROFILE=1
+# export KINETO=1
+# export IPEX_ZE_TRACING=1
+# export DNNL_VERBOSE=1
+
+# settings for torchccl
+export TORCH_LLM_ALLREDUCE=1
+
+# For platform not support flash attention
+Run_llama3-8b_peft_singlecard() {
+
+    python llama3_ft.py \
+        --model_name_or_path ${model} \
+        --custom_mp True \
+        --use_peft True \
+        --max_seq_length 128 \
+        --output_dir="output" \
+        --evaluation_strategy="epoch" \
+        --learning_rate=1e-3 \
+        --auto_find_batch_size=True \
+        --num_train_epochs=1 \
+        --save_steps=100 \
+        --logging_steps=1 \
+        --save_total_limit=8 2>&1 | tee llama3-8b_ft_singlecard.log
+}
+
+Run_llama3-8b_peft_singlecard_flashattn() {
+
+    python llama3_ft.py \
+        --model_name_or_path ${model} \
+        --use_flashattn True \
+        --custom_mp True \
+        --use_peft True \
+        --max_seq_length 128 \
+        --output_dir="output" \
+        --evaluation_strategy="epoch" \
+        --learning_rate=1e-3 \
+        --auto_find_batch_size=True \
+        --num_train_epochs=1 \
+        --save_steps=500 \
+        --logging_steps=1 \
+        --save_total_limit=8 2>&1 | tee llama3-8b_ft_singlecard_flashattn.log
+}
+
+Run_llama3-8b_peft_fsdp() {
+
+    accelerate launch --main_process_port "29800" --config_file "fsdp_config.yaml"  llama3_ft.py \
+        --model_name_or_path ${model} \
+        --use_flashattn True \
+        --bf16 True \
+        --use_peft True \
+        --max_seq_length 128 \
+        --output_dir="output" \
+        --evaluation_strategy="epoch" \
+        --learning_rate=1e-3 \
+        --gradient_accumulation_steps=1 \
+        --per_device_train_batch_size=8 \
+        --per_device_eval_batch_size=8 \
+        --num_train_epochs=1 \
+        --save_steps=500 \
+        --logging_steps=1 \
+        --save_total_limit=8 \
+        --fsdp "full_shard auto_wrap" \
+        --fsdp_transformer_layer_cls_to_wrap 'LlamaDecoderLayer' 2>&1 | tee llama3-8b_ft_fsdp.log
+
+}
+
+
+main() {
+
+    model='/path_to_llama3/llama3-8b' 
+
+    Run_llama3-8b_peft_singlecard
+    Run_llama3-8b_peft_singlecard_flashattn
+    Run_llama3-8b_peft_fsdp
+
+}
+
+main
