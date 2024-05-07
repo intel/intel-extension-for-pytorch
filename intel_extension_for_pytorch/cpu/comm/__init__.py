1d27e5a6c8ea5383aa52a5c0e7eb550129b54b29
xingyuan li <108672484+hoshibara@users.noreply.github.com>
2024-05-07 14:53:37 +0800

    [Rebase] fix tests/cpu/test_ipex_tensor_parallel.py (#4225)
    Co-authored-by: Xiao Wang <wang.xiao@intel.com>

--- intel_extension_for_pytorch/cpu/comm/__init__.py
+++ intel_extension_for_pytorch/cpu/comm/__init__.py
@@ -4,5 +4,5 @@ import intel_extension_for_pytorch._C as torch_ipex_cpp
 get_world_size = torch_ipex_cpp.get_world_size
 get_rank = torch_ipex_cpp.get_rank
 barrier = torch_ipex_cpp.barrier
-# allreduce_add = torch.ops.torch_ipex.all_reduce_add
+allreduce_add = torch.ops.torch_ipex.all_reduce_add
 # allgather = torch.ops.torch_ipex.allgather
