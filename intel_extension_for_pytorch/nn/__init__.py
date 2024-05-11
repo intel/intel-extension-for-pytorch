b6a044cb8486c75ccf639c11c0106ca8e727f80f
Cui, Yifeng <yifeng.cui@intel.com>
2024-05-11 16:07:28 +0800

    Fix the conversion from BF16 torch tensor to ndarray when BUILD_WITH_CPU is OFF (#4201)
    Co-authored-by: Jinghui <jinghui.gu@intel.com>

--- intel_extension_for_pytorch/nn/__init__.py
+++ intel_extension_for_pytorch/nn/__init__.py
@@ -1,5 +1,6 @@
 from intel_extension_for_pytorch.utils.utils import has_cpu
 
+from . import functional
+
 if has_cpu():
     from .modules import FrozenBatchNorm2d
-    from . import functional
--- intel_extension_for_pytorch/nn/functional/__init__.py
+++ intel_extension_for_pytorch/nn/functional/__init__.py
@@ -1,4 +1,8 @@
-from ...cpu.nn import _embeddingbag
+from intel_extension_for_pytorch.utils.utils import has_cpu
+
 from . import _tensor_method
-from ...cpu.nn.interaction import interaction, InteractionFunc
-from ...cpu.nn import _roi_align_helper
+
+if has_cpu():
+    from ...cpu.nn import _embeddingbag
+    from ...cpu.nn.interaction import interaction, InteractionFunc
+    from ...cpu.nn import _roi_align_helper
