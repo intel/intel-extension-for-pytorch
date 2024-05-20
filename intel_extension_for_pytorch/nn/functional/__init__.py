264d3cf1f3261bd25265dab507d290f9fa318099
Yu, Guangye <106960996+guangyey@users.noreply.github.com>
2024-05-20 17:44:19 +0800

    [Rebase] fix import error when BUILD_WITH_CPU=OFF (#4267)

--- intel_extension_for_pytorch/nn/functional/__init__.py
+++ intel_extension_for_pytorch/nn/functional/__init__.py
@@ -6,3 +6,8 @@ if has_cpu():
     from ...cpu.nn import _embeddingbag
     from ...cpu.nn.interaction import interaction, InteractionFunc
     from ...cpu.nn import _roi_align_helper
+else:
+    _embeddingbag = None
+    interaction = None
+    InteractionFunc = None
+    _roi_align_helper = None
