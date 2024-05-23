41df96b8c79dc7ceb7fd643b38d1dc8b8a8815c3
Yu, Guangye <106960996+guangyey@users.noreply.github.com>
2024-05-23 21:28:14 +0800

    [Rebase] removed onednn dependency from native dpcpp extension ut (#4282)

--- tests/gpu/native/dpcpp_extension/test_sycl_queue.py
+++ tests/gpu/native/dpcpp_extension/test_sycl_queue.py
@@ -3,37 +3,31 @@ import intel_extension_for_pytorch  # noqa
 from torch.xpu.cpp_extension import load, IS_LINUX
 import os
 
-dnnl_path = os.getenv("DNNLROOT")
 
-if dnnl_path is not None:
-    module = load(
-        name="mod_test_sycl_queue",
-        sources=["test_sycl_queue.cpp"],
-        extra_cflags=["-O2"],
-        verbose=True,
-    )
+module = load(
+    name="mod_test_sycl_queue",
+    sources=["test_sycl_queue.cpp"],
+    extra_cflags=["-O2"],
+    verbose=True,
+)
 
-    import mod_test_sycl_queue
-    from torch.testing._internal.common_utils import TestCase
+import mod_test_sycl_queue
+from torch.testing._internal.common_utils import TestCase
 
-    class TestTorchMethod(TestCase):
-        def test_sycl_queue(self):
-            s = torch.xpu.current_stream()
-            q_ptr = s.sycl_queue
-            self.assertTrue(mod_test_sycl_queue.is_sycl_queue(q_ptr))
 
-            if IS_LINUX:
-                import ctypes
+class TestTorchMethod(TestCase):
+    def test_sycl_queue(self):
+        s = torch.xpu.current_stream()
+        q_ptr = s.sycl_queue
+        self.assertTrue(mod_test_sycl_queue.is_sycl_queue(q_ptr))
 
-                path = os.path.join(os.path.expanduser("~/.cache"), "torch_extensions")
-                for root, dirs, files in os.walk(path):
-                    if "mod_test_sycl_queue.so" in files:
-                        so_file = os.path.join(root, "mod_test_sycl_queue.so")
-                        break
-                lib = ctypes.CDLL(so_file, mode=ctypes.RTLD_GLOBAL)
-                self.assertTrue(lib.isSYCLQueue(s))
+        if IS_LINUX:
+            import ctypes
 
-else:
-    print(
-        "Please source <oneapi_dir>/dnnl/<version>/env/vars.sh, and re-run this test case."
-    )
+            path = os.path.join(os.path.expanduser("~/.cache"), "torch_extensions")
+            for root, dirs, files in os.walk(path):
+                if "mod_test_sycl_queue.so" in files:
+                    so_file = os.path.join(root, "mod_test_sycl_queue.so")
+                    break
+            lib = ctypes.CDLL(so_file, mode=ctypes.RTLD_GLOBAL)
+            self.assertTrue(lib.isSYCLQueue(s))
