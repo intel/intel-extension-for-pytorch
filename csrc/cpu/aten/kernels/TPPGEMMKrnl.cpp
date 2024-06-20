fd8414354017ba593eb058320373489d77ba6201
jianan-gu <jianan.gu@intel.com>
2024-06-19 22:02:06 -0700

    support 2D activation for TPP GEMM

--- csrc/cpu/aten/kernels/TPPGEMMKrnl.cpp
+++ csrc/cpu/aten/kernels/TPPGEMMKrnl.cpp
@@ -17,16 +17,22 @@ at::Tensor tpp_linear_bias_kernel_impl(
     const at::Tensor& t_in,
     const at::Tensor& t_wt,
     const at::Tensor& t_bias) {
-  auto sizes = t_in.sizes().vec();
+  bool reshape_activation = false;
+  auto t_in_ = t_in;
+  if (t_in.dim() == 2) {
+     reshape_activation = true;
+     t_in_ = t_in.unsqueeze(0);
+  }
+  auto sizes = t_in_.sizes().vec();
   auto wt_sizes = t_wt.sizes();
   sizes[2] = wt_sizes[0] * wt_sizes[3];
 
-  auto t_out = t_in.new_empty(sizes);
+  auto t_out = t_in_.new_empty(sizes);
   auto dt = t_wt.dtype();
   if (dt == at::kFloat) {
-    torch_ipex::tpp::tpp_linear_bias<float>(t_in, t_wt, t_bias, t_out);
+    torch_ipex::tpp::tpp_linear_bias<float>(t_in_, t_wt, t_bias, t_out);
   } else if (dt == at::kBFloat16) {
-    torch_ipex::tpp::tpp_linear_bias<at::BFloat16>(t_in, t_wt, t_bias, t_out);
+    torch_ipex::tpp::tpp_linear_bias<at::BFloat16>(t_in_, t_wt, t_bias, t_out);
   } else {
     AT_ASSERT(
         0,
@@ -34,30 +40,41 @@ at::Tensor tpp_linear_bias_kernel_impl(
         __FILE__,
         __LINE__);
   }
-
+  if (reshape_activation){
+     return t_out.squeeze(0);
+  }
   return t_out;
 }
 
 at::Tensor tpp_linear_nobias_kernel_impl(
     const at::Tensor& t_in,
     const at::Tensor& t_wt) {
-  auto sizes = t_in.sizes().vec();
+  bool reshape_activation = false;
+  auto t_in_ = t_in;
+  if (t_in.dim() == 2) {
+     reshape_activation = true;
+     t_in_ = t_in.unsqueeze(0);
+  }
+  auto sizes = t_in_.sizes().vec();
   auto wt_sizes = t_wt.sizes();
   sizes[2] = wt_sizes[0] * wt_sizes[3];
 
-  auto t_out = t_in.new_empty(sizes);
+  auto t_out = t_in_.new_empty(sizes);
 
   auto dt = t_wt.dtype();
   if (dt == at::kFloat) {
-    torch_ipex::tpp::tpp_linear_no_bias<float>(t_in, t_wt, t_out);
+    torch_ipex::tpp::tpp_linear_no_bias<float>(t_in_, t_wt, t_out);
   } else if (dt == at::kBFloat16) {
-    torch_ipex::tpp::tpp_linear_no_bias<at::BFloat16>(t_in, t_wt, t_out);
+    torch_ipex::tpp::tpp_linear_no_bias<at::BFloat16>(t_in_, t_wt, t_out);
   } else {
     AT_ASSERT(
         0,
         "TPP does not support current weight dtype %s:%d\n",
         __FILE__,
         __LINE__);
+  }
+  if (reshape_activation){
+     return t_out.squeeze(0);
   }
   return t_out;
 }
@@ -71,26 +88,31 @@ at::Tensor tpp_linear_gelu_kernel_impl(
       algorithm == "none" || algorithm == "tanh",
       "tpp_linear_gelu: Invalid gelu algorithm %s\n",
       algorithm);
-
-  auto sizes = t_in.sizes().vec();
+  bool reshape_activation = false;
+  auto t_in_ = t_in;
+  if (t_in.dim() == 2) {
+     reshape_activation = true;
+     t_in_ = t_in.unsqueeze(0);
+  }
+  auto sizes = t_in_.sizes().vec();
   auto wt_sizes = t_wt.sizes();
   sizes[2] = wt_sizes[0] * wt_sizes[3];
 
-  auto t_out = t_in.new_empty(sizes);
+  auto t_out = t_in_.new_empty(sizes);
 
   auto dt = t_wt.dtype();
   if (dt == at::kFloat) {
     if (algorithm == "none") {
-      torch_ipex::tpp::tpp_linear_gelu<float>(t_in, t_wt, t_bias, t_out);
+      torch_ipex::tpp::tpp_linear_gelu<float>(t_in_, t_wt, t_bias, t_out);
     } else { // tanh
-      torch_ipex::tpp::tpp_linear_gelu_tanh<float>(t_in, t_wt, t_bias, t_out);
+      torch_ipex::tpp::tpp_linear_gelu_tanh<float>(t_in_, t_wt, t_bias, t_out);
     }
   } else if (dt == at::kBFloat16) {
     if (algorithm == "none") {
-      torch_ipex::tpp::tpp_linear_gelu<at::BFloat16>(t_in, t_wt, t_bias, t_out);
+      torch_ipex::tpp::tpp_linear_gelu<at::BFloat16>(t_in_, t_wt, t_bias, t_out);
     } else { // tanh
       torch_ipex::tpp::tpp_linear_gelu_tanh<at::BFloat16>(
-          t_in, t_wt, t_bias, t_out);
+          t_in_, t_wt, t_bias, t_out);
     }
   } else {
     AT_ASSERT(
@@ -98,6 +120,9 @@ at::Tensor tpp_linear_gelu_kernel_impl(
         "TPP does not support current weight dtype %s:%d\n",
         __FILE__,
         __LINE__);
+  }
+  if (reshape_activation){
+     return t_out.squeeze(0);
   }
   return t_out;
 }
@@ -108,28 +133,37 @@ at::Tensor tpp_fused_gate_up_proj_kernel_impl(
     const at::Tensor& t_bias_gate,
     const at::Tensor& t_wt_up,
     const at::Tensor& t_bias_up) {
-  auto sizes = t_in.sizes().vec();
+  bool reshape_activation = false;
+  auto t_in_ = t_in;
+  if (t_in.dim() == 2) {
+     reshape_activation = true;
+     t_in_ = t_in.unsqueeze(0);
+  }
+  auto sizes = t_in_.sizes().vec();
   AT_ASSERT(
       t_wt_gate.sizes() == t_wt_up.sizes(),
       "Expect t_wt_gate.sizes() == t_wt_up.sizes()");
   auto wt_sizes = t_wt_gate.sizes();
   sizes[2] = wt_sizes[0] * wt_sizes[3];
 
-  auto t_out = t_in.new_empty(sizes);
+  auto t_out = t_in_.new_empty(sizes);
 
   auto dt = t_wt_gate.dtype();
   if (dt == at::kFloat) {
     torch_ipex::tpp::tpp_fused_gate_up_proj<float>(
-        t_in, t_wt_gate, t_bias_gate, t_wt_up, t_bias_up, t_out);
+        t_in_, t_wt_gate, t_bias_gate, t_wt_up, t_bias_up, t_out);
   } else if (dt == at::kBFloat16) {
     torch_ipex::tpp::tpp_fused_gate_up_proj<at::BFloat16>(
-        t_in, t_wt_gate, t_bias_gate, t_wt_up, t_bias_up, t_out);
+        t_in_, t_wt_gate, t_bias_gate, t_wt_up, t_bias_up, t_out);
   } else {
     AT_ASSERT(
         0,
         "TPP does not support current weight dtype %s:%d\n",
         __FILE__,
         __LINE__);
+  }
+  if (reshape_activation){
+     return t_out.squeeze(0);
   }
   return t_out;
 }
@@ -138,23 +172,32 @@ at::Tensor tpp_linear_silu_kernel_impl(
     const at::Tensor& t_in,
     const at::Tensor& t_wt,
     const at::Tensor& t_bias) {
-  auto sizes = t_in.sizes().vec();
+  bool reshape_activation = false;
+  auto t_in_ = t_in;
+  if (t_in.dim() == 2) {
+     reshape_activation = true;
+     t_in_ = t_in.unsqueeze(0);
+  }
+  auto sizes = t_in_.sizes().vec();
   auto wt_sizes = t_wt.sizes();
   sizes[2] = wt_sizes[0] * wt_sizes[3];
 
-  auto t_out = t_in.new_empty(sizes);
+  auto t_out = t_in_.new_empty(sizes);
 
   auto dt = t_wt.dtype();
   if (dt == at::kFloat) {
-    torch_ipex::tpp::tpp_linear_silu<float>(t_in, t_wt, t_bias, t_out);
+    torch_ipex::tpp::tpp_linear_silu<float>(t_in_, t_wt, t_bias, t_out);
   } else if (dt == at::kBFloat16) {
-    torch_ipex::tpp::tpp_linear_silu<at::BFloat16>(t_in, t_wt, t_bias, t_out);
+    torch_ipex::tpp::tpp_linear_silu<at::BFloat16>(t_in_, t_wt, t_bias, t_out);
   } else {
     AT_ASSERT(
         0,
         "TPP does not support current weight dtype %s:%d\n",
         __FILE__,
         __LINE__);
+  }
+  if (reshape_activation){
+     return t_out.squeeze(0);
   }
   return t_out;
 }
@@ -163,23 +206,32 @@ at::Tensor tpp_linear_relu_kernel_impl(
     const at::Tensor& t_in,
     const at::Tensor& t_wt,
     const at::Tensor& t_bias) {
-  auto sizes = t_in.sizes().vec();
+  bool reshape_activation = false;
+  auto t_in_ = t_in;
+  if (t_in.dim() == 2) {
+     reshape_activation = true;
+     t_in_ = t_in.unsqueeze(0);
+  }
+  auto sizes = t_in_.sizes().vec();
   auto wt_sizes = t_wt.sizes();
   sizes[2] = wt_sizes[0] * wt_sizes[3];
 
-  auto t_out = t_in.new_empty(sizes);
+  auto t_out = t_in_.new_empty(sizes);
 
   auto dt = t_wt.dtype();
   if (dt == at::kFloat) {
-    torch_ipex::tpp::tpp_linear_relu<float>(t_in, t_wt, t_bias, t_out);
+    torch_ipex::tpp::tpp_linear_relu<float>(t_in_, t_wt, t_bias, t_out);
   } else if (dt == at::kBFloat16) {
-    torch_ipex::tpp::tpp_linear_relu<at::BFloat16>(t_in, t_wt, t_bias, t_out);
+    torch_ipex::tpp::tpp_linear_relu<at::BFloat16>(t_in_, t_wt, t_bias, t_out);
   } else {
     AT_ASSERT(
         0,
         "TPP does not support current weight dtype %s:%d\n",
         __FILE__,
         __LINE__);
+  }
+  if (reshape_activation){
+     return t_out.squeeze(0);
   }
   return t_out;
 }
@@ -191,20 +243,39 @@ at::Tensor tpp_linear_add_add_kernel_impl(
     const at::Tensor& t_wt,
     const at::Tensor& t_bias,
     double scale) {
-  auto t_out = at::empty_like(t_in1);
+  bool reshape_activation = false;
+  auto t_in_ = t_in;
+  auto t_in1_ = t_in1;
+  auto t_in2_ = t_in2;
+  if (t_in.dim() == 2) {
+     reshape_activation = true;
+     t_in_ = t_in.unsqueeze(0);
+  }
+  if (t_in1.dim() == 2) {
+     reshape_activation = true;
+     t_in1_ = t_in1.unsqueeze(0);
+  }
+  if (t_in2.dim() == 2) {
+     reshape_activation = true;
+     t_in2_ = t_in2.unsqueeze(0);
+  }
+  auto t_out = at::empty_like(t_in1_);
   auto dt = t_wt.dtype();
   if (dt == at::kFloat) {
     torch_ipex::tpp::tpp_linear_add_add<float>(
-        t_in, t_in1, t_in2, t_wt, t_bias, t_out, scale);
+        t_in_, t_in1_, t_in2_, t_wt, t_bias, t_out, scale);
   } else if (dt == at::kBFloat16) {
     torch_ipex::tpp::tpp_linear_add_add<at::BFloat16>(
-        t_in, t_in1, t_in2, t_wt, t_bias, t_out, scale);
+        t_in_, t_in1_, t_in2_, t_wt, t_bias, t_out, scale);
   } else {
     AT_ASSERT(
         0,
         "TPP does not support current weight dtype %s:%d\n",
         __FILE__,
         __LINE__);
+  }
+  if (reshape_activation){
+     return t_out.squeeze(0);
   }
   return t_out;
 }
@@ -215,20 +286,34 @@ at::Tensor tpp_linear_add_kernel_impl(
     const at::Tensor& t_wt,
     const at::Tensor& t_bias,
     double scale) {
-  auto t_out = at::empty_like(t_in1);
+  bool reshape_activation = false;
+  auto t_in_ = t_in;
+  auto t_in1_ = t_in1;
+  if (t_in.dim() == 2) {
+     reshape_activation = true;
+     t_in_ = t_in.unsqueeze(0);
+  }
+  if (t_in1.dim() == 2) {
+     reshape_activation = true;
+     t_in1_ = t_in1.unsqueeze(0);
+  }
+  auto t_out = at::empty_like(t_in1_);
   auto dt = t_wt.dtype();
   if (dt == at::kFloat) {
     torch_ipex::tpp::tpp_linear_add<float>(
-        t_in, t_in1, t_wt, t_bias, t_out, scale);
+        t_in_, t_in1_, t_wt, t_bias, t_out, scale);
   } else if (dt == at::kBFloat16) {
     torch_ipex::tpp::tpp_linear_add<at::BFloat16>(
-        t_in, t_in1, t_wt, t_bias, t_out, scale);
+        t_in_, t_in1_, t_wt, t_bias, t_out, scale);
   } else {
     AT_ASSERT(
         0,
         "TPP does not support current weight dtype %s:%d\n",
         __FILE__,
         __LINE__);
+  }
+  if (reshape_activation){
+     return t_out.squeeze(0);
   }
   return t_out;
 }
@@ -238,19 +323,33 @@ at::Tensor tpp_linear_mul_kernel_impl(
     const at::Tensor& t_in1,
     const at::Tensor& t_wt,
     const at::Tensor& t_bias) {
+  bool reshape_activation = false;
+  auto t_in_ = t_in;
+  auto t_in1_ = t_in1;
+  if (t_in.dim() == 2) {
+     reshape_activation = true;
+     t_in_ = t_in.unsqueeze(0);
+  }
+  if (t_in1.dim() == 2) {
+     reshape_activation = true;
+     t_in1_ = t_in1.unsqueeze(0);
+  }
   auto t_out = at::empty_like(t_in1);
   auto dt = t_wt.dtype();
   if (dt == at::kFloat) {
-    torch_ipex::tpp::tpp_linear_mul<float>(t_in, t_in1, t_wt, t_bias, t_out);
+    torch_ipex::tpp::tpp_linear_mul<float>(t_in_, t_in1_, t_wt, t_bias, t_out);
   } else if (dt == at::kBFloat16) {
     torch_ipex::tpp::tpp_linear_mul<at::BFloat16>(
-        t_in, t_in1, t_wt, t_bias, t_out);
+        t_in_, t_in1_, t_wt, t_bias, t_out);
   } else {
     AT_ASSERT(
         0,
         "TPP does not support current weight dtype %s:%d\n",
         __FILE__,
         __LINE__);
+  }
+  if (reshape_activation){
+     return t_out.squeeze(0);
   }
   return t_out;
 }
