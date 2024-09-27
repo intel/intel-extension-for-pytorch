e59046fe81bbdfb00536c5309af3d3537635cf65
zhuhong61 <95205772+zhuhong61@users.noreply.github.com>
2024-09-27 18:59:51 +0800

    Add weight only quantization support for NF4 (#4819)
    * Add nf4 dequant + gemm support
    
    * Update code
    
    * change format
    
    ---------
    
    Co-authored-by: Ye Ting <ting.ye@intel.com>

---
+++ csrc/gpu/aten/operators/WoqLinearNF4.cpp
@@ -0,0 +1,253 @@
+#include <ATen/ATen.h>
+// #include <c10/util/Exception.h>
+#include <core/Device.h>
+#include <core/Memory.h>
+#include <float.h>
+#include <oneDNN/oneDNN.h>
+#include <runtime/Utils.h>
+#include <torch/torch.h>
+#include <utils/DPCPP.h>
+#include <iostream>
+#include "BlasImpl.h"
+
+#include "utils/CustomOperatorRegistration.h"
+
+using namespace torch_ipex::xpu::dpcpp;
+
+namespace at {
+namespace AtenIpexTypeXPU {
+
+typedef enum DataType_t {
+  WOQ_DTYPE_INT8 = 1,
+  WOQ_DTYPE_INT4 = 2,
+  WOQ_DTYPE_NF4 = 3,
+} DataType_t;
+
+template <typename T>
+T dDequantizeNF4(uint8_t val) {
+  if ((val & 0b1000) == 8)
+    if ((val & 0b0100) == 4) // 1
+      if ((val & 0b0010) == 2) // 11
+        if ((val & 0b0001) == 1) // 111
+          return 1.0f;
+        else
+          return 0.7229568362236023f;
+      else if ((val & 0b0001) == 1) // 110
+        return 0.5626170039176941f;
+      else
+        return 0.44070982933044434f;
+    else if ((val & 0b0010) == 2) // 10
+      if ((val & 0b0001) == 1) // 101
+        return 0.33791524171829224f;
+      else
+        return 0.24611230194568634f;
+    else if ((val & 0b0001) == 1) // 100
+      return 0.16093020141124725f;
+    else
+      return 0.07958029955625534f;
+
+  else if ((val & 0b0100) == 4) // 0
+    if ((val & 0b0010) == 2) // 01
+      if ((val & 0b0001) == 1) // 011
+        return 0.0f;
+      else
+        return -0.09105003625154495f;
+    else if ((val & 0b0001) == 1) // 010
+      return -0.18477343022823334f;
+    else
+      return -0.28444138169288635f;
+  else if ((val & 0b0010) == 2) // 00
+    if ((val & 0b0001) == 1) // 001
+      return -0.39491748809814453f;
+    else
+      return -0.5250730514526367f;
+  else if ((val & 0b0001) == 1) // 000
+    return -0.6961928009986877f;
+  else
+    return -1.0f;
+}
+
+template <typename T, int TILE_SIZE, int THREADS, int NUM_PER_TH, int DATA_TYPE>
+void kDequantizeBlockwise_kernel(
+    float* code,
+    uint8_t* A,
+    T* absmax,
+    T* out,
+    const int blocksize,
+    const int n,
+    sycl::nd_item<1>& item) {
+  const int n_load = (item.get_group_range(0) * TILE_SIZE);
+
+  const int base_idx = (item.get_group(0) * TILE_SIZE);
+
+  uint8_t qvals[NUM_PER_TH]; // quantized weight
+  T vals[NUM_PER_TH * 2]; // dequantized weight
+
+  T local_abs_max = -FLT_MAX;
+
+  for (unsigned int i = base_idx; i < n_load;
+       i += item.get_group_range(0) * TILE_SIZE) {
+    local_abs_max =
+        absmax[(i + item.get_local_id(0) * NUM_PER_TH) / (blocksize)];
+
+#pragma unroll NUM_PER_TH
+    for (int j = 0; j < NUM_PER_TH; j++) {
+      // load A to qvals
+      qvals[j] = A[(i + item.get_local_id(0) * NUM_PER_TH + j)];
+
+      // unpack to val and dequant
+      vals[j * 2] = dDequantizeNF4<T>(qvals[j] >> 4) * local_abs_max;
+      vals[j * 2 + 1] = dDequantizeNF4<T>(qvals[j] & 0x0F) * local_abs_max;
+
+      // write to output
+      out[i * 2 + item.get_local_id(0) * NUM_PER_TH * 2 + j * 2] =
+          dDequantizeNF4<T>(qvals[j] & 0x0F) * local_abs_max;
+      out[i * 2 + item.get_local_id(0) * NUM_PER_TH * 2 + j * 2 + 1] =
+          dDequantizeNF4<T>(qvals[j] >> 4) * local_abs_max;
+    }
+  }
+}
+
+template <typename T, int TILE_SIZE, int THREADS, int NUM_PER_TH, int DATA_TYPE>
+struct kDequantizeBlockwiseFunctor {
+  void operator()(sycl::nd_item<1> item) const {
+    kDequantizeBlockwise_kernel<T, TILE_SIZE, THREADS, NUM_PER_TH, DATA_TYPE>(
+        code, A, absmax, out, blocksize, n, item);
+  }
+
+  kDequantizeBlockwiseFunctor(
+      float* code_,
+      uint8_t* A_,
+      T* absmax_,
+      T* out_,
+      const int blocksize_,
+      const int n_)
+      : code(code_),
+        A(A_),
+        absmax(absmax_),
+        out(out_),
+        blocksize(blocksize_),
+        n(n_) {}
+
+ private:
+  float* code;
+  uint8_t* A;
+  T* absmax;
+  T* out;
+  const int blocksize;
+  const int n;
+};
+
+template <typename T, int DATA_TYPE>
+void dequantizeBlockwise(
+    float* code,
+    uint8_t* A,
+    T* absmax,
+    T* out,
+    int blocksize,
+    const int n) {
+  auto& sycl_queue = dpcppGetCurrentQueue();
+  auto dev_id = dpcppGetDeviceIdOfCurrentQueue();
+
+  int num_blocks = n / blocksize;
+  num_blocks = n % blocksize == 0 ? num_blocks : num_blocks + 1;
+  int tile_size = 1024;
+  int work_group_num = (n + tile_size - 1) / tile_size;
+  int work_group_size = 64;
+
+  auto cgf = DPCPP_Q_CGF(cgh) {
+    kDequantizeBlockwiseFunctor<T, 512, 64, 8, DATA_TYPE> kfn(
+        code, A, absmax, out, blocksize / 2, n);
+    cgh.parallel_for<decltype(kfn)>(
+        sycl::nd_range<1>(
+            sycl::range<1>(work_group_size * work_group_num),
+            sycl::range<1>(work_group_size)),
+        kfn);
+  };
+  DPCPP_Q_SUBMIT(sycl_queue, cgf);
+}
+
+at::Tensor woq_linear(
+    const at::Tensor& input,
+    const at::Tensor& qweight,
+    const c10::string_view& weight_dtype,
+    const std::vector<int64_t>& weight_shape,
+    const at::Tensor& weight_scales,
+    const c10::optional<at::Tensor>& weight_zeros,
+    const c10::optional<at::Tensor>& bias,
+    const c10::optional<at::Tensor>& g_idx,
+    int64_t group_size,
+    int64_t lowp_mode,
+    int64_t act_quant_mode,
+    const c10::optional<at::Tensor>& compensation) {
+  static const std::map<c10::string_view, int64_t> WOQ_DTYPE_MAP = {
+      {"int8", WOQ_DTYPE_INT8},
+      {"int4", WOQ_DTYPE_INT4},
+      {"nf4", WOQ_DTYPE_NF4},
+  };
+
+  TORCH_CHECK(
+      WOQ_DTYPE_MAP.find(weight_dtype) != WOQ_DTYPE_MAP.end(),
+      "Unsupported weight dtype: ",
+      weight_dtype);
+  TORCH_CHECK(
+      WOQ_DTYPE_MAP.at(weight_dtype) == WOQ_DTYPE_NF4,
+      "Only NF4 is supported Now!");
+
+  // step 1: dequant weight
+  int64_t n = weight_shape[0] * weight_shape[1];
+
+  auto dqout_shape = weight_shape;
+  at::Tensor dq_output =
+      at::zeros(dqout_shape, input.options().dtype(input.scalar_type()));
+
+  // Output dtype is set the same as input activation
+  uint8_t* qweight_ptr = (uint8_t*)qweight.data_ptr();
+
+  if (dq_output.scalar_type() == at::ScalarType::Float) {
+    float* absmax_ptr = (float*)weight_scales.data_ptr();
+    auto dq_output_ptr = (float*)dq_output.data_ptr();
+    dequantizeBlockwise<float, WOQ_DTYPE_NF4>(
+        NULL, qweight_ptr, absmax_ptr, dq_output_ptr, group_size, n);
+
+  }
+
+  else if (dq_output.scalar_type() == at::ScalarType::Half) {
+    auto dq_output_ptr = (at::Half*)dq_output.data_ptr();
+    at::Half* absmax_ptr = (at::Half*)weight_scales.data_ptr();
+    dequantizeBlockwise<at::Half, WOQ_DTYPE_NF4>(
+        NULL, qweight_ptr, absmax_ptr, dq_output_ptr, group_size, n);
+
+  }
+
+  else if (dq_output.scalar_type() == at::ScalarType::BFloat16) {
+    auto dq_output_ptr = (at::BFloat16*)dq_output.data_ptr();
+    at::BFloat16* absmax_ptr = (at::BFloat16*)weight_scales.data_ptr();
+    dequantizeBlockwise<at::BFloat16, WOQ_DTYPE_NF4>(
+        NULL, qweight_ptr, absmax_ptr, dq_output_ptr, group_size, n);
+  }
+
+  // step 2: OneDNN gemm
+  Attr attr;
+  auto inputf = input.flatten(0, -2);
+
+  int size_m = inputf.sizes()[0];
+  int size_n = dq_output.sizes()[1];
+  auto result = at::empty({size_m, size_n}, input.options());
+
+  torch_ipex::xpu::oneDNN::matmul(
+      result, inputf, dq_output, at::Tensor(), true, Attr());
+
+  return result;
+}
+
+} // namespace AtenIpexTypeXPU
+} // namespace at
+
+namespace {
+IPEX_LIBRARY_FRAGMENT() {
+  IPEX_OP_REGISTER_DISPATCH(
+      "woq_linear", at::AtenIpexTypeXPU::woq_linear, c10::DispatchKey::XPU);
+}
+
+} // namespace
---
+++ tests/gpu/examples/test_woqlinear_nf4.py
@@ -0,0 +1,181 @@
+import torch
+import intel_extension_for_pytorch  # noqa
+from torch.testing._internal.common_utils import TestCase
+
+
+NF4_QUANT_TABLE = [
+    -1.0 - 1e-2,  # 0b0000
+    -0.8480964004993439,  # 0b0001
+    -0.6106329262256622,  # 0b0010
+    -0.4599952697753906,  # 0b0011
+    -0.33967943489551544,  # 0b0100
+    -0.23460740596055984,  # 0b0101
+    -0.13791173323988914,  # 0b0110
+    -0.045525018125772476,  # 0b0111
+    0.03979014977812767,  # 0b1000
+    0.1202552504837513,  # 0b1001
+    0.2035212516784668,  # 0b1010
+    0.2920137718319893,  # 0b1011
+    0.3893125355243683,  # 0b1100
+    0.5016634166240692,  # 0b1101
+    0.6427869200706482,  # 0b1110
+    0.8614784181118011,  # 0b1111
+]
+
+
+NF4_DEQUANT_TABLE = [
+    -1.0,
+    -0.6961928009986877,
+    -0.5250730514526367,
+    -0.39491748809814453,
+    -0.28444138169288635,
+    -0.18477343022823334,
+    -0.09105003625154495,
+    0.0,
+    0.07958029955625534,
+    0.16093020141124725,
+    0.24611230194568634,
+    0.33791524171829224,
+    0.44070982933044434,
+    0.5626170039176941,
+    0.7229568362236023,
+    1.0,
+]
+
+
+checking_atol = 1e-2
+checking_rtol = 1e-2
+
+
+class Test4bitDequant(TestCase):
+
+    def quantize_nf4(
+        self,
+        A,
+        blocksize=64,
+        quant_type="nf4",
+    ):
+        n = A.numel()
+        input_shape = A.shape
+        blocks = n // blocksize
+        blocks += 1 if n % blocksize > 0 else 0
+
+        absmax = torch.zeros((blocks,), device=A.device, dtype=A.dtype)
+        out = torch.zeros(((n + 1) // 2), dtype=torch.uint8, device=A.device)
+
+        rem = n % blocksize
+        has_rem = rem > 0
+
+        # Scale tensor to [-1, 1]
+        A_reshaped = A.reshape(n)
+        A_com = A_reshaped[: n - rem]
+        A_com_reshaped = A_com.reshape(n // blocksize, blocksize)
+        absmax[: blocks - has_rem] = torch.abs(A_com_reshaped).max(dim=-1)[0]
+        scaled_A = torch.clamp(
+            A_com_reshaped * (1 / absmax[: blocks - has_rem].view(-1, 1)), -1, 1
+        )
+        scaled_A = scaled_A.reshape(-1)
+
+        if has_rem:
+            absmax[-1] = torch.abs(A_reshaped[n - rem :]).max()
+            scaled_A_rem = torch.clamp(A_reshaped[n - rem :] * (1 / absmax[-1]), -1, 1)
+            scaled_A = torch.cat([scaled_A, scaled_A_rem], dim=0)
+
+        # map [-1, 1] to nf4
+        out_uint8 = torch.empty(scaled_A.shape, dtype=torch.uint8, device=A.device)
+
+        for i in range(len(NF4_QUANT_TABLE)):
+            out_uint8[scaled_A > NF4_QUANT_TABLE[i]] = i
+
+        if out_uint8.size(-1) % 2:
+            out_uint8 = torch.nn.functional.pad(out_uint8, (0, 1), value=0)
+        out[:] = out_uint8[1::2].bitwise_left_shift(4).bitwise_or_(out_uint8[::2])
+
+        state = {}
+        state["absmax"] = absmax
+        state["shape"] = input_shape
+        state["dtype"] = A.dtype
+        state["blocksize"] = blocksize
+        state["quant_type"] = quant_type
+
+        return out, state
+
+    def dequantize_nf4(
+        self,
+        A,
+        quant_state,
+        quant_type="nf4",
+    ):
+        absmax = quant_state["absmax"]
+        blocksize = quant_state["blocksize"]
+
+        out = torch.empty(
+            quant_state["shape"], dtype=quant_state["dtype"], device=A.device
+        )
+
+        n = out.numel()
+        # Map nf4 to [-1, 1]
+        out_uint8 = torch.empty(A.size(0) * 2, dtype=torch.uint8, device=A.device)
+        out_uint8[::2] = A.bitwise_and(0xF)
+        out_uint8[1::2] = A.bitwise_right_shift(4)
+        out_dq = torch.empty(out_uint8.shape).to(quant_state["dtype"]).to(A.device)
+
+        NF4_LUT = torch.tensor(NF4_DEQUANT_TABLE, device=A.device)
+        for i in range(len(NF4_LUT)):
+            NF4_LUT = NF4_LUT.to(quant_state["dtype"])
+            out_dq[out_uint8 == i] = NF4_LUT[i]
+
+        # Apply scales
+        if out_dq.numel() != n:
+            assert out_dq.numel() == n + 1
+            out_dq = torch.narrow(out_dq, 0, 0, n)
+        blocks = n // blocksize
+        blocks += 1 if n % blocksize > 0 else 0
+        rem = n % blocksize
+        has_rem = rem > 0
+        out_reshaped = out.reshape(-1)
+        out_reshaped[: n - rem] = (
+            out_dq[: n - rem].view(-1, blocksize)
+            * absmax[: blocks - has_rem].view(-1, 1)
+        ).reshape(-1)
+        if has_rem:
+            out_reshaped[n - rem :] = out_dq[n - rem :] * absmax[-1]
+
+        return out
+
+    def test_nf4_woq_linear(self):
+        shapes = [(4096, 4096), (4096, 11008), (11008, 4096)]
+        blocksize = 128
+        for dtype in [torch.bfloat16, torch.float16]:
+            for shape in shapes:
+                weight = torch.randn(shape, dtype=dtype, device="xpu")
+                input = torch.randn((8, shape[0]), dtype=dtype, device="xpu")
+
+                # quantize
+                quant_weight, state = self.quantize_nf4(
+                    weight, blocksize=blocksize, quant_type="nf4"
+                )
+
+                # dequantize ref
+                dequant_ref = self.dequantize_nf4(quant_weight, state, quant_type="nf4")
+                output_ref = torch.nn.functional.linear(input, dequant_ref.t())
+
+                # nf4 woq linear
+                output = torch.ops.torch_ipex.woq_linear(
+                    input,
+                    quant_weight.reshape([weight.shape[0], weight.shape[1] // 2]),
+                    "nf4",
+                    weight.shape,
+                    state["absmax"].view(weight.shape[0], weight.shape[1] // blocksize),
+                    None,
+                    None,
+                    None,
+                    blocksize,
+                    0,
+                    -1,
+                    None,
+                )
+
+                self.assertEqual(
+                    output_ref, output, atol=checking_atol, rtol=checking_rtol
+                )
