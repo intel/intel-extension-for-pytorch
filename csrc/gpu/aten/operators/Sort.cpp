e06f2876b5fb97ed33be2cf3c7e01e4c75b153cc
Huaiyu, Zheng <huaiyu.zheng@intel.com>
2024-11-15 18:14:18 +0800

    override sort in yaml file (#5032)(#5036)
    * override sort to WA PR for TGI TP=2 issue: RuntimeError: UR backend failed. UR backend returns:40 (UR_RESULT_ERROR_OUT_OF_RESOURCES)

--- csrc/gpu/aten/operators/Sort.cpp
+++ csrc/gpu/aten/operators/Sort.cpp
@@ -1,9 +1,6 @@
 #include <core/Memory.h>
 #include <core/detail/IndexUtils.h>
 #include <core/detail/TensorInfo.h>
-#ifdef USE_OVERRIDE_OP
-#include "utils/CustomOperatorRegistration.h"
-#endif
 #include <utils/DPCPP.h>
 #include "comm/ATDispatch.h"
 #include "comm/Numerics.h"
@@ -58,56 +55,5 @@ Tensor argsort(const Tensor& self, bool stable, int64_t dim, bool descending) {
   return std::get<1>(
       sort_out_stable(self, stable, dim, descending, sorted, indices));
 }
-#ifdef USE_OVERRIDE_OP
-std::tuple<at::Tensor, at::Tensor> sort_ipex(
-    const at::Tensor& self,
-    int64_t dim,
-    bool descending) {
-  return at::AtenIpexTypeXPU::sort(self, dim, descending);
-}
-
-std::tuple<Tensor&, Tensor&> sort_values(
-    const Tensor& input,
-    int64_t dim,
-    bool order,
-    Tensor& sorted,
-    Tensor& indices) {
-  return at::AtenIpexTypeXPU::sort_out(input, dim, order, sorted, indices);
-}
-
-std::tuple<Tensor, Tensor> sort_stable(
-    const Tensor& self,
-    c10::optional<bool> stable,
-    int64_t dim,
-    bool descending) {
-  return at::AtenIpexTypeXPU::sort(self, stable, dim, descending);
-}
-
-std::tuple<Tensor&, Tensor&> sort_values_stable(
-    const Tensor& self,
-    c10::optional<bool> stable,
-    int64_t dim,
-    bool descending,
-    Tensor& values,
-    Tensor& indices) {
-  return at::AtenIpexTypeXPU::sort_out(
-      self, stable, dim, descending, values, indices);
-}
-#endif
 } // namespace AtenIpexTypeXPU
 } // namespace at
-
-#ifdef USE_OVERRIDE_OP
-namespace {
-
-IPEX_TORCH_LIBRARY_IMPL(aten, XPU, m) {
-  m.impl("sort", TORCH_FN((&at::AtenIpexTypeXPU::sort_ipex)));
-  m.impl("sort.stable", TORCH_FN((&at::AtenIpexTypeXPU::sort_stable)));
-  m.impl("sort.values", TORCH_FN((&at::AtenIpexTypeXPU::sort_values)));
-  m.impl(
-      "sort.values_stable",
-      TORCH_FN((&at::AtenIpexTypeXPU::sort_values_stable)));
-}
-
-} // namespace
-#endif
