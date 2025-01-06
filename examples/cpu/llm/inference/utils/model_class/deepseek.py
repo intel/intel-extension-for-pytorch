45884306a1f2b3c985bdaf27f2b53e5ec58926c3
blzheng <beilei.zheng@intel.com>
2025-01-06 17:33:50 +0800

    [deepseek] fix woq int8 load (#3428)

--- examples/cpu/llm/inference/utils/model_class/deepseek.py
+++ examples/cpu/llm/inference/utils/model_class/deepseek.py
@@ -1,5 +1,10 @@
 from .llm import LLMConfig, EXAMPLE_INPUTS_MODE
 
+import torch
+from transformers import AutoModelForCausalLM
+
+import intel_extension_for_pytorch as ipex
+
 
 class DeepseekV2Config(LLMConfig):
     def __init__(self, model_id):
@@ -10,3 +15,32 @@ class DeepseekV2Config(LLMConfig):
 
         self.use_global_past_key_value = True
         self.use_ipex_autotune = True
+
+    def get_user_model(self, config, benchmark):
+        if benchmark:
+            try:
+                with ipex.OnDevice(dtype=torch.float, device="meta"):
+                    self.model = AutoModelForCausalLM.from_config(
+                        config, trust_remote_code=True
+                    )
+            except (RuntimeError, AttributeError):
+                self.model = AutoModelForCausalLM.from_config(
+                    config, trust_remote_code=True
+                )
+            except Exception:
+                self.model = AutoModelForCausalLM.from_pretrained(
+                    self.model_id,
+                    torch_dtype=torch.bfloat16,
+                    config=config,
+                    low_cpu_mem_usage=True,
+                    trust_remote_code=True,
+                )
+        else:
+            self.model = AutoModelForCausalLM.from_pretrained(
+                self.model_id,
+                torch_dtype=torch.bfloat16,
+                config=config,
+                low_cpu_mem_usage=True,
+                trust_remote_code=True,
+            )
+        return self.model
