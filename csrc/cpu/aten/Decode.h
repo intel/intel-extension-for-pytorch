e082dfe340ed2945f7873501b19015b531118e31
blzheng <beilei.zheng@intel.com>
2025-03-17 11:00:33 +0800

    MLA: optimize greedy idx (#3580)

--- csrc/cpu/aten/Decode.h
+++ csrc/cpu/aten/Decode.h
@@ -17,6 +17,14 @@ at::Tensor decode_attention_forward_cpu(
     double scaling,
     double logit_cap,
     int64_t offset);
+at::Tensor decode_attention_opt_forward_cpu(
+    at::Tensor& query,
+    at::Tensor& output,
+    at::Tensor& kv_cache,
+    at::Tensor& attn_logits,
+    double scaling,
+    double logit_cap,
+    int64_t offset);
 } // namespace
 
 using decode_attention_kernel_fn = at::Tensor (*)(
@@ -28,7 +36,16 @@ using decode_attention_kernel_fn = at::Tensor (*)(
     const double scaling,
     const double logit_cap,
     int64_t offset);
+using decode_attention_opt_kernel_fn = at::Tensor (*)(
+    at::Tensor& query,
+    at::Tensor& output,
+    at::Tensor& kv_cache,
+    at::Tensor& attn_logits,
+    const double scaling,
+    const double logit_cap,
+    int64_t offset);
 IPEX_DECLARE_DISPATCH(decode_attention_kernel_fn, decode_attention_kernel_stub);
+IPEX_DECLARE_DISPATCH(decode_attention_opt_kernel_fn, decode_attention_opt_kernel_stub);
 
 } // namespace cpu
 } // namespace torch_ipex
