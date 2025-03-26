0d52e5f8fca22475bf18d4b75c6126c5c404ed97
Huaiyu, Zheng <huaiyu.zheng@intel.com>
2025-03-26 13:27:16 +0800

    remove code of batch_norm group_norm (#5470)
    which are no longer used in IPEX.

--- csrc/gpu/aten/operators/torch-xpu-ops/comm/Macros.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#ifdef _WIN32
-#define RESTRICT __restrict
-#else
-#define RESTRICT __restrict__
-#endif
--- csrc/gpu/aten/operators/torch-xpu-ops/comm/SYCLContext.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "aten/operators/torch-xpu-ops/comm/DeviceProperties.h"
-#include "aten/operators/torch-xpu-ops/comm/Macros.h"
-#include "aten/operators/torch-xpu-ops/comm/Runtime.h"
-#include "aten/operators/torch-xpu-ops/comm/SYCLHelpers.h"
-
-using namespace at::xpu;
-using namespace xpu::sycl;
