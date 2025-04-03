287ea164df6d67bb00b87001f799129771e3c450
Zheng, Beilei <beilei.zheng@intel.com>
2025-04-03 03:38:49 -0700

    fix load

--- examples/cpu/llm/inference/utils/model_class/qwen3moe.py
+++ examples/cpu/llm/inference/utils/model_class/qwen3moe.py
@@ -1,4 +1,7 @@
 from .llm import LLMConfig, EXAMPLE_INPUTS_MODE
+import torch
+from transformers import AutoModelForCausalLM
+import intel_extension_for_pytorch as ipex
 
 
 class Qwen3moeConfig(LLMConfig):
@@ -11,3 +14,32 @@ class Qwen3moeConfig(LLMConfig):
         # for smooth quant
         self.use_global_past_key_value = True
         self.use_ipex_autotune = True
+
+    def get_user_model(self, config, load_to_meta_device):
+        if load_to_meta_device:
+            try:
+                with ipex.OnDevice(dtype=torch.float, device="meta"):
+                    self.model = AutoModelForCausalLM.from_config(
+                        config, trust_remote_code=True
+                    )
+            except (RuntimeError, AttributeError):
+                self.model = AutoModelForCausalLM.from_config(
+                    config, trust_remote_code=True
+                )
+            except Exception:
+                self.model = AutoModelForCausalLM.from_pretrained(
+                    self.model_id,
+                    torch_dtype="auto",
+                    config=config,
+                    low_cpu_mem_usage=True,
+                    trust_remote_code=True,
+                )
+        else:
+            self.model = AutoModelForCausalLM.from_pretrained(
+                self.model_id,
+                torch_dtype="auto",
+                config=config,
+                low_cpu_mem_usage=True,
+                trust_remote_code=True,
+            )
+        return self.model
