dc73aea2ca395a37fccad682284d4d847c07e2ca
Yu, Guangye <106960996+guangyey@users.noreply.github.com>
2025-04-02 18:50:35 -0700

    Remove ABI=0 build script support (#5487)
    Regarding pytorch/pytorch#149888, this PR removes ABI=0 build script support for IPEX build as well. The background is that PyTorch drops ABI=0 support in their build script. The user can still build PyTorch with ABI=0 from the source code by themselves. But PyTorch only releases ABI=1 binary. Now both PyTorch and IPEX supports ABI-neutral builds, with the support from SYCL compiler.

--- intel_extension_for_pytorch/xpu/cpp_extension.py
+++ intel_extension_for_pytorch/xpu/cpp_extension.py
@@ -239,7 +239,6 @@ class DpcppBuildExtension(build_ext, object):
                 if val is not None and not IS_WINDOWS:
                     self._add_compile_flag(extension, f'-DPYBIND11_{name}="{val}"')
             self._define_torch_extension_name(extension)
-            self._add_gnu_cpp_abi_flag(extension)
 
         # Save the original _compile method for later.
         if self.compiler.compiler_type == "msvc":
@@ -625,13 +624,6 @@ class DpcppBuildExtension(build_ext, object):
         name = names[-1]
         define = f"-DTORCH_EXTENSION_NAME={name}"
         self._add_compile_flag(extension, define)
-
-    def _add_gnu_cpp_abi_flag(self, extension):
-        # use the same CXX ABI as what PyTorch was compiled with
-        self._add_compile_flag(
-            extension,
-            "-D_GLIBCXX_USE_CXX11_ABI=" + str(int(torch._C._GLIBCXX_USE_CXX11_ABI)),
-        )
 
 
 SUBPROCESS_DECODE_ARGS = ("oem",) if IS_WINDOWS else ()
@@ -1223,10 +1215,6 @@ def _write_ninja_file_to_build_library(
 
     common_cflags += [f"-I{include}" for include in user_includes]
     common_cflags += [f"-isystem {include}" for include in system_includes]
-
-    common_cflags += [
-        "-D_GLIBCXX_USE_CXX11_ABI=" + str(int(torch._C._GLIBCXX_USE_CXX11_ABI))
-    ]
 
     if IS_WINDOWS:
         cflags = common_cflags + COMMON_MSVC_FLAGS + extra_cflags
