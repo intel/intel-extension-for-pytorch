afeb31fa36d2cbc33d7eb85f03163a89a2a47b63
Fanli Lin <fanli0116@gmail.com>
2025-11-18 17:29:01 +0800

    Remove outdated functions for HF model loading (#5850)
    * remove outdated functions
    
    Signed-off-by: Fanli Lin <fanli.lin@intel.com>
    
    * remove unused imports
    
    Signed-off-by: Fanli Lin <fanli.lin@intel.com>
    
    ---------
    
    Signed-off-by: Fanli Lin <fanli.lin@intel.com>

--- intel_extension_for_pytorch/llm/__init__.py
+++ intel_extension_for_pytorch/llm/__init__.py
@@ -14,8 +14,6 @@ try:
         _get_relative_imports,
         _gradient_checkpointing_disable,
         _gradient_checkpointing_enable,
-        _get_class_from_dynamic_module,
-        _get_cached_module_file,
         _get_imports,
         _pad,
         load_low_precision_checkpoint,
@@ -25,10 +23,6 @@ try:
 
     transformers.dynamic_module_utils.get_relative_imports = _get_relative_imports
     transformers.dynamic_module_utils.get_imports = _get_imports
-    transformers.dynamic_module_utils.get_cached_module_file = _get_cached_module_file
-    transformers.dynamic_module_utils.get_class_from_dynamic_module = (
-        _get_class_from_dynamic_module
-    )
 
     from packaging import version
 
